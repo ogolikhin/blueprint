3ea8464326719c701461c74bb18fd997f803566e
Riad Baghbanli <rbaghbanli@yahoo.ca>
2015-08-27 19:40:49 -0400

    adds File Store web service

---
+++ Svc/FileStore/Controllers/FilesController.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using FileStore.Models;
+
+namespace FileStore.Controllers
+{
+	[RoutePrefix("api/books")]
+	public class FilesController : ApiController
+	{
+		private FileStoreContext db = new FileStoreContext();
+
+		// GET: api/Files
+		public IQueryable<FileRecord> GetFileRecords()
+		{
+			return db.FileRecords;
+		}
+
+		// GET: api/Files/5
+		[ResponseType(typeof(FileRecord))]
+		public async Task<IHttpActionResult> GetFileRecord(string id)
+		{
+			FileRecord fileRecord = await db.FileRecords.FindAsync(id);
+			if (fileRecord == null)
+			{
+				return NotFound();
+			}
+
+			return Ok(fileRecord);
+		}
+
+		// PUT: api/Files/5
+		[ResponseType(typeof(void))]
+		public async Task<IHttpActionResult> PutFileRecord(string id, FileRecord fileRecord)
+		{
+			if (!ModelState.IsValid)
+			{
+				return BadRequest(ModelState);
+			}
+
+			if (id != fileRecord.FileId)
+			{
+				return BadRequest();
+			}
+
+			db.Entry(fileRecord).State = EntityState.Modified;
+
+			try
+			{
+				await db.SaveChangesAsync();
+			}
+			catch (DbUpdateConcurrencyException)
+			{
+				if (!FileRecordExists(id))
+				{
+					return NotFound();
+				}
+				else
+				{
+					throw;
+				}
+			}
+
+			return StatusCode(HttpStatusCode.NoContent);
+		}
+
+		// POST: api/Files
+		[ResponseType(typeof(FileRecord))]
+		public async Task<IHttpActionResult> PostFileRecord(FileRecord fileRecord)
+		{
+			if (!ModelState.IsValid)
+			{
+				return BadRequest(ModelState);
+			}
+
+			db.FileRecords.Add(fileRecord);
+
+			try
+			{
+				await db.SaveChangesAsync();
+			}
+			catch (DbUpdateException)
+			{
+				if (FileRecordExists(fileRecord.FileId))
+				{
+					return Conflict();
+				}
+				else
+				{
+					throw;
+				}
+			}
+
+			return CreatedAtRoute("DefaultApi", new { id = fileRecord.FileId }, fileRecord);
+		}
+
+		// DELETE: api/Files/5
+		[ResponseType(typeof(FileRecord))]
+		public async Task<IHttpActionResult> DeleteFileRecord(string id)
+		{
+			FileRecord fileRecord = await db.FileRecords.FindAsync(id);
+			if (fileRecord == null)
+			{
+				return NotFound();
+			}
+
+			db.FileRecords.Remove(fileRecord);
+			await db.SaveChangesAsync();
+
+			return Ok(fileRecord);
+		}
+
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing)
+			{
+				db.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		private bool FileRecordExists(string id)
+		{
+			return db.FileRecords.Count(e => e.FileId == id) > 0;
+		}
+	}
+}
---
+++ Svc/FileStore/Models/FileStoreContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace FileStore.Models
+{
+	public class FileStoreContext : DbContext
+	{
+		// You can add custom code to this file. Changes will not be overwritten.
+		// 
+		// If you want Entity Framework to drop and regenerate your database
+		// automatically whenever you change your model schema, please use data migrations.
+		// For more information refer to the documentation:
+		// http://msdn.microsoft.com/en-us/data/jj591621.aspx
+
+		public FileStoreContext() : base("name=FileStoreContext")
+		{
+		}
+
+		public System.Data.Entity.DbSet<FileStore.Models.FileRecord> FileRecords { get; set; }
+	}
+}

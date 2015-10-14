0790198f3aaee5fe65febb939f4a0aa6212f2f4c
CRichards <colin.richards@blueprintsys.com>
2015-10-14 15:36:00 -0400

    Task 175788: Modify FileStore web service to connect to Raptor db and retrieve FileStream object if the file is not found in the FileStore db. Use ContentReadStream class to retrieve FILESTREAM data.

---
+++ svc/FileStore.Tests/FileStreamTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Results;
+using System.Text;
+using System.Linq;
+using Moq;
+using FileStore.Repositories;
+using FileStore.Controllers;
+using System.Threading.Tasks;
+using System.Globalization;
+using FileStore.Models;
+
+namespace FileStore.Tests
+{
+    [TestClass]
+    public class FileStreamTest
+    {
+        [TestCategory("FileStreamSvc-UnitTests")]
+        [TestMethod]
+        public void GetFileFromFileStream_ImproperGuid_FormatException()
+        {
+            // Arrange
+
+            FileStreamRepository fsapi = new FileStreamRepository();
+            try
+            {
+                // Act 
+                File file = fsapi.GetFile(File.ConvertFileId("333333333!@#@!@!@!33333333333333333333333"), null);
+            }
+            catch (FormatException)
+            {
+                // assert
+                return;
+            }
+            Assert.Fail("No exception was thrown.");
+
+        }
+    }
+}

c1c39872e403eb56e6fcee8435e71b3db95bfbdd
Chris Dufour <chris.dufour@blueprintsys.com>
2015-10-29 16:35:55 -0400

    Created PowerShell script to create an empty database Initial FileStorage datbase migration

---
+++ svc/db/FileStorage/Scripts/EnableFullText.sql
@@ -0,0 +1,15 @@
+﻿/******************************************************************************************************************************
+Name:			EnableFullText
+
+Description: 
+			
+Change History:
+Date			Name					Change
+2015/10/28		Chris Dufour			Initial Version
+******************************************************************************************************************************/
+
+IF (1 = FULLTEXTSERVICEPROPERTY('IsFullTextInstalled'))
+begin
+	EXEC [FileStore].[dbo].[sp_fulltext_database] @action = 'enable'
+end
+GO

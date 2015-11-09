fb96dfb1d208a712ad454f53e1470f897169ff9a
Chris Dufour <chris.dufour@blueprintsys.com>
2015-11-09 08:57:18 -0500

    Refactoring code

---
+++ svc/db/FileStorage/Procedures/SetSchemaVersion.sql
@@ -0,0 +1,39 @@
+﻿/******************************************************************************************************************************
+Name:			SetSchemaVersion
+
+Description: 
+			
+Change History:
+Date			Name					Change
+2015/10/28		Chris Dufour			Initial Version
+******************************************************************************************************************************/
+
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[SetSchemaVersion]') AND type in (N'P', N'PC'))
+DROP PROCEDURE [dbo].[SetSchemaVersion]
+GO
+
+CREATE PROCEDURE [dbo].[SetSchemaVersion]
+(
+	@value AS nvarchar(max)
+)
+AS
+PRINT 'Setting Schema Version to ' + @value;
+-- Integrity check
+DECLARE @value1 AS int = CAST(PARSENAME(@value, 1) AS int);
+DECLARE @value2 AS int = CAST(PARSENAME(@value, 2) AS int);
+DECLARE @value3 AS int = CAST(PARSENAME(@value, 3) AS int);
+DECLARE @value4 AS int = CAST(PARSENAME(@value, 4) AS int);
+
+IF EXISTS (SELECT * FROM [dbo].[DbVersionInfo])
+	BEGIN 
+		UPDATE [dbo].[DbVersionInfo] SET [SchemaVersion] = @value FROM [dbo].[DbVersionInfo];
+	END
+ELSE
+	BEGIN 
+		INSERT INTO [dbo].[DbVersionInfo] SELECT 1, @value;
+	END 
+
+GO
+
+--GRANT  EXECUTE  ON [dbo].[SetSchemaVersion]  TO [Blueprint]
+--GO
---
+++ svc/db/FileStorage/Tables/Files.sql
@@ -0,0 +1,35 @@
+﻿/******************************************************************************************************************************
+Name:			Files
+
+Description: 
+			
+Change History:
+Date			Name					Change
+2015/10/28		Chris Dufour			Initial Version
+******************************************************************************************************************************/
+IF  EXISTS (SELECT * FROM dbo.sysobjects WHERE id = OBJECT_ID(N'[dbo].[DF__Files__FileId__117F9D94]') AND type = 'D')
+BEGIN
+ALTER TABLE [dbo].[Files] DROP CONSTRAINT [DF__Files__FileId__117F9D94]
+END
+GO
+
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Files]') AND type in (N'U'))
+DROP TABLE [dbo].[Files]
+GO
+
+CREATE TABLE [dbo].[Files](
+	[FileId] [uniqueidentifier] NOT NULL,
+	[StoredTime] [datetime] NOT NULL,
+	[FileName] [nvarchar](256) NOT NULL,
+	[FileType] [nvarchar](128) NOT NULL,
+	[FileContent] [varbinary](max) NULL,
+	[FileSize] [bigint] NOT NULL,
+ CONSTRAINT [PK_Files] PRIMARY KEY CLUSTERED 
+(
+	[FileId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[Files] ADD  DEFAULT (newsequentialid()) FOR [FileId]
+GO

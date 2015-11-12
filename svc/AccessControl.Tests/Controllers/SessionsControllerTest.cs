0d64f434fca05146c216fbf4df8dad5965b6a3c4
Anton Trinkunas <anton.trinkunas@blueprintsys.com>
2015-11-12 15:11:20 -0500

    #175391. Unit tests started

--- svc/AccessControl.Tests/Controllers/SessionsControllerTest.cs
+++ svc/AccessControl.Tests/Controllers/SessionsControllerTest.cs
@@ -1,8 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web.Http;
 using AccessControl.Controllers;
+using AccessControl.Models;
+using AccessControl.Repositories;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 
@@ -12,17 +18,31 @@ namespace AccessControl.Tests.Controllers
     public class SessionsControllerTest
     {
         [TestMethod]
-        public void GetSession_ReturnsCorrectSession()
+        public async Task GetSession_ReturnsCorrectSession()
         {
-            //GlobalConfiguration.Configure(WebApiConfig.Register);
-            //var tokensList = new List<string>();
-            //tokensList.Add("Token1");
-            //var sessionController = new SessionsController();
-            //sessionController.Request.Headers.Add("Session-Token", tokensList);
+            var newGuid = Guid.NewGuid();
+            //var guids = new Guid?[] { newGuid, null };
+            var repositoryMock = new Mock<ISessionsRepository>();
+            var session = new Session();
+            repositoryMock.Setup(r => r.GetSession(newGuid)).Returns(Task.FromResult(session));
+            //repositoryMock.Setup(r => r.BeginSession(It.IsAny<int>())).Returns(Task.FromResult(guids));
+            //repositoryMock.Setup(r => r.EndSession(It.IsAny<Guid>())).Returns(Task.FromResult(new object()));
+           
 
-            //var session = sessionController.GetSession(-1);
+            int uid = 999;
+            var sessionController = new SessionsController(repositoryMock.Object);
 
-            Assert.IsTrue(true);
+
+            sessionController.Request = new HttpRequestMessage
+            {
+                RequestUri = new Uri("http://localhost/files")                
+            };
+            sessionController.Request.Properties.Add(System.Net.Http.HttpPropertyKeys.HttpConfigurationKey, new HttpConfiguration());
+            sessionController.Request.Headers.Add("Session-Token", newGuid.ToString("N"));
+
+            var resultSession = await sessionController.GetSession(uid);
+
+            Assert.IsNotNull(resultSession);
 
         }
     }

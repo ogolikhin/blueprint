9ba74706a6f628fa95e6a88c9a8e9611a26ddcc7
Riad Baghbanli <rbaghbanli@yahoo.ca>
2015-11-24 10:52:54 -0500

    fix minor issues

--- svc/AccessControl/Models/Session.cs
+++ svc/AccessControl/Models/Session.cs
@@ -3,30 +3,30 @@ using System;
 
 namespace AccessControl.Models
 {
-    [JsonObject]
-    public class Session
-    {
-        [JsonProperty]
-        public int UserId { get; set; }
-        [JsonIgnore] //do not send session id
-        public Guid SessionId { get; set; }
-        [JsonProperty]
-        public DateTime? BeginTime { get; set; }
-        [JsonProperty]
-        public DateTime? EndTime { get; set; }
+	[JsonObject]
+	public class Session
+	{
 		[JsonProperty]
-	    public string UserName { get; set; }
+		public int UserId { get; set; }
+		[JsonIgnore] //do not send session id
+		public Guid SessionId { get; set; }
 		[JsonProperty]
-	    public int LicenseLevel { get; set; }
+		public DateTime? BeginTime { get; set; }
+		[JsonProperty]
+		public DateTime? EndTime { get; set; }
+		[JsonProperty]
+		public string UserName { get; set; }
+		[JsonProperty]
+		public int LicenseLevel { get; set; }
 
-	    public static string Convert(Guid guid)
-        {
-            return guid.ToString(("N"));
-        }
+		public static string Convert(Guid guid)
+		{
+			return guid.ToString(("N"));
+		}
 
-        public static Guid Convert(string val)
-        {
-            return Guid.ParseExact(val, "N");
-        }
-    }
+		public static Guid Convert(string val)
+		{
+			return Guid.ParseExact(val, "N");
+		}
+	}
 }

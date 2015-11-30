d6b30e7320d99a170142c5255b456e9cb86ac115
Riad Baghbanli <rbaghbanli@yahoo.ca>
2015-11-30 11:34:44 -0500

    adds prevention of back dated expiration to POST

--- svc/FileStore/Repositories/SqlFilesRepository.cs
+++ svc/FileStore/Repositories/SqlFilesRepository.cs
@@ -24,20 +24,20 @@ namespace FileStore.Repositories
 			ConnectionWrapper = connectionWrapper;
 		}
 
-        public DbConnection CreateConnection()
-        {
-            // create a connection for operations that require holding an open connection to the db
-            return ConnectionWrapper.CreateConnection();
-        }
+		public DbConnection CreateConnection()
+		{
+			// create a connection for operations that require holding an open connection to the db
+			return ConnectionWrapper.CreateConnection();
+		}
 
-        public async Task<Guid> PostFileHead(File file)
+		public async Task<Guid> PostFileHead(File file)
 		{
 			var prm = new DynamicParameters();
 			prm.Add("@FileName", file.FileName);
 			prm.Add("@FileType", file.FileType);
 			prm.Add("@ExpiredTime", file.ExpiredTime);
 			prm.Add("@ChunkCount", file.ChunkCount);
-            prm.Add("@FileSize", file.FileSize);
+			prm.Add("@FileSize", file.FileSize);
 			prm.Add("@FileId", dbType: DbType.Guid, direction: ParameterDirection.Output);
 			await ConnectionWrapper.ExecuteAsync("InsertFileHead", prm, commandType: CommandType.StoredProcedure);
 			return file.FileId = prm.Get<Guid>("FileId");
@@ -54,14 +54,14 @@ namespace FileStore.Repositories
 			return chunk.ChunkNum + 1;
 		}
 
-	    public async Task UpdateFileHead(Guid fileId, long fileSize, int chunkCount)
-	    {
-            var prm = new DynamicParameters();
-            prm.Add("@FileId", fileId);
-            prm.Add("@ChunkCount", chunkCount);
-            prm.Add("@FileSize", fileSize);
-            await ConnectionWrapper.ExecuteAsync("UpdateFileHead", prm, commandType: CommandType.StoredProcedure);
-        }
+		public async Task UpdateFileHead(Guid fileId, long fileSize, int chunkCount)
+		{
+			var prm = new DynamicParameters();
+			prm.Add("@FileId", fileId);
+			prm.Add("@ChunkCount", chunkCount);
+			prm.Add("@FileSize", fileSize);
+			await ConnectionWrapper.ExecuteAsync("UpdateFileHead", prm, commandType: CommandType.StoredProcedure);
+		}
 		public async Task<File> GetFileHead(Guid guid)
 		{
 			var prm = new DynamicParameters();
@@ -76,44 +76,44 @@ namespace FileStore.Repositories
 			prm.Add("@ChunkNum", num);
 			return (await ConnectionWrapper.QueryAsync<FileChunk>("ReadFileChunk", prm, commandType: CommandType.StoredProcedure)).FirstOrDefault();
 		}
- 
+
 		public async Task<Guid?> DeleteFile(Guid guid, DateTime? expired = null)
 		{
 			var prm = new DynamicParameters();
 			prm.Add("@FileId", guid);
-			prm.Add("@ExpiredTime", (expired.HasValue && expired.Value > DateTime.UtcNow) ? expired.Value : DateTime.UtcNow);
+			prm.Add("@ExpiredTime", expired);
 			return (await ConnectionWrapper.ExecuteAsync("DeleteFile", prm, commandType: CommandType.StoredProcedure)) > 0 ? guid : (Guid?)null;
 		}
 
-        public Models.File GetFileInfo(Guid fileId)
-        {
-            var prm = new DynamicParameters();
-            prm.Add("@FileId", fileId);
+		public Models.File GetFileInfo(Guid fileId)
+		{
+			var prm = new DynamicParameters();
+			prm.Add("@FileId", fileId);
 
-            return ConnectionWrapper.Query<Models.File>("ReadFileHead", prm, commandType: CommandType.StoredProcedure).FirstOrDefault();
-        }
+			return ConnectionWrapper.Query<Models.File>("ReadFileHead", prm, commandType: CommandType.StoredProcedure).FirstOrDefault();
+		}
 
-        public byte[] ReadChunkContent(DbConnection dbConnection, Guid guid, int num)
-        {
-            // Note: this method may be called hundreds of times to retrieve chunk records if the 
-            // stored file is large. It will reuse the open database connection that is passed 
-            // in as a parameter.
+		public byte[] ReadChunkContent(DbConnection dbConnection, Guid guid, int num)
+		{
+			// Note: this method may be called hundreds of times to retrieve chunk records if the 
+			// stored file is large. It will reuse the open database connection that is passed 
+			// in as a parameter.
 
-            // Note: After all the read operations are finsihed the dbConnection object must be closed
-            // and disposed by the calling procedure.
+			// Note: After all the read operations are finsihed the dbConnection object must be closed
+			// and disposed by the calling procedure.
 
-            var prm = new DynamicParameters();
-            prm.Add("@FileId", guid);
-            prm.Add("@ChunkNum", num);
+			var prm = new DynamicParameters();
+			prm.Add("@FileId", guid);
+			prm.Add("@ChunkNum", num);
 
-            if (dbConnection == null || dbConnection.State == ConnectionState.Closed)
-            {
-                throw new ArgumentNullException("The database connection must be open prior to use.");
-            } 
-             
-            return dbConnection.ExecuteScalar<byte[]>("ReadChunkContent", prm, commandType: CommandType.StoredProcedure);
- 
-        }
+			if (dbConnection == null || dbConnection.State == ConnectionState.Closed)
+			{
+				throw new ArgumentNullException("The database connection must be open prior to use.");
+			}
 
-    }
+			return dbConnection.ExecuteScalar<byte[]>("ReadChunkContent", prm, commandType: CommandType.StoredProcedure);
+
+		}
+
+	}
 }

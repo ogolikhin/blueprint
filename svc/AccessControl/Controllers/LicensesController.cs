b3a78fb0918f7ef9df0b0977614f66aae5a10dda
Dmitry Agapitov <dmitry.agapitov@blueprintsys.com>
2015-12-10 15:22:54 -0500

    Add licenses status service and sql repository

---
+++ svc/AccessControl/Controllers/LicensesController.cs
@@ -0,0 +1,44 @@
+﻿using System.Threading.Tasks;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using AccessControl.Repositories;
+
+namespace AccessControl.Controllers
+{
+    [RoutePrefix("licenses")]
+    public class LicensesController : ApiController
+    {
+	    private readonly ILicensesRepository _repo;
+
+	    public LicensesController(): this(new SqlLicensesRepository(WebApiConfig.AdminStorage))
+        {
+        }
+
+	    internal LicensesController(ILicensesRepository repo)
+	    {
+		    _repo = repo;
+	    }
+
+	    [HttpGet]
+        [Route("status")]
+        [ResponseType(typeof(HttpResponseMessage))]
+        public async Task<IHttpActionResult> GetActiveLicenses()
+        {
+	        try
+	        {
+		        var licenses = await _repo.GetLicensesStatus(WebApiConfig.LicenseHoldTime);
+
+		        var response = Request.CreateResponse(HttpStatusCode.OK, licenses);
+		        response.Headers.Add("Cache-Control", "no-cache, no-store, must-revalidate"); // HTTP 1.1.
+		        response.Headers.Add("Pragma", "no-cache"); // HTTP 1.0.
+		        return ResponseMessage(response);
+	        }
+	        catch
+	        {
+		        return InternalServerError();
+	        }
+        }
+    }
+}

c87e8b3e0be4f8dbc0f327d7c4ce357732ab1091
Bogdan Tkachenko <bogdan.tkachenko@blueprintsys.com>
2015-12-11 14:31:28 -0500

    update tests - silver02 is working

--- blueprint-automationframework/Blueprint-Tests/Tests/AccessControlTests/SessionsTests.cs
+++ blueprint-automationframework/Blueprint-Tests/Tests/AccessControlTests/SessionsTests.cs
@@ -26,7 +26,6 @@ namespace AccessControlTests
         }
 
         [Test]
-        [Explicit(IgnoreReasons.ProductBug)]
         public static void PostNewSession_OK()
         {
             Session expectSession = Session.CreateSession();
@@ -35,10 +34,10 @@ namespace AccessControlTests
             Assert.AreEqual(session.StatusCode, HttpStatusCode.OK, "'GET {0}' should return {1}, but failed with {2}",
                 _sessionUrl, HttpStatusCode.OK, session.StatusCode);
             Assert.True(expectSession.Equals(session.GetBlueprintObject<Session>()), "UserID from returned session object must be equal to UserID used during session creation");
+            DeleteSession(headers);
         }
 
         [Test]
-        [Explicit(IgnoreReasons.ProductBug)]
         public static void PutSession_OK()
         {
             int artifactId = RandomGenerator.RandomNumber();
@@ -52,69 +51,59 @@ namespace AccessControlTests
             Assert.AreEqual(response.StatusCode, HttpStatusCode.OK, "'PUT {0}' should return {1}, but failed with {2}",
                 _sessionUrl + action + expectedSession.UserId, HttpStatusCode.OK, response.StatusCode);
             Assert.True(expectedSession.Equals(session));
+            DeleteSession(headers);
         }
 
         [Test]
-        [Explicit(IgnoreReasons.ProductBug)]
         public static void PutSession_BadRequest()
         {
             Session expectedSession = Session.CreateSession();
-            var requestToken = WebRequestFacade.GetWebResponseFacade(CreateSessionUrl(expectedSession.UserId,
-                expectedSession.UserName, expectedSession.LicenseLevel), "POST");
-
-            Assert.AreEqual(requestToken.StatusCode, HttpStatusCode.OK, "'POST {0}' should return {1}, but failed with {2}",
-                _sessionUrl + expectedSession.UserId, HttpStatusCode.OK, requestToken.StatusCode);
-
+            Dictionary<string, string> headers = GetSessionToken(expectedSession);
+            
             string action = "do_some_action/";//in current implementation it is an optional parameter to identify operation
             var session = WebRequestFacade.GetWebResponseFacade(_sessionUrl + action + RandomGenerator.RandomNumber(), "PUT");
 
             Assert.AreEqual(HttpStatusCode.BadRequest, session.StatusCode, "'PUT {0}' should return {1}, but failed with {2}",
                 _sessionUrl + action, HttpStatusCode.BadRequest, session.StatusCode);
+            DeleteSession(headers);
         }
 
         [Test]
-        [Explicit(IgnoreReasons.ProductBug)]
-        public static void GetSessionsWithoutSessionToken_NotFound()
+        public static void GetSessionsWithoutSessionToken_OK()
         {
-            Session expectedSession = Session.CreateSession();
-            GetSessionToken(expectedSession);
-
-            var response = WebRequestFacade.GetWebResponseFacade(CreateSessionUrl(expectedSession.UserId,
-                expectedSession.UserName, 3));
-            var session = response.GetBlueprintObject<Session>();
-
-            Assert.AreEqual(HttpStatusCode.NotFound, response.StatusCode, "'GET {0}' should return {1}, but failed with {2}",
-                _sessionUrl + expectedSession.UserId, HttpStatusCode.NotFound, response.StatusCode);
-        }
-
-        [Test]
-        [Explicit(IgnoreReasons.ProductBug)]
-        public static void DeleteSessions_OK()
-        {
-            int userId = 2311;//in current implementation of AccessControl we can put any Int
-            string userName = RandomGenerator.RandomAlphaNumeric(7);
             Session expectedSession = Session.CreateSession();
             Dictionary<string, string> headers = GetSessionToken(expectedSession);
 
-            var response = WebRequestFacade.GetWebResponseFacade(_sessionUrl, "DELETE", headers);
+            var response = WebRequestFacade.GetWebResponseFacade(CreateSessionUrl(expectedSession));
+            var session = response.GetBlueprintObject<Session>();
+
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode, "'GET {0}' should return {1}, but failed with {2}",
+                _sessionUrl + expectedSession.UserId, HttpStatusCode.NotFound, response.StatusCode);
+            DeleteSession(headers);
+        }
+
+        [Test]
+        public static void DeleteSessions_OK()
+        {
+            Session expectedSession = Session.CreateSession();
+            Dictionary<string, string> headers = GetSessionToken(expectedSession);
+
+            var response = DeleteSession(headers);
 
             Assert.AreEqual(HttpStatusCode.OK, response.StatusCode, "'DELETE {0}' should return {1}, but failed with {2}",
                 _sessionUrl, HttpStatusCode.OK, response.StatusCode);
         }
 
         [Test]
-        [Explicit(IgnoreReasons.ProductBug)]
         public static void DeleteSessions_BadRequest()
         {
             Session expectedSession = Session.CreateSession();
-            var requestToken = WebRequestFacade.GetWebResponseFacade(CreateSessionUrl(expectedSession.UserId,
-                expectedSession.UserName, 3), "POST");
+            Dictionary<string, string> headers = GetSessionToken(expectedSession);
 
-            Assert.AreEqual(requestToken.StatusCode, HttpStatusCode.OK, "'POST {0}' should return {1}, but failed with {2}",
-                _sessionUrl + expectedSession.UserId, HttpStatusCode.OK, requestToken.StatusCode);
             var response = WebRequestFacade.GetWebResponseFacade(_sessionUrl, "DELETE");
             Assert.AreEqual(HttpStatusCode.BadRequest, response.StatusCode, "'DELETE {0}' should return {1}, but failed with {2}",
                 _sessionUrl, HttpStatusCode.BadRequest, response.StatusCode);
+            DeleteSession(headers);
         }
 
         /// <summary>
@@ -127,8 +116,8 @@ namespace AccessControlTests
         /// <returns>Header with valid Session-Token</returns>
         private static Dictionary<string, string> GetSessionToken(Session session)
         {
-            var requestToken = WebRequestFacade.GetWebResponseFacade(CreateSessionUrl(session.UserId, session.UserName,
-                session.LicenseLevel), "POST");
+            var requestToken = WebRequestFacade.GetWebResponseFacade(CreateSessionUrl(session) + "?userName=" +
+                session.UserName + "&licenseLevel=" + session.LicenseLevel + "&isSso=" + session.IsSso, "POST");
 
             Assert.AreEqual(requestToken.StatusCode, HttpStatusCode.OK, "'POST {0}' should return {1}, but failed with {2}",
                 _sessionUrl + session.UserId, HttpStatusCode.OK, requestToken.StatusCode);
@@ -141,6 +130,11 @@ namespace AccessControlTests
             return headers;
         }
 
+        private static WebResponseFacade DeleteSession(Dictionary <string, string> headers)
+        {
+            return WebRequestFacade.GetWebResponseFacade(_sessionUrl, "DELETE", headers);
+        }
+
         /// <summary>
         /// Creates url for PostSession request.
         /// </summary>
@@ -149,10 +143,9 @@ namespace AccessControlTests
         /// <param name="licenseLevel">license - now any int</param>
         /// <param name="isSso">boolean value</param>
         /// <returns>url for Session object</returns>
-        private static string CreateSessionUrl(int userId, string userName, int licenseLevel, bool isSso = true)
+        private static string CreateSessionUrl(Session session)
         {
-            return _sessionUrl + userId.ToString() + "?userName=" + userName + "&licenseLevel=" +
-                   licenseLevel.ToString() + "&isSso=" + isSso.ToString();
+            return _sessionUrl + session.UserId;
         }
     }
 }

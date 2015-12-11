a9c3f7f7353e9d57a0a64ecdc5413ae7112dab54
Chris Just <chris.just@blueprintsys.com>
2015-12-11 12:05:44 -0500

    Added GetFileStoreFromTestConfig() to FileStoreFactory and changed FileStore tests to use it, also updated ProjectTests to get rid of a warning.

--- blueprint-automationframework/Blueprint-Tests/Helper/Factories/FileStoreFactory.cs
+++ blueprint-automationframework/Blueprint-Tests/Helper/Factories/FileStoreFactory.cs
@@ -1,5 +1,8 @@
-﻿using Model;
+﻿using System.Data;
+using Logging;
+using Model;
 using Model.Impl;
+using TestConfig;
 
 namespace Helper.Factories
 {
@@ -14,6 +17,27 @@ namespace Helper.Factories
         {
             IFileStore filestore = new FileStore(address);
             return filestore;
+        }
+
+        /// <summary>
+        /// Creates a FileStore object with the settings defined in the TestConfiguration.
+        /// </summary>
+        /// <returns>The FileStore object.</returns>
+        /// <exception cref="DataException">If there was an error reading required information from the TestConfiguration.</exception>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1024:UsePropertiesWhereAppropriate")]   // Ignore this warning.
+        public static IFileStore GetFileStoreFromTestConfig()
+        {
+            TestConfiguration testConfig = TestConfiguration.GetInstance();
+            const string keyName = "FileStore";
+
+            if (!testConfig.Services.ContainsKey(keyName))
+            {
+                string msg = string.Format("No <Service> tag named '{0}' was found in the TestConfiguration.xml file!  Please update it.", keyName);
+                Logger.WriteError(msg);
+                throw new DataException(msg);
+            }
+
+            return CreateFileStore(testConfig.Services[keyName].Address);
         }
     }
 }
--- blueprint-automationframework/Blueprint-Tests/Tests/OpenAPITests/ProjectTests.cs
+++ blueprint-automationframework/Blueprint-Tests/Tests/OpenAPITests/ProjectTests.cs
@@ -1,4 +1,5 @@
 ﻿
+using System;
 using System.Collections.Generic;
 using CustomAttributes;
 using Helper.Factories;
@@ -53,12 +54,27 @@ namespace OpenAPITests
 
         [Test]
         [Ignore(IgnoreReasons.UnderDevelopment)]
+        // TODO: Need to implement Project.Equals(), CreateProject() & DeleteProject().
         public void GetProject()
         {
-            // TODO: Create some projects and collect the projectID
-            // TODO: Verify that the projects we created were returned in the list of projects.
-            IProject projects = new Project().GetProject(_server.Address, 1, _user);
-            // TODO: Delete the projects we added.
+            // Setup: Create a project and collect the projectID
+            IProject project = ProjectFactory.CreateProject();
+
+            try
+            {
+                // Try to get the project we added.
+                IProject foundProject = new Project().GetProject(_server.Address, project.Id, _user);
+
+                // Verify that the projects we created were returned in the list of projects.
+                Assert.That(project.Equals(foundProject),
+                    "The project we found on the server is not the same as the project we added!\n--> Added project = {0}\n--> Found project = {1}",
+                    project.ToString(), foundProject.ToString());
+            }
+            finally
+            {
+                // Cleanup: Delete the project we added.
+                project.DeleteProject();
+            }
         }
     }
 }

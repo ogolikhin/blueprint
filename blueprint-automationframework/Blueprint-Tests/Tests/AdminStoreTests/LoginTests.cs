7f56375a3c73352b41a0e9229e433ed4164e5a1d
Bogdan Tkachenko <bogdan.tkachenko@blueprintsys.com>
2015-12-17 17:34:57 -0500

    add ServiceErrorMessage, add test for existing user/wrong password, fix references for NUnit

--- blueprint-automationframework/Blueprint-Tests/Tests/AdminStoreTests/LoginTests.cs
+++ blueprint-automationframework/Blueprint-Tests/Tests/AdminStoreTests/LoginTests.cs
@@ -11,12 +11,13 @@ using Logging;
 using Model.Impl;
 using TestConfig;
 using Model.Facades;
+using Model.Factories;
 
 namespace AdminStoreTests
 {
     [TestFixture]
     [Category(Categories.AdminStore)]
-    public class LoginTests
+    public static class LoginTests
     {
         private const string _serviceRoute = "svc/adminstore/";
         private const string _sessionRoute = "sessions/";
@@ -25,13 +26,13 @@ namespace AdminStoreTests
         private static IUser _user = null;
 
         [SetUp]
-        public void SetUp()
+        public static void SetUp()
         {
             _user = UserFactory.CreateUserAndAddToDatabase();
         }
 
         [TearDown]
-        public void TearDown()
+        public static void TearDown()
         {
             if (_user != null)
             {
@@ -44,9 +45,19 @@ namespace AdminStoreTests
         [Test]
         public static void Login_ValidUser_OK()
         {
-            WebResponseFacade _response = Login(_user);
+            WebResponseFacade _response = Login(_user.Username, _user.Password);
             Assert.AreEqual(HttpStatusCode.OK, _response.StatusCode, "Login failed with {0} code and {1}", _response.StatusCode,
                 _response.ResponseString);
+        }
+
+        [Test]
+        public static void Login_ValidUserBadPassword_OK()
+        {
+            WebResponseFacade _response = Login(_user.Username, RandomGenerator.RandomAlphaNumeric(8));
+            IServiceErrorMessage expectedMessage = ServiceErrorMessageFactory.CreateServiceErrorMessage(2000, "Invalid username or password");
+            var recievedMessage = _response.GetBlueprintObject<ServiceErrorMessage>();
+            Assert.AreEqual(HttpStatusCode.Unauthorized, _response.StatusCode, "Login failed with {0}", _response.StatusCode);
+            Assert.True(expectedMessage.Equals(recievedMessage), "Response message is different from expected");
         }
 
         /// <summary>
@@ -54,11 +65,11 @@ namespace AdminStoreTests
         /// </summary>
         /// <param name="user">User for login.</param>
         /// <returns>The WebResponseFacade.</returns>
-        private static WebResponseFacade Login(IUser user)
+        private static WebResponseFacade Login(string username, string password)
         {
             //string encodedUsername = HashingUtilities.GenerateSaltedHash(user.Username, string.Empty);
-            string encodedUsername = EncodeTo64UTF8(user.Username);
-            string encodedPassword = EncodeTo64UTF8(user.Password);
+            string encodedUsername = EncodeTo64UTF8(username);
+            string encodedPassword = EncodeTo64UTF8(password);
             Dictionary<string, string> headers = new Dictionary<string, string> { { "Content-Type", "Application/json" } };
             return WebRequestFacade.GetWebResponseFacade(_sessionUrl + "?login=" + encodedUsername, "POST", headers, encodedPassword);
         }

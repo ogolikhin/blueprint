ac65c7aac87da4e82dfbb34141ad2468853effea
Chris Dufour <chris.dufour@blueprintsys.com>
2015-12-18 15:04:18 -0500

    Create the ability to log to WIndows log - used when the Log Service fails

---
+++ svc/lib/ServiceLibrary/LocalEventLog/LocalLog.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceLibrary.LocalEventLog
+{
+    public class LocalLog : ILocalLog
+    {
+        public static LocalLog Log = new LocalLog();
+
+        private const string LOGNAME = "BlueprintSys";
+        private const string LOGSOURCE = "BlueprintSys-Admin";
+
+        private void CreateLogs()
+        {
+            if (!EventLog.SourceExists(LOGSOURCE))
+            {
+                EventLog.CreateEventSource(LOGSOURCE, LOGNAME);
+            }
+        }
+
+        public void LogError(string message)
+        {
+            CreateLogs();
+            EventLog.WriteEntry(LOGSOURCE, message, EventLogEntryType.Error);
+        }
+
+        public void LogWarning(string message)
+        {
+            CreateLogs();
+            EventLog.WriteEntry(LOGSOURCE, message, EventLogEntryType.Warning);
+        }
+    }
+}

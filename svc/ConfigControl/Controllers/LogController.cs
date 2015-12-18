fc2d06b9b25bf23456317ec0e043fa3973d9fabe
Chris Dufour <chris.dufour@blueprintsys.com>
2015-12-18 15:00:10 -0500

    Create Log webservice and hook up Database Log sink

---
+++ svc/ConfigControl/Controllers/LogController.cs
@@ -0,0 +1,88 @@
+﻿using ServiceLibrary.EventSources;
+using ServiceLibrary.Helpers;
+using ServiceLibrary.Models;
+using System.Web;
+using System.Web.Http;
+
+namespace ConfigControl.Controllers
+{
+    [RoutePrefix("log")]
+    public class LogController : ApiController
+    {
+        internal readonly IHttpClientProvider _httpClientProvider;
+
+        public LogController() : this(new HttpClientProvider())
+        {
+        }
+
+        internal LogController(IHttpClientProvider httpClientProvider)
+        {
+            _httpClientProvider = httpClientProvider;
+        }
+
+        [HttpPost]
+        [Route("")]
+        public IHttpActionResult Log([FromBody]LogEntry logEntry)
+        {
+            var ipAdress = "";
+            if (this.Request != null && this.Request.Properties.ContainsKey("MS_HttpContext"))
+            {
+                var ctx = Request.Properties["MS_HttpContext"] as HttpContextWrapper;
+                if (ctx != null)
+                {
+                    ipAdress = ctx.Request.UserHostAddress;
+                }
+            }
+
+            switch (logEntry.LogLevel)
+            {
+                case LogLevelEnum.Error:
+                    BlueprintEventSource.Log.Error(
+                        ipAdress,
+                        logEntry.Source,
+                        logEntry.Message,
+                        logEntry.MethodName,
+                        logEntry.FilePath,
+                        logEntry.LineNumber,
+                        logEntry.StackTrace);
+                    break;
+                case LogLevelEnum.Warning:
+                    BlueprintEventSource.Log.Warning(
+                        ipAdress,
+                        logEntry.Source,
+                        logEntry.Message,
+                        logEntry.MethodName,
+                        logEntry.FilePath,
+                        logEntry.LineNumber,
+                        logEntry.StackTrace);
+                    break;
+                case LogLevelEnum.Informational:
+                    BlueprintEventSource.Log.Informational(
+                        ipAdress,
+                        logEntry.Source,
+                        logEntry.Message,
+                        logEntry.MethodName,
+                        logEntry.FilePath,
+                        logEntry.LineNumber,
+                        logEntry.StackTrace);
+                    break;
+                case LogLevelEnum.Verbose:
+                    BlueprintEventSource.Log.Verbose(
+                        ipAdress,
+                        logEntry.Source,
+                        logEntry.Message,
+                        logEntry.MethodName,
+                        logEntry.FilePath,
+                        logEntry.LineNumber,
+                        logEntry.StackTrace);
+                    break;
+                default:
+                    break;
+            }
+
+            return Ok();
+        }
+
+    }
+
+}
--- svc/ConfigControl/Global.asax.cs
+++ svc/ConfigControl/Global.asax.cs
@@ -1,12 +1,58 @@
-﻿using System.Web.Http;
+﻿using Logging.Database;
+using Microsoft.Practices.EnterpriseLibrary.SemanticLogging;
+using ServiceLibrary.EventSources;
+using System;
+using System.Diagnostics.Tracing;
+using System.Web.Http;
 
 namespace ConfigControl
 {
     public class WebApiApplication : System.Web.HttpApplication
     {
+        private EventListener dbListener;
+
         protected void Application_Start()
         {
+            this.SetupEventTracing();
+
             GlobalConfiguration.Configure(WebApiConfig.Register);
         }
+
+        public sealed override void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                base.Dispose();
+                this.DisposeEventTracing();
+            }
+        }
+
+        private void SetupEventTracing()
+        {
+            // Log all events to DB 
+            this.dbListener = BlueprintSqlDatabaseLog.CreateListener(
+                "BlueprintSys-Blueprint-Blueprint",
+                WebApiConfig.AdminStorage,
+                bufferingInterval: TimeSpan.FromSeconds(3),
+                bufferingCount: 200);
+            dbListener.EnableEvents(BlueprintEventSource.Log, EventLevel.LogAlways, Keywords.All);
+
+        }
+
+        private void DisposeEventTracing()
+        {
+            if (dbListener != null)
+            {
+                this.dbListener.DisableEvents(BlueprintEventSource.Log);
+                this.dbListener.Dispose();
+            }
+        }
+
     }
 }

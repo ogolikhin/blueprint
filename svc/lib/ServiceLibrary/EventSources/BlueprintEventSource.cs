706ebd67822f4beb0289c2c8c747bfbedc9a0ccb
Chris Dufour <chris.dufour@blueprintsys.com>
2015-12-18 15:02:10 -0500

    Create BlueprintEventSource

---
+++ svc/lib/ServiceLibrary/EventSources/BlueprintEventSource.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.Linq;
+using System.Web;
+
+namespace ServiceLibrary.EventSources
+{
+    [EventSource(Name = "BlueprintSys-Blueprint-Blueprint")]
+    public sealed class BlueprintEventSource : EventSource
+    {
+        public static BlueprintEventSource Log = new BlueprintEventSource();
+
+        [Event(100, Level = EventLevel.Error, Message = "{2}")]
+        public void Error(string ipAddress, string source, string message, string methodName, string filePath, int lineNumber, string stackTrace)
+        {
+            if (IsEnabled(EventLevel.Error, EventKeywords.None))
+            {
+                WriteEvent(100, ipAddress, source, message, methodName, filePath, lineNumber, stackTrace);
+            }
+        }
+
+        [Event(200, Level = EventLevel.Warning, Message = "{2}")]
+        public void Warning(string ipAddress, string source, string message, string methodName, string filePath, int lineNumber, string stackTrace)
+        {
+            if (IsEnabled(EventLevel.Warning, EventKeywords.None))
+            {
+                WriteEvent(200, ipAddress, source, message, methodName, filePath, lineNumber, stackTrace);
+            }
+        }
+
+        [Event(300, Level = EventLevel.Informational, Message = "{2}")]
+        public void Informational(string ipAddress, string source, string message, string methodName, string filePath, int lineNumber, string stackTrace)
+        {
+            if (IsEnabled(EventLevel.Informational, EventKeywords.None))
+            {
+                WriteEvent(300, ipAddress, source, message, methodName, filePath, lineNumber, stackTrace);
+            }
+        }
+
+        [Event(400, Level = EventLevel.Verbose, Message = "{2}")]
+        public void Verbose(string ipAddress, string source, string message, string methodName, string filePath, int lineNumber, string stackTrace)
+        {
+            if (IsEnabled(EventLevel.Verbose, EventKeywords.None))
+            {
+                WriteEvent(400, ipAddress, source, message, methodName, filePath, lineNumber, stackTrace);
+            }
+        }
+
+    }
+}
---
+++ svc/lib/ServiceLibrary/Models/LogEntry.cs
@@ -0,0 +1,52 @@
+﻿using Newtonsoft.Json;
+
+namespace ServiceLibrary.Models
+{
+    [JsonObject]
+    public class LogEntry
+    {
+        private LogEntry()
+        {
+            //disable
+        }
+
+        public LogEntry(
+            LogLevelEnum logLevel,
+            string source,
+            string message,
+            string methodName,
+            string filePath,
+            int lineNumber,
+            string stackTrace)
+        {
+            LogLevel = logLevel;
+            Source = source == null ? string.Empty : source;
+            Message = message == null ? string.Empty: message;
+            MethodName = methodName==null ? string.Empty: methodName;
+            FilePath = filePath == null ? string.Empty : filePath;
+            LineNumber = lineNumber;
+            StackTrace = stackTrace == null ? string.Empty : stackTrace;
+        }
+
+        [JsonProperty]
+        public string Source { get; set; }
+
+        [JsonProperty]
+        public LogLevelEnum LogLevel { get; set; }
+
+        [JsonProperty]
+        public string Message { get; set; }
+
+        [JsonProperty]
+        public string MethodName { get; set; }
+
+        [JsonProperty()]
+        public string FilePath { get; set; }
+
+        [JsonProperty]
+        public int LineNumber { get; set; }
+
+        [JsonProperty]
+        public string StackTrace { get; set; }
+    }
+}

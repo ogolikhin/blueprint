14e33be136ae24180532b2815ca5784b97db11db
Chris Just <chris.just@blueprintsys.com>
2016-01-04 11:55:20 -0500

    Adding a TestDouble for AccessControl that currently acts purely as a proxy to the real AccessControl.

---
+++ blueprint-automationframework/Test-Doubles/AccessControlDouble/AccessControlDouble/Controllers/StatusController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Configuration;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using CommonUtilities;
+
+namespace AccessControlDouble.Controllers
+{
+    [RoutePrefix("status")]
+    public class StatusController : ApiController
+    {
+        private const string SVC = "status";
+
+        /// <summary>
+        /// Method to return current status of AccessControl Web Service.
+        /// </summary>
+        /// <returns>200 OK if no issue is detected.</returns>
+        [HttpGet]
+        [Route("")]
+        [ResponseType(typeof(HttpResponseMessage))]
+        public async Task<IHttpActionResult> GetStatus()
+        {
+            HttpClient http = new HttpClient();
+            string accessControl = ConfigurationManager.AppSettings["AccessControl"];
+            http.BaseAddress = new Uri(accessControl);
+            WebUtils.CopyHttpRequestHeaders(Request.Headers, http.DefaultRequestHeaders);
+
+            string path = Request.RequestUri.LocalPath.Replace("/svc/accesscontrol/", string.Empty);
+            Uri uri = new Uri(accessControl + path);
+
+            var result = await http.GetAsync(uri);
+
+            return ResponseMessage(result);
+        }
+    }
+}

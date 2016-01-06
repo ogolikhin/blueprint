4eea9d5492547ac8da733f0f336116b079252bbb
munishsaini <munish.saini@blueprintsys.com>
2016-01-06 17:23:37 -0500

    modify filestore to validate token provided through either cookie or header

--- svc/lib/ServiceLibrary/Repositories/AccessControl/ServiceSessionRepository.cs
+++ svc/lib/ServiceLibrary/Repositories/AccessControl/ServiceSessionRepository.cs
@@ -13,6 +13,10 @@ namespace ServiceLibrary.Repositories.ConfigControl
 {
     public class ServiceSessionRepository : ISessionRepository
     {
+        private const string BlueprintSessionToken = "Session-Token";
+        private const string BlueprintSessionCookie = "BLUEPRINT_SESSION_TOKEN";
+        private const string AccessControl = "AccessControl";
+
         internal readonly IHttpClientProvider _httpClientProvider;
         public ServiceSessionRepository()
             : this(new HttpClientProvider())
@@ -27,21 +31,34 @@ namespace ServiceLibrary.Repositories.ConfigControl
         // Wrapper call for Access Control Put method
         public async Task GetAccessAsync(HttpRequestMessage request)
         {
-            var uri = ConfigurationManager.AppSettings["AccessControl"];
+            var uri = ConfigurationManager.AppSettings[AccessControl] + op + "/" + aid.ToString();
             using (var http = _httpClientProvider.Create())
             {
                 http.BaseAddress = new Uri(uri);
                 http.DefaultRequestHeaders.Accept.Clear();
-                http.DefaultRequestHeaders.Add("Session-Token", GetHeaderSessionToken(request));
+                http.DefaultRequestHeaders.Add(BlueprintSessionToken, GetHeaderSessionToken(request));
                 var result = await http.PutAsync("sessions", null);
                 result.EnsureSuccessStatusCode();
             }
         }
         private string GetHeaderSessionToken(HttpRequestMessage request)
         {
-            if (request.Headers.Contains("Session-Token") == false)
-                throw new ArgumentNullException();
-            return request.Headers.GetValues("Session-Token").FirstOrDefault();
+            if (request.Headers.Contains(BlueprintSessionToken) == false)
+            {
+                if (request.Method != HttpMethod.Get)
+                {
+                    throw new ArgumentNullException();
+                }
+                var sessionTokenCookie = request.Headers.GetCookies("BLUEPRINT_SESSION_TOKEN").FirstOrDefault();
+                if (sessionTokenCookie == null)
+                {
+                    throw new ArgumentNullException();
+                }
+                var value = sessionTokenCookie[BlueprintSessionCookie].Value;
+
+
+                return value;
+            }
+            return request.Headers.GetValues(BlueprintSessionToken).FirstOrDefault();
         }
     }
-}

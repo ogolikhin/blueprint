1892c73f18a67a26415f1a7ad9427cbf4afcc012
Areag Osman <areag.osman@blueprintsys.com>
2016-03-09 17:20:42 -0500

    Update migration sql and increment version to match blueprint-current repo release-dylan branch

--- svc/SolutionAssemblyVersion.cs
+++ svc/SolutionAssemblyVersion.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
 // The assembly file verison allows us to see when the solution was built using the format yyyy.mm.dd.hhmm
-[assembly: AssemblyFileVersion("7.0.0.263")]
+[assembly: AssemblyFileVersion("7.0.1.57")]

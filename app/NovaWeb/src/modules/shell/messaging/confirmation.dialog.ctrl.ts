cab49aa2b06349f1d90bca4320b765ba8ffb3321
Nikita Pliassounov <nikita.pliassounov@blueprintsys.com>
2016-03-21 11:51:39 -0400

    Adding logic to confirm session override + handling more error conditions

---
+++ app/NovaWeb/src/modules/shell/messaging/confirmation.dialog.ctrl.ts
@@ -0,0 +1,19 @@
+﻿import "angular";
+
+export class ConfirmationDialogCtrl {
+    public msg: string;
+    public acceptButtonName: string = "OK";
+    public cancelButtonName: string = "Cancel";
+
+    static $inject: [string] = ["$uibModalInstance"];
+    constructor(private $uibModalInstance: ng.ui.bootstrap.IModalServiceInstance) {
+    }
+
+    public accept() {
+        this.$uibModalInstance.close(true);
+    }
+
+    public cancel() {
+        this.$uibModalInstance.close(false);
+    }
+}

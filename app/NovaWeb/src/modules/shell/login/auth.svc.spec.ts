71d63d0c6eaf85ede9b87f78bd9d2a046675e0f3
Nikita Pliassounov <nikita.pliassounov@blueprintsys.com>
2016-03-21 16:49:48 -0400

    Adding unit tests for auth and session services

--- app/NovaWeb/src/modules/shell/login/auth.svc.spec.ts
+++ app/NovaWeb/src/modules/shell/login/auth.svc.spec.ts
@@ -27,5 +27,93 @@ describe("AuthSvc", () => {
             $httpBackend.verifyNoOutstandingRequest();
         }));
 
+        it("resolve successfully", inject(($httpBackend: ng.IHttpBackendService, auth: IAuth) => {
+            // Arrange
+            $httpBackend.expectGET("/svc/adminstore/users/loginuser")
+                .respond(200, <IUser>{
+                    DisplayName: "Default Instance Admin", Login: "admin"
+                }
+            );
+
+            // Act
+            var error: any;
+            var user: IUser;
+            var result = auth.getCurrentUser().then((responce) => { user = responce; }, (err) => error = err);
+            $httpBackend.flush();
+
+            // Assert
+            expect(error).toBe(undefined, "responce got error");
+            expect(user.Login).toBe("admin", "user login does not match");
+            $httpBackend.verifyNoOutstandingExpectation();
+            $httpBackend.verifyNoOutstandingRequest();
+        }));
+    });
+
+    describe("login", () => {
+        it("reject on error with default error message", inject(($httpBackend: ng.IHttpBackendService, auth: IAuth) => {
+            // Arrange
+            var status: number = 401;
+            var message: string = "Login Failed";
+            $httpBackend.expectPOST("/svc/adminstore/sessions/?login=" + AuthSvc.encode("admin")+"&force=false", angular.toJson(AuthSvc.encode("changeme")))
+                .respond(status, { Message: message, ErrorCode: 2000 });
+
+            // Act
+            var error: any;
+            var result = auth.login("admin", "changeme", false).then(() => { }, (err) => error = err);
+            $httpBackend.flush();
+
+            // Assert
+            expect(error.statusCode).toBe(401, "error.statusCode is not 401");
+            expect(error.message).toBe("Login Failed", "error.message does not match");
+            $httpBackend.verifyNoOutstandingExpectation();
+            $httpBackend.verifyNoOutstandingRequest();
+        }));
+
+        it("respond with success", inject(($httpBackend: ng.IHttpBackendService, auth: IAuth) => {
+            // Arrange
+            $httpBackend.expectPOST("/svc/adminstore/sessions/?login=" + AuthSvc.encode("admin") + "&force=false", angular.toJson(AuthSvc.encode("changeme")))
+                .respond(200, <IUser>{
+                    DisplayName: "Default Instance Admin", Login: "admin"
+                }
+            );
+            $httpBackend.expectPOST("/svc/shared/licenses/verify", "")
+                .respond(200);
+            $httpBackend.expectGET("/svc/adminstore/users/loginuser")
+                .respond(200, <IUser>{
+                    DisplayName: "Default Instance Admin", Login: "admin"
+                }
+            );
+
+            // Act
+            var error: any;
+            var user: IUser;
+            var result = auth.login("admin", "changeme",false).then((responce) => { user = responce; }, (err) => error = err);
+            $httpBackend.flush();
+
+            // Assert
+            expect(error).toBe(undefined, "responce got error");
+            expect(user.Login).toBe("admin", "user login does not match");
+            $httpBackend.verifyNoOutstandingExpectation();
+            $httpBackend.verifyNoOutstandingRequest();
+        }));
+    });
+
+    describe("logout", () => {
+        it("complete logout", inject(($httpBackend: ng.IHttpBackendService, auth: IAuth) => {
+            // Arrange
+            $httpBackend.expectDELETE("/svc/adminstore/sessions")
+                .respond(200);
+
+            // Act
+            var error: any;
+            var user: IUser = <IUser>{ DisplayName: "Default Instance Admin", Login: "admin" };
+            var result = auth.logout(user, true).then(() => {}, (err) => error = err);
+            $httpBackend.flush();
+
+            // Assert
+            expect(error).toBe(undefined, "responce got error");
+            $httpBackend.verifyNoOutstandingExpectation();
+            $httpBackend.verifyNoOutstandingRequest();
+        }));
     });
 });
---
+++ app/NovaWeb/src/modules/shell/login/session.svc.spec.ts
@@ -0,0 +1,75 @@
+﻿import "angular";
+import "angular-mocks"
+import {ISession, SessionSvc} from "./session.svc";
+import {IUser, IAuth} from "./auth.svc";
+
+export class AuthSvcMock implements IAuth {
+
+    public static $inject = ["$q"];
+    constructor(private $q: ng.IQService) {
+    }
+
+    public getCurrentUser(): ng.IPromise<IUser> {
+        var deferred = this.$q.defer<IUser>();
+        var user: IUser = <IUser>{ DisplayName: "Default Instance Admin", Login: "admin" };
+        deferred.resolve(user);
+        return deferred.promise;
+    }
+
+    public login(userName: string, password: string, overrideSession: boolean): ng.IPromise<IUser> {
+        var deferred = this.$q.defer<IUser>();
+        var user: IUser = <IUser>{ DisplayName: "Default Instance Admin", Login: "admin" };
+        deferred.resolve(user);
+        return deferred.promise;
+    }
+
+    public logout(userInfo: IUser, skipSamlLogout: boolean): ng.IPromise<any> {
+        var deferred = this.$q.defer<any>();
+        deferred.resolve();
+        return deferred.promise;
+    }
+}
+
+export class ModalServiceMock implements ng.ui.bootstrap.IModalService {
+    public open(options: ng.ui.bootstrap.IModalSettings): ng.ui.bootstrap.IModalServiceInstance {
+        return null;
+    }
+}
+
+describe("SessionSvc", () => {
+    beforeEach(angular.mock.module(($provide: ng.auto.IProvideService) => {
+        $provide.service("session", SessionSvc);
+        $provide.service("auth", AuthSvcMock);
+        $provide.service("$uibModal", ModalServiceMock);
+    }));
+
+    describe("getCurrentUser", () => {
+        it("return current user from auth service", inject(($rootScope: ng.IRootScopeService, session: ISession) => {
+            // Arrange
+
+            // Act
+            var error: any;
+            var result = session.ensureAuthenticated().then(() => {}, (err) => error = err);
+            $rootScope.$digest();
+
+            // Assert
+            expect(error).toBe(undefined, "error is set");
+            expect(session.currentUser).toBeDefined();
+            expect(session.currentUser.Login).toBe("admin", "current user is not admin");
+        }))
+    });
+
+    describe("logout", () => {
+        it("complete logout successfully", inject(($rootScope: ng.IRootScopeService, session: ISession) => {
+            // Arrange
+
+            // Act
+            var error: any;
+            var result = session.logout().then(() => { }, (err) => { error = err; });
+            $rootScope.$digest();
+
+            // Assert
+            expect(error).toBe(undefined, "error is set");
+        }));
+    });
+});

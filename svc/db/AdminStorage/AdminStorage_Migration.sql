736618842d33fcc32849ca877f7a0fdbddc3bda8
Areag Osman <areag.osman@blueprintsys.com>
2016-03-29 13:38:59 -0400

    #178064 - Updates ApplicationLabels db table creation

--- svc/db/AdminStorage/AdminStorage_Migration.sql
+++ svc/db/AdminStorage/AdminStorage_Migration.sql
@@ -736,6 +736,7 @@ Description:
 Change History:
 Date			Name					Change
 2015/11/03		Chris Dufour			Initial Version
+2016/09/29		Areag Osman				Extends character limit for Key & Text columns, adds index for table
 ******************************************************************************************************************************/
 
 IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[ApplicationLabels]') AND type in (N'U'))
@@ -747,10 +748,8 @@ CREATE TABLE [dbo].[ApplicationLabels](
 	[Key] [nvarchar](128) NOT NULL,
 	[Locale] [nvarchar](32) NOT NULL,
 	[Text] [nvarchar](512) NOT NULL,
- CONSTRAINT [PK_ApplicationLabels_ApplicationLabelId_Locale] PRIMARY KEY CLUSTERED 
-(
-	[ApplicationLabelId] ASC
-)) ON [PRIMARY]
+ CONSTRAINT [PK_ApplicationLabels_ApplicationLabelId] PRIMARY KEY CLUSTERED ([ApplicationLabelId] ASC)
+) ON [PRIMARY]
 GO
 
 

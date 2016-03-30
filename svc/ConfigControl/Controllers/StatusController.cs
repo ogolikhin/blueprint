4fd9b2a5eb9e5cbd99ca0d9215112d8191f975bf
Cristian Poll <cristian@cristianpoll.com>
2016-03-30 09:57:41 -0400

    177090 change getstatus sp for fs

--- svc/ConfigControl/Controllers/StatusController.cs
+++ svc/ConfigControl/Controllers/StatusController.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
+﻿using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web.Http;
 using System.Web.Http.Description;
-using ConfigControl;
 using ConfigControl.Repositories;
 using ServiceLibrary.Attributes;
 using ServiceLibrary.Helpers;
@@ -24,8 +21,7 @@ namespace ConfigControl.Controllers
             : this(new StatusControllerHelper(
                         new List<IStatusRepository>
                         {
-                            new SqlStatusRepository(SqlConfigRepository.AdminStorageDatabase, "AdminStorage"),
-                            new ServiceDependencyStatusRepository(new Uri(WebApiConfig.AccessControl), "AccessControlEndpoint")
+                            new SqlStatusRepository(SqlConfigRepository.AdminStorageDatabase, "AdminStorage")
                         },
                         new ServiceLogRepository(),
                         WebApiConfig.LogSourceStatus

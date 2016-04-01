9a384205979da9df2d266c01c1313b7dc93a3137
Darren Martell <darren.martell@blueprintsys.com>
2016-04-01 11:00:22 -0400

    Adds delete methods to Process model; Add first delete task test

---
+++ blueprint-automationframework/Blueprint-Tests/Tests/StorytellerTests/DeleteTaskTests.cs
@@ -0,0 +1,110 @@
+﻿using System.Linq;
+using CustomAttributes;
+using Model;
+using Model.OpenApiModel;
+using Model.Factories;
+using NUnit.Framework;
+using System.Collections.Generic;
+using Helper;
+using Model.StorytellerModel;
+using Model.StorytellerModel.Impl;
+
+namespace StorytellerTests
+{
+    [TestFixture]
+    [Category(Categories.Storyteller)]
+    public class DeleteTaskTests
+    {
+        private IAdminStore _adminStore;
+        private IBlueprintServer _blueprintServer;
+        private IStoryteller _storyteller;
+        private IUser _user;
+        private IProject _project;
+        private bool _deleteChildren = true;
+
+        #region Setup and Cleanup
+
+        [TestFixtureSetUp]
+        public void ClassSetUp()
+        {
+            _adminStore = AdminStoreFactory.GetAdminStoreFromTestConfig();
+            _blueprintServer = BlueprintServerFactory.GetBlueprintServerFromTestConfig();
+            _storyteller = StorytellerFactory.GetStorytellerFromTestConfig();
+            _user = UserFactory.CreateUserAndAddToDatabase();
+            _project = ProjectFactory.GetProject(_user);
+
+            // Get a valid Access Control token for the user (for the new Storyteller REST calls).
+            ISession session = _adminStore.AddSession(_user.Username, _user.Password);
+            _user.SetToken(session.SessionId);
+
+            Assert.IsFalse(string.IsNullOrWhiteSpace(_user.Token.AccessControlToken), "The user didn't get an Access Control token!");
+
+            // Get a valid OpenApi token for the user (for the OpenApi artifact REST calls).
+            _blueprintServer.LoginUsingBasicAuthorization(_user, string.Empty);
+
+            Assert.IsFalse(string.IsNullOrWhiteSpace(_user.Token.OpenApiToken), "The user didn't get an OpenApi token!");
+        }
+
+        [TestFixtureTearDown]
+        public void ClassTearDown()
+        {
+            if (_storyteller.Artifacts != null)
+            {
+                // TODO: implement discard artifacts for test cases that doesn't publish artifacts
+                // Delete all the artifacts that were added.
+                foreach (var artifact in _storyteller.Artifacts.ToArray())
+                {
+                    _storyteller.DeleteProcessArtifact(artifact, deleteChildren: _deleteChildren);
+                }
+            }
+
+            if (_adminStore != null)
+            {
+                // Delete all the sessions that were created.
+                foreach (var session in _adminStore.Sessions.ToArray())
+                {
+                    _adminStore.DeleteSession(session);
+                }
+            }
+
+            if (_user != null)
+            {
+                _user.DeleteUser();
+                _user = null;
+            }
+        }
+
+        #endregion Setup and Cleanup
+
+        [TestCase]
+        [Description("Get the default process after creating and saving a new process artifact.  Verify that the" +
+                     "returned process has the same Id as the process artifact Id and that the numbers of " +
+                     "shapes, links, artifact path links and property values are as expected.")]
+        public void GetDefaultProcess_VerifyReturnedProcess()
+        {
+            // Create and get the default process
+            var process = StorytellerTestHelper.CreateAndGetDefaultProcess(_storyteller, _project, _user);
+
+            // Find precondition task
+            var preconditionTask = process.GetProcessShapeByShapeName(Process.DefaultPreconditionName);
+
+            // Find outgoing process link for precondition task
+            var preconditionOutgoingLink = process.GetOutgoingLinkForShape(preconditionTask);
+
+            Assert.IsNotNull(preconditionOutgoingLink, "Process link was not found.");
+
+            // Add user/system Task immediately after the precondition
+            var userTask = process.AddUserAndSystemTask(preconditionOutgoingLink);
+
+            // Save the process
+            var returnedProcess = _storyteller.UpdateProcess(_user, process);
+
+            var userTaskToBeDeleted = returnedProcess.GetProcessShapeByShapeName(userTask.Name);
+
+            returnedProcess.DeleteUserAndSystemTask(userTaskToBeDeleted);
+
+            // Update and Verify the modified process
+            StorytellerTestHelper.UpdateVerifyAndPublishProcess(returnedProcess, _storyteller, _user);
+        }
+    }
+}

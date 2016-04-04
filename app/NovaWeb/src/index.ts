6e1138baddd425581bfc6c15d7f240a818f427b3
Alex Folomechine <alex.folomechine@blueprintsys.com>
2016-04-04 15:30:33 -0400

    Load the application after checking versions of the browser and the system in order to show the unsupported page even if the browser does not support angularjs. The eviroment detector is re-implemented in javascript, user story 177834

--- app/NovaWeb/src/index.ts
+++ app/NovaWeb/src/index.ts
@@ -1,42 +1,13 @@
 import "angular";
 import "./modules/main/main.module";
-import {ExecutionEnvironmentDetector} from "./modules/shell/execution-environment/execution-environment-detector";
 
 // load our default (non specific) css
 import "./styles/screen.scss";
 
-function initApp() {
-    var app = angular.module("app", ["app.main"])
+declare var appBootstrap: any;
 
-    angular.bootstrap(document, ["app"], {
-        strictDi: true
-    });
-}
-
-window.onload = () => {
-    var executionEnvironmentDetector = new ExecutionEnvironmentDetector();
-
-    if (executionEnvironmentDetector.isSupportedVersion()) {
-        initApp();
-        return;
+(function() {
+    if (appBootstrap.isSupportedVersion) {
+        appBootstrap.initApp();
     }
-
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4) {
-
-            var divUnsupportedBrowser = document.createElement("div");
-            divUnsupportedBrowser.id = "unsupported-browser-container";
-            divUnsupportedBrowser.innerHTML = xhr.responseText;
-            document.body.insertBefore(divUnsupportedBrowser, document.body.firstChild);
-
-            var innerScript = divUnsupportedBrowser.getElementsByTagName("script");
-            for(var i = 0; i < innerScript.length;i++)
-            {
-                eval(innerScript[i].text);
-            }
-        }
-    };
-    xhr.open('GET', '/novaweb/static/unsupported-browser.html');
-    xhr.send();
-};
+})();

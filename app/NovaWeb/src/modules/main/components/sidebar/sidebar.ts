2e89b381ef9072f89216cc3c2c9928204d57d41e
Fabio Gallo, Blueprint <fabio.gallo@blueprintsys.com>
2016-04-07 16:27:02 -0400

    Refactoring of sidebars

--- app/NovaWeb/src/modules/main/components/sidebar/sidebar.ts
+++ app/NovaWeb/src/modules/main/components/sidebar/sidebar.ts
@@ -3,39 +3,36 @@
 
 
 interface ISidebarController {
-    location: string;
     isToggled: boolean;
     toggle(evt: ng.IAngularEvent): void
 }
-
 
 export class Sidebar implements ng.IComponentOptions { 
     public template: string;
     public controller: Function;
     public bindings: any; 
     public transclude: boolean = true;
+
     constructor() {
         this.template = require("./sidebar.html");
         this.controller = SidebarCtrl;
         this.bindings = {
-            visible: "=",
-            alignment: "@"
         };
     }
 }
 
-class SidebarCtrl implements ISidebarController{
-
-    public location: string;
-    private loc: string;
+class SidebarCtrl implements ISidebarController {
+    static $inject: [string] = ["$element"];
     public isToggled: boolean;
-    constructor() {
+
+    constructor(private $element) {
         this.isToggled = false;
     }
 
-    public toggle(evt: ng.IAngularEvent)
-    {
+    public toggle(evt: ng.IAngularEvent) {
         evt.preventDefault();
         this.isToggled = !this.isToggled;
+        if(this.isToggled) this.$element.addClass('show');
+        else this.$element.removeClass('show');
     }
 }

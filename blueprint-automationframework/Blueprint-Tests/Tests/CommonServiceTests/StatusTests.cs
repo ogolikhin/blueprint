a65927c5b54b06de5dea62f856df6176c9d6bc6e
Chris Just <chris.just@blueprintsys.com>
2016-04-08 11:26:37 -0400

    Fixed the [Description] attributes.

--- blueprint-automationframework/Blueprint-Tests/Tests/CommonServiceTests/StatusTests.cs
+++ blueprint-automationframework/Blueprint-Tests/Tests/CommonServiceTests/StatusTests.cs
@@ -11,7 +11,7 @@ namespace CommonServiceTests
         private readonly IBlueprintServer _blueprintSite = BlueprintServerFactory.GetBlueprintServerFromTestConfig();
 
         [TestCase]
-        [System.ComponentModel.Description("Calls the /status endpoint for the main Blueprint site with a valid preAuthorizedKey and verifies that it returns 200 OK and returns the proper data content.")]
+        [Description("Calls the /status endpoint for the main Blueprint site with a valid preAuthorizedKey and verifies that it returns 200 OK and returns the proper data content.")]
         public void Status_ValidateReturnedContent()
         {
             string content = null;
@@ -26,7 +26,7 @@ namespace CommonServiceTests
 
         [TestCase(null)]
         [TestCase("ABCDEFG123456")]
-        [System.ComponentModel.Description("Calls the /status endpoint for the main Blueprint site and passes invalid preAuthorizedKey values.  Verifies that it returns a 401 error.")]
+        [Description("Calls the /status endpoint for the main Blueprint site and passes invalid preAuthorizedKey values.  Verifies that it returns a 401 error.")]
         public void StatusWithBadKeys_Expect401Unauthorized(string preAuthorizedKey)
         {
             Assert.Throws<Http401UnauthorizedException>(() =>

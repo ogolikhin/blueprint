06e11aac0566f0200e19871b52c5241674b99118
Dmitry Lopyrev <dmitry.lopyrev@blueprintsys.com>
2016-04-15 14:53:48 -0400

    US#177974: [Nova] Open Project - Project Picker - added dialog service - added Open Project dialog (place holder) - added labels

---
+++ app/NovaWeb/src/modules/main/components/dialogs/openprojectcontroller.ts
@@ -0,0 +1,24 @@
+﻿import "angular";
+//import {ILocalizationService} from "../../../core/localization";
+import {IDialogParams, BaseDialogController} from "./dialog.svc";
+
+export class OpenProjectController extends BaseDialogController {
+
+    public hasCloseButton: boolean = true;
+
+    private projectId: number = 25;
+
+    static $inject = ["$uibModalInstance", "params"];
+
+    constructor($uibModalInstance: ng.ui.bootstrap.IModalServiceInstance, params: IDialogParams) {
+        super($uibModalInstance, params);
+    }
+
+    public accept = () => {
+        this.$instance.close(this.projectId);
+    };
+
+}
+
+
+

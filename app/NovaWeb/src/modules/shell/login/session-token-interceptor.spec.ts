c2c4a5bae098e66a903d8faac9aebed0aa5b882e
Dmitry Agapitov <dmitry.agapitov@blueprintsys.com>
2016-04-19 11:49:12 -0400

    Add session token interceptor

---
+++ app/NovaWeb/src/modules/shell/login/session-token-interceptor.spec.ts
@@ -0,0 +1,43 @@
+﻿import "angular";
+import "angular-mocks"
+import {SessionTokenInterceptor} from "./session-token-interceptor";
+import {SessionTokenHelper} from "./session.token.helper";
+
+describe("SessionTokenInterceptor", () => {
+    beforeEach(angular.mock.module(($provide: ng.auto.IProvideService) => {
+    
+    }));
+    
+    it("return http config when Session-Token already defined", () => {
+        // Arrange
+        let tokenHelperSpy = spyOn(SessionTokenHelper, "getSessionToken");
+        let config = <ng.IRequestConfig>{};
+        config.headers = {};
+        config.headers[SessionTokenHelper.SESSION_TOKEN_KEY] = "test";
+        let interceptor = new SessionTokenInterceptor();
+
+        // Act
+        let result = interceptor.request(config);
+
+        // Assert
+        expect(result).toEqual(config, "Config is different");
+        expect(tokenHelperSpy).not.toHaveBeenCalled();
+    });    
+
+    it("add Session-Token when not defined", () => {
+        // Arrange
+        const token = "TEST-TOKEN";
+        let tokenHelperSpy = spyOn(SessionTokenHelper, "getSessionToken").and.returnValue(token);
+        let config = <ng.IRequestConfig>{};
+        let interceptor = new SessionTokenInterceptor();
+
+        // Act
+        let result = interceptor.request(config);
+
+        // Assert
+        expect(tokenHelperSpy).toHaveBeenCalled();
+        expect(result.headers).toBeDefined("headers are not defined");
+        expect(result.headers[SessionTokenHelper.SESSION_TOKEN_KEY]).toEqual(token, "token is different");
+    });    
+
+});
---
+++ app/NovaWeb/src/modules/shell/login/session-token-interceptor.ts
@@ -0,0 +1,20 @@
+﻿import "angular";
+import {SessionTokenHelper} from "./session.token.helper";
+
+export class SessionTokenInterceptor {
+    constructor() { }
+
+    public request = (config: ng.IRequestConfig) => {
+        if (config.headers && config.headers[SessionTokenHelper.SESSION_TOKEN_KEY]) {
+            return config;
+        }
+
+        var token = SessionTokenHelper.getSessionToken();
+        if (!config.headers) {
+            config.headers = {};
+        }
+        config.headers[SessionTokenHelper.SESSION_TOKEN_KEY] = token;
+        return config;
+    };
+}
+

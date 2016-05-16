fdc28613199ad2233e2db80d4bb1cb76875b0807
Cristian Poll <cristian@cristianpoll.com>
2016-05-16 16:05:49 -0400

    add failing unit test

---
+++ app/NovaWeb/src/modules/shell/app.component.spec.ts
@@ -0,0 +1,31 @@
+﻿import "angular";
+import "angular-mocks";
+import "angular-sanitize";
+import "./index";
+import {ComponentTest} from "../util/component.test";
+import {AppComponent, AppController} from "./app.component";
+
+describe("Component AppComponent", () => {
+
+    beforeEach(angular.mock.module("app.shell"));
+
+    var componentTest: ComponentTest<AppController>;
+
+    beforeEach(() => {
+        componentTest = new ComponentTest<AppController>("<app></app>", "AppComponent");
+    });
+
+    //TODO: Mock session and configvaluehelper
+    //TODO: Mock window and check values
+
+    describe("the component is created", () => {
+        it("should be visible by default", () => {
+
+            //Arrange
+            var vm: AppController = componentTest.createComponent({});
+
+            //Assert
+            expect(vm.currentUser).toBe("me");
+        });
+    });
+});

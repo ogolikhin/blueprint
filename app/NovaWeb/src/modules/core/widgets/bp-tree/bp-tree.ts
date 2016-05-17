f6226b3683d17ff00aea90204e40e8e4cea391f8
Fabio Gallo, Blueprint <fabio.gallo@blueprintsys.com>
2016-05-17 00:53:36 -0400

    Loading animation when requesting folder's content in ag-grid

--- app/NovaWeb/src/modules/core/widgets/bp-tree/bp-tree.ts
+++ app/NovaWeb/src/modules/core/widgets/bp-tree/bp-tree.ts
@@ -280,19 +280,29 @@ export class BPTreeController  {
     private rowGroupOpened = (params: any) => {
         let self = this;
         let node = params.node;
+
+        // this is a bit of a trick, to get the actual row expanded
+        let rowIndex = node.rowTop / self.options.rowHeight;
+        let row = self.$element[0].querySelectorAll(".ag-body .ag-body-viewport-wrapper .ag-row")[rowIndex];
+
         if (node.data.Type === `Folder`) {
             if (typeof self.onExpand === `function`) {
                 if (node.data.Children && !node.data.Children.length && !node.data.alreadyLoadedFromServer) {
                     if (node.expanded) {
+                        row.className += " ag-row-loading";
+
                         self.onExpand({ prms: node.data })
                             .then((data: any) => { //pSvc.IProjectNode[]
                                 node.data.Children = data;
                                 node.data.open = true;
                                 node.data.alreadyLoadedFromServer = true;
+                                row.className = row.className.replace(/ ag-row-loading/g, "");
                                 self.options.api.setRowData(self.rowData);
                             }, (error) => {
                                 //self.showError(error);
                             });
+                    } else {
+                        row.className = row.className.replace(/ ag-row-loading/g, "");
                     }
                 }
                 node.data.open = node.expanded;

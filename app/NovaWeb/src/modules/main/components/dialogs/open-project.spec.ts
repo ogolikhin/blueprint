5bb7891618de799c01975d0a3dff424023d1b382
Dmitry Lopyrev <dmitry.lopyrev@blueprintsys.com>
2016-05-25 17:32:18 -0400

    Added more unit tests

--- app/NovaWeb/src/modules/main/components/dialogs/open-project.spec.ts
+++ app/NovaWeb/src/modules/main/components/dialogs/open-project.spec.ts
@@ -1,7 +1,11 @@
 ﻿import "angular";
 import "angular-mocks";
+import { NotificationService} from "../../../core/notification";
 import {IOpenProjectResult, OpenProjectController} from "./open-project";
+import {IBPTreeController, BPTreeControllerMock, ITreeNode} from "../../../core/widgets/bp-tree/bp-tree.mock";
 import {LocalizationServiceMock} from "../../../shell/login/mocks.spec";
+import {ProjectRepositoryMock} from "../../services/project-repository.mock";
+import {ProjectManager, IProjectManager, Models, SubscriptionEnum} from "../../managers/project-manager";
 
 export class ModalServiceInstanceMock implements ng.ui.bootstrap.IModalServiceInstance {
 
@@ -9,7 +13,7 @@ export class ModalServiceInstanceMock implements ng.ui.bootstrap.IModalServiceIn
 
     public dismiss(reason?: any): void {}
 
-    public result: angular.IPromise<any>;
+    public result: angular.IPromise<any>;   
 
     public opened: angular.IPromise<any>;
 
@@ -21,71 +25,73 @@ export class ModalServiceInstanceMock implements ng.ui.bootstrap.IModalServiceIn
 describe("Open Project.", () => {
     var controller: OpenProjectController;
 
-    beforeEach(() => {
+
+    beforeEach( ()  => {
         controller = new OpenProjectController(
             null,
             new LocalizationServiceMock(),
             new ModalServiceInstanceMock(),
             null,
             null, null, null);
+
+        controller["tree"] = new BPTreeControllerMock();
     });
 
     describe("Return value.", () => {
         it("check return empty value", () => {
 
-        // Arrange
+            // Arrange
             var result: IOpenProjectResult = <IOpenProjectResult>{
                 id: -1,
                 name: "",
                 description: ""
             };
-        // Act
+            // Act
 
-        // Assert
-        expect(controller.returnvalue).toBeDefined();
-        expect(controller.returnvalue).toEqual(result);
-    });
-    });
-    it("innerRenderer", () => {
-        // Arrange
-        var paramsMock = {
-            data: {
-                name: "artifact"
-            }
-        };
-        var paramsMockFolder = {
-            data: {
-                type: "Folder",
-                name: "folder"
-            }
-        };
-        var paramsMockProject = {
-            data: {
-                type: "Project",
-                name: "<button onclick=\"alert('HEY!')\";>project</button>"
-            },
-            eGridCell: document.createElement("div")
-        };
+            // Assert
+            expect(controller.returnvalue).toBeDefined();
+            expect(controller.returnvalue).toEqual(result);
+        });
 
-        // Act
-        var columns = controller.columns;
-        var cellRenderer = columns[0].cellRendererParams.innerRenderer(paramsMock);
-        var cellRendererFolder = columns[0].cellRendererParams.innerRenderer(paramsMockFolder);
-        var cellRendererProject = columns[0].cellRendererParams.innerRenderer(paramsMockProject);
+        it("innerRenderer", () => {
+            // Arrange
+            var paramsMock = {
+                data: {
+                    name: "artifact"
+                }
+            };
+            var paramsMockFolder = {
+                data: {
+                    type: "Folder",
+                    name: "folder"
+                }
+            };
+            var paramsMockProject = {
+                data: {
+                    type: "Project",
+                    name: "<button onclick=\"alert('HEY!')\";>project</button>"
+                },
+                eGridCell: document.createElement("div")
+            };
 
-        // Assert
-        expect(cellRenderer).toEqual("artifact");
-        expect(cellRendererFolder).toEqual("folder");
-        expect(cellRendererProject).toContain("project");
-        expect(cellRendererProject).not.toContain("<button");
-    });
+            // Act
+            var columns = controller.columns;
+            var cellRenderer = columns[0].cellRendererParams.innerRenderer(paramsMock);
+            var cellRendererFolder = columns[0].cellRendererParams.innerRenderer(paramsMockFolder);
+            var cellRendererProject = columns[0].cellRendererParams.innerRenderer(paramsMockProject);
 
-    describe("Verify control.", () => {
+            // Assert
+            expect(cellRenderer).toEqual("artifact");
+            expect(cellRendererFolder).toEqual("folder");
+            expect(cellRendererProject).toContain("project");
+            expect(cellRendererProject).not.toContain("<button");
+        });
+
         it("Checking options: ", () => {
 
-        // Arrange
+            // Arrange
 
-        // Act
+            // Act
             var columns = controller.columns;
 
             //// Assert
@@ -99,48 +105,80 @@ describe("Open Project.", () => {
             expect(columns[0].cellRendererParams.innerRenderer).toBeDefined();
         });
     });
-    });
+    
+    describe("Embedded ag-grid events", () => {
+        let controller: OpenProjectController;
+        let $scope ;
+        let elem;
 
-describe("Embedded ag-grid events", () => {
-    var controller: OpenProjectController;
-    var $scope, elem;
+        beforeEach(angular.mock.module(($provide: ng.auto.IProvideService) => {
+            $provide.service("localization", LocalizationServiceMock);
+            $provide.service("projectRepository", ProjectRepositoryMock);
+            $provide.service("notification", NotificationService);
+            $provide.service("manager", ProjectManager);
 
-    beforeEach(inject(function (_$q_, _$rootScope_, _$compile_) {
-        $scope = _$rootScope_.$new();
+        }));
 
-        elem = angular.element(`<div ag-grid="ctrl.gridOptions" class="ag-grid"></div>`);
+        beforeEach(inject(($q: ng.IQService, $rootScope: ng.IRootScopeService, $compile: ng.ICompileService, manager: IProjectManager) => {
+            $scope = $rootScope.$new();
 
-        controller = new OpenProjectController(
-            $scope,
-            new LocalizationServiceMock(),
-            new ModalServiceInstanceMock(),
-            null,
-            null,
-            null,
-            null);
-        _$compile_(elem)($scope);
+            elem = angular.element(`<div ag-grid="ctrl.gridOptions" class="ag-grid"></div>`);
 
-        $scope.$digest();
-    }));
+            controller = new OpenProjectController(
+                $scope,
+                new LocalizationServiceMock(),
+                new ModalServiceInstanceMock(),
+                manager,
+                null,
+                null,
+                null);
+            $compile(elem)($scope);
+            $scope.$digest();
+        }));
 
-    it("onEnterKeyOnProject", () => {
-        // Arrange
-        var event = new Event("keydown");
-        var div = document.createElement("div");
-        var paramsMock = {
+        it("onEnterKeyOnProject", () => {
+            // Arrange
+            var event = new Event("keydown");
+            var div = document.createElement("div");
+            var paramsMock = {
                 data: {
-                type: "Project",
-                name: "project"
+                    type: "Project",
+                    name: "project"
                 },
-            eGridCell: div
-        };
+                eGridCell: div
+            };
 
-        // Act
-        var columns = controller.columns;
-        var cellRenderer = columns[0].cellRendererParams.innerRenderer(paramsMock);
-        div.dispatchEvent(event);
+            // Act
+            var columns = controller.columns;
+            var cellRenderer = columns[0].cellRendererParams.innerRenderer(paramsMock);
+            div.dispatchEvent(event);
 
-        // Assert
-        expect(cellRenderer).toContain("project");
+            // Assert
+            expect(cellRenderer).toContain("project");
+        });
+
+        it("Load data", inject(( $rootScope: ng.IRootScopeService) => {
+            // Arrange
+            controller.doLoad({ id: 1 });
+            var event = new Event("keydown");
+            var div = document.createElement("div");
+            var paramsMock = {
+                data: {
+                    type: "Project",
+                    name: "project"
+                },
+                eGridCell: div
+            };
+
+            // Act
+            var columns = controller.columns;
+            var cellRenderer = columns[0].cellRendererParams.innerRenderer(paramsMock);
+            div.dispatchEvent(event);
+
+            // Assert
+            expect(cellRenderer).toContain("project");
+        }));
+
+
     });
 });
--- app/NovaWeb/src/modules/main/components/projectexplorer/project-explorer.spec.ts
+++ app/NovaWeb/src/modules/main/components/projectexplorer/project-explorer.spec.ts
@@ -2,48 +2,10 @@
 import "angular-mocks";
 import { NotificationService} from "../../../core/notification";
 import {ILocalizationService} from "../../../core/localization";
-import {IBPTreeController, ITreeNode, BPTreeController } from "../../../core/widgets/bp-tree/bp-tree";
+import {IBPTreeController, BPTreeControllerMock, ITreeNode} from "../../../core/widgets/bp-tree/bp-tree.mock";
 import {ProjectRepositoryMock} from "../../services/project-repository.mock";
-import {ProjectManager, Models, SubscriptionEnum} from "../../managers/project-manager";
+import {ProjectManager, IProjectManager, Models, SubscriptionEnum} from "../../managers/project-manager";
 import {ProjectExplorerController} from "./project-explorer"
-
-class BPTreeControllerMock implements IBPTreeController {
-    private add(id: number) {
-        return {
-            id: id,
-            name: `Artifact ${id}`,
-            type: 1,
-        } as ITreeNode;
-
-    }
-    public _datasource: ITreeNode[] = [];
-    public addNode(data: any[], index?: number, propertyMap?: any) {
-        for (let i = 0; i < 10; i++) {
-            this._datasource.push(this.add(i));
-        }
-    }
-    
-    public addNodeChildren(id: number, data: any[], propertyMap?: any) {
-        let node = this._datasource[0];
-        node.children = [];
-        for (let i = 100; i < 105; i++) {
-            this._datasource.push(this.add(i));
-        }
-        node.hasChildren = true;
-        node.loaded = true;
-    }
-
-    public removeNode(id: number) {
-        this._datasource = this._datasource.filter(function (it) {
-            return it.id !== id;
-        });
-    }
-
-    public selectNode(id: number) { }
-
-    public refresh() {}
-}
-
 
 
 describe("Project Explorer Test", () => {
@@ -53,24 +15,35 @@ describe("Project Explorer Test", () => {
         $provide.service("notification", NotificationService);
         $provide.service("manager", ProjectManager);
     }));
+    let explorer;
+    beforeEach(inject((manager: IProjectManager) => {
+        explorer = new ProjectExplorerController(manager);
+        explorer.tree = new BPTreeControllerMock();
+    }));
 
     describe("Load project: ", () => {
-        it("load project", inject(($rootScope: ng.IRootScopeService, manager: ProjectManager) => {
-            let explorer = new ProjectExplorerController(manager);
-            explorer.tree = new BPTreeControllerMock();
-            manager.notify(SubscriptionEnum.ProjectLoaded, {
-                id: 1,
-                name: "Project 1",
-                artifacts: []
-            } as Models.IProject);
+        it("load project", inject(($rootScope: ng.IRootScopeService, manager: IProjectManager) => {
+            // Arrange
+            let dsBefore = explorer.tree["_datasource"].splice();
+            
+            // Act
+            manager.notify(SubscriptionEnum.ProjectLoaded);
+            $rootScope.$digest();
+
+            // Assert
+            let dsAfter = explorer.tree["_datasource"];
+
+            expect(dsBefore).toEqual(jasmine.any(Array));
+            expect(dsAfter).toEqual(jasmine.any(Array));
+            expect(dsAfter.length).toBeGreaterThan(dsBefore.length);
+
         }));
         
-        //TODO: finish unit test
 
     });
 
     describe("Remove project:", () => {
-
+        //TODO
     });
 
 

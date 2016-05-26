cb693faeac2fa492f2b73171dd606f26a3eff702
Dmitry Lopyrev <dmitry.lopyrev@blueprintsys.com>
2016-05-26 16:55:01 -0400

    Add more unit test to project explorer

--- app/NovaWeb/src/modules/core/widgets/bp-tree/bp-tree.ts
+++ app/NovaWeb/src/modules/core/widgets/bp-tree/bp-tree.ts
@@ -365,3 +365,41 @@ export class BPTreeController implements IBPTreeController  {
         }
     };
 }
+
+
+export class BPTreeControllerMock implements IBPTreeController {
+    private add(id: number) {
+        return {
+            id: id,
+            name: `Artifact ${id}`,
+            type: 1,
+        } as ITreeNode;
+
+    }
+    public _datasource: ITreeNode[] = [];
+    public addNode(data: any[], index?: number, propertyMap?: any) {
+        for (let i = 0; i < 10; i++) {
+            this._datasource.push(this.add(i));
+        }
+    }
+
+    public addNodeChildren(id: number, data: any[], propertyMap?: any) {
+        let node = this._datasource[0];
+        node.children = [];
+        for (let i = 100; i < 105; i++) {
+            node.children.push(this.add(i));
+        }
+        node.hasChildren = true;
+        node.loaded = true;
+    }
+
+    public removeNode(id: number) {
+        this._datasource = this._datasource.filter(function (it) {
+            return it.id !== id;
+        });
+    }
+
+    public selectNode(id: number) { }
+
+    public refresh() { }
+}

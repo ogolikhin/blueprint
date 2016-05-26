c9338beaf62b98bf640662d93781054de689facc
Chris Just <chris.just@blueprintsys.com>
2016-05-26 11:32:26 -0400

    Changing the last batch of Storyteller tests over to use TestHelper.  Also made some fixes to the Dispose() methods and how we delete child artifacts.

--- blueprint-automationframework/Blueprint-Tests/Tests/StorytellerTests/DragAndDropTests.cs
+++ blueprint-automationframework/Blueprint-Tests/Tests/StorytellerTests/DragAndDropTests.cs
@@ -1,75 +1,34 @@
-﻿using System;
-using CustomAttributes;
+﻿using CustomAttributes;
 using Model;
 using Model.Factories;
 using NUnit.Framework;
 using Helper;
-using Model.StorytellerModel;
 using Model.StorytellerModel.Impl;
+using TestCommon;
 
 namespace StorytellerTests
 {
     [TestFixture]
     [Category(Categories.Storyteller)]
-    public class DragAndDropTests
+    public class DragAndDropTests : TestBase
     {
-        private IAdminStore _adminStore;
-        private IBlueprintServer _blueprintServer;
-        private IStoryteller _storyteller;
         private IUser _user;
         private IProject _project;
-        private bool _deleteChildren = true;
 
         #region Setup and Cleanup
 
         [TestFixtureSetUp]
         public void ClassSetUp()
         {
-            _adminStore = AdminStoreFactory.GetAdminStoreFromTestConfig();
-            _blueprintServer = BlueprintServerFactory.GetBlueprintServerFromTestConfig();
-            _storyteller = StorytellerFactory.GetStorytellerFromTestConfig();
-            _user = UserFactory.CreateUserAndAddToDatabase();
+            Helper = new TestHelper();
+            _user = Helper.CreateUserAndAuthenticate(TestHelper.AuthenticationTokenTypes.BothAccessControlAndOpenApiTokens);
             _project = ProjectFactory.GetProject(_user);
-
-            // Get a valid Access Control token for the user (for the new Storyteller REST calls).
-            ISession session = _adminStore.AddSession(_user.Username, _user.Password);
-            _user.SetToken(session.SessionId);
-
-            Assert.IsFalse(string.IsNullOrWhiteSpace(_user.Token.AccessControlToken), "The user didn't get an Access Control token!");
-
-            // Get a valid OpenApi token for the user (for the OpenApi artifact REST calls).
-            _blueprintServer.LoginUsingBasicAuthorization(_user, string.Empty);
-
-            Assert.IsFalse(string.IsNullOrWhiteSpace(_user.Token.OpenApiToken), "The user didn't get an OpenApi token!");
         }
 
         [TestFixtureTearDown]
         public void ClassTearDown()
         {
-            if (_storyteller.Artifacts != null)
-            {
-                // TODO: implement discard artifacts for test cases that doesn't publish artifacts
-                // Delete all the artifacts that were added.
-                foreach (var artifact in _storyteller.Artifacts.ToArray())
-                {
-                    _storyteller.DeleteProcessArtifact(artifact, deleteChildren: _deleteChildren);
-                }
-            }
-
-            if (_adminStore != null)
-            {
-                // Delete all the sessions that were created.
-                foreach (var session in _adminStore.Sessions.ToArray())
-                {
-                    _adminStore.DeleteSession(session);
-                }
-            }
-
-            if (_user != null)
-            {
-                _user.DeleteUser();
-                _user = null;
-            }
+            Helper?.Dispose();
         }
 
         #endregion Setup and Cleanup
@@ -88,7 +47,7 @@ namespace StorytellerTests
                 [S]--[P]--+--[UT3]--+--[ST4]--+--[UT1]--+--[ST2]--+--[E]
             */
 
-            var process = StorytellerTestHelper.CreateAndGetDefaultProcessWithTwoSequentialUserTasks(_storyteller, _project, _user);
+            var process = StorytellerTestHelper.CreateAndGetDefaultProcessWithTwoSequentialUserTasks(Helper.Storyteller, _project, _user);
 
             var defaultUserTask = process.GetProcessShapeByShapeName(Process.DefaultUserTaskName);
 
@@ -97,7 +56,7 @@ namespace StorytellerTests
             process.MoveUserAndSystemTaskBeforeShape(defaultUserTask, endShape);
 
             // Update and Verify the modified process
-            StorytellerTestHelper.UpdateVerifyAndPublishProcess(process, _storyteller, _user);
+            StorytellerTestHelper.UpdateVerifyAndPublishProcess(process, Helper.Storyteller, _user);
         }
     }
 }

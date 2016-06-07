2ec1c7289749fd0c41b9c1306a172b7d52a33024
David He <david.he@blueprintsys.com>
2016-06-07 12:55:32 -0400

    unit test fix

--- svc/ArtifactStore.Tests/App_Start/WebApiConfigTests.cs
+++ svc/ArtifactStore.Tests/App_Start/WebApiConfigTests.cs
@@ -22,7 +22,7 @@ namespace ArtifactStore
             config.EnsureInitialized();
 
             // Assert
-            config.AssertTotalRoutes(5, "Please update asserts in WebApiConfigTests when changing routes.");
+            config.AssertTotalRoutes(6, "Please update asserts in WebApiConfigTests when changing routes.");
             config.AssertAction<StatusController>("GetStatus", HttpMethod.Get, "status");
             config.AssertAction<StatusController>("GetStatusUpCheck", HttpMethod.Get, "status/upcheck");
             config.AssertAction<ArtifactController>("GetProjectChildren", HttpMethod.Get, "projects/1/children");

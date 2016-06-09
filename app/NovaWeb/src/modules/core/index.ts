9ca5429b8f8b088d26ed08a67bd60670447c0bb6
Pavlo Pochapsky <pavlo.pochapsky@blueprintsys.com>
2016-06-09 09:21:44 -0400

    US177843: added infinite scroll directive

--- app/NovaWeb/src/modules/core/index.ts
+++ app/NovaWeb/src/modules/core/index.ts
@@ -8,6 +8,8 @@ import { IDialogSettings, IDialogService, DialogService } from "./services/dialo
 import { BPTreeInlineEditing } from "./widgets/bp-tree-inline-editing/bp-tree-inline-editing";
 import { BPTreeDragndrop } from "./widgets/bp-tree-dragndrop/bp-tree-dragndrop";
 import { BPTooltip } from "./widgets/bp-tooltip/bp-tooltip";
+import { BPInfiniteScroll } from "./widgets/bp-infinite-scroll/bp-infinite-scroll";
+
 
 let module = angular.module("app.core", ["ui.router", "ui.bootstrap"])
     .service("localization", LocalizationService)
@@ -16,7 +18,8 @@ let module = angular.module("app.core", ["ui.router", "ui.bootstrap"])
     .service("configValueHelper", ConfigValueHelper)
     .directive("bpTreeInlineEditing", BPTreeInlineEditing.factory())
     .directive("bpTreeDragndrop", BPTreeDragndrop.factory())
-    .directive("bpTooltip", BPTooltip.factory());
+    .directive("bpTooltip", BPTooltip.factory())
+    .directive("bpInfiniteScroll", BPInfiniteScroll.factory());
 
 export default module.name;
 export {

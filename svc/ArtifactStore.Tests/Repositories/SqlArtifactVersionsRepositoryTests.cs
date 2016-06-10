aec8040e3d8f4d11a34c0c7737a35e1ad23bb8a6
David He <david.he@blueprintsys.com>
2016-06-10 14:19:15 -0400

    starting point for server side unit tests for getting artifact versions.

---
+++ svc/ArtifactStore.Tests/Repositories/SqlArtifactVersionsRepositoryTests.cs
@@ -0,0 +1,32 @@
+﻿using ArtifactStore.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ServiceLibrary.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArtifactStore.Repositories
+{
+    [TestClass]
+    public class SqlArtifactVersionsRepositoryTests
+    {
+        public async Task SampleTest(int artifactId, int limit, int offset, int? userId, bool asc, int sessionUserId )
+        {
+            // Arrange
+            var cxn = new SqlConnectionWrapperMock();
+            var repository = new SqlArtifactVersionsRepository(cxn.Object);
+            var prm = new Dictionary<string, object>();
+            var testResult = new List<ArtifactHistoryVersion>();
+            cxn.SetupQueryAsync("GetArtifactChildren", prm, testResult);
+            // Act
+            var actual = await repository.GetArtifactVersions(artifactId, limit, offset, userId, asc, sessionUserId);
+            // Assert
+            cxn.Verify();
+            //string errorMessage;
+            //Assert.IsTrue(CompareArtifactChildren(expected, actual, out errorMessage), errorMessage);
+        }
+
+    }
+}

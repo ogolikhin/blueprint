e2e6aac5220790a0b5b778f51e2a7f1e8b1ccb91
David He <david.he@blueprintsys.com>
2016-06-13 15:31:19 -0400

    extracted some dapper setup calls to private methods.

--- svc/ArtifactStore/Repositories/SqlArtifactVersionsRepository.cs
+++ svc/ArtifactStore/Repositories/SqlArtifactVersionsRepository.cs
@@ -38,6 +38,26 @@ namespace ArtifactStore.Repositories
             return false;
         }
 
+        private async Task<IEnumerable<ArtifactHistoryVersion>> GetPublishedArtifactHistory(int artifactId, int limit, int offset, int? userId, bool asc)
+        {
+            var artifactVersionsPrm = new DynamicParameters();
+            artifactVersionsPrm.Add("@artifactId", artifactId);
+            artifactVersionsPrm.Add("@lim", limit);
+            artifactVersionsPrm.Add("@offset", offset);
+            if (userId.HasValue) { artifactVersionsPrm.Add("@userId", userId.Value); }
+            else { artifactVersionsPrm.Add("@userId", null); }
+            artifactVersionsPrm.Add("@ascd", asc);
+            return await ConnectionWrapper.QueryAsync<ArtifactHistoryVersion>("GetArtifactVersions", artifactVersionsPrm, commandType: CommandType.StoredProcedure);
+        }
+
+        private async Task<IEnumerable<UserInfo>> GetUserInfos(IEnumerable<int> UserIds)
+        {
+            var userInfosPrm = new DynamicParameters();
+            var userIdsTable = DapperHelper.GetIntCollectionTableValueParameter(UserIds);
+            userInfosPrm.Add("@userIds", userIdsTable);
+            return await ConnectionWrapper.QueryAsync<UserInfo>("GetUserInfos", userInfosPrm, commandType: CommandType.StoredProcedure);
+        }
+
         public async Task<ArtifactHistoryResultSet> GetArtifactVersions(int artifactId, int limit, int offset, int? userId, bool asc, int sessionUserId)
         {
             if (artifactId < 1)
@@ -49,14 +69,7 @@ namespace ArtifactStore.Repositories
             if (userId.HasValue && userId < 1)
                 throw new ArgumentOutOfRangeException(nameof(userId));
 
-            var artifactVersionsPrm = new DynamicParameters();
-            artifactVersionsPrm.Add("@artifactId", artifactId);
-            artifactVersionsPrm.Add("@lim", limit);
-            artifactVersionsPrm.Add("@offset", offset);
-            if (userId.HasValue) { artifactVersionsPrm.Add("@userId", userId.Value); }
-            else { artifactVersionsPrm.Add("@userId", null); }
-            artifactVersionsPrm.Add("@ascd", asc);
-            var artifactVersions = (await ConnectionWrapper.QueryAsync<ArtifactHistoryVersion>("GetArtifactVersions", artifactVersionsPrm, commandType: CommandType.StoredProcedure)).ToList();
+            var artifactVersions = (await GetPublishedArtifactHistory(artifactId, limit, offset, userId, asc)).ToList();
             var distinctUserIds = artifactVersions.Select(a => a.UserId).Distinct();
 
             if (await IncludeDraftVersion(userId, sessionUserId, artifactId))
@@ -77,17 +90,14 @@ namespace ArtifactStore.Repositories
                 }
             }
 
-            var userInfosPrm = new DynamicParameters();
-            var userIdsTable = DapperHelper.GetIntCollectionTableValueParameter(distinctUserIds);
-            userInfosPrm.Add("@userIds", userIdsTable);
-            var userInfos = (await ConnectionWrapper.QueryAsync<UserInfo>("GetUserInfos", userInfosPrm, commandType: CommandType.StoredProcedure)).ToDictionary(a => a.UserId);
+            var userInfoDictionary = (await GetUserInfos(distinctUserIds)).ToDictionary(a => a.UserId);
 
             var artifactHistoryVersionWithUserInfos = new List<ArtifactHistoryVersionWithUserInfo>();
 
             foreach (var artifactVersion in artifactVersions)
             {
                 UserInfo userInfo;
-                userInfos.TryGetValue(artifactVersion.UserId, out userInfo);
+                userInfoDictionary.TryGetValue(artifactVersion.UserId, out userInfo);
                 artifactHistoryVersionWithUserInfos.Add(
                     new ArtifactHistoryVersionWithUserInfo {
                                                              VersionId = artifactVersion.VersionId,

a7a421cd60b79c18fad1d9e1c9aa584719ef7fee
Chris Just <chris.just@blueprintsys.com>
2016-06-17 19:11:35 -0400

    Refactoring ArtifactType & PropertyType classes and adding Nova* and OpenApi* versions of both.

--- blueprint-automationframework/Blueprint-Tests/Model/ArtifactModel/Impl/ArtifactType.cs
+++ blueprint-automationframework/Blueprint-Tests/Model/ArtifactModel/Impl/ArtifactType.cs
@@ -1,44 +1,32 @@
 ﻿using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
-using Newtonsoft.Json;
 using Model.ArtifactModel;
 using Model.ArtifactModel.Impl;
-using Utilities;
 
 namespace Model.Impl
 {
-    public class ArtifactType : IArtifactType
+    public class ArtifactTypeBase
     {
-        #region Properties
-        [JsonProperty("Id")]
         public int Id { get; set; }
-
-        [JsonProperty("Name")]
         public string Name { get; set; }
-
-        [JsonProperty("Description")]
         public string Description { get; set; }
-
-        [JsonProperty("Prefix")]
         public string Prefix { get; set; }
+    }
 
-        [JsonProperty("BaseArtifactType")]
+    public class OpenApiArtifactType : ArtifactTypeBase
+    {
         public BaseArtifactType BaseArtifactType { get; set; }
+        public List<OpenApiPropertyType> PropertyTypes { get; } = new List<OpenApiPropertyType>();
+    }
 
-        [SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
-        [JsonConverter(typeof(Deserialization.ConcreteConverter<List<PropertyType>>))]
-        [JsonProperty("PropertyTypes")]
-        public List<PropertyType> PropertyTypes { get; set; }
+    public class NovaArtifactType : ArtifactTypeBase
+    {
+        public BaseArtifactType BaseType { get; set; }
 
-        #endregion Properties
-
-        #region Constructors
-
-        public ArtifactType()
-        {
-            PropertyTypes = new List<PropertyType>();
-        }
-
-        #endregion Constructors
+        public int ProjectId { get; set; }
+        public int VersionId { get; set; }
+        public int InstanceItemTypeId { get; set; }
+        public int IconImageId { get; set; }
+        public bool UsedInThisProject { get; set; }
+        public List<int> CustomPropertyTypeIds { get; } = new List<int>();
     }
 }

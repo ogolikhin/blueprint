e2e178d882976bc6c80e314445080cdff6ca21eb
David He <david.he@blueprintsys.com>
2016-06-20 13:57:07 -0400

    created starting point for attachment repository unit tets.

---
+++ svc/ArtifactStore.Tests/Repositories/SqlAttachmentsRepositoryTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ServiceLibrary.Repositories;
+using System.Threading.Tasks;
+
+namespace ArtifactStore.Repositories
+{
+    [TestClass]
+    public class SqlAttachmentsRepositoryTests
+    {
+        [TestMethod]
+        public async Task GetAttachmentsAndDocumentReferences_NotSubArtifactAddDrafts_ResultsReturned()
+        {
+            // Arrange
+            int artifactId = 1;
+            int userId = 1;
+            bool addDrafts = true;
+            var cxn = new SqlConnectionWrapperMock();
+            var repository = new SqlAttachmentsRepository(cxn.Object);
+            await repository.GetAttachmentsAndDocumentReferences(artifactId, userId, null, addDrafts);
+            cxn.Verify();
+        }
+    }
+}

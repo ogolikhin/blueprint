463ed609fe7ad6d2da66624262e75466b77c13e9
David He <david.he@blueprintsys.com>
2016-06-20 14:37:29 -0400

    no subartifact id

--- svc/ArtifactStore.Tests/Repositories/SqlAttachmentsRepositoryTests.cs
+++ svc/ArtifactStore.Tests/Repositories/SqlAttachmentsRepositoryTests.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using ArtifactStore.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ServiceLibrary.Helpers;
 using ServiceLibrary.Repositories;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace ArtifactStore.Repositories
@@ -13,11 +16,24 @@ namespace ArtifactStore.Repositories
             // Arrange
             int artifactId = 1;
             int userId = 1;
+            int? subArtifactId = null;
             bool addDrafts = true;
+
             var cxn = new SqlConnectionWrapperMock();
+            cxn.SetupQueryAsync("GetItemAttachments", new Dictionary<string, object> { { "itemId", artifactId }, { "userId", userId }, { "addDrafts", addDrafts } }, new List<Attachment> { new Attachment { Name = "Test File Name", FileGuid = new System.Guid() } });
+            cxn.SetupQueryAsync("GetDocumentReferenceArtifacts", new Dictionary<string, object> { { "itemId", artifactId }, { "userId", userId }, { "addDrafts", addDrafts } }, new List<int> { 1 });
+            cxn.SetupQueryAsync("GetOnlyDocumentArtifacts", new Dictionary<string, object> { { "artifactIds", DapperHelper.GetIntCollectionTableValueParameter(new List<int> { artifactId })}, { "userId", userId }, { "addDrafts", addDrafts } }, new List<DocumentReference> { new DocumentReference { Name = "Test File Name", VersionArtifactId = 1 } });
             var repository = new SqlAttachmentsRepository(cxn.Object);
-            await repository.GetAttachmentsAndDocumentReferences(artifactId, userId, null, addDrafts);
+
+            // Act
+            var result = await repository.GetAttachmentsAndDocumentReferences(artifactId, userId, subArtifactId, addDrafts);
+
+            // Assert
             cxn.Verify();
+            Assert.AreEqual(result.ArtifactId, 1);
+            Assert.AreEqual(result.SubartifactId, null);
+            Assert.AreEqual(result.Attachments.Count, 1);
+            Assert.AreEqual(result.DocumentReferences.Count, 1);
         }
     }
 }

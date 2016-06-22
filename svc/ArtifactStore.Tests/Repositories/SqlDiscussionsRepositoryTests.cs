db89c73d811ff7aeb5aabbc6579edcc49566257d
David He <david.he@blueprintsys.com>
2016-06-22 10:15:34 -0400

    added mock class for user repository

---
+++ svc/ArtifactStore.Tests/Repositories/SqlDiscussionsRepositoryTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using ServiceLibrary.Repositories;
+
+namespace ArtifactStore.Repositories
+{
+    [TestClass]
+    public class SqlDiscussionsRepositoryTests
+    {
+        /*private IUsersRepository userRepository;
+        private IInstanceSettingsRepository instanceSettingsRepository;
+        private IArtifactPermissionsRepository artifactPermissionsRepository;
+        private Mock<ISqlConnectionWrapper> cxn;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            cxn = new SqlConnectionWrapperMock();
+            userRepository = new SqlUsersRepository(cxn.Object);
+        }
+
+        [TestMethod]
+        public void basdjkbdas()
+        {
+            var repository = new SqlDiscussionsRepository(cxn.Object);
+        }*/
+    }
+}
---
+++ svc/lib/ServiceLibrary.Tests/Repositories/SqlUserRepositoryMock.cs
@@ -0,0 +1,43 @@
+﻿using ServiceLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceLibrary.Repositories
+{
+    public class SqlUserRepositoryMock: IUsersRepository
+    {
+        public async Task<IEnumerable<UserInfo>> GetUserInfos(IEnumerable<int> userIds)
+        {
+            await Task.Run(() => { });
+
+            var result = new List<UserInfo>();
+            foreach (int userId in userIds)
+            {
+                result.Add(new UserInfo
+                           {
+                                UserId = userId,
+                                DisplayName = "User"+userId,
+                                ImageId = userId,
+                                IsEnabled = true,
+                                IsGuest = false
+                           });
+            }
+            return new List<UserInfo>();
+        }
+        public async Task<IEnumerable<UserInfo>> GetUsersByEmail(string email, bool? guestsOnly = false)
+        {
+            await Task.Run(() => { });
+            return new List<UserInfo> { new UserInfo
+                           {
+                                UserId = 1,
+                                DisplayName = "User1",
+                                ImageId = 1,
+                                IsEnabled = true,
+                                IsGuest = false
+                           }};
+        }
+    }
+}

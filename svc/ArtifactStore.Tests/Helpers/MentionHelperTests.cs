4722f81af9006717a5569ac970523605aa55c8b9
David He <david.he@blueprintsys.com>
2016-06-23 15:11:29 -0400

    added tests for IsEmailBlocked and AreEmailDiscussionsDisabled

--- svc/ArtifactStore.Tests/Helpers/MentionHelperTests.cs
+++ svc/ArtifactStore.Tests/Helpers/MentionHelperTests.cs
@@ -16,15 +16,40 @@ namespace ArtifactStore.Helpers
         private IUsersRepository userRepository;
         private IInstanceSettingsRepository instanceSettingsRepository;
         private IArtifactPermissionsRepository artifactPermissionsRepository;
+        private EmailSettings fakeEmailSettings;
         private MentionHelper mentionHelper;
         private SqlConnectionWrapperMock cxn;
 
         [TestInitialize]
         public void init()
         {
+
+            fakeEmailSettings =  new EmailSettings
+            {
+                Id = "Fake",
+                Authenticated = false,
+                Domains = "Domain;MyDomain",
+                EnableAllUsers = true,
+                EnableDomains = true,
+                EnableEmailDiscussion = false,
+                EnableEmailReplies = false,
+                EnableNotifications = false,
+                EnableSSL = false,
+                HostName = "FakeHostName",
+                IncomingEnableSSL = false,
+                IncomingHostName = "FakeIncomingHostName",
+                IncomingPassword = "FakeIncomingPassword",
+                IncomingPort = 1234,
+                IncomingServerType = 1,
+                IncomingUserName = "FakeIncomingUserName",
+                Password = "FakePassword",
+                Port = 1234,
+                SenderEmailAddress = "FakeSenderAddress",
+                UserName = "FakeUserName"
+            };
             cxn = new SqlConnectionWrapperMock();
             userRepository = new SqlUserRepositoryMock();
-            instanceSettingsRepository = new SqlInstanceSettingsRepositoryMock();
+            instanceSettingsRepository = new SqlInstanceSettingsRepositoryMock(fakeEmailSettings);
             artifactPermissionsRepository = new SqlArtifactPermissionsRepository(cxn.Object);
             mentionHelper = new MentionHelper(userRepository, instanceSettingsRepository, artifactPermissionsRepository);
         }
@@ -113,5 +138,52 @@ namespace ArtifactStore.Helpers
             Assert.IsTrue(result);
         }
 
+        [TestMethod]
+        public async Task IsEmailBlocked_GuestUserDisabled_ReturnsTrue()
+        {
+            // Arrange
+            var email = "DisabledUser@MyDomain";
+            // Act
+            var result = await mentionHelper.IsEmailBlocked(email);
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public async Task IsEmailBlocked_NotGuestUserEnabled_ReturnsFalse()
+        {
+            // Arrange
+            var email = "User@MyDomain";
+            // Act
+            var result = await mentionHelper.IsEmailBlocked(email);
+            // Assert
+            Assert.IsFalse(result);
+        }
+        [TestMethod]
+        public async Task AreEmailDiscussionsEnabled_DisabledEmailReplies_ReturnsFalse()
+        {
+            // Arrange
+            var projectId = 1;
+            fakeEmailSettings.EnableEmailReplies = false;
+            instanceSettingsRepository = new SqlInstanceSettingsRepositoryMock(fakeEmailSettings);
+            // Act
+            var result = await mentionHelper.AreEmailDiscussionsEnabled(projectId);
+            // Assert
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public async Task AreEmailDiscussionsEnabled_EnableEmailRepliesProjectHasPermission_ReturnsTrue()
+        {
+            // Arrange
+            var projectId = 1;
+            fakeEmailSettings.EnableEmailReplies = true;
+            instanceSettingsRepository = new SqlInstanceSettingsRepositoryMock(fakeEmailSettings);
+            cxn.SetupExecuteScalarAsync("GetProjectPermissions", new Dictionary<string, object> { { "ProjectId", projectId} },  ProjectPermissions.AreEmailRepliesEnabled);
+            // Act
+            var result = await mentionHelper.AreEmailDiscussionsEnabled(projectId);
+            // Assert
+            Assert.IsTrue(result);
+        }
     }
 }

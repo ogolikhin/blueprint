fc292af5fcab8c5b89bc2564e61882b67dd7dae9
David He <david.he@blueprintsys.com>
2016-06-24 14:08:31 -0400

    starting point for mentionprocessor tests. added new test project for htmllibrary

---
+++ svc/HtmlLibrary.Tests/MentionProcessorTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+
+namespace HtmlLibrary.Tests
+{
+    [TestClass]
+    public class MentionProcessorTests
+    {
+        private IMentionValidator mentionHelper;
+        private MentionProcessor mentionProcessor;
+
+        [TestInitialize]
+        public void init()
+        {
+            mentionHelper = new MentionHelperMock();
+            mentionProcessor = new MentionProcessor(mentionHelper);
+        }
+
+        [TestMethod]
+        public async Task ProcessComment_DiscussionsDisabled_CommentProcessed()
+        {
+            // Arrange
+            string comment = "<html><a linkassemblyqualifiedname=\"BluePrintSys.RC.Client.SL.RichText.RichTextMentionLink, BluePrintSys.RC.Client.SL.RichText, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null\" isvalid=\"True\" mentionid=\"5\" isgroup=\"False\" email=\"user@user.com\" style=\"-c1-editable: false; font-size: 11px; line-height: 1.45000004768372\"><span style=\"font-size: 11px; font-weight: normal; font-family: &#39;Portable User Interface&#39;; line-height: 1.45000004768372\">user@user.com</span></a></html>";
+            bool areEmailDiscussionsEnabled = false;
+
+            // Act
+            var result = await mentionProcessor.ProcessComment(comment, areEmailDiscussionsEnabled);
+
+            // Assert
+            //Assert.AreEqual(expectedProcessedComment, comment);
+        }
+        [TestMethod]
+        public async Task ProcessComment_DiscussionsEnabled_CommentProcessed()
+        {
+            // Arrange
+            string comment = "<html><a linkassemblyqualifiedname=\"BluePrintSys.RC.Client.SL.RichText.RichTextMentionLink, BluePrintSys.RC.Client.SL.RichText, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null\" isvalid=\"True\" mentionid=\"5\" isgroup=\"False\" email=\"user@user.com\" style=\"-c1-editable: false; font-size: 11px; line-height: 1.45000004768372\"><span style=\"font-size: 11px; font-weight: normal; font-family: &#39;Portable User Interface&#39;; line-height: 1.45000004768372\">user@user.com</span></a></html>";
+            bool areEmailDiscussionsEnabled = true;
+
+            // Act
+            var result = await mentionProcessor.ProcessComment(comment, areEmailDiscussionsEnabled);
+        }
+    }
+}
---
+++ svc/HtmlLibrary.Tests/MentionsHelperMock.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HtmlLibrary.Tests
+{
+    public class MentionHelperMock : IMentionValidator
+    {
+        public async Task<bool> IsEmailBlocked(string email)
+        { 
+            return (await Task.FromResult(false));
+        }
+
+    }
+}

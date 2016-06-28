96f45bca3c5b86bfb3dceb56415527588b6c0cfe
David He <david.he@blueprintsys.com>
2016-06-28 10:04:46 -0400

    added model for relationship

--- svc/ArtifactStore/Models/RelationshipResultSet.cs
+++ svc/ArtifactStore/Models/RelationshipResultSet.cs
@@ -5,7 +5,33 @@ using System.Web;
 
 namespace ArtifactStore.Models
 {
+    public enum TraceDirection
+    {
+        To,
+        From,
+        ToWay
+    }
+    public enum TraceType
+    {
+        ParentChild,
+        Manual,
+        Reuse,
+        Other
+    }
     public class RelationshipResultSet
     {
+        List<Relationship> Relationships;
+    }
+    public class Relationship
+    {
+        public int ArtifactId;
+        public string ArtifactName;
+        public int? SubartifactId;
+        public string SubartifactName;
+        public int ProjectId;
+        public string ProjectName;
+        public TraceDirection TraceDirection;
+        public TraceType TraceType;
+        public bool Suspect;
     }
 }

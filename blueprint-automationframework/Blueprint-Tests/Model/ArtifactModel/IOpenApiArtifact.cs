b69a042c0296c4687c25c0b9fdb2fc505f30e61b
Chris Just <chris.just@blueprintsys.com>
2016-07-06 14:28:33 -0400

    Adding GetArtifact() to OpenApiArtifact.

--- blueprint-automationframework/Blueprint-Tests/Model/ArtifactModel/IOpenApiArtifact.cs
+++ blueprint-automationframework/Blueprint-Tests/Model/ArtifactModel/IOpenApiArtifact.cs
@@ -25,6 +25,41 @@ namespace Model.ArtifactModel
         List<DiscardArtifactResult> Discard(IUser user = null, List<HttpStatusCode> expectedStatusCodes = null, bool sendAuthorizationAsCookie = false);
 
         /// <summary>
+        /// Retrieves a single artifact by Project ID and Artifact ID and returns information about the artifact.
+        /// (Runs:  /api/v1/projects/{projectId}/artifacts/{artifactId}?status={status}&comments={comments}&traces={traces}&attachments={attachments}&richtextasplain={richtextasplain}&inlinecss={inlinecss}&content={content})
+        /// </summary>
+        /// <param name="project">The project where the artifact exists.</param>
+        /// <param name="user">The user to authenticate with.</param>
+        /// <param name="getStatus">(optional) Defines whether or not the status of the artifact should be loaded.  The default is false if not specified.
+        /// The default is true if the parameter is included in the URI with no given value.</param>
+        /// <param name="getComments">(optional) Indicates whether to retreive comments of the artifact.  The default is false if not specified.
+        /// The default is true if the parameter is included in the URI with no given value.</param>
+        /// <param name="getTraces">(optional) Indicates whether to retrieve traces of the artifact.  The default is None if not specified.
+        /// The default is All if the parameter is included in the URI with no given value.</param>
+        /// <param name="getAttachments">(optional) Indicates whether to retrieve information about the attachments of the artifact.  The default is false if not specified.
+        /// The default is true if the parameter is included in the URI with no given value.</param>
+        /// <param name="richTextAsPlain">(optional) Defines whether or not to retrieve all rich-text properties as Plain Text instead of HTML.  The default is false if not specified.
+        /// The default is true if the parameter is included in the URI with no given value.</param>
+        /// <param name="getInlineCSS">(optional) Defines whether or not to retrieve all rich-text properties with locally defined or inline styles.  The default is false if not specified.
+        /// The default is true if the parameter is included in the URI with no given value.  When this parameter is set to false, rich-text properties return internal styles that are defined
+        /// within the &lt;head&gt; section of the HTML.</param>
+        /// <param name="getContent">(optional) Defines whether or not to retrieve the artifact's content.  The default is false if not specified.
+        /// Defines whether or not to retrieve the artifact's content. This parameter can be set to true or false. The default is false if not specified. The default is true if the parameter is included in the URI with no given value.
+        /// The default is true if the parameter is included in the URI with no given value.</param>
+        /// <param name="expectedStatusCodes">(optional) A list of expected status codes.  If null, only 200 OK is expected.</param>
+        /// <returns>The artifact with all the additional details you requested.</returns>
+        IOpenApiArtifact GetArtifact(IProject project,
+            IUser user,
+            bool? getStatus = null,
+            bool? getComments = null,
+            OpenApiArtifact.ArtifactTraceType? getTraces = null,
+            bool? getAttachments = null,
+            bool? richTextAsPlain = null,
+            bool? getInlineCSS = null,
+            bool? getContent = null,
+            List<HttpStatusCode> expectedStatusCodes = null);
+
+        /// <summary>
         /// Gets the Version property of an Artifact via OpenAPI call
         /// </summary>
         /// <param name="user">The user to authenticate to Blueprint.</param>

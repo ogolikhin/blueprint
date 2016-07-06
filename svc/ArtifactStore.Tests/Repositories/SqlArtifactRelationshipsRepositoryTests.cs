a61ade3f3366e169f2909c4bdcc6e9a29776c226
David He <david.he@blueprintsys.com>
2016-07-06 14:01:45 -0400

    get relationships unit tests

--- svc/ArtifactStore.Tests/Repositories/SqlArtifactRelationshipsRepositoryTests.cs
+++ svc/ArtifactStore.Tests/Repositories/SqlArtifactRelationshipsRepositoryTests.cs
@@ -38,6 +38,54 @@ namespace ArtifactStore.Repositories
             var result = await relationshipsRepository.GetRelationships(itemId, userId, addDrafts);
 
             Assert.IsNotNull(result);
+            Assert.AreEqual(1, result.ManualTraces.Count);
+            Assert.AreEqual(LinkType.Manual, result.ManualTraces[0].TraceType);
+            Assert.AreEqual(1, result.OtherTraces.Count);
+            Assert.AreEqual(LinkType.Association, result.OtherTraces[0].TraceType);
+        }
+
+        [TestMethod]
+        public async Task GetRelationships_ToRelationships_Success()
+        {
+            // Arrange
+            int itemId = 1;
+            int userId = 1;
+            bool addDrafts = true;
+
+            var mockLinkInfoList = new List<LinkInfo>();
+            mockLinkInfoList.Add(new LinkInfo { DestinationArtifactId = 2, DestinationItemId = 2, DestinationProjectId = 0, IsSuspect = false, LinkType = LinkType.Manual, SourceArtifactId = 1, SourceItemId = 1, SourceProjectId = 0 });
+
+            cxn.SetupQueryAsync("GetRelationshipLinkInfo", new Dictionary<string, object> { { "itemId", itemId }, { "userId", userId }, { "addDrafts", addDrafts } }, mockLinkInfoList);
+
+            // Act
+            var result = await relationshipsRepository.GetRelationships(itemId, userId, addDrafts);
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(1, result.ManualTraces.Count);
+            Assert.AreEqual(LinkType.Manual, result.ManualTraces[0].TraceType);
+            Assert.AreEqual(TraceDirection.To, result.ManualTraces[0].TraceDirection);
+        }
+
+        [TestMethod]
+        public async Task GetRelationships_FromRelationships_Success()
+        {
+            // Arrange
+            int itemId = 1;
+            int userId = 1;
+            bool addDrafts = true;
+
+            var mockLinkInfoList = new List<LinkInfo>();
+            mockLinkInfoList.Add(new LinkInfo { DestinationArtifactId = 1, DestinationItemId = 1, DestinationProjectId = 0, IsSuspect = false, LinkType = LinkType.Manual, SourceArtifactId = 2, SourceItemId = 2, SourceProjectId = 0 });
+
+            cxn.SetupQueryAsync("GetRelationshipLinkInfo", new Dictionary<string, object> { { "itemId", itemId }, { "userId", userId }, { "addDrafts", addDrafts } }, mockLinkInfoList);
+
+            // Act
+            var result = await relationshipsRepository.GetRelationships(itemId, userId, addDrafts);
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(1, result.ManualTraces.Count);
+            Assert.AreEqual(LinkType.Manual, result.ManualTraces[0].TraceType);
+            Assert.AreEqual(TraceDirection.From, result.ManualTraces[0].TraceDirection);
         }
 
         [TestMethod]
@@ -58,6 +106,9 @@ namespace ArtifactStore.Repositories
             var result = await relationshipsRepository.GetRelationships(itemId, userId, addDrafts);
 
             Assert.IsNotNull(result);
+            Assert.AreEqual(1, result.ManualTraces.Count);
+            Assert.AreEqual(LinkType.Manual, result.ManualTraces[0].TraceType);
+            Assert.AreEqual(TraceDirection.TwoWay, result.ManualTraces[0].TraceDirection);
         }
 
     }

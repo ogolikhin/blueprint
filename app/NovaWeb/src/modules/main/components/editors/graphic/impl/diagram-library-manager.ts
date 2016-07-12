ed993b29ff6c3de511e119028b17553e2509ab8b
julia-bougaev <julia.bougaev@blueprintsys.com>
2016-07-12 15:39:11 -0400

    storyboard

--- app/NovaWeb/src/modules/main/components/editors/graphic/impl/diagram-library-manager.ts
+++ app/NovaWeb/src/modules/main/components/editors/graphic/impl/diagram-library-manager.ts
@@ -3,6 +3,7 @@ import {GenericDiagramShapeFactory} from "./generic-diagram";
 import {UsecaseShapeFactory} from "./activity-flow-diagram";
 import {DomainDiagramShapeFactory} from "./domain-diagram";
 import {IShapeTemplateFactory} from "./abstract-diagram-factory";
+import {StoryboardShapeFactory} from "./storyboard";
 
 export class DiagramLibraryManager {
     public getDiagramFactory(diagramType: string): IShapeTemplateFactory {
@@ -14,6 +15,8 @@ export class DiagramLibraryManager {
                 return new GenericDiagramShapeFactory();
             case Diagrams.DOMAIN_DIAGRAM:
                 return new DomainDiagramShapeFactory();
+            case Diagrams.STORYBOARD:
+                return new StoryboardShapeFactory();
             // case Diagrams.UIMOCKUP:
             //     return new UiMockupShapeFactory();
             // case Diagrams.STORYBOARD:

333e1acc9795eb36c9eecd003179c776c1e16608
Dmitry Lopyrev <dmitry.lopyrev@blueprintsys.com>
2016-07-21 09:35:03 -0400

    Added new artifact editors (Project)

--- app/NovaWeb/src/modules/main/components/content/pagecontent.ts
+++ app/NovaWeb/src/modules/main/components/content/pagecontent.ts
@@ -51,12 +51,12 @@ class PageContentCtrl {
             if (!artifact) {
                 return;
             }
-            this.contentType = this.getContentType(artifact);
 
-            _context.artifact = angular.copy(artifact);
+            _context.artifact = artifact;
             _context.project = this.projectManager.currentProject.getValue();
             _context.type = this.projectManager.getArtifactType(_context.artifact, _context.project);
             _context.propertyTypes = this.projectManager.getArtifactPropertyTypes(_context.artifact);
+            this.contentType = this.getContentType(artifact);
 
         } catch (ex) {
             this.messageService.addError(ex.message);
@@ -67,7 +67,11 @@ class PageContentCtrl {
     private getContentType(artifact: Models.IArtifact): string {
         if (this.diagramService.isDiagram(artifact.predefinedType)) {
             return "diagram";
+        } else if (Models.ItemTypePredefined.Project == artifact.predefinedType) {
+            return "project";
+        } else if (Models.ItemTypePredefined.CollectionFolder == artifact.predefinedType) {
+            return "collection";
         }
-        return "other";
+        return "general";
     }
 }
--- app/NovaWeb/src/modules/main/components/editors/general/bp-artifact-editor.ts
+++ app/NovaWeb/src/modules/main/components/editors/general/bp-artifact-editor.ts
@@ -1,237 +1,87 @@
-﻿import { Models} from "../../..";
-import {tinymceMentionsData} from "../../../../util/tinymce-mentions.mock"; //TODO: added just for testing
+﻿import {Models} from "../../../";
+import {IMessageService} from "../../../../shell/";
+import {IArtifactService} from "../../../services/";
+import {BpBaseEditor, PropertyEditor, FieldContext, IEditorContext} from "./bp-base-editor";
 
-enum LookupEnum {
-    ByName = 0,
-    ById = 1,
-    ByType = 2,
+
+
+export class BpArtifactEditor implements ng.IComponentOptions {
+    public template: string = require("./bp-artifact-editor.html");
+    public controller: Function = BpArtifactEditorController;
+    public controllerAs = "$ctrl";
+    public bindings: any = {
+        context: "<",
+    };
+
 }
 
-export interface IArtifactEditor {
-    getFields(): AngularFormly.IFieldConfigurationObject[];
-    getModel(): any;
-}
 
-export class FieldContext implements Models.IPropertyType {
-    public id: number;
-    public versionId: number;
-    public name: string;
-    public primitiveType: Models.PrimitiveType;
-    public instancePropertyTypeId: number;
-    public isRichText: boolean;
-    public decimalDefaultValue: number;
-    public dateDefaultValue: Date;
-    public userGroupDefaultValue: any[];
-    public stringDefaultValue: string;
-    public decimalPlaces: number;
-    public maxNumber: number;
-    public minNumber: number;
-    public maxDate: Date;
-    public minDate: Date;
-    public isMultipleAllowed: boolean;
-    public isRequired: boolean;
-    public isValidated: boolean;
-    public validValues: Models.IOption[];
-    public defaultValidValueId: number;
-    public propertyTypePredefined: Models.PropertyTypePredefined;
-    public disabled: boolean;
-    //extension
-    public isSystem: boolean;
-    public fieldPropertyName: string;
-    public modelPropertyName: string | number;
-    public lookup: LookupEnum;
 
-    constructor(type: Models.IPropertyType, modelPropertyName?: string ) {
-        angular.extend(this, type);
 
-        let txtType: string = String(Models.PropertyTypePredefined[this.propertyTypePredefined]);
-        if (modelPropertyName) {
-            this.lookup = LookupEnum.ByName;
-            this.fieldPropertyName = modelPropertyName;
-            this.modelPropertyName = modelPropertyName;
-        } else if (angular.isDefined(type.propertyTypePredefined)) {
-            this.lookup = LookupEnum.ByType;
-            this.fieldPropertyName = txtType.toLowerCase();
-            this.modelPropertyName = <number>this.propertyTypePredefined;
-        } else {
-            this.lookup = LookupEnum.ById;
-            this.fieldPropertyName = `property_${this.id.toString()}`;
-            this.modelPropertyName = this.id;
-        }
-        this.isSystem = ["name", "itemtype", "createdby", "createdon", "lasteditedby", "lasteditedon"].indexOf(txtType.toLowerCase()) >= 0;
-        //}
+
+export class BpArtifactEditorController extends BpBaseEditor {
+    public static $inject: [string] = ["messageService", "artifactService"];
+
+    constructor(messageService: IMessageService, artifactService: IArtifactService) {
+        super(messageService, artifactService);
     }
 
-    
-}
 
-export class PropertyEditor  {
-    private _artifact: Models.IArtifact;
-    private _project: Models.IProject;
 
-    private _fields: AngularFormly.IFieldConfigurationObject[];
-    private _model: any = {};
+    public activeTab: number;
 
-    constructor(artifact: Models.IArtifact, filedContexts: FieldContext[]) {
+    public systemFields: AngularFormly.IFieldConfigurationObject[];
+    public customFields: AngularFormly.IFieldConfigurationObject[];
+    public richTextFields: AngularFormly.IFieldConfigurationObject[];
 
-        if (!artifact || !filedContexts) {
-            return;
-        }
-        this._artifact = artifact;
-        this._fields = [];
 
-        filedContexts.forEach((it: FieldContext) => {
-            if (it.fieldPropertyName && it.modelPropertyName) {
-                let field = this.createPropertyField(it);
-                let value: any;
+    public get isCustomPropertyAvailable(): boolean {
+        return this.systemFields && this.systemFields.length > 0;
+    }
+    public get isSystemPropertyAvailable(): boolean {
+        return this.customFields && this.customFields.length > 0;
+    }
+    public get isTabPropertyAvailable(): boolean {
+        return this.richTextFields && this.richTextFields.length > 0;
+    }
 
-                //Get property valie 
-                if (it.lookup === LookupEnum.ByName) {
-                    value = angular.isDefined(this._artifact[it.modelPropertyName]) ? this._artifact[it.modelPropertyName] : undefined;
-                } else { 
-                    var propertyValue = (this._artifact.propertyValues || []).filter((value) => {
-                        return it.lookup === LookupEnum.ById ?
-                            value.propertyTypeId === <number>it.modelPropertyName :
-                            value.propertyTypePredefined === <number>it.modelPropertyName;
-                    })[0];
-                    value = propertyValue ? propertyValue.value : undefined;
-                    }
-                
-                //create model property
-                if (angular.isDefined(value)) {
-                    if (it.primitiveType === Models.PrimitiveType.Date) {
-                        value = new Date(value);
-                    } else if (it.primitiveType === Models.PrimitiveType.Choice) {
-                        if (value.validValueIds) {
-                            value = value.validValueIds[0];  // Temporary user only one value for single select
-                        } 
-                        value = value.toString();
-                        
-                    } else if (it.primitiveType === Models.PrimitiveType.User) {
-                        if (value.userGroups) {
-                            value = value.map((val: any) => {
-                                return val.id;
-                            })[0];
-                        }
-                        value = value.toString();
-                    } 
-                    this._model[it.fieldPropertyName] = value;
-                    }
-                this._fields.push(field);
-                }
+
+    public onPropertyChange($viewValue, $modelValue, scope) {
+    };
+
+
+    public contextLoading(context: IEditorContext) {
+        this.artifactService.getArtifact(context.artifact.id).then((it) => {
+            //TODO: change
+            angular.extend(context.artifact, { propertyValues: it.propertyValues });
+            this.contextLoaded(context);
         });
     }
 
-    public destroy() {
-        delete this._fields;
-        delete this._model;
-        delete this._artifact;
-        delete this._project;
-    }
+    public contextLoaded(context: IEditorContext) {
+        try {
+            super.contextLoaded(context);
 
-    public getFields(): AngularFormly.IFieldConfigurationObject[] {
+            this.systemFields = [];
+            this.customFields = [];
+            this.richTextFields = [];
+            
+            this.fields.forEach((it: AngularFormly.IFieldConfigurationObject) => {
+                if (true === it.data["isSystem"]) {
+                    this.systemFields.push(it);
+                } else if (true === it.data["isRichText"]) {
+                    this.richTextFields.push(it);
+                } else {
+                    this.customFields.push(it);
+                } 
+            });
+            if (this.form) {
+                this.form.$setPristine();
+            } 
 
-        return this._fields || [];
-    }
-
-    public getModel(): any {
-        return this._model || {};
-    }
-
-        
-    private createPropertyField(context: FieldContext): AngularFormly.IFieldConfigurationObject {
-        
-        let field: AngularFormly.IFieldConfigurationObject = {
-            key: context.fieldPropertyName,
-            data: context,
-            templateOptions: {
-                label: context.name,
-                required: context.isRequired,
-                disabled: context.disabled,
-            },
-            expressionProperties: {}
-        };
-        //        this.data = this.propertyType;
-        switch (context.primitiveType) {
-            case Models.PrimitiveType.Text:
-                field.type = context.isRichText ? "frmlyInlineTinymce" : (context.isMultipleAllowed ? "textarea" : "input");
-                field.defaultValue = context.stringDefaultValue;
-                if (context.isRichText) {
-                    field.templateOptions["tinymceOption"] = {};
-                    //field.templateOptions["tinymceOption"].fixed_toolbar_container: ".form-tinymce-toolbar." + context.fieldPropertyName
-                    //TODO: added just for testing
-                    if (true) { //here we need something to decide if the tinyMCE editor should have mentions
-                        field.templateOptions["tinymceOption"].mentions = {
-                            source: tinymceMentionsData,
-                            delay: 100,
-                            items: 5,
-                            queryBy: "fullname",
-                            insert: function (item) {
-                                return `<a class="mceNonEditable" href="mailto:` + item.emailaddress + `" title="ID# ` + item.id + `">` + item.fullname + `</a>`;
-                            }
-                        }
-                    }
-                }
-                break;
-            case Models.PrimitiveType.Date:
-                field.type = "frmlyDatepicker";
-                field.templateOptions["datepickerOptions"] = {
-                    maxDate: context.maxDate,
-                    minDate: context.minDate
-                };
-                field.defaultValue = context.dateDefaultValue || new Date();
-                break;
-            case Models.PrimitiveType.Number:
-                field.type = "frmlyNumber";
-                field.defaultValue = context.decimalDefaultValue;
-                field.templateOptions.min = context.minNumber;
-                field.templateOptions.max = context.maxNumber;
-                break;
-            case Models.PrimitiveType.Choice:
-                field.type = "select";
-                if (angular.isNumber(context.defaultValidValueId)) {
-                    field.defaultValue = context.defaultValidValueId.toString();
-                }
-                field.templateOptions.options = [];
-                if (context.validValues && context.validValues.length) {
-                    field.templateOptions.options = context.validValues.map(function (it) {
-                        return <AngularFormly.ISelectOption>{ value: it.id.toString(), name: it.value };
-                    });
-                }
-                break;
-            case Models.PrimitiveType.User:
-                field.type = "select"; // needs to be changed to user selection
-                if (angular.isNumber(context.defaultValidValueId)) {
-                    field.defaultValue = context.defaultValidValueId.toString();
-                }
-                field.templateOptions.options = [];
-                if (context.validValues && context.validValues.length) {
-                    field.templateOptions.options = context.validValues.map(function (it) {
-                        return <AngularFormly.ISelectOption>{ value: it.id.toString(), name: it.value };
-                    });
-                }
-                break;
-            default:
-            //case Models.PrimitiveType.Image:
-                field.type = "input"; // needs to be changed to image editor
-                field.defaultValue = (context.defaultValidValueId || 0).toString();
-                field.templateOptions.options = [];
-                if (context.validValues) {
-                    field.templateOptions.options = context.validValues.map(function (it) {
-                        return <AngularFormly.ISelectOption>{ value: it.id.toString(), name: it.value };
-                    });
-                }
-                break;
+        } catch (ex) {
+            this.messageService.addError(ex["message"]);
         }
 
-        return field;
-
-
-            
-
-    } 
-
-
-
+    }
 }
-

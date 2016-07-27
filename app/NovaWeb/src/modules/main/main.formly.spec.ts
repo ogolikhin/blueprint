b5fd986c4110bdf53e1d79e8496c49b90bb23cf3
Fabio Gallo, Blueprint <fabio.gallo@blueprintsys.com>
2016-07-27 15:41:17 -0400

    Unit test for Text/Multi

--- app/NovaWeb/src/modules/main/main.formly.spec.ts
+++ app/NovaWeb/src/modules/main/main.formly.spec.ts
@@ -69,6 +69,121 @@ describe("Formly", () => {
         });
     });
 
+    describe("Text", () => {
+        it("should be initialized properly", function () {
+            compileAndSetupStuff({model: {text: ""}});
+
+            let fieldNode = node.querySelectorAll(".formly-field-bpFieldText");
+            let fieldScope = angular.element(fieldNode[0]).isolateScope();
+
+            expect(fieldNode.length).toBe(2);
+            expect(fieldNode[0]).toBeDefined();
+            expect(fieldScope).toBeDefined();
+        });
+
+        it("should fail if empty", function () {
+            compileAndSetupStuff({model: {text: ""}});
+
+            let fieldNode = node.querySelectorAll(".formly-field-bpFieldText")[0];
+            let fieldScope = angular.element(fieldNode).isolateScope();
+
+            expect((<any>fieldScope).fc.$valid).toBeFalsy();
+            expect((<any>fieldScope).fc.$invalid).toBeTruthy();
+            expect((<any>fieldScope).fc.$error.required).toBeTruthy();
+        });
+
+        it("should succeed if empty, as not required", function () {
+            compileAndSetupStuff({model: {textNotVal: ""}});
+
+            let fieldNode = node.querySelectorAll(".formly-field-bpFieldText")[1];
+            let fieldScope = angular.element(fieldNode).isolateScope();
+
+            expect((<any>fieldScope).fc.$valid).toBeTruthy();
+            expect((<any>fieldScope).fc.$invalid).toBeFalsy();
+            expect((<any>fieldScope).fc.$error.required).toBeUndefined();
+        });
+
+        it("should allow changing the value", function () {
+            compileAndSetupStuff({model: {text: "abcdefg"}});
+
+            let fieldNode = node.querySelectorAll(".formly-field-bpFieldText")[0];
+            let fieldScope = angular.element(fieldNode).isolateScope();
+            let fieldInput = fieldNode.querySelector("input");
+
+            fieldInput.value = "hijklmno";
+            angular.element(fieldInput).triggerHandler("change");
+
+            expect((<any>fieldScope).fc.$valid).toBeTruthy();
+            expect((<any>fieldScope).fc.$invalid).toBeFalsy();
+            expect(scope.model.text).not.toBe("abcdefg");
+            expect(scope.model.text).toBe("hijklmno");
+        });
+
+        it("should blur on Enter key", function () {
+            compileAndSetupStuff({model: {text: 10}});
+
+            let fieldNode = node.querySelectorAll(".formly-field-bpFieldText")[0];
+            let fieldInput = fieldNode.querySelector("input");
+
+            fieldInput.focus();
+            expect(fieldInput === document.activeElement).toBeTruthy();
+
+            triggerKey(fieldInput, 13, "keyup");
+            expect(fieldInput === document.activeElement).toBeFalsy();
+        });
+    });
+
+    describe("TextMulti", () => {
+        it("should be initialized properly", function () {
+            compileAndSetupStuff({model: {textmulti: ""}});
+
+            let fieldNode = node.querySelectorAll(".formly-field-bpFieldTextMulti");
+            let fieldScope = angular.element(fieldNode[0]).isolateScope();
+
+            expect(fieldNode.length).toBe(2);
+            expect(fieldNode[0]).toBeDefined();
+            expect(fieldScope).toBeDefined();
+        });
+
+        it("should fail if empty", function () {
+            compileAndSetupStuff({model: {textmulti: ""}});
+
+            let fieldNode = node.querySelectorAll(".formly-field-bpFieldTextMulti")[0];
+            let fieldScope = angular.element(fieldNode).isolateScope();
+
+            expect((<any>fieldScope).fc.$valid).toBeFalsy();
+            expect((<any>fieldScope).fc.$invalid).toBeTruthy();
+            expect((<any>fieldScope).fc.$error.required).toBeTruthy();
+        });
+
+        it("should succeed if empty, as not required", function () {
+            compileAndSetupStuff({model: {textmultiNotVal: ""}});
+
+            let fieldNode = node.querySelectorAll(".formly-field-bpFieldTextMulti")[1];
+            let fieldScope = angular.element(fieldNode).isolateScope();
+
+            expect((<any>fieldScope).fc.$valid).toBeTruthy();
+            expect((<any>fieldScope).fc.$invalid).toBeFalsy();
+            expect((<any>fieldScope).fc.$error.required).toBeUndefined();
+        });
+
+        it("should allow changing the value", function () {
+            compileAndSetupStuff({model: {textmulti: "abcdefg"}});
+
+            let fieldNode = node.querySelectorAll(".formly-field-bpFieldTextMulti")[0];
+            let fieldScope = angular.element(fieldNode).isolateScope();
+            let fieldTextarea = fieldNode.querySelector("textarea");
+
+            fieldTextarea.value = "hijklmno";
+            angular.element(fieldTextarea).triggerHandler("change");
+
+            expect((<any>fieldScope).fc.$valid).toBeTruthy();
+            expect((<any>fieldScope).fc.$invalid).toBeFalsy();
+            expect(scope.model.textmulti).not.toBe("abcdefg");
+            expect(scope.model.textmulti).toBe("hijklmno");
+        });
+    });
+
     describe("Number", () => {
         it("should be initialized properly", function () {
             compileAndSetupStuff({model: {number: 10}});
@@ -385,6 +500,28 @@ function createModule() {
                         {
                             type: "bpFieldInlineTinymce",
                             key: "inlineTinymce"
+                        },
+                        {
+                            type: "bpFieldText",
+                            key: "text",
+                            templateOptions: {
+                                required: true
+                            }
+                        },
+                        {
+                            type: "bpFieldText",
+                            key: "textNotVal"
+                        },
+                        {
+                            type: "bpFieldTextMulti",
+                            key: "textmulti",
+                            templateOptions: {
+                                required: true
+                            }
+                        },
+                        {
+                            type: "bpFieldTextMulti",
+                            key: "textmultiNotVal"
                         },
                         {
                             type: "bpFieldNumber",

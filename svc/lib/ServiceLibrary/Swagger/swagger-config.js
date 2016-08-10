c56c49f6ade1041d6ffce42032a3b53c053aacdf
Glen Stone <glen.stone@blueprintsys.com>
2016-08-10 14:55:28 -0400

    Centralize and update Swagger to support Session-Token header

---
+++ svc/lib/ServiceLibrary/Swagger/swagger-config.js
@@ -0,0 +1,17 @@
+﻿(function () {
+    $(function () {
+        $("#input_apiKey").attr("placeholder", "Session-Token");
+        $("#input_apiKey").off("change");
+        $("#input_apiKey").on("change", function () {
+            var token = this.value;
+            if (token && token.trim() !== "") {
+                var apiKeyAuth = new SwaggerClient.ApiKeyAuthorization("Session-Token", token, "header");
+                window.swaggerUi.api.clientAuthorizations.add("apiKey", apiKeyAuth);
+                console.log("added token " + token);
+            } else {
+                window.swaggerUi.api.clientAuthorizations.remove("apiKey");
+                console.log("removed token");
+            }
+        });
+    });
+})();

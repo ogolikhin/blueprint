1948f184908a0c57a214a584686e0835f223faa9
dagapitov <dmitry.agapitov@blueprintsys.com>
2016-08-12 15:09:03 -0400

    Add tests for retry logic

--- app/NovaWeb/src/modules/shell/error/http-error-interceptor.spec.ts
+++ app/NovaWeb/src/modules/shell/error/http-error-interceptor.spec.ts
@@ -37,12 +37,90 @@ describe("HttpErrorInterceptor", () => {
             expect(processedResponse.status).toBe(HttpHandledErrorStatusCodes.handledUnauthorizedStatus);
         }));
 
+        it("process 401 error and do successfull retry", inject(($httpBackend: ng.IHttpBackendService, $rootScope: ng.IRootScopeService,
+            $q: ng.IQService, httpErrorInterceptor: HttpErrorInterceptor) => {
+            // Arrange
+            var processedResponse: ng.IHttpPromiseCallbackArg<any>;
+            var response: ng.IHttpPromiseCallbackArg<any> = $q.defer();
+            var config = <IHttpInterceptorConfig>{ url: "/test-end-point", method: "GET" };
+            response.config = config;
+            response.status = 401;
+            $httpBackend.expectGET("/test-end-point").respond(200, "OK");
+            
+            // Act
+            httpErrorInterceptor.responseError(response).then(
+                (okResponse) => {
+                    processedResponse = okResponse;
+                },
+                (errResponse: any) => {
+                    processedResponse = errResponse;
+                }
+            );
+            $httpBackend.flush();          
+
+            // Assert
+            expect(config.dontRetry).toBeTruthy();
+            $httpBackend.verifyNoOutstandingRequest();
+            expect(processedResponse.status).toBe(200);
+        }));
+
+        it("process 401 error and do failed retry", inject(($httpBackend: ng.IHttpBackendService, $rootScope: ng.IRootScopeService,
+            $q: ng.IQService, httpErrorInterceptor: HttpErrorInterceptor) => {
+            // Arrange
+            var processedResponse: ng.IHttpPromiseCallbackArg<any>;
+            var response: ng.IHttpPromiseCallbackArg<any> = $q.defer();
+            var config = <IHttpInterceptorConfig>{ url: "/test-end-point", method: "GET" };
+            response.config = config;
+            response.status = 401;
+            $httpBackend.expectGET("/test-end-point").respond(500, "Any Error");
+            
+            // Act
+            httpErrorInterceptor.responseError(response).then(
+                (okResponse) => {
+                    processedResponse = okResponse;
+                },
+                (errResponse: any) => {
+                    processedResponse = errResponse;
+                }
+            );
+            $httpBackend.flush();          
+
+            // Assert
+            $httpBackend.verifyNoOutstandingRequest();
+            expect(processedResponse.status).toBe(500);
+        }));
+
         it("process 401 error from http request that should be ignored", inject(($rootScope: ng.IRootScopeService,
             $q: ng.IQService, httpErrorInterceptor: HttpErrorInterceptor) => {
             // Arrange
             var processedResponse: ng.IHttpPromiseCallbackArg<any>;
             var response: ng.IHttpPromiseCallbackArg<any> = $q.defer();
             var config = <IHttpInterceptorConfig>{ ignoreInterceptor: true };
+            response.config = config;
+            response.status = 401;
+
+            // Act
+            httpErrorInterceptor.responseError(response).then(
+                () => {
+                    processedResponse = undefined;
+                },
+                (resp: any) => {
+                    processedResponse = resp;
+                }
+            );
+            $rootScope.$digest();
+
+            // Assert
+            expect(processedResponse).toBeDefined();
+            expect(processedResponse.status).toBe(401);
+        }));
+
+        it("process 401 error from http request that should not by retried", inject(($rootScope: ng.IRootScopeService,
+            $q: ng.IQService, httpErrorInterceptor: HttpErrorInterceptor) => {
+            // Arrange
+            var processedResponse: ng.IHttpPromiseCallbackArg<any>;
+            var response: ng.IHttpPromiseCallbackArg<any> = $q.defer();
+            var config = <IHttpInterceptorConfig>{ dontRetry: true };
             response.config = config;
             response.status = 401;
 

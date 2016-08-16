61585f284a7a65705c91c3197ac0b25fed5cc5b0
Chris Dufour <chris.dufour@blueprintsys.com>
2016-08-16 14:52:56 -0400

    Modify default growth settings for databases

---
+++ svc/db/FileStorage/Migration/7.3.0.0/ModifyAutogrowth.sql
@@ -0,0 +1,16 @@
+﻿-- -----------------------------------------------------------------------------
+-- Modify the database filegrowth if it has not changed from the prior defaults
+-- -----------------------------------------------------------------------------
+DECLARE @db_name AS NVARCHAR(128) = DB_NAME();
+DECLARE @sql AS NVARCHAR(max);
+DECLARE @file_name AS SYSNAME
+
+SELECT @file_name = d.name FROM sys.database_files d WHERE d.type = 0 AND d.is_percent_growth = 0 AND d.growth = 1280
+IF (@file_name IS NOT NULL) 
+BEGIN
+    SET @sql = N'ALTER DATABASE [' + @db_name + '] MODIFY FILE ( NAME = N''' + @file_name + ''', FILEGROWTH = 10% )'
+
+    EXEC(@sql);
+END 
+
+GO

b5c8f6232d85f6a133759af8adc578be77e85498
Roman Dolgov <roman.dolgov@blueprintsys.com>
2016-08-19 09:12:08 -0400

    Displays the information of selected Process

--- app/NovaWeb/src/modules/editors/bp-process/components/header/bp-process-header.ts
+++ app/NovaWeb/src/modules/editors/bp-process/components/header/bp-process-header.ts
@@ -1,6 +1,14 @@
+import {IArtifact, IItemType, ItemTypePredefined, IEditorContext} from "../../../../main/models/models";
+import {Helper} from "../../../../shared";
+
 export class BpProcessHeader implements ng.IComponentOptions {
-    template: string = require("./bp-process-header.html");
-    controller: Function = BpProcessHeaderController;
+    public template: string = require("./bp-process-header.html");
+    public controller: Function = BpProcessHeaderController;
+    public controllerAs: string = "$ctrl";
+    public bindings: any = {
+        context: "<"
+    };
+    public transclude: boolean = true;
 }
 
 interface IArtifactHeader {
@@ -10,27 +18,95 @@ interface IArtifactHeader {
 }
 
 export class BpProcessHeaderController implements ng.IComponentController, IArtifactHeader {
+    static $inject: [string] = [
+        "$element"
+    ];
+
+    private _artifact: IArtifact;
+    private _type: IItemType;
+
+    constructor(
+        private $element: ng.IAugmentedJQuery
+    ) {
+    }
+
     public $onInit(): void {
     }
 
     public $onChanges(changesObj: any): void {
+        if (changesObj.context) {
+            let editorContext = changesObj.context.currentValue as IEditorContext;
+            this._artifact = editorContext.artifact;
+            this._type = editorContext.type;
+        }
     }
 
     public $onDestroy(): void {
+        delete this._artifact;
+        delete this._type;
     }
 
     public $postLink(): void {
     }
 
     public get name(): string {
-        return "Placeholder Name";
+        if (!this._artifact) {
+            return null;
+        }
+
+        return this._artifact.name;
     }
 
     public get iconClass(): string {
-        return "icon-process";
+        if (!this._artifact) {
+            return null;
+        }
+
+        return `icon-${Helper.toDashCase(ItemTypePredefined[this._artifact.predefinedType] || "process")}`;
+    }
+
+    private get type(): string {
+        if (this._type) {
+            return this._type.name || ItemTypePredefined[this._type.predefinedType] || "";
+        }
+
+        if (this._artifact) {
+            return ItemTypePredefined[this._artifact.predefinedType] || "";
+        }
+
+        return null;
     }
 
     public get typeDescription(): string {
-        return "Process - PRO1234";
+        if (!this._artifact) {
+            return null;
+        }
+
+        return `${this.type} - ${(this._artifact.prefix || "")}${this._artifact.id}`;
+    }
+
+    public get headingMinWidth() {
+        let style = {};
+
+        if (this.$element.length) {
+            let container: HTMLElement = this.$element[0];
+            let toolbar: Element = container.querySelector(".page-top-toolbar");
+            let heading: Element = container.querySelector(".artifact-heading");
+            let iconWidth: number = heading && heading.querySelector(".icon") ? heading.querySelector(".icon").scrollWidth : 0;
+            let nameWidth: number = heading && heading.querySelector(".name") ? heading.querySelector(".name").scrollWidth : 0;
+            let typeWidth: number = heading && heading.querySelector(".type-id") ? heading.querySelector(".type-id").scrollWidth : 0;
+            let indicatorsWidth: number = heading && heading.querySelector(".indicators") ? heading.querySelector(".indicators").scrollWidth : 0;
+            let headingWidth: number = iconWidth + (
+                typeWidth > nameWidth + indicatorsWidth ? typeWidth : nameWidth + indicatorsWidth
+                ) + 20 + 5; // heading's margins + wiggle room
+            
+            if (heading && toolbar) {
+                style = {
+                    "min-width": (headingWidth > toolbar.clientWidth ? toolbar.clientWidth : headingWidth) + "px"
+                };
+            }
+        }
+
+        return style;
     }
 }

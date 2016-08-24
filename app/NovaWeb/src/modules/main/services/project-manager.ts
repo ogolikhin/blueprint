095271e748c5ff1aa2eb29c666c27eb3a011c4f5
Anton Trinkunas <anton.trinkunas@blueprintsys.com>
2016-08-24 11:00:12 -0400

    Trello bug fix: https://trello.com/c/QUmf9zYP

--- app/NovaWeb/src/modules/main/services/project-manager.ts
+++ app/NovaWeb/src/modules/main/services/project-manager.ts
@@ -300,6 +300,15 @@ export class ProjectManager implements IProjectManager {
                 primitiveType: Models.PrimitiveType.Text,
                 isRichText: true
             });
+        }
+
+        if (subArtifact.predefinedType === Models.ItemTypePredefined.GDShape ||
+            subArtifact.predefinedType === Models.ItemTypePredefined.DDShape ||
+            subArtifact.predefinedType === Models.ItemTypePredefined.SBShape ||
+            subArtifact.predefinedType === Models.ItemTypePredefined.UIShape ||
+            subArtifact.predefinedType === Models.ItemTypePredefined.UCDShape ||
+            subArtifact.predefinedType === Models.ItemTypePredefined.PROShape ||
+            subArtifact.predefinedType === Models.ItemTypePredefined.BPShape) {
 
             properties.push(<Models.IPropertyType>{
                 name: "X",

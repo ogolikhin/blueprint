154368c675f1206b3a4e8307447d6bbd09188468
Alex Folomechine <alex.folomechine@blueprintsys.com>
2016-08-24 10:22:39 -0400

    Created web layer for GetExpandedTreeToArtifact, user story 1844

--- svc/ArtifactStore.Tests/App_Start/WebApiConfigTests.cs
+++ svc/ArtifactStore.Tests/App_Start/WebApiConfigTests.cs
@@ -22,11 +22,12 @@ namespace ArtifactStore
             config.EnsureInitialized();
 
             // Assert
-            config.AssertTotalRoutes(11, "Please update asserts in WebApiConfigTests when changing routes.");
+            config.AssertTotalRoutes(12, "Please update asserts in WebApiConfigTests when changing routes.");
             config.AssertAction<StatusController>("GetStatus", HttpMethod.Get, "status");
             config.AssertAction<StatusController>("GetStatusUpCheck", HttpMethod.Get, "status/upcheck");
             config.AssertAction<ArtifactController>("GetProjectChildren", HttpMethod.Get, "projects/1/children");
             config.AssertAction<ArtifactController>("GetArtifactChildren", HttpMethod.Get, "projects/1/artifacts/2/children");
+            config.AssertAction<ArtifactController>("GetExpandedTreeToArtifact", HttpMethod.Get, "projects/1/artifacts?expandedToArtifactId=2");
             config.AssertAction<ProjectMetaController>("GetProjectTypes", HttpMethod.Get, "projects/1/meta/customtypes");
             config.AssertAction<DiscussionController>("GetDiscussions", HttpMethod.Get, "artifacts/1/discussions");
             config.AssertAction<DiscussionController>("GetReplies", HttpMethod.Get, "artifacts/1/discussions/1/replies");

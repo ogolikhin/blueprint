1fbb2393d55fc400c1931f9d890f50eb515840f7
Chris Just <chris.just@blueprintsys.com>
2016-08-29 17:48:00 -0400

    Added more UsersAndGroups tests. - Added another CreateGroup() to GroupFactory. - Renamed 'displayNameOrEmail' to 'search' in ISvcShared.FindUserOrGroup() to shorten it and make it consistent with the REST call parameter name. - Added extra Validate* functions for SearchUsersTests.

--- blueprint-automationframework/Blueprint-Tests/Model/ISvcShared.cs
+++ blueprint-automationframework/Blueprint-Tests/Model/ISvcShared.cs
@@ -11,14 +11,14 @@ namespace Model
         /// Searches for users or groups that match the search criteria.
         /// </summary>
         /// <param name="user">The user to authenticate the REST call.</param>
-        /// <param name="displayNameOrEmail">(optional) The display name or E-mail address of the user or group to search for.</param>
+        /// <param name="search">(optional) The sub-string to search for in the display name or E-mail address of the user or group.</param>
         /// <param name="allowEmptyEmail">(optional) If false, do not return users and groups with an empty E-mail address.  Default (null) = false.</param>
         /// <param name="limit">(optional) The max number of results to return.  Default (null) = 5.  The min is 1 and max is 500.</param>
         /// <param name="includeGuests">(optional) If false, do not return guests.  Default (null) = true.</param>
         /// <param name="expectedStatusCodes">(optional) Expected status codes for the request.  By default only 200 OK is expected.</param>
         /// <returns>The list of UserOrGroupInfo that was returned.</returns>
         List<UserOrGroupInfo> FindUserOrGroup(IUser user,
-            string displayNameOrEmail = null,
+            string search = null,
             bool? allowEmptyEmail = null,
             int? limit = null,
             bool? includeGuests = null,

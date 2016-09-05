47f3a8cd613d8ab938076f84222d55abf2d81da5
Pavlo Pochapsky <pavlo.pochapsky@blueprintsys.com>
2016-09-05 13:43:59 -0400

    initial implementation of get/remove for attachments

--- app/NovaWeb/src/modules/managers/artifact-manager/attachments/attachments.ts
+++ app/NovaWeb/src/modules/managers/artifact-manager/attachments/attachments.ts
@@ -1,21 +1,50 @@
 import { Models, Enums } from "../../../main/models";
-import { IStatefulArtifact, IArtifactAttachments, IArtifactAttachment } from "../interfaces";
+import { IArtifactAttachmentsResultSet } from "../../../shell/bp-utility-panel/bp-attachments-panel/artifact-attachments.svc";
+import { 
+    IStatefulArtifact, 
+    IArtifactAttachments, 
+    IArtifactAttachment, 
+    IArtifactManager 
+} from "../interfaces";
 
 export class ArtifactAttachments implements IArtifactAttachments {
     private attachments: IArtifactAttachment[];
     private subject: Rx.BehaviorSubject<IArtifactAttachment[]>;
     private state: IStatefulArtifact;
+    private manager: IArtifactManager;
 
     constructor(artifactState: IStatefulArtifact) {
         this.attachments = [];
+        this.manager = artifactState.manager;
         this.state = artifactState;
         this.subject = new Rx.BehaviorSubject<IArtifactAttachment[]>(this.attachments);
     }
 
+    // TODO: how would this work for subartifact attachments?
     public get value(): ng.IPromise<IArtifactAttachment[]> {
-        // try to get attachments through a service
-        return {} as ng.IPromise<IArtifactAttachment[]>;
-    }    
+        let subArtifactId = null;
+
+        const deferred = this.manager.$q.defer<IArtifactAttachment[]>();
+        const requestObj: ng.IRequestConfig = {
+            url: `/svc/artifactstore/artifacts/${this.state.id}/attachment`, 
+            method: "GET",
+            params: {
+                subartifactId: subArtifactId,
+                addDrafts: true
+            },
+            timeout: null
+        };
+
+        this.manager.request<Models.IArtifact>(requestObj).then((result: ng.IHttpPromiseCallbackArg<IArtifactAttachmentsResultSet>) => {
+            this.attachments = result.data.attachments;
+            deferred.resolve(this.attachments);
+        
+        }).catch((err) => {
+            deferred.reject(err);
+        });
+
+        return deferred.promise;
+    }
 
     public get observable(): Rx.IObservable<IArtifactAttachment[]> {
         return this.subject.asObservable();
@@ -29,11 +58,24 @@ export class ArtifactAttachments implements IArtifactAttachments {
     }
 
     public update(attachment: IArtifactAttachment): ng.IPromise<IArtifactAttachment[]> {
-        // this.state.manager.lockArtifact();
-        return {} as ng.IPromise<IArtifactAttachment[]>;
+        throw Error("operation not supported");
     }
 
     public remove(attachment: IArtifactAttachment): ng.IPromise<IArtifactAttachment[]> {
-        return {} as ng.IPromise<IArtifactAttachment[]>;
+        const deferred = this.manager.$q.defer<IArtifactAttachment[]>();
+        const foundAttachmentIndex = this.attachments.indexOf(attachment);
+        let deletedAttachment: IArtifactAttachment;
+
+        if (foundAttachmentIndex > -1) {
+            deletedAttachment = this.attachments.splice(foundAttachmentIndex, 1)[0];
+            
+            // TODO: add changeset tracking
+
+            deferred.resolve(this.attachments);
+        } else {
+            deferred.reject("Attachment not found");
+        }
+
+        return deferred.promise;
     }
 }

7a0b4a77f355c3fda49d54d5dc3120d660af93d0
Dmitry Lopyrev <dmitry.lopyrev@blueprintsys.com>
2016-09-12 15:41:44 -0400

    Changes index

--- app/NovaWeb/src/modules/managers/index.ts
+++ app/NovaWeb/src/modules/managers/index.ts
@@ -1,23 +1,13 @@
 import "./artifact-manager";
-<<<<<<< Updated upstream
-import "./selection-manager";
-//import { } from "./project-manager";
-
-angular.module("bp.managers", [
-    "bp.managers.artifact",
-    "bp.managers.selection"
-]);
-
-export { IArtifactManager } from  "./models";
-=======
 import "./project-manager";
+import "./selection-manager";
 
 angular.module("bp.managers", [
     "bp.managers.artifact",
+    "bp.managers.selection",
     "bp.managers.project"
 ]);
 
 export { IArtifactManager } from  "./models";
 export { IProjectManager } from  "./project-manager";
   
->>>>>>> Stashed changes

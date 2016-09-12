eb25e88168afb80643105cbd5fe3faa0e42b0313
Dmitry Lopyrev <dmitry.lopyrev@blueprintsys.com>
2016-09-12 17:26:45 -0400

    Removes old project repository files

--- app/NovaWeb/src/modules/main/services/index.ts
+++ app/NovaWeb/src/modules/main/services/index.ts
@@ -1,20 +1,20 @@
-﻿import { IProjectRepository, ProjectRepository } from "./project-repository";
+﻿//import { IProjectRepository, ProjectRepository } from "./project-repository";
 import { IArtifactService, ArtifactService } from "./artifact.svc";
-import { IProjectManager, ProjectManager } from "./project-manager";
+//import { IProjectManager, ProjectManager } from "./project-manager";
 import { ISelectionManager, SelectionManager, SelectionSource, ISelection } from "./selection-manager";
 import { IWindowManager, WindowManager, IMainWindow, ResizeCause } from "./window-manager";
 
 angular.module("bp.main.services", [])
-    .service("projectRepository", ProjectRepository)
+//    .service("projectRepository", ProjectRepository)
 //    .service("projectManager", ProjectManager)
     .service("selectionManager", SelectionManager)
     .service("artifactService", ArtifactService)
     .service("windowManager", WindowManager);
 
 export {
-    IProjectRepository, ProjectRepository,
+//    IProjectRepository, ProjectRepository,
     IArtifactService, ArtifactService,
-    IProjectManager, ProjectManager,
+//    IProjectManager, ProjectManager,
     ISelectionManager, ISelection, SelectionManager, SelectionSource,
     IWindowManager, WindowManager, IMainWindow, ResizeCause
 }

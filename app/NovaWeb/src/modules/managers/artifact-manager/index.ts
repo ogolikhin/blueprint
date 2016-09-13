c7f94edcd47f4def378d9ebe8dc31bc93a3c5c61
Pavlo Pochapsky <pavlo.pochapsky@blueprintsys.com>
2016-09-13 11:37:58 -0400

    added stateful artifact factory

--- app/NovaWeb/src/modules/managers/artifact-manager/index.ts
+++ app/NovaWeb/src/modules/managers/artifact-manager/index.ts
@@ -1,5 +1,7 @@
 
 import { ArtifactManager } from "./artifact-manager";
+import { StatefulArtifactFactory } from "./artifact/artifact.factory";
 
 angular.module("bp.managers.artifact", [])
-    .service("artifactManager", ArtifactManager );
+    .service("artifactManager", ArtifactManager )
+    .service("statefulArtifactFactory", StatefulArtifactFactory );
--- app/NovaWeb/src/modules/managers/index.ts
+++ app/NovaWeb/src/modules/managers/index.ts
@@ -10,4 +10,3 @@ angular.module("bp.managers", [
 
 export { IArtifactManager } from  "./models";
 export { IProjectManager } from  "./project-manager";
-  

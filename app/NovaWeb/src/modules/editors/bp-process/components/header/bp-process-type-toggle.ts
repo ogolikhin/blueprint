513f118cc47fe8b89b6f107ad1ace445a561ab54
Roman Dolgov <roman.dolgov@blueprintsys.com>
2016-09-12 22:18:38 -0400

    Makes process toggle control more generic

--- app/NovaWeb/src/modules/editors/bp-process/components/header/bp-process-type-toggle.ts
+++ app/NovaWeb/src/modules/editors/bp-process/components/header/bp-process-type-toggle.ts
@@ -4,7 +4,30 @@ export class BpProcessTypeToggle implements ng.IComponentOptions {
     public controllerAs: string = "$ctrl";
 }
 
+interface IProcessTypeToggleOption {
+    id: number;
+    value: string;
+    iconClass: string;
+    isChecked: boolean;
+}
+
 class BpProcessTypeToggleController implements ng.IComponentController {
+    public options: IProcessTypeToggleOption[];
+
     constructor() {
+        this.options = [
+            {
+                id: 0,
+                value: "business",
+                iconClass: "fonticon2-user-user",
+                isChecked: false
+            },
+            {
+                id: 1,
+                value: "userToSystem",
+                iconClass: "fonticon2-user-system",
+                isChecked: false
+            }
+        ];
     }
 }

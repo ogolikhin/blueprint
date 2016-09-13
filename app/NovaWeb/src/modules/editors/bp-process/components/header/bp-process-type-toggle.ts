a07886353586a35d3de496be8596c9ec397cec79
Roman Dolgov <roman.dolgov@blueprintsys.com>
2016-09-13 08:30:30 -0400

    Implements logic of toggling process type

--- app/NovaWeb/src/modules/editors/bp-process/components/header/bp-process-type-toggle.ts
+++ app/NovaWeb/src/modules/editors/bp-process/components/header/bp-process-type-toggle.ts
@@ -1,4 +1,3 @@
-import {IStateManager, ItemState} from "../../../../core";
 import {ICommunicationManager} from "../../";
 
 export class BpProcessTypeToggle implements ng.IComponentOptions {
@@ -12,53 +11,7 @@ export class BpProcessTypeToggle implements ng.IComponentOptions {
 
 export interface IProcessTypeToggleOption {
     id: number;
-    value: string;
     iconClass: string;
-    isChecked: boolean;
-    isEnabled: boolean;
-}
-
-export class ProcessTypeToggleOption implements IProcessTypeToggleOption {
-    constructor(
-        private _id: number, 
-        private _value: string, 
-        private _iconClass: string, 
-        private _isChecked: boolean, 
-        private _isEnabled: boolean,
-        private communicationManager?: ICommunicationManager) {
-    }
-
-    public get id(): number {
-        return this._id;
-    }
-
-    public get value(): string {
-        return this._value;
-    }
-
-    public get iconClass(): string {
-        return this._iconClass;
-    }
-
-    public get isChecked(): boolean {
-        return this._isChecked;
-    }
-
-    public set isChecked(value: boolean) {
-        this._isChecked = value;
-
-        if (this._isChecked && this.communicationManager) {
-            this.communicationManager.toolbarCommunicationManager.toggleProcessType(this.id);
-        }
-    }
-
-    public get isEnabled(): boolean {
-        return this._isEnabled;
-    }
-
-    public set isEnabled(value: boolean) {
-        this._isEnabled = value;
-    }
 }
 
 export class BpProcessTypeToggleController implements ng.IComponentController {
@@ -67,13 +20,14 @@ export class BpProcessTypeToggleController implements ng.IComponentController {
     ];
 
     public options: IProcessTypeToggleOption[];
+    public currentProcessType: number;
 
     constructor(
         private communicationManager: ICommunicationManager
     ) {
         this.options = [
-            new ProcessTypeToggleOption(1, "business", "fonticon2-user-user", false, true, communicationManager),
-            new ProcessTypeToggleOption(2, "userToSystem", "fonticon2-user-system", false, true, communicationManager)
+            { id: 1, iconClass: "fonticon2-user-user" },
+            { id: 2, iconClass: "fonticon2-user-system" }
         ];
     }
 
@@ -85,11 +39,17 @@ export class BpProcessTypeToggleController implements ng.IComponentController {
         this.communicationManager.toolbarCommunicationManager.removeEnableProcessTypeToggleObserver(this.onEnableProcessTypeToggle);
     }
 
+    public processTypeChanged() {
+        this.communicationManager.toolbarCommunicationManager.toggleProcessType(this.currentProcessType);
+    }
+
     private onEnableProcessTypeToggle = (status: any) => {
         for (let i = 0; i < this.options.length; i++) {
             let option = this.options[i];
-            option.isEnabled = status.value;
-            option.isChecked = status.processType === option.id
+            
+            if (option.id === status.processType) {
+                this.currentProcessType = option.id;
+            }
         }
     }
 }

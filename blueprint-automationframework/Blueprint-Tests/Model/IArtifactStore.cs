805bf582934e5753928c70afa56a1a8e3bc00e08
Bogdan Tkachenko <bogdan.tkachenko@blueprintsys.com>
2016-09-14 17:18:14 -0400

    add implementation for NovaSubArtifact, first test for GetSubArtifact

--- blueprint-automationframework/Blueprint-Tests/Model/IArtifactStore.cs
+++ blueprint-automationframework/Blueprint-Tests/Model/IArtifactStore.cs
@@ -181,5 +181,15 @@ namespace Model
         /// <param name="expectedStatusCodes">(optional) Expected status codes for the request.  By default only 200 OK is expected.</param>
         /// <returns>RelationshipsDetails object for the specified artifact/subartifact.</returns>
         TraceDetails GetRelationshipsDetails(IUser user, IArtifactBase artifact, bool? addDrafts = null, List<HttpStatusCode> expectedStatusCodes = null);
+
+        /// <summary>
+        /// Gets list of subartifacts for the artifact with the specified ID.
+        /// (Runs: GET svc/artifactstore/artifacts/{artifactId}/subartifacts)
+        /// </summary>
+        /// <param name="user">The user to authenticate with.</param>
+        /// <param name="artifactId">Id of artifact.</param>
+        /// <param name="expectedStatusCodes">(optional) Expected status codes for the request.  By default only 200 OK is expected.</param>
+        /// <returns>List of subartifacts.</returns>
+        List<INovaSubArtifact> GetSubartifacts(IUser user, int artifactId, List<HttpStatusCode> expectedStatusCodes = null);
     }
 }

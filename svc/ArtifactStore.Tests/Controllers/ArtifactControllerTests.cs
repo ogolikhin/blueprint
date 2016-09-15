a7dd6a096bf4c97f54fb739cd9c1440a1ebc9ba7
David He <david.he@blueprintsys.com>
2016-09-15 14:19:18 -0400

    artifact controller tests.

--- svc/ArtifactStore.Tests/Controllers/ArtifactControllerTests.cs
+++ svc/ArtifactStore.Tests/Controllers/ArtifactControllerTests.cs
@@ -9,6 +9,7 @@ using ServiceLibrary.Exceptions;
 using ServiceLibrary.Helpers;
 using ServiceLibrary.Models;
 using ServiceLibrary.Repositories.ConfigControl;
+using System.Web.Http;
 
 namespace ArtifactStore.Controllers
 {
@@ -115,5 +116,60 @@ namespace ArtifactStore.Controllers
 
             //Assert
         }
+
+        [TestMethod]
+        [ExpectedException(typeof(HttpResponseException))]
+        public async Task GetSubArtifactTreeAsync_NoPermissions_ThrowsNoPermissionsException()
+        {
+            // Arrange
+            const int userId = 1;
+            var session = new Session { UserId = userId };
+            const int artifactId = 2;
+            var artifactIds = new[] { artifactId };
+            var permissionsDictionary = new Dictionary<int, RolePermissions>();
+            permissionsDictionary.Add(artifactId, RolePermissions.None);
+            var mockArtifactRepository = new Mock<ISqlArtifactRepository>();
+            var mockArtifactPermissionsRepository = new Mock<IArtifactPermissionsRepository>();
+            mockArtifactPermissionsRepository.Setup(r => r.GetArtifactPermissions(artifactIds, userId, false, int.MaxValue, true)).ReturnsAsync(permissionsDictionary);
+            var mockServiceLogRepository = new Mock<IServiceLogRepository>();
+
+            var artifactController = new ArtifactController(mockArtifactRepository.Object, mockArtifactPermissionsRepository.Object, mockServiceLogRepository.Object) { Request = new HttpRequestMessage() };
+            artifactController.Request.Properties[ServiceConstants.SessionProperty] = session;
+
+            // Act
+            await artifactController.GetSubArtifactTreeAsync(artifactId);
+
+            //Assert
+        }
+
+        [TestMethod]
+        public async Task GetSubArtifactTreeAsync_HasPermissions_OK()
+        {
+            // Arrange
+            const int userId = 1;
+            var session = new Session { UserId = userId };
+            const int artifactId = 2;
+            var artifactIds = new[] { artifactId };
+            var permissionsDictionary = new Dictionary<int, RolePermissions>();
+            permissionsDictionary.Add(artifactId, RolePermissions.Read);
+            var subArtifacts = new List<SubArtifact> { new SubArtifact { Id = 1 } };
+            var mockArtifactRepository = new Mock<ISqlArtifactRepository>();
+            mockArtifactRepository.Setup(r => r.GetSubArtifactTreeAsync(artifactId, userId, int.MaxValue, true)).ReturnsAsync(subArtifacts);
+            var mockArtifactPermissionsRepository = new Mock<IArtifactPermissionsRepository>();
+            mockArtifactPermissionsRepository.Setup(r => r.GetArtifactPermissions(artifactIds, userId, false, int.MaxValue, true)).ReturnsAsync(permissionsDictionary);
+            var mockServiceLogRepository = new Mock<IServiceLogRepository>();
+
+            var artifactController = new ArtifactController(mockArtifactRepository.Object, mockArtifactPermissionsRepository.Object, mockServiceLogRepository.Object) { Request = new HttpRequestMessage() };
+            artifactController.Request.Properties[ServiceConstants.SessionProperty] = session;
+
+            // Act
+            var result = await artifactController.GetSubArtifactTreeAsync(artifactId);
+
+            //Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(1, result[0].Id);
+        }
+
+
     }
 }

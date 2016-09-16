84ba027f04dcea45bd98c3c84d2bfe2314fa7035
Pavlo Pochapsky <pavlo.pochapsky@blueprintsys.com>
2016-09-16 13:41:58 -0400

    commented out old selection-manager

--- app/NovaWeb/src/modules/main/services/index.ts
+++ app/NovaWeb/src/modules/main/services/index.ts
@@ -1,7 +1,7 @@
 ﻿//import { IProjectRepository, ProjectRepository } from "./project-repository";
 //import { IArtifactService, ArtifactService } from "./artifact.svc";
 //import { IProjectManager, ProjectManager } from "./project-manager";
-import { ISelectionManager, SelectionManager, SelectionSource, ISelection } from "./selection-manager";
+// import { ISelectionManager, SelectionManager, SelectionSource, ISelection } from "./selection-manager";
 import { IWindowManager, WindowManager, IMainWindow, ResizeCause } from "./window-manager";
 
 angular.module("bp.main.services", [])

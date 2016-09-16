1672f798dd7ece5675ce4c742d372056dec3a033
Michael Talis <Michael.Talis@blueprintsys.com>
2016-09-16 16:24:31 -0400

    Fixed https://trello.com/c/VMm62uWR

--- app/NovaWeb/src/modules/editors/bp-process/components/modal-dialogs/sub-artifact-editor-modal-controller.ts
+++ app/NovaWeb/src/modules/editors/bp-process/components/modal-dialogs/sub-artifact-editor-modal-controller.ts
@@ -246,23 +246,25 @@ export class SubArtifactEditorModalController extends BaseModalDialogController<
 
     private showMore(type: string, event: any) {
         // select tab
-        this.$scope.$applyAsync((s) => {
-            if (type === "label") {
-                this.isShowMore = !this.isShowMore;
-            } else if (type === "info") {
-                this.isShowMore = true;
-                this.showMoreActiveTabIndex = 0;
-            } else if (type === "include") {
-                this.isShowMore = true;
-                this.showMoreActiveTabIndex = 1;
-            }
+        if (type === "label") {
+            this.isShowMore = !this.isShowMore;
+        } else if (type === "info") {
+            this.isShowMore = true;
+            this.showMoreActiveTabIndex = 0;
+        } else if (type === "include") {
+            this.isShowMore = true;
+            this.showMoreActiveTabIndex = 1;
+        }
+        this.refreshView();
+        event.stopPropagation();
+    }
 
-            setTimeout(() => {
-                var elem: any = document.getElementsByClassName("modal-dialog")[0].parentElement;
-                elem.style.width = "" + (elem.clientWidth - 1) + "px"; 
-                elem.style.width = "" + (elem.clientWidth + 1) + "px"; 
-            }, 100);
-        });
+    private refreshView() {
+        setTimeout(() => {
+            var elem: any = document.getElementsByClassName("modal-dialog")[0].parentElement;
+            elem.style.width = "" + (elem.clientWidth - 1) + "px"; 
+            elem.style.width = "" + (elem.clientWidth + 1) + "px"; 
+        }, 100);
     }
 
     public getActiveHeader(): string {

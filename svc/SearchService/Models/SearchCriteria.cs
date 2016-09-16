5eab56e6e27a5888e3311a795834fe58a15087ba
Chris Dufour <chris.dufour@blueprintsys.com>
2016-09-16 10:36:39 -0400

    Make modifications based on feedback

--- svc/SearchService/Models/SearchCriteria.cs
+++ svc/SearchService/Models/SearchCriteria.cs
@@ -1,15 +1,22 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace SearchService.Models
 {
+
     public class SearchCriteria
     {
         [Required]
-        public int? UserId { get; set; } 
+        public int? UserId { get; set; }
+
         [Required]
         public string Query { get; set; }
+
         public IEnumerable<int> ProjectIds { get; set; }
+
         public IEnumerable<int> ItemTypeIds { get; set; }
+
+        public bool IncludeMetaData { get; set; } = true;
     }
 }

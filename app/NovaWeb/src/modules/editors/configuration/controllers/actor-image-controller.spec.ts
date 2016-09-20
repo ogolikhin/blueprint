b9dd1554e50a3cfbeda40be73b26b30780c94514
amalivanchuk <amalivanchuk@VM1091PC>
2016-09-20 14:01:19 -0400

    US 1283: - fix for comment for unit tests for actor-image controller

--- app/NovaWeb/src/modules/editors/configuration/controllers/actor-image-controller.spec.ts
+++ app/NovaWeb/src/modules/editors/configuration/controllers/actor-image-controller.spec.ts
@@ -57,28 +57,7 @@ describe("Actor image controller tests", () => {
 
             actorImageController(scope, localization, $window, messageService, dialogService, settingsService);
 
-            function fixBinary (bin) {
-                var length = bin.length;
-                var buf = new ArrayBuffer(length);
-                var arr = new Uint8Array(buf);
-                for (var i = 0; i < length; i++) {
-                    arr[i] = bin.charCodeAt(i);
-                }
-                return buf;
-            }
-
-            var base64 =
-                "iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAB1klEQVR42n2TzytEURTHv3e8N1joRhZG" +
-                "zJsoCjsLhcw0jClKWbHwY2GnLGUlIfIP2IjyY2djZTHSMJNQSilFNkz24z0/Ms2MrnvfvMu8mcfZvPvu" +
-                "Pfdzz/mecwgKLNYKb0cFEgXbRvwV2s2HuWazCbzKA5LvNecDXayBjv9NL7tEpSNgbYzQ5kZmAlSXgsGG" +
-                "XmS+MjhKxDHgC+quyaPKQtoPYMQPOh5U9H6tBxF+Icy/aolqAqLP5wjWd5r/Ip3YXVILrF4ZRYAxDhCO" +
-                "J/yCwiMI+/xgjOEzmzIhAio04GeGayIXjQ0wGoAuQ5cmIjh8jNo0GF78QwNhpyvV1O9tdxSSR6PLl51F" +
-                "nIK3uQ4JJQME4sCxCIRxQbMwPNSjqaobsfskm9l4Ky6jvCzWEnDKU1ayQPe5BbN64vYJ2vwO7CIeLIi3" +
-                "ciYAoby0M4oNYBrXgdgAbC/MhGCRhyhCZwrcEz1Ib3KKO7f+2I4iFvoVmIxHigGiZHhPIb0bL1bQApFS" +
-                "9U/AC0ulSXrrhMotka/lQy0Ic08FDeIiAmDvA2HX01W05TopS2j2/H4T6FBVbj4YgV5+AecyLk+Ctvms" +
-                "QWK8WZZ+Hdf7QGu7fobMuZHyq1DoJLvUqQrfM966EU/qYGwAAAAASUVORK5CYII=";
-
-
+            var base64 = "";
 
             var files = [{
                 file: new Blob([base64], {type: 'image/png'}),
@@ -126,7 +105,6 @@ describe("Actor image controller tests", () => {
             actorImageController(scope, localization, $window, messageService, dialogService, settingsService);
 
             scope.onActorImageDelete();
-
 
             expect(scope.model.image === null).toBeTruthy();
 

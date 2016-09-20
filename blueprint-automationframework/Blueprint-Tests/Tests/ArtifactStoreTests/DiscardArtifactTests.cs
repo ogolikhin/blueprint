d825082a0a8afa98de7cc4a4221616200ed48440
dhalexkim <dhalexkim@gmail.com>
2016-09-20 17:53:50 -0400

    Changes: - changed based on comments

--- blueprint-automationframework/Blueprint-Tests/Tests/ArtifactStoreTests/DiscardArtifactTests.cs
+++ blueprint-automationframework/Blueprint-Tests/Tests/ArtifactStoreTests/DiscardArtifactTests.cs
@@ -34,8 +34,7 @@ namespace ArtifactStoreTests
         #region 200 OK Tests
 
         [TestCase(2, BaseArtifactType.Actor)]
-        [TestRail(0)]
-        [Explicit(IgnoreReasons.UnderDevelopment)]
+        [TestRail(166133)]
         [Description("Set draft artifacts by just saving. Execute Discard - Must return successful discard response")]
         //Create process artifact, save, don't publish, discard - must return successfully discarded.
         public void DiscardArtifacts_DiscardSavedArtifacts_VerifyArtifactDiscarded(int numberOfArtifacts, BaseArtifactType artifactType)
@@ -43,28 +42,31 @@ namespace ArtifactStoreTests
             // Setup:
             // Create artifact(s) with save for discard test
             var savedArtifacts = Helper.CreateAndSaveMultipleArtifacts(_project, _user, artifactType, numberOfArtifacts);
-
-            INovaPublishResponse discardArtifactResponse = null;
-
-            // Execute:
-            Assert.DoesNotThrow(() => discardArtifactResponse = Helper.ArtifactStore.DiscardArtifacts(savedArtifacts, _user), "DiscardArtifacts() failed when discarding saved artifact(s)!");
-
-            // Validation: Verify that discarded artifact information from discardedArtifactResponse match with that from savedArtifacts
-
-            Assert.That(discardArtifactResponse.Artifacts.Count.Equals(savedArtifacts.Count), "Number of discarded artifact is {0} but discarded item count from the response of the discard is {1}", savedArtifacts.Count, discardArtifactResponse.Artifacts.Count);
-
-            DiscardVerification(discardArtifactResponse, savedArtifacts);
-
-            // Preperation for cleanup:
-            foreach (var artifact in savedArtifacts)
+            try
             {
-                artifact.IsSaved = false;
+                INovaPublishResponse discardArtifactResponse = null;
+
+                // Execute:
+                Assert.DoesNotThrow(() => discardArtifactResponse = Helper.ArtifactStore.DiscardArtifacts(savedArtifacts, _user), "DiscardArtifacts() failed when discarding saved artifact(s)!");
+
+                // Validation: Verify that discarded artifact information from discardedArtifactResponse match with that from savedArtifacts
+
+                Assert.That(discardArtifactResponse.Artifacts.Count.Equals(savedArtifacts.Count), "Number of discarded artifact is {0} but discarded item count from the response of the discard is {1}", savedArtifacts.Count, discardArtifactResponse.Artifacts.Count);
+
+                DiscardVerification(discardArtifactResponse, savedArtifacts);
+            }
+            finally
+            {
+                // Preperation for cleanup:
+                foreach (var artifact in savedArtifacts)
+                {
+                    artifact.IsSaved = false;
+                }
             }
         }
 
         [TestCase(2, BaseArtifactType.Actor)]
-        [TestRail(0)]
-        [Explicit(IgnoreReasons.UnderDevelopment)]
+        [TestRail(166134)]
         [Description("Set draft artifacts by just saving. Execute Discard with optional parameter all=true - Must return successful discard response")]
         //Create process artifact, save, don't publish, discard - must return successfully discarded.
         public void DiscardArtifacts_DiscardSavedArtifactsWithDiscardAll_VerifyArtifactDiscarded(int numberOfArtifacts, BaseArtifactType artifactType)
@@ -72,41 +74,43 @@ namespace ArtifactStoreTests
             // Setup:
             // Create artifact(s) with save for discard test
             var savedArtifacts = Helper.CreateAndSaveMultipleArtifacts(_project, _user, artifactType, numberOfArtifacts);
-
-            INovaPublishResponse discardArtifactResponse = null;
-
-            // Execute:
-            Assert.DoesNotThrow(() => discardArtifactResponse = Helper.ArtifactStore.DiscardArtifacts(savedArtifacts, _user, all: true), "DiscardArtifacts() failed when discarding saved artifact(s)!");
-
-            // Validation: Verify that discarded artifact information from discardedArtifactResponse match with that from savedArtifacts
-
-            Assert.That(discardArtifactResponse.Artifacts.Count.Equals(savedArtifacts.Count), "Number of discarded artifact is {0} but discarded item count from the response of the discard is {1}", savedArtifacts.Count, discardArtifactResponse.Artifacts.Count);
-
-            DiscardVerification(discardArtifactResponse, savedArtifacts);
-
-            // Preperation for cleanup:
-            foreach (var artifact in savedArtifacts)
+            try
             {
-                artifact.IsSaved = false;
+                INovaPublishResponse discardArtifactResponse = null;
+
+                // Execute:
+                Assert.DoesNotThrow(() => discardArtifactResponse = Helper.ArtifactStore.DiscardArtifacts(savedArtifacts, _user, all: true), "DiscardArtifacts() failed when discarding saved artifact(s)!");
+
+                // Validation: Verify that discarded artifact information from discardedArtifactResponse match with that from savedArtifacts
+
+                Assert.That(discardArtifactResponse.Artifacts.Count.Equals(savedArtifacts.Count), "Number of discarded artifact is {0} but discarded item count from the response of the discard is {1}", savedArtifacts.Count, discardArtifactResponse.Artifacts.Count);
+
+                DiscardVerification(discardArtifactResponse, savedArtifacts);
+
+            }
+            finally
+            {
+                // Preperation for cleanup:
+                foreach (var artifact in savedArtifacts)
+                {
+                    artifact.IsSaved = false;
+                }
             }
         }
 
         [TestCase(2, BaseArtifactType.Actor)]
-        [TestRail(0)]
-        [Explicit(IgnoreReasons.UnderDevelopment)]
+        [TestRail(166135)]
         [Description("Set draft artifacts by saving, publishing, and saving. Execute Discard - Must return successful discard response")]
         public void DiscardArtifacts_DiscardChangesFromPreviouslyPublishedArtifacts_VerifyArtifactDiscarded(int numberOfArtifacts, BaseArtifactType artifactType)
         {
             // Setup:
             // Create artifact(s) with save and publish for discard test
-            var publishedArtifacts = Helper.CreateAndPublishMultipleArtifacts(_project, _user, artifactType, numberOfArtifacts).ConvertAll(x => (IArtifact)x);
+            var changedPublishedArtifacts = Helper.CreateAndPublishMultipleArtifacts(_project, _user, artifactType, numberOfArtifacts);
 
-            foreach (var publishedArtifact in publishedArtifacts)
+            foreach (var publishedArtifact in changedPublishedArtifacts.ConvertAll(x => (IArtifact)x))
             {
                 publishedArtifact.Save();
             }
-
-            var changedPublishedArtifacts = publishedArtifacts.ConvertAll(x => (IArtifactBase)x);
 
             INovaPublishResponse discardArtifactResponse = null;
 
@@ -120,21 +124,18 @@ namespace ArtifactStoreTests
         }
 
         [TestCase(2, BaseArtifactType.Actor)]
-        [TestRail(0)]
-        [Explicit(IgnoreReasons.UnderDevelopment)]
+        [TestRail(166136)]
         [Description("Set draft artifacts by saving, publishing, and saving. Execute Discard with optional parameter all=true - Must return successful discard response")]
         public void DiscardArtifacts_DiscardChangesFromPreviouslyPublishedArtifactsWithDiscardAll_VerifyArtifactDiscarded(int numberOfArtifacts, BaseArtifactType artifactType)
         {
             // Setup:
             // Create artifact(s) with save and publish for discard test
-            var publishedArtifacts = Helper.CreateAndPublishMultipleArtifacts(_project, _user, artifactType, numberOfArtifacts).ConvertAll(x => (IArtifact)x);
+            var changedPublishedArtifacts = Helper.CreateAndPublishMultipleArtifacts(_project, _user, artifactType, numberOfArtifacts);
 
-            foreach (var publishedArtifact in publishedArtifacts)
+            foreach (var publishedArtifact in changedPublishedArtifacts.ConvertAll(x => (IArtifact)x))
             {
                 publishedArtifact.Save();
             }
-
-            var changedPublishedArtifacts = publishedArtifacts.ConvertAll(x => (IArtifactBase)x);
 
             INovaPublishResponse discardArtifactResponse = null;
 
@@ -148,8 +149,7 @@ namespace ArtifactStoreTests
         }
 
         [TestCase(2, BaseArtifactType.Actor)]
-        [TestRail(0)]
-        [Explicit(IgnoreReasons.UnderDevelopment)]
+        [TestRail(166137)]
         [Description("Set published artifacts by saving and publishing. Execute Discard with optional parameter all=true - Must return the 200 nothing to discard")]
         public void DiscardArtifacts_DiscardPublishedArtifactsWithNoChildrenWithDiscardAll_VerifyNothingToDiscard(int numberOfArtifacts, BaseArtifactType artifactType)
         {
@@ -165,13 +165,36 @@ namespace ArtifactStoreTests
             Assert.That(discardArtifactResponse.Artifacts.Count.Equals(0), "Number of discarded artifact is {0} but discarded item count from the response of the discard is {1}", 0, discardArtifactResponse.Artifacts.Count);
         }
 
+        [TestCase(2, BaseArtifactType.Actor)]
+        [TestRail(166138)]
+        [Description("Set published artifacts by saving and publishing. Execute Discard with optional parameter all=true - Must return the 200 nothing to discard")]
+        public void DiscardArtifacts_DiscardMixedListOfPublishedAndSavedArtifactsWithDiscardAll_VerifyNothingToDiscard(int numberOfArtifacts, BaseArtifactType artifactType)
+        {
+            // Setup:
+            // Create artifact(s) with save and publish for discard test
+            var publishedArtifacts = Helper.CreateAndPublishMultipleArtifacts(_project, _user, artifactType, numberOfArtifacts);
+
+            // Create artifact(s) with save for discard test
+            List<IArtifactBase> savedArtifacts = Helper.CreateAndSaveMultipleArtifacts(_project, _user, artifactType, numberOfArtifacts);
+
+            List<IArtifactBase> mixedArtifacts = new List<IArtifactBase>();
+            mixedArtifacts.AddRange(publishedArtifacts);
+            mixedArtifacts.AddRange(savedArtifacts);
+
+            INovaPublishResponse discardArtifactResponse = null;
+
+            // Execute:
+            Assert.DoesNotThrow(() => discardArtifactResponse = Helper.ArtifactStore.DiscardArtifacts(mixedArtifacts, _user, all: true), "DiscardArtifacts() failed when discarding saved artifact(s)!");
+
+            Assert.That(discardArtifactResponse.Artifacts.Count.Equals(numberOfArtifacts), "Number of discarded artifact is {0} but discarded item count from the response of the discard is {1}", numberOfArtifacts, discardArtifactResponse.Artifacts.Count);
+        }
+
         #endregion 200 OK Tests
 
         #region 400 Bad Request tests
 
         [TestCase(2, BaseArtifactType.Actor)]
-        [TestRail(0)]
-        [Explicit(IgnoreReasons.UnderDevelopment)]
+        [TestRail(166139)]
         [Description("Set published artifacts by saving and publishing. Execute Discard - Must return nothing to discard")]
         public void DiscardArtifacts_DiscardPublishedArtifactsWithNoChildren_VerifyNothingToDiscard(int numberOfArtifacts, BaseArtifactType artifactType)
         {
@@ -191,8 +214,7 @@ namespace ArtifactStoreTests
         }
 
         [TestCase(2, BaseArtifactType.Actor)]
-        [TestRail(0)]
-        [Explicit(IgnoreReasons.UnderDevelopment)]
+        [TestRail(166140)]
         [Description("Set mixed list of published and saved artifacts. Execute Discard - Must return nothing to discard")]
         public void DiscardArtifacts_DiscardMixedListOfPublishedAndSavedArtifacts_VerifyNothingToDiscard(int numberOfArtifacts, BaseArtifactType artifactType)
         {
@@ -210,7 +232,7 @@ namespace ArtifactStoreTests
             INovaPublishResponse discardArtifactResponse = null;
 
             // Execute:
-            var ex = Assert.Throws<Http400BadRequestException>(() => discardArtifactResponse = Helper.ArtifactStore.DiscardArtifacts(publishedArtifacts, _user),
+            var ex = Assert.Throws<Http400BadRequestException>(() => discardArtifactResponse = Helper.ArtifactStore.DiscardArtifacts(mixedArtifacts, _user),
                 "We should get a 400 BadRequestException when a user trying to discard published artifact(s) which has nothing to discard!");
 
             // Validation: Exception should contain expected message.
@@ -219,8 +241,7 @@ namespace ArtifactStoreTests
         }
 
         [TestCase(2, BaseArtifactType.Actor)]
-        [TestRail(0)]
-        [Explicit(IgnoreReasons.UnderDevelopment)]
+        [TestRail(166141)]
         [Description("Set mixed list of saved and published artifacts. Execute Discard - Must return nothing to discard")]
         public void DiscardArtifacts_DiscardMixedListOfSavedPublishedArtifacts_VerifyNothingToDiscard(int numberOfArtifacts, BaseArtifactType artifactType)
         {
@@ -238,7 +259,7 @@ namespace ArtifactStoreTests
             INovaPublishResponse discardArtifactResponse = null;
 
             // Execute:
-            var ex = Assert.Throws<Http400BadRequestException>(() => discardArtifactResponse = Helper.ArtifactStore.DiscardArtifacts(publishedArtifacts, _user),
+            var ex = Assert.Throws<Http400BadRequestException>(() => discardArtifactResponse = Helper.ArtifactStore.DiscardArtifacts(mixedArtifacts, _user),
                 "We should get a 400 BadRequestException when a user trying to discard published artifact(s) which has nothing to discard!");
 
             // Validation: Exception should contain expected message.
@@ -261,16 +282,22 @@ namespace ArtifactStoreTests
         #endregion 409 Conflict tests
 
         #region private call
-        public static void DiscardVerification(INovaPublishResponse discardArtifactResponse, List<IArtifactBase> artifacts)
+
+        /// <summary>
+        /// Asserts that returned artifact details from the discard call match with artifacts that were discarded.
+        /// </summary>
+        /// <param name="discardArtifactResponse">The response from Nova discard call.</param>
+        /// <param name="artifactsTodiscard">artifacts that are being discarded</param>
+        public static void DiscardVerification(INovaPublishResponse discardArtifactResponse, List<IArtifactBase> artifactsTodiscard)
         {
             ThrowIf.ArgumentNull(discardArtifactResponse, nameof(discardArtifactResponse));
-            ThrowIf.ArgumentNull(artifacts, nameof(artifacts));
+            ThrowIf.ArgumentNull(artifactsTodiscard, nameof(artifactsTodiscard));
             List<int> tempIds = new List<int>();
             discardArtifactResponse.Artifacts.ForEach(a => tempIds.Add(a.Id));
 
-            for (int i = 0; i < artifacts.Count; i++)
+            for (int i = 0; i < artifactsTodiscard.Count; i++)
             {
-                Assert.That(tempIds.Contains(artifacts[i].Id), "The discarded artifact whos Id is {0} does not exist on the response from the discard call.", artifacts[i].Id);
+                Assert.That(tempIds.Contains(artifactsTodiscard[i].Id), "The discarded artifact whose Id is {0} does not exist on the response from the discard call.", artifactsTodiscard[i].Id);
             }
         }
 

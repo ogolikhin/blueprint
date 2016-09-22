09843e4d74d7b1831f5c1d473c73c54d02e9cb9b
amalivanchuk <amalivanchuk@VM1091PC>
2016-09-22 09:02:51 -0400

    US1283 - Refactor tests and formly-config

--- app/NovaWeb/src/modules/editors/configuration/controllers/actor-image-controller.spec.ts
+++ app/NovaWeb/src/modules/editors/configuration/controllers/actor-image-controller.spec.ts
@@ -6,6 +6,7 @@ import "angular-sanitize";
 import "angular-ui-bootstrap";
 import "angular-ui-tinymce";
 import "ui-select";
+import "angular-formly";
 import "tinymce";
 
 import { ILocalizationService } from "../../../core/localization";
@@ -14,123 +15,98 @@ import { MessageServiceMock } from "../../../core/messages/message.mock";
 import { IMessageService } from "../../../core/messages";
 import { DialogServiceMock, IDialogService } from "../../../shared/widgets/bp-dialog/bp-dialog";
 import { SettingsService, ISettingsService } from "../../../core";
-import { actorImageController } from "./actor-image-controller";
+import { BPFieldImageController } from "../types/field-image";
 import { ActorImagePickerDialogServiceMock } from "./actor-image-choose-window-mock";
+import { ComponentTest } from "../../../util/component.test";
 
 describe("Actor image controller tests", () => {
 
+    let controller: BPFieldImageController,
+        scope,
+        rootScope,
+        compile,
+        apply,
+        createController,
+        $controller : ng.IControllerService,
+        imageName = 'default.png'
+
     beforeEach(angular.mock.module(($provide: ng.auto.IProvideService) => {
-        $provide.service("localization", LocalizationServiceMock);
-        $provide.service("messageService", MessageServiceMock);
+        $provide.service("localization", LocalizationServiceMock)
+        $provide.service("messageService", MessageServiceMock)
         $provide.service("dialogService", ActorImagePickerDialogServiceMock);
         $provide.service("settingsService", SettingsService);
     }));
 
-
-    let compile, scope, rootScope, module, imageName, newImageName;
-
     beforeEach(
         inject(
-            ($compile: ng.ICompileService, $rootScope: ng.IRootScopeService) => {
+            ($compile: ng.ICompileService, $rootScope: ng.IRootScopeService,
+                $controller) => {
                 rootScope = $rootScope;
                 compile = $compile;
                 scope = rootScope.$new();
-                scope.model = {};
-                scope.options = {};
-                scope.options.key = 'image';
 
-                imageName = 'test.png';
-                newImageName = 'test2.png';
+                scope.model = {
+                    image: imageName
+                };
 
-                scope.model.image = imageName;
+                scope["to"] = {
+                    onChange($value: any, $field: AngularFormly.IFieldConfigurationObject, $scope: ng.IScope) {}
+                };
 
+                scope.options = {
+                    key: "image"
+                };
+
+                controller = $controller(BPFieldImageController, {$scope: scope});
             }
         )
     );
+
+    beforeEach(angular.mock.inject(function(_$controller_){
+        $controller = _$controller_;
+    }));
 
     describe("On file select function test ", () => {
 
         it("On file select function test", inject(($timeout: ng.ITimeoutService, localization: ILocalizationService,
                                                    $window: ng.IWindowService,
-                                                                   messageService: IMessageService, dialogService: IDialogService,
+                                                   messageService: IMessageService, dialogService: IDialogService,
                                                    settingsService: ISettingsService) => {
-
-            actorImageController(scope, localization, $window, messageService, dialogService, settingsService);
 
             var base64 = "";
 
             var files = [{
                 file: new Blob([base64], {type: 'image/png'}),
                 guid: '2788d782-4d7f-e611-82cc-a0999b0c8c40',
-                name: newImageName,
+                name: imageName,
                 url: 'svc/bpfilestore/file/2788d782-4d7f-e611'
             }];
 
             var readerSpy = spyOn(FileReader.prototype, 'readAsDataURL');
 
             scope.onFileSelect(files, function(){});
-
             $timeout.flush();
-
             expect(readerSpy).toHaveBeenCalled();
-
         }));
-
-        it("If no file selected function test", inject(($timeout: ng.ITimeoutService, localization: ILocalizationService,
-                                                   $window: ng.IWindowService,
-                                                   messageService: IMessageService, dialogService: IDialogService,
-                                                   settingsService: ISettingsService) => {
-
-            actorImageController(scope, localization, $window, messageService, dialogService, settingsService);
-
-            var files = [];
-
-            var readerSpy = spyOn(FileReader.prototype, 'readAsDataURL');
-
-            scope.onFileSelect(files, function(){});
-
-            $timeout.flush();
-
-            expect(readerSpy).not.toHaveBeenCalled();
-        }));
-    });
+    })
 
     describe("delete image for actor", () => {
 
         it("delete image for actor without readonly mode", inject(($timeout: ng.ITimeoutService, localization: ILocalizationService, $window: ng.IWindowService,
-                                                                       messageService: IMessageService, dialogService: IDialogService, settingsService: ISettingsService) => {
-
-            actorImageController(scope, localization, $window, messageService, dialogService, settingsService);
-
-            scope.to = {
-                onChange($value: any, $field: AngularFormly.IFieldConfigurationObject, $scope: ng.IScope) {
-                }
-            };
+                                                                   messageService: IMessageService, dialogService: IDialogService, settingsService: ISettingsService) => {
 
             scope.onActorImageDelete();
-
             expect(scope.model.image === null).toBeTruthy();
-
         }));
 
-        it("delete image for actor with read only mode", inject(($timeout: ng.ITimeoutService, localization: ILocalizationService, $window: ng.IWindowService,
-                                                                 messageService: IMessageService, dialogService: IDialogService, settingsService: ISettingsService) => {
+        it("delete image for actor with read only mode",
+                inject(($timeout: ng.ITimeoutService, localization: ILocalizationService, $window: ng.IWindowService,
+                        messageService: IMessageService, dialogService: IDialogService, settingsService: ISettingsService) => {
 
-            actorImageController(scope, localization, $window, messageService, dialogService, settingsService);
-
-            scope.to = {
-                onChange($value: any, $field: AngularFormly.IFieldConfigurationObject, $scope: ng.IScope) {
-                }
-            };
-
-            scope.onActorImageDelete(true);
-
-
+           scope.onActorImageDelete(true);
             expect(scope.model.image === imageName).toBeTruthy()
 
         }));
     })
-
-
 })
 
--- app/NovaWeb/src/modules/editors/configuration/types/field-image.ts
+++ app/NovaWeb/src/modules/editors/configuration/types/field-image.ts
@@ -7,7 +7,7 @@ import { IDialogSettings, IDialogService } from "../../../shared";
 import { IUploadStatusDialogData } from "../../../shared/widgets";
 import { BpFileUploadStatusController } from "../../../shared/widgets/bp-file-upload-status/bp-file-upload-status";
 import { BPFieldBaseController } from "./base-controller";
-
+import { Models } from "../../../main/models";
 
 export class BPFieldImage implements AngularFormly.ITypeOptions {
     public name: string = "bpFieldImage";
@@ -20,20 +20,95 @@ export class BPFieldImage implements AngularFormly.ITypeOptions {
 }
 
 export class BPFieldImageController extends BPFieldBaseController {
-    static $inject: [string] = ["$scope", "localization", "$window", "messageService", "dialogService", "settingsService"];
+    static $inject: [string] = ["$scope", "localization", "$window", "messageService", "dialogService"];
 
     constructor(
         private $scope: AngularFormly.ITemplateScope,
         private localization: ILocalizationService,
         private $window: ng.IWindowService,
         private messageService: IMessageService,
-        private dialogService: IDialogService,
-        private settingsService: ISettingsService
+        private dialogService: IDialogService
     ) {
         super();
 
-        $scope["changeLabelText"] = localization.get("App_UP_Document_File_Change", "Change");
+        const templateOptions: AngularFormly.ITemplateOptions = $scope["to"];
+        let onChange = (templateOptions["onChange"] as AngularFormly.IExpressionFunction); //notify change function. injected on field creation.
+        const maxAttachmentFilesizeDefault: number = 1048576; // 1 MB
+        const maxNumberAttachmentsDefault: number = 1;
+        const allowedExtensions = ['png', 'jpg', 'jpeg'];
 
-        setFields(currentModelVal);
+        let setFields = (model: any) => {
+            if (model) {
+                $scope.model["image"] =  model.image;
+            }
+        }
+
+        let currentModelVal = $scope.model[$scope.options["key"]];
+        //let currentModelVal = <Models.IActorImagePropertyValue>$scope.model[$scope.options.key];
+        if (!currentModelVal) {
+            currentModelVal = <Models.IActorImagePropertyValue>{};
+        }
+
+        function chooseActorImage(files: File[], callback?: Function) {
+            const dialogSettings = <IDialogSettings>{
+                okButton: localization.get("App_Button_Ok", "OK"),
+                template: require("../../../shared/widgets/bp-file-upload-status/bp-file-upload-status.html"),
+                controller: BpFileUploadStatusController,
+                css: "nova-file-upload-status",
+                header: localization.get("App_UP_Attachments_Upload_Dialog_Header", "File Upload"),
+                backdrop: false
+            };
+
+            const dialogData: IUploadStatusDialogData = {
+                files: files,
+                maxAttachmentFilesize: maxAttachmentFilesizeDefault,
+                maxNumberAttachments: maxNumberAttachmentsDefault,
+                allowedExtentions: allowedExtensions
+            };
+
+
+            let ds = dialogService.open(dialogSettings, dialogData).then((uploadList: any[]) => {
+                // TODO: add state manager handling
+
+                if (uploadList && uploadList.length > 0) {
+                    let image = uploadList[0];
+                    var reader = new FileReader();
+                    reader.readAsDataURL(image.file);
+
+                    reader.onload = function (e) {
+                        currentModelVal.url = e.target['result'];
+                        currentModelVal.guid = image.guid;
+                        $scope.model[$scope.options["key"]] = currentModelVal;
+                        onChange(image.file, getImageField(), $scope);
+                    }
+                }
+            }).finally(() => {
+                if (callback) {
+                    callback();
+                }
+            });
+        }
+
+        function getImageField(): any {
+            if (!$scope.fields) {
+                return null;
+            }
+            return $scope.fields[0];
+        }
+
+        $scope["onFileSelect"] = (files: File[], callback?: Function) => {
+            chooseActorImage(files, callback);
+        };
+
+        $scope["onActorImageDelete"] = (isReadOnly: boolean) => {
+            if(isReadOnly && isReadOnly === true){
+                return ;
+            }
+            onChange(null, getImageField(), $scope);
+            $scope.model[$scope.options["key"]] = null;
+        };
+
+        //onChange(image.file, getImageField(), $scope);
+        //setFields(currentModelVal);
     }
 }

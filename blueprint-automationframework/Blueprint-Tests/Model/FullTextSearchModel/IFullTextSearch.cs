84c99710db9294c87ad039d1a7677e6b6591b5b4
Darren Martell <darren.martell@blueprintsys.com>
2016-09-22 15:41:44 -0400

    Implements Search and SearchMetadata methods

--- blueprint-automationframework/Blueprint-Tests/Model/FullTextSearchModel/IFullTextSearch.cs
+++ blueprint-automationframework/Blueprint-Tests/Model/FullTextSearchModel/IFullTextSearch.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Net;
 using Model.FullTextSearchModel.Impl;
 using Model.Impl;
 
@@ -12,11 +14,12 @@ namespace Model.FullTextSearchModel
         /// <param name="user">The user performing the search.</param>
         /// <param name="searchCriteria">The criteria for the search request. (i.e. "search", 
         /// "search phrase", "search ph*)</param>
-        /// <param name="page">An index to a subset of search results the length of which is determined 
+        /// <param name="page">(optional)An index to a subset of search results the length of which is determined 
         /// by the pageSize argument</param>
-        /// <param name="pageSize">The number of search results to return in a single request.</param>
+        /// <param name="pageSize">(optional)The number of search results to return in a single request.</param>
+        /// <param name="expectedStatusCodes">(optional) Expected status codes for the request.  By default only 200 OK is expected.</param>
         /// <returns>The subset of search results.</returns>
-        FullTextSearchResult Search(User user, FullTextSearchCriteria searchCriteria, int page, int pageSize);
+        FullTextSearchResult Search(User user, FullTextSearchCriteria searchCriteria, int? page, int? pageSize, List<HttpStatusCode> expectedStatusCodes = null);
 
         /// <summary>
         /// Returns the search result metadata indicating what would be returned if the full search 
@@ -25,7 +28,8 @@ namespace Model.FullTextSearchModel
         /// <param name="user">The user performing the search.</param>
         /// <param name="searchCriteria">The criteria for the search request. (i.e. "search", 
         /// "search phrase", "search ph*)</param>
+        /// <param name="expectedStatusCodes">(optional) Expected status codes for the request.  By default only 200 OK is expected.</param>
         /// <returns>The search result metadata.</returns>
-        FullTextSearchMetaDataResult SearchMetaData(User user, FullTextSearchCriteria searchCriteria);
+        FullTextSearchMetaDataResult SearchMetaData(User user, FullTextSearchCriteria searchCriteria, List<HttpStatusCode> expectedStatusCodes = null);
     }
 }
--- blueprint-automationframework/Blueprint-Tests/Model/FullTextSearchModel/Impl/FullTextSearch.cs
+++ blueprint-automationframework/Blueprint-Tests/Model/FullTextSearchModel/Impl/FullTextSearch.cs
@@ -1,21 +1,84 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System.Net;
 using Common;
 using Model.Impl;
+using Utilities;
+using Utilities.Facades;
 
 namespace Model.FullTextSearchModel.Impl
 {
     public class FullTextSearch : IFullTextSearch
     {
-        #region Members inherited from IFullTextSearch
+        public string Address { get; }
 
-        public FullTextSearchResult Search(User user, FullTextSearchCriteria searchCriteria, int page, int pageSize)
+        #region Constructor
+
+        public FullTextSearch(string address)
         {
-            throw new NotImplementedException();
+            ThrowIf.ArgumentNull(address, nameof(address));
+
+            Address = address;
         }
 
-        public FullTextSearchMetaDataResult SearchMetaData(User user, FullTextSearchCriteria searchCriteria)
+        #endregion Constructor
+
+        #region Members inherited from IFullTextSearch
+
+        public FullTextSearchResult Search(User user, FullTextSearchCriteria searchCriteria, int? page, int? pageSize, List<HttpStatusCode> expectedStatusCodes = null)
         {
-            throw new NotImplementedException();
+            Logger.WriteTrace("{0}.{1}", nameof(FullTextSearch), nameof(Search));
+
+            ThrowIf.ArgumentNull(user, nameof(user));
+            ThrowIf.ArgumentNull(searchCriteria, nameof(searchCriteria));
+
+            var queryParams = new Dictionary<string, string>();
+
+            if (page != null)
+            {
+                queryParams.Add("page", page.ToString());
+            }
+
+            if (pageSize != null)
+            {
+                queryParams.Add("pageSize", pageSize.ToString());
+            }
+
+            var tokenValue = user.Token?.AccessControlToken;
+
+            var restApi = new RestApiFacade(Address, tokenValue);
+
+            Logger.WriteInfo("{0} Projects: {1} Item Types: {2} Search criteria: {3}", nameof(FullTextSearch), searchCriteria.ProjectIds, searchCriteria.ItemTypeIds, searchCriteria.Query);
+
+            var restResponse = restApi.SendRequestAndDeserializeObject<FullTextSearchResult, FullTextSearchCriteria>(
+                RestPaths.Svc.SearchService.FULLTEXTSEARCH,
+                RestRequestMethod.POST,
+                searchCriteria,
+                queryParameters: queryParams,
+                expectedStatusCodes: expectedStatusCodes);
+
+            return restResponse;
+        }
+
+        public FullTextSearchMetaDataResult SearchMetaData(User user, FullTextSearchCriteria searchCriteria, List<HttpStatusCode> expectedStatusCodes = null)
+        {
+            Logger.WriteTrace("{0}.{1}", nameof(FullTextSearch), nameof(SearchMetaData));
+
+            ThrowIf.ArgumentNull(user, nameof(user));
+            ThrowIf.ArgumentNull(searchCriteria, nameof(searchCriteria));
+
+            var tokenValue = user.Token?.AccessControlToken;
+
+            var restApi = new RestApiFacade(Address, tokenValue);
+
+            Logger.WriteInfo("{0} Projects: {1} Item Types: {2} Search criteria: {3}", nameof(FullTextSearch), searchCriteria.ProjectIds, searchCriteria.ItemTypeIds, searchCriteria.Query);
+
+            var restResponse = restApi.SendRequestAndDeserializeObject<FullTextSearchMetaDataResult, FullTextSearchCriteria>(
+                RestPaths.Svc.SearchService.FullTextSearch.METADATA,
+                RestRequestMethod.POST,
+                searchCriteria,
+                expectedStatusCodes: expectedStatusCodes);
+
+            return restResponse;
         }
 
         #endregion Members inherited from IFullTextSearch

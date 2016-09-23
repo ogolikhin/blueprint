ec5478d3879c82a39e5f6b5d1e6b11f4c1b7e0d4
dhalexkim <dhalexkim@gmail.com>
2016-09-23 10:06:06 -0400

    Chages: - Added FullTextSearch searvice on TestHelper - Added SearchServiceFactory in Factories directory - Added a single FullTextSearch test case on FullTextSearchTests

---
+++ blueprint-automationframework/Blueprint-Tests/Model/Factories/SearchServiceFactory.cs
@@ -0,0 +1,32 @@
+﻿using CustomAttributes;
+using Model.FullTextSearchModel;
+using Model.FullTextSearchModel.Impl;
+
+namespace Model.Factories
+{
+    public static class SearchServiceFactory
+    {
+        /// <summary>
+        /// Creates a new IFullTextSearch.
+        /// </summary>
+        /// <param name="address">The URI address of the searchservice.</param>
+        /// <returns>An IFullTextSearch object.</returns>
+        public static IFullTextSearch CreateFullTextSearch(string address)
+        {
+            IFullTextSearch fullTextSearch = new FullTextSearch(address);
+            return fullTextSearch;
+        }
+
+        /// <summary>
+        /// Creates a SearchService object with the settings defined in the TestConfiguration.
+        /// </summary>
+        /// <returns>The FileStore object.</returns>
+        /// <exception cref="DataException">If there was an error reading required information from the TestConfiguration.</exception>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1024:UsePropertiesWhereAppropriate")]   // Ignore this warning.
+        public static IFullTextSearch GetSearchServiceFromTestConfig()
+        {
+            string address = FactoryCommon.GetServiceAddressFromTestConfig(Categories.SearchService);
+            return CreateFullTextSearch(address);
+        }
+    }
+}
--- blueprint-automationframework/Blueprint-Tests/Tests/SearchServiceTests/FullTextSearchTests.cs
+++ blueprint-automationframework/Blueprint-Tests/Tests/SearchServiceTests/FullTextSearchTests.cs
@@ -1,39 +1,63 @@
-﻿using CustomAttributes;
+﻿using Common;
+using CustomAttributes;
 using Helper;
 using Model;
 using Model.Factories;
+using Model.FullTextSearchModel.Impl;
 using NUnit.Framework;
 using System;
+using System.Collections.Generic;
 using TestCommon;
 
 namespace SearchServiceTests
 {
     [TestFixture]
     [Category(Categories.SearchService)]
-    public static class FullTextSearchTests //: TestBase
+    public class FullTextSearchTests : TestBase
     {
         const string FULLTEXTSEARCH_PATH = RestPaths.Svc.SearchService.FULLTEXTSEARCH;
+        const int DEFAULT_PAGE_VALUE = 1;
+        const int DEFAULT_PAGESIZE_VALUE = 10;
 
-        //private IUser _user = null;
+        private IUser _user = null;
         //private IProject _project = null;
 
         [SetUp]
-        public static void SetUp()
+        public void SetUp()
         {
-            throw new NotImplementedException();
-            //Helper = new TestHelper();
-            //_user = Helper.CreateUserAndAuthenticate(TestHelper.AuthenticationTokenTypes.BothAccessControlAndOpenApiTokens);
+            Helper = new TestHelper();
+            _user = Helper.CreateUserAndAuthenticate(TestHelper.AuthenticationTokenTypes.BothAccessControlAndOpenApiTokens);
             //_project = ProjectFactory.GetProject(_user);
         }
 
         [TearDown]
-        public static void TearDown()
+        public void TearDown()
         {
-            throw new NotImplementedException();
-            //Helper?.Dispose();
+            Helper?.Dispose();
         }
 
         #region 200 OK Tests
+
+        [TestCase]
+        [TestRail(166155)]
+        [Explicit(IgnoreReasons.UnderDevelopment)]
+        [Description("Searching without both optional parameters, page and pagesize. Execute Search - Must return SearchResult that uses FirstPage and DefaultPageSize")]
+        public void FullTextSearch_SearchWithoutBothPageAndPageSize_VerifySearchResultUsesFirstPageAndDefaultPageSize()
+        {
+            FullTextSearchResult fullTextSearchResult = null;
+
+            // Setup: Create searchable artifact(s) with unique search terms
+            var projectIds = new List<int>() { 2, 4 };
+            FullTextSearchCriteria searchCriteria = new FullTextSearchCriteria("NonExistingSearchTerm", projectIds);
+
+            // Execute: Execute FullTextSearch with search terms
+            Assert.DoesNotThrow(() => fullTextSearchResult = Helper.FullTextSearch.Search(_user, searchCriteria), "Search() call failed when using following search term: {0}artifact(s)!", searchCriteria.Query);
+
+            // Validation: Verify that searchResult uses FirstPage and DefaultPageSize
+            Assert.That(fullTextSearchResult.Page.Equals(DEFAULT_PAGE_VALUE), "The expected default page value is {0} but {1} was found from the returned searchResult.", DEFAULT_PAGE_VALUE, fullTextSearchResult.Page);
+            Assert.That(fullTextSearchResult.PageSize.Equals(DEFAULT_PAGESIZE_VALUE), "The expected default pagesize value is {0} but {1} was found from the returned searchResult.", DEFAULT_PAGESIZE_VALUE, fullTextSearchResult.PageSize);
+        }
+
         #endregion 200 OK Tests
 
         #region 400 Bad Request Tests
@@ -49,6 +73,15 @@ namespace SearchServiceTests
         #endregion 409 Conflict Tests
 
         #region Private Functions
+        /// <summary>
+        /// Asserts that returned searchResult from the FullTextSearch call match with artifacts that are being searched.
+        /// </summary>
+        /// <param name="discardArtifactResponse">The response from Nova discard call.</param>
+        /// <param name="artifactsTodiscard">artifacts that are being discarded</param>
+        //private static void FullTextSearchResultValidation()
+        //{
+
+        //}
         #endregion Private Functions
 
     }

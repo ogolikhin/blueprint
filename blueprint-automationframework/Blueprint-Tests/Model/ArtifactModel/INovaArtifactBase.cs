922ad57f83d6fe38005431cb405ef14c10d2be58
Chris Just <chris.just@blueprintsys.com>
2016-09-23 13:08:35 -0400

    Fixing broken tests due to changes by development. - Made ItemTypeId nullable because now it throws a 501 if you pass it to UpdateArtifact. - Made InstanceItemTypeId & IconImageId nullable in NovaArtifactType because the REST call usually doesn't return them. - Development changed the ValidValues property of NovaPropertyType to be a class with Id & Value instead of just a string, so I changed that accordingly. - Fixes to StorytellerUserStory & StorytellerTests.UserStoryTests so they pass on both Silver02 and my localhost (which has the Agile pack installed).

--- blueprint-automationframework/Blueprint-Tests/Model/ArtifactModel/INovaArtifactBase.cs
+++ blueprint-automationframework/Blueprint-Tests/Model/ArtifactModel/INovaArtifactBase.cs
@@ -9,7 +9,7 @@ namespace Model.ArtifactModel
         #region Serialized JSON Properties
 
         int Id { get; set; }
-        int ItemTypeId { get; set; }
+        int? ItemTypeId { get; set; }
         string Name { get; set; }
         int ParentId { get; set; }
         int ProjectId { get; set; }

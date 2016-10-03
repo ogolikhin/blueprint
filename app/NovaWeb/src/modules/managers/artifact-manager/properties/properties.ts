76ff475407d8a5e1e16ce82c85018d94a83f5226
Albert Wong <albert.wong@blueprintsys.com>
2016-10-03 16:38:39 -0400

    Fixed problem where sometimes getting properties would mark the artifact as loaded when nothing has been loaded. Added flags to check if properties are initialized.

--- app/NovaWeb/src/modules/managers/artifact-manager/properties/properties.ts
+++ app/NovaWeb/src/modules/managers/artifact-manager/properties/properties.ts
@@ -9,6 +9,7 @@ export interface IArtifactProperties extends IDispose {
     set(id: number, value: any): Models.IPropertyValue;
     changes(): Models.IPropertyValue[];
     discard();
+    isLoaded: boolean;
 }
 
 export class ArtifactProperties implements IArtifactProperties  {
@@ -19,6 +20,7 @@ export class ArtifactProperties implements IArtifactProperties  {
     constructor(private statefulItem: IIStatefulItem, properties?: Models.IPropertyValue[]) {
         this.properties = properties || [];
         this.changeset = new ChangeSetCollector(statefulItem);
+        this._isLoaded = false;
 //        this.subject = new Rx.BehaviorSubject<Models.IPropertyValue>(null);
         // this.subject.subscribeOnNext((it: Models.IPropertyValue) => {
         //     this.addChangeSet(it);
@@ -28,6 +30,12 @@ export class ArtifactProperties implements IArtifactProperties  {
 
     public initialize(properties: Models.IPropertyValue[])  {
         this.properties = properties || [];
+        this._isLoaded = true;
+    }
+
+    private _isLoaded: boolean;
+    public get isLoaded(): boolean {
+        return this._isLoaded;
     }
 
     public dispose() {

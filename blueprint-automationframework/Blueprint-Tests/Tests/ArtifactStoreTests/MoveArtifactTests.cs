2464851856887fbdebbbbfd5b32dfb0c02a4f5dd
akapsh <alex.kapsh@blueprintsys.com>
2016-10-03 12:31:16 -0400

    Fixed review points

--- blueprint-automationframework/Blueprint-Tests/Tests/ArtifactStoreTests/MoveArtifactTests.cs
+++ blueprint-automationframework/Blueprint-Tests/Tests/ArtifactStoreTests/MoveArtifactTests.cs
@@ -123,7 +123,7 @@ namespace ArtifactStoreTests
             INovaArtifactDetails movedArtifactDetails = null;
 
             // Setup:
-            IArtifact artifact = Helper.CreateAndPublishArtifact(_project, _user, artifactType, numberOfVersions: 2);
+            IArtifact artifact = Helper.CreateAndPublishArtifact(_project, _user, artifactType, numberOfVersions: CURRENT_VERSION_OF_ARTIFACT);
             IArtifact newParentArtifact = Helper.CreateAndPublishArtifact(_project, _user, artifactType);
 
             artifact.Lock();
@@ -173,26 +173,18 @@ namespace ArtifactStoreTests
 
         #region 403 Forbidden tests
 
-        //        [Ignore(IgnoreReasons.UnderDevelopment)] //Not fixed yet
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="artifactType"></param>
         [TestCase(BaseArtifactType.Process)]
         [TestRail(182405)]
-        [Description("Create & publish 2 artifacts.  Each on ein different folder. Move the artifact to be a child of the other in different project. Verify returned code 409 Conflict.")]
+        [Description("Create & publish 2 artifacts.  Each one in different project. Move the artifact to be a child of the other in different project. Verify returned code 403 Forbidden.")]
         public void MoveArtifact_MoveOneArtifactToBeAChildOfAnotherInDifferentProject_403Forbidden(BaseArtifactType artifactType)
         {
             // Setup:
-            var projects = ProjectFactory.GetAllProjects(_user, shouldRetrievePropertyTypes: true);
+            var projects = ProjectFactory.GetProjects(_user, numberOfProjects: 2);
             Assert.GreaterOrEqual(projects.Count, 2, "This test requires at least 2 projects to exist!");
 
             IProject firstProject = projects[0];
             IProject secondProject = projects[1];
 
-            /* For interanl testing only. Will be removed after review approved 
-                        IProject CustomDataProject = ProjectFactory.GetProject(_user, "Custom Data");
-            */
             IArtifact artifact1 = Helper.CreateAndPublishArtifact(firstProject, _user, artifactType);
             IArtifact artifact2 = Helper.CreateAndPublishArtifact(secondProject, _user, artifactType);
 
@@ -209,7 +201,7 @@ namespace ArtifactStoreTests
         }
 
         [Ignore(IgnoreReasons.UnderDevelopment)] //Not implemented yet. There is no ability to test Baseline and Review. This will be tested manually
-        [TestCase(BaseArtifactType.Collection)]
+//        [TestCase(BaseArtifactType.Collection)]
         [TestRail(182408)]
         [Description("Create & publish 2 artifacts of unsupported artifact type. Move an artifact to be a child of the other one.   Verify returned code 409 Conflict.")]
         public void MoveArtifact_PublishedArtifactCannotBeMovedForUnsupportedArtifactTypes_403Forbidden(BaseArtifactType artifactType)
@@ -232,7 +224,7 @@ namespace ArtifactStoreTests
 
         [TestCase(BaseArtifactType.Process)]
         [TestRail(182416)]
-        [Description("Create & publish 2 artifacts. Move an artifact to be a child of the other one with user that does not have proper permissions.  Verify returned code 409 Conflict.")]
+        [Description("Create & publish 2 artifacts. Move an artifact to be a child of the other one with user that does not have proper permissions.  Verify returned code 403 Forbidden.")]
         public void MoveArtifact_PublishedArtifactCannotBeMovedForUserWithoutProperPermissions_403Forbidden(BaseArtifactType artifactType)
         {
             // Setup:
@@ -255,7 +247,7 @@ namespace ArtifactStoreTests
 
         [TestCase(BaseArtifactType.Process)]
         [TestRail(182424)]
-        [Description("Create & publish 2 artifacts. Move an artifact to be a child of the other one to which user does not have proper permissions.  Verify returned code 409 Conflict.")]
+        [Description("Create & publish 2 artifacts. Move an artifact to be a child of the other one to which user does not have proper permissions.  Verify returned code 403 Forbidden.")]
         public void MoveArtifact_PublishedArtifactCannotBeMovedToArtifactWhichUserDoesNotHaveProperPermissions_403Forbidden(BaseArtifactType artifactType)
         {
             // Setup:
@@ -305,11 +297,8 @@ namespace ArtifactStoreTests
             }
             finally
             {
-                var artifacts = new List<IArtifactBase>();
-                artifacts.Add(artifact1);
-
                 Helper.ArtifactStore.DeleteArtifact(artifact1, userWithoutPermissions);
-                Helper.ArtifactStore.PublishArtifacts(artifacts, userWithoutPermissions);
+                Helper.ArtifactStore.PublishArtifact(artifact1, userWithoutPermissions);
             }
         }
 
@@ -321,7 +310,7 @@ namespace ArtifactStoreTests
         [TestCase(BaseArtifactType.Process, int.MaxValue)]
         [TestRail(182429)]
         [Description("Create & publish an artifact. Move an artifact to be a child of the non existing artifact.  Verify returned code 404 Not Found.")]
-        public void MoveArtifact_CannotMoveArtifactToNotExistingOne_404NotFound(BaseArtifactType artifactType, int artifactId)
+        public void MoveArtifact_CannotMoveArtifactToNonExistingArtifact_404NotFound(BaseArtifactType artifactType, int artifactId)
         {
             // Setup:
             IArtifact artifact = Helper.CreateAndPublishArtifact(_project, _user, artifactType);
@@ -339,9 +328,9 @@ namespace ArtifactStoreTests
         }
 
         [TestCase(BaseArtifactType.Process)]
-        [TestRail(0)]
-        [Description("Create & publish 2 artifacts. Delete 2nd one. Move first artifact to be a child of second artifact.  Verify returned code 404 Not Found.")]
-        public void MoveArtifact_CannotMoveArtifactTosDeletedOne_404NotFound(BaseArtifactType artifactType)
+        [TestRail(182432)]
+        [Description("Create & publish second artifacts. Delete 2nd one. Move first artifact to be a child of second artifact.  Verify returned code 404 Not Found.")]
+        public void MoveArtifact_CannotMoveArtifactToDeletedArtifact_404NotFound(BaseArtifactType artifactType)
         {
             // Setup:
             IArtifact artifact1 = Helper.CreateAndPublishArtifact(_project, _user, artifactType);
@@ -349,6 +338,27 @@ namespace ArtifactStoreTests
 
             artifact2.Delete();
             artifact1.Lock();
+
+            // Execute:
+            var ex = Assert.Throws<Http404NotFoundException>(() => Helper.ArtifactStore.MoveArtifact(artifact1, artifact2, _user),
+                "'POST {0}' should return 404 Not Found when user tries to move artifact to one that does not exist", SVC_PATH);
+
+            // Verify:
+            string expectedExceptionMessage = "You have attempted to access an artifact that does not exist or has been deleted.";
+            Assert.That(ex.RestResponse.Content.Contains(expectedExceptionMessage),
+                "{0} when user tries to move an artifact to artifact that was removed", expectedExceptionMessage);
+        }
+
+        [TestCase(BaseArtifactType.Process)]
+        [TestRail(182394)]
+        [Description("Create & publish 2 artifacts. Delete first one. Move first artifact to be a child of second artifact.  Verify returned code 404 Not Found.")]
+        public void MoveArtifact_CannotMoveDeletedArtifactToArtifact_404NotFound(BaseArtifactType artifactType)
+        {
+            // Setup:
+            IArtifact artifact1 = Helper.CreateAndPublishArtifact(_project, _user, artifactType);
+            IArtifact artifact2 = Helper.CreateAndPublishArtifact(_project, _user, artifactType);
+
+            artifact1.Delete();
 
             // Execute:
             var ex = Assert.Throws<Http404NotFoundException>(() => Helper.ArtifactStore.MoveArtifact(artifact1, artifact2, _user),
@@ -386,27 +396,6 @@ namespace ArtifactStoreTests
             Assert.That(ex.RestResponse.Content.Contains(expectedExceptionMessage),
                 "{0} did not find version in returned message of move artifact call due to incorrect one sent with the request.", expectedExceptionMessage);
         }
-         
-        [TestCase(BaseArtifactType.Process)]
-        [TestRail(182394)]
-        [Description("Create & publish 2 artifacts.  Move one artifact to be a child of the other. Move parent to be a child of child. Send correct version of artifact with the message. Verify returned code 409 Conflict.")]
-        public void MoveArtifact_PublishedArtifactBecomesChildOfPublishedArtifact_MoveParentToBeAChildOfAChild_409Conflict(BaseArtifactType artifactType)
-        {
-            // Setup:
-            IArtifact parentArtifact = Helper.CreateAndPublishArtifact(_project, _user, artifactType);
-            IArtifact childArtifact = Helper.CreateAndPublishArtifact(_project, _user, artifactType, parent: parentArtifact);
-
-            parentArtifact.Lock();
-
-            // Execute:
-            var ex = Assert.Throws<Http409ConflictException>(() => Helper.ArtifactStore.MoveArtifact(parentArtifact, childArtifact, _user),
-                "'POST {0}' should return 409 Conflict when parent moved to its child", SVC_PATH);
-
-            // Verify:
-            string expectedExceptionMessage = "This move will result in a circular relationship between the artifact and its new parent.";
-            Assert.That(ex.RestResponse.Content.Contains(expectedExceptionMessage),
-                "{0} when user tries to move parent artifact to its child", expectedExceptionMessage);
-        }
 
         [TestCase(BaseArtifactType.Process)]
         [TestRail(182401)]
@@ -427,40 +416,32 @@ namespace ArtifactStoreTests
                 "{0} when user tries to move parent artifact to its child without previously locking it", expectedExceptionMessage);
         }
 
-        [TestCase(BaseArtifactType.Process)]
-        [TestRail(182403)]
-        [Description("Create & publish an artifact.  Move the artifact to be a child of itself. Verify returned code 409 Conflict.")]
-        public void MoveArtifact_MoveOneArtifactToBeAChildOfItself_409Conflict(BaseArtifactType artifactType)
+        [TestCase(BaseArtifactType.Process, 1)]
+        [TestCase(BaseArtifactType.Process, 2)]
+        [TestCase(BaseArtifactType.Process, 3)]
+        [TestRail(182406)]
+        [Description("Create & publish number of artifacts.  Move the first created artifact to be a child of one of its descendents. If one created it will be circular to itself. Verify returned code 409 Conflict.")]
+        public void MoveArtifact_CreateCircularDependency_409Conflict(BaseArtifactType artifactType, int numberOfArtifacts)
         {
             // Setup:
-            IArtifact artifact = Helper.CreateAndPublishArtifact(_project, _user, artifactType);
+            List<BaseArtifactType> artifactTypes = CreateListOfArtifactTypes(numberOfArtifacts, artifactType);
 
-            artifact.Lock();
+            var artifactList = Helper.CreatePublishedArtifactChain(_project, _user, artifactTypes.ToArray());
 
-            // Execute:
-            var ex = Assert.Throws<Http409ConflictException>(() => Helper.ArtifactStore.MoveArtifact(artifact, artifact, _user),
-                "'POST {0}' should return 409 Conflict when artifact moved to itself", SVC_PATH);
+            Assert.IsNotNull(artifactList, "Artifact List is not created");
 
-            // Verify:
-            string expectedExceptionMessage = "This move will result in a circular relationship between the artifact and its new parent.";
-            Assert.That(ex.RestResponse.Content.Contains(expectedExceptionMessage),
-                "{0} when user tries to move an artifact to itself", expectedExceptionMessage);
-        }
+            IArtifact firstArtifact = artifactList[0];
+            IArtifact lastArtifact = null ;
 
-        [TestCase(BaseArtifactType.Process)]
-        [TestRail(182406)]
-        [Description("Create & publish 3 artifacts.  Move the artifact to be a child of one of descendents. Verify returned code 409 Conflict.")]
-        public void MoveArtifact_MoveParentToOneOfDescendentArtifacts_409Conflict(BaseArtifactType artifactType)
-        {
-            // Setup: 
-            IArtifact grandParentArtifact = Helper.CreateAndPublishArtifact(_project, _user, artifactType);
-            IArtifact parentArtifact = Helper.CreateAndPublishArtifact(_project, _user, artifactType, grandParentArtifact);
-            IArtifact childArtifact = Helper.CreateAndPublishArtifact(_project, _user, artifactType, parentArtifact);
+            firstArtifact.Lock();
 
-            grandParentArtifact.Lock();
+            if (artifactList.Count == 1)
+                lastArtifact = firstArtifact;
+            else
+                lastArtifact = artifactList[artifactList.Count - 1];
 
             // Execute:
-            var ex = Assert.Throws<Http409ConflictException>(() => Helper.ArtifactStore.MoveArtifact(grandParentArtifact, childArtifact, _user),
+            var ex = Assert.Throws<Http409ConflictException>(() => Helper.ArtifactStore.MoveArtifact(firstArtifact, lastArtifact, _user),
                 "'POST {0}' should return 409 Conflict when artifact moved to one of its descendents", SVC_PATH);
 
             // Verify:
@@ -468,6 +449,29 @@ namespace ArtifactStoreTests
             Assert.That(ex.RestResponse.Content.Contains(expectedExceptionMessage),
                 "{0} when user tries to move an artifact to one of its descendents", expectedExceptionMessage);
         }
+
         #endregion 409 Conflict tests
+
+        #region private call
+
+        /// <summary>
+        /// Creates a list of artifact types.
+        /// </summary>
+        /// <param name="numberOfArtifacts">The number of artifact types to add to the list.</param>
+        /// <param name="artifactType">The artifact type.</param>
+        /// <returns>A list of artifact types.</returns>
+        private static List<BaseArtifactType> CreateListOfArtifactTypes(int numberOfArtifacts, BaseArtifactType artifactType)
+        {
+            List<BaseArtifactType> artifactTypes = new List<BaseArtifactType>();
+
+            for (int i = 0; i < numberOfArtifacts; i++)
+            {
+                artifactTypes.Add(artifactType);
+            }
+
+            return artifactTypes;
+        }
+
+        #endregion private call
     }
 }

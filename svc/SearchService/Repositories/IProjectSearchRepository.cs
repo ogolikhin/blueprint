1a3e69a1ac56484f75007abec236230552cec528
Anton Trinkunas <anton.trinkunas@blueprintsys.com>
2016-10-04 11:29:16 -0400

    US2613: Remove spaces

--- svc/SearchService/Repositories/IProjectSearchRepository.cs
+++ svc/SearchService/Repositories/IProjectSearchRepository.cs
@@ -6,6 +6,6 @@ namespace SearchService.Repositories
 {
     public interface IProjectSearchRepository
     {
-        Task<IEnumerable<ProjectSearchResult>> GetProjectsByName(int userId, string searchText, int resultCount);        
+        Task<IEnumerable<ProjectSearchResult>> GetProjectsByName(int userId, string searchText, int resultCount);
     }
 }

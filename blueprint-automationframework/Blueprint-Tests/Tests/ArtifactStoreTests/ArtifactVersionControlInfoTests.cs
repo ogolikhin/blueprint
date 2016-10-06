b99e5281a1f8bb4fed612b3f03e2ee9e50dd53c2
akapsh <alex.kapsh@blueprintsys.com>
2016-10-06 17:20:10 -0400

    Added sub-artifact section tests

--- blueprint-automationframework/Blueprint-Tests/Tests/ArtifactStoreTests/ArtifactVersionControlInfoTests.cs
+++ blueprint-automationframework/Blueprint-Tests/Tests/ArtifactStoreTests/ArtifactVersionControlInfoTests.cs
@@ -4,7 +4,9 @@ using Model;
 using Model.ArtifactModel;
 using Model.ArtifactModel.Impl;
 using Model.Factories;
+using Model.StorytellerModel.Impl;
 using NUnit.Framework;
+using System.Collections.Generic;
 using TestCommon;
 
 namespace ArtifactStoreTests
@@ -57,7 +59,7 @@ namespace ArtifactStoreTests
 
             artifactDetails.AssertEquals(artifactBaseInfo);
 
-            Assert.IsTrue(artifactBaseInfo.HasChanges.HasValue, "HasChanges property should be null");
+            Assert.IsTrue(artifactBaseInfo.HasChanges.HasValue, "HasChanges property should have value");
             Assert.IsFalse(artifactBaseInfo.HasChanges.Value, "HasChanges property should be false");
         }
 
@@ -114,14 +116,13 @@ namespace ArtifactStoreTests
         [TestCase(BaseArtifactType.Actor)]
         [TestRail(182500)]
         [Description("Create, publish & save an artifact.  Create manual trace to the artifact using another user. Verify another user gets basic artifact information with HasChanges flag set to true.")]
-        public void VersionControlInfo_CreateTraceForArtifactWithAnotherUser_ReturnsArtifactInfoWithHasChangesTrueForAnotherUser_200OK(BaseArtifactType artifactType)
+        public void VersionControlInfo_CreateTraceForArtifact_ReturnsArtifactInfoWithHasChangesTrue_200OK(BaseArtifactType artifactType)
         {
             //Setup
-            IUser anotherUser = Helper.CreateUserAndAuthenticate(TestHelper.AuthenticationTokenTypes.BothAccessControlAndOpenApiTokens);
-            IArtifact artifact = CreateArtifactWithTrace(artifactType, TraceDirection.From, anotherUser);
+            IArtifact artifact = CreateArtifactWithTrace(artifactType, TraceDirection.From, _user);
 
             //Execute
-            var basicArtifactInfo = GetArtifactBaseInfo(artifact.Id, anotherUser);
+            var basicArtifactInfo = GetArtifactBaseInfo(artifact.Id, _user);
 
             // Verify:
             var artifactDetails = Helper.ArtifactStore.GetArtifactDetails(_user, basicArtifactInfo.Id);
@@ -134,7 +135,7 @@ namespace ArtifactStoreTests
         [TestCase(BaseArtifactType.Actor)]
         [TestRail(182504)]
         [Description("Create, publish & save an artifact.  Create manual trace to the artifact with current user. Verify another user gets basic artifact information with HasChanges flag set to false.")]
-        public void VersionControlInfo_CreateTraceForArtifact_ReturnsArtifactInfoWithHasChangesTrueForAnotherUser_200OK(BaseArtifactType artifactType)
+        public void VersionControlInfo_CreateTraceForArtifactByDifferentUser_ReturnsArtifactInfoWithHasChangesTrue_200OK(BaseArtifactType artifactType)
         {
             //Setup
             IUser anotherUser = Helper.CreateUserAndAuthenticate(TestHelper.AuthenticationTokenTypes.BothAccessControlAndOpenApiTokens);
@@ -153,6 +154,133 @@ namespace ArtifactStoreTests
 
         #endregion Artifact Changes
 
+        #region Sub-Artifact
+
+        [TestCase(BaseArtifactType.UseCase)]
+        [TestCase(BaseArtifactType.Process)]
+        [TestRail(182512)]
+        [Description("Create & publish an artifact.  Add to this artifact sub-artifact & save. Verify user gets basic artifact information with subartifact Id.")]
+        public void VersionControlInfo_CreateAndSaveSubArtifactInPublishedArtifact_ReturnsArtifactInfo_200OK(BaseArtifactType artifactType)
+        {
+            // Setup:
+            IArtifact artifact = Helper.CreateAndPublishArtifact(_project, _user, artifactType);
+
+            List<INovaSubArtifact> subArtifacts = Helper.ArtifactStore.GetSubartifacts(_user, artifact.Id);
+
+            Assert.IsTrue(subArtifacts.Count > 0, "There is no sub-artifact in this artifact");
+            subArtifacts[0].DisplayName = "Sub-artifact";
+
+            artifact.Save();
+
+            // Execute
+            var basicArtifactInfo = GetArtifactBaseInfo(subArtifacts[0].Id, _user);
+
+            // Verify
+            Assert.IsNotNull(basicArtifactInfo.SubArtifactId, "There is no sub-artifact id in the returned basic artifact info responce");
+            Assert.AreEqual(subArtifacts[0].Id, basicArtifactInfo.SubArtifactId, "Sub-artifact Id in Basic Artifact info is different from Id of sub-artifact sent in get artifact base infor request");
+
+            Assert.IsTrue(basicArtifactInfo.HasChanges.HasValue, "HasChanges property should be null");
+            Assert.IsTrue(basicArtifactInfo.HasChanges.Value, "HasChanges property should be true");
+        }
+
+        [TestCase(BaseArtifactType.UseCase)]
+        [TestCase(BaseArtifactType.Process)]
+        [TestRail(182543)]
+        [Description("Create & save an artifact.  Add to this artifact sub-artifact & save. Verify user gets basic artifact information with subartifact Id.")]
+        public void VersionControlInfo_CreateAndSaveSubArtifactInSavedArtifact_ReturnsArtifactInfo_200OK(BaseArtifactType artifactType)
+        {
+            // Setup:
+            IArtifact artifact = Helper.CreateAndPublishArtifact(_project, _user, artifactType);
+
+            List<INovaSubArtifact> subArtifacts = Helper.ArtifactStore.GetSubartifacts(_user, artifact.Id);
+
+            Assert.IsTrue(subArtifacts.Count > 0, "There is no sub-artifact in this artifact");
+            subArtifacts[0].DisplayName = "Sub-artifact";
+
+            artifact.Save();
+
+            // Execute
+            var basicArtifactInfo = GetArtifactBaseInfo(subArtifacts[0].Id, _user);
+
+            // Verify
+            Assert.IsNotNull(basicArtifactInfo.SubArtifactId, "There is no sub-artifact id in the returned basic artifact info responce");
+            Assert.AreEqual(subArtifacts[0].Id, basicArtifactInfo.SubArtifactId, "Sub-artifact Id in Basic Artifact info is different from Id of sub-artifact sent in get artifact base infor request");
+
+            Assert.IsTrue(basicArtifactInfo.HasChanges.HasValue, "HasChanges property should be null");
+            Assert.IsTrue(basicArtifactInfo.HasChanges.Value, "HasChanges property should be true");
+        }
+
+        [TestCase(BaseArtifactType.UseCase)]
+        [TestCase(BaseArtifactType.Process)]
+        [TestRail(182544)]
+        [Description("Create & save an artifact.  Add to this artifact sub-artifact & save. Verify user gets basic artifact information with subartifact Id.")]
+        public void VersionControlInfo_CreateAndPublishSubArtifactInPublishedArtifact_ReturnsArtifactInfo_200OK(BaseArtifactType artifactType)
+        {
+            // Setup:
+            IArtifact artifact = Helper.CreateAndPublishArtifact(_project, _user, artifactType);
+
+            List<INovaSubArtifact> subArtifacts = Helper.ArtifactStore.GetSubartifacts(_user, artifact.Id);
+
+            Assert.IsTrue(subArtifacts.Count > 0, "There is no sub-artifact in this artifact");
+            subArtifacts[0].DisplayName = "Sub-artifact";
+
+            artifact.Save();
+            artifact.Publish();
+
+            // Execute
+            var basicArtifactInfo = GetArtifactBaseInfo(subArtifacts[0].Id, _user);
+
+            // Verify
+            var artifactDetails = Helper.ArtifactStore.GetArtifactDetails(_user, basicArtifactInfo.Id);
+            artifactDetails.AssertEquals(basicArtifactInfo);
+
+            Assert.IsNotNull(basicArtifactInfo.SubArtifactId, "There is no sub-artifact id in the returned basic artifact info responce");
+            Assert.AreEqual(subArtifacts[0].Id, basicArtifactInfo.SubArtifactId, "Sub-artifact Id in Basic Artifact info is different from Id of sub-artifact sent in get artifact base infor request");
+
+            Assert.IsTrue(basicArtifactInfo.HasChanges.HasValue, "HasChanges property should be null");
+            Assert.IsFalse(basicArtifactInfo.HasChanges.Value, "HasChanges property should be false");
+        }
+
+        [Ignore(IgnoreReasons.UnderDevelopment)] // Update artifact with sub-artifact is not implemented yet (Better to avoid artifact.Sae() in this test since Save changes also artifact description.
+        [Description("Create & save an artifact.  Add to this artifact sub-artifact & save. Verify user gets basic artifact information with subartifact Id.")]
+        public void VersionControlInfo_CreateAndSaveSubArtifactInPublishedArtifactForAnotherUser_ReturnsArtifactInfo_200OK()
+        {
+            // Setup:
+
+            // Create a Process artifact
+            var processArtifact = Helper.Storyteller.CreateAndSaveProcessArtifact(project: _project, user: _user);
+
+            // Get the process artifact
+            var process = Helper.Storyteller.GetProcess(_user, processArtifact.Id);
+
+            // Add UserTasks - iteration
+            var precondition = process.GetProcessShapeByShapeName(Process.DefaultPreconditionName);
+
+            // Find outgoing process link for precondition task
+            var processLink = process.GetOutgoingLinkForShape(precondition);
+
+            var userTask = process.AddUserAndSystemTask(processLink);
+
+            NovaArtifactDetails retrievedArtifact = Helper.ArtifactStore.GetArtifactDetails(_user, processArtifact.Id);
+            retrievedArtifact.Name = "Sub-artifact_" + retrievedArtifact.Name;
+            Artifact.UpdateArtifact(processArtifact, _user, retrievedArtifact, Helper.BlueprintServer.Address);
+
+            IUser anotherUser = Helper.CreateUserAndAuthenticate(TestHelper.AuthenticationTokenTypes.BothAccessControlAndOpenApiTokens);
+
+            // Execute
+            var basicArtifactInfo = GetArtifactBaseInfo(userTask.Id, anotherUser);
+
+            // Verify
+            var artifactDetails = Helper.ArtifactStore.GetArtifactDetails(_user, basicArtifactInfo.Id);
+            artifactDetails.AssertEquals(basicArtifactInfo);
+
+            Assert.IsNotNull(basicArtifactInfo.SubArtifactId, "There is no sub-artifact id in the returned basic artifact info responce");
+            Assert.AreEqual(userTask.Id, basicArtifactInfo.SubArtifactId, "Sub-artifact Id in Basic Artifact info is different from Id of sub-artifact sent in get artifact base infor request");
+
+            Assert.IsTrue(basicArtifactInfo.HasChanges.HasValue, "HasChanges property should be null");
+            Assert.IsFalse(basicArtifactInfo.HasChanges.Value, "HasChanges property should be false");
+        }
+
         // TODO: Unpublished Sub-artifact in published artifact.
         // TODO: Unpublished Sub-artifact in unpublished artifact.
         // TODO: Published Sub-artifact.
@@ -161,6 +289,7 @@ namespace ArtifactStoreTests
         // TODO: Published Sub-artifact in locked Artifact without changes for another user.
         // TODO: Published Sub-artifact in locked Artifact with changes for another user.
 
+        #endregion Sub-Artifact
         // TODO: Published deleted Artifact(- isDeleted= false).
         // TODO: Saved deleted Artifact.
         // TODO: Saved deleted Artifact for another user.
@@ -187,6 +316,13 @@ namespace ArtifactStoreTests
 
         #region private calls
 
+        /// <summary>
+        /// Creates two artifacts and adds trace by user specified in parameteres
+        /// </summary>
+        /// <param name="artifactType">Artifact type of artifacts to be created</param>
+        /// <param name="trace">Direction of trace</param>
+        /// <param name="user"></param>
+        /// <returns>Artifact with the trace</returns>
         private IArtifact CreateArtifactWithTrace(BaseArtifactType artifactType, TraceDirection trace, IUser user)
         {
             IArtifact sourceArtifact = Helper.CreateAndPublishArtifact(_project, user, artifactType);
@@ -198,6 +334,12 @@ namespace ArtifactStoreTests
             return sourceArtifact;
         }
 
+        /// <summary>
+        /// Gets Artifact Base Information from artifact with specific Id
+        /// </summary>
+        /// <param name="artifactId">The Id of artifact</param>
+        /// <param name="user">User who tries to get basic artifact information</param>
+        /// <returns>Basic artifact information</returns>
         private INovaVersionControlArtifactInfo GetArtifactBaseInfo(int artifactId, IUser user)
         {
             INovaVersionControlArtifactInfo artifactBaseInfo = null;

9bee313572b216974a1ab4a400f0ee7b4de131a2
munishsaini <munish.saini@blueprintsys.com>
2016-10-07 09:28:24 -0400

    Create base controller for search

---
+++ svc/SearchService/Controllers/BaseSearchController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Linq;
+using SearchService.Helpers;
+using SearchService.Models;
+using ServiceLibrary.Exceptions;
+using ServiceLibrary.Helpers;
+using ServiceLibrary.Models;
+
+namespace SearchService.Controllers
+{
+    public abstract class BaseSearchController : LoggableApiController
+    {
+        #region Protected
+
+        protected int ValidateAndExtractUserId()
+        {
+            // get the UserId from the session
+            var userId = GetUserId();
+            if (!userId.HasValue)
+            {
+                throw new AuthenticationException("Authorization is requried", ErrorCodes.UnauthorizedAccess);
+            }
+            return userId.Value;
+        }
+
+        protected void ValidateCriteria(ISearchCriteria searchCriteria)
+        {
+
+            if (!ModelState.IsValid || !ValidateSearchCriteria(searchCriteria))
+            {
+                throw new BadRequestException("Please provide correct search criteria", ErrorCodes.IncorrectSearchCriteria);
+            }
+        }
+
+        protected int GetPageSize(ISearchConfigurationProvider searchConfigurationProvider, int? requestedPageSize, int maxPageSize = Int32.MaxValue)
+        {
+            int searchPageSize = requestedPageSize.GetValueOrDefault(searchConfigurationProvider.PageSize);
+            if (searchPageSize <= 0)
+            {
+                searchPageSize = searchConfigurationProvider.PageSize;
+            }
+
+            if (searchPageSize > maxPageSize)
+            {
+                searchPageSize = maxPageSize;
+            }
+            return searchPageSize;
+        }
+
+        protected int GetStartCounter(int? requestedStartCounter, int minStartCounter, int defaultCounterValue)
+        {
+
+            int startCounter = requestedStartCounter.GetValueOrDefault(defaultCounterValue);
+            if (startCounter < minStartCounter)
+            {
+                startCounter = defaultCounterValue;
+            }
+            return startCounter;
+            //searchPage = page.HasValue && page.Value > 0 ? page.Value : 1;
+            //return userId;
+        }
+
+        protected bool ValidateSearchCriteria(ISearchCriteria searchCriteria, int minSearchQueryLimit = 1)
+        {
+            if (string.IsNullOrWhiteSpace(searchCriteria?.Query) ||
+                searchCriteria.Query.Trim().Length < minSearchQueryLimit ||
+                !searchCriteria.ProjectIds.Any())
+            {
+                return false;
+            }
+            return true;
+        }
+
+        #endregion
+
+        #region Private
+
+        private int? GetUserId()
+        {
+            object sessionValue;
+            if (!Request.Properties.TryGetValue(ServiceConstants.SessionProperty, out sessionValue))
+            {
+                return null;
+            }
+            var session = sessionValue as Session;
+            return session?.UserId;
+        }
+
+        #endregion
+    }
+}

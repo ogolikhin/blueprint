964ce9f971664e4e7b602e2e8c1dd03e8354bf3f
munishsaini <munish.saini@blueprintsys.com>
2016-10-11 15:46:35 -0400

    Modify client type value on change of value from header

--- app/NovaWeb/src/modules/editors/bp-process/components/diagram/viewmodel/process-viewmodel.ts
+++ app/NovaWeb/src/modules/editors/bp-process/components/diagram/viewmodel/process-viewmodel.ts
@@ -189,6 +189,12 @@ export class ProcessViewModel implements IProcessViewModel {
 
     public set processType(value: ProcessEnums.ProcessType) {
         this.propertyValues["clientType"].value = value;
+        const statefulArtifact : IStatefulArtifact = this.getStatefulArtifact();
+        //checkgin for specialProperties so that unit tests pass. 
+        //It is additional work to make the specialProperties work with our IProcess model in unit tests 
+        if (statefulArtifact && statefulArtifact.specialProperties){
+            statefulArtifact.specialProperties.set(Enums.PropertyTypePredefined.ClientType, value);
+        }        
     }
 
     public get isUserToSystemProcess(): boolean {
@@ -316,11 +322,12 @@ export class ProcessViewModel implements IProcessViewModel {
     public addStatefulShape(processShape: ProcessModels.IProcessShape) {
 
         let statefulShape = new StatefulProcessSubArtifact(this.process,
-            processShape, this.process.getServices());
-
-        let statefulArtifact: IStatefulArtifact = this.process; 
-
-        statefulArtifact.subArtifactCollection.add(statefulShape);
+            processShape, this.process.getServices());         
+        
+        const statefulArtifact : IStatefulArtifact = this.getStatefulArtifact();
+        if (statefulArtifact){
+            statefulArtifact.subArtifactCollection.add(statefulShape);
+        }
     }
      
     public addShape(processShape: ProcessModels.IProcessShape) {
@@ -337,8 +344,11 @@ export class ProcessViewModel implements IProcessViewModel {
     public removeStatefulShape(shapeId: number) {
         this.shapes = this.shapes.filter(shape => { return shape.id !== shapeId; });
         // cast process as an IStatefulArtifact 
-        let statefulArtifact: IStatefulArtifact = this.process;
-        statefulArtifact.subArtifactCollection.remove(shapeId);
+        
+        const statefulArtifact : IStatefulArtifact = this.getStatefulArtifact();
+        if (statefulArtifact){
+            statefulArtifact.subArtifactCollection.remove(shapeId);
+        }
     }
 
     public updateTree() {
@@ -491,5 +501,10 @@ export class ProcessViewModel implements IProcessViewModel {
             this.processGraphModel = null;
         }
 
+    }
+
+    private getStatefulArtifact() : IStatefulArtifact{
+        let statefulArtifact: IStatefulArtifact = this.process;
+        return statefulArtifact;
     }
 }

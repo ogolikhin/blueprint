f8326e151b049e073d2774eb6b51ee31b1cab63a
Cristian Poll <cristian@cristianpoll.com>
2016-10-12 10:11:41 -0400

    fix selection issue in refresh

--- app/NovaWeb/src/modules/main/components/bp-artifact-info/actions/refresh-action.ts
+++ app/NovaWeb/src/modules/main/components/bp-artifact-info/actions/refresh-action.ts
@@ -2,25 +2,25 @@ import {ILocalizationService} from "../../../../core";
 import {ILoadingOverlayService} from "../../../../core/loading-overlay";
 import {BPButtonAction} from "../../../../shared";
 import {IProjectManager} from "../../../../managers/project-manager";
-import {IArtifactManager} from "../../../../managers/artifact-manager";
+import {IStatefulArtifact} from "../../../../managers/artifact-manager";
 
 export class RefreshAction extends BPButtonAction {
     constructor(
+        artifact: IStatefulArtifact,
         localization: ILocalizationService,
         projectManager: IProjectManager,
-        artifactManager: IArtifactManager,
         loadingOverlayService: ILoadingOverlayService
     ) {
+        if (!artifact) {
+            throw new Error("Artifact not provided or is null");
+        }
+
         if (!localization) {
             throw new Error("Localization service not provided or is null");
         }
 
         if (!projectManager) {
             throw new Error("Project manager not provided or is null");
-        }
-
-        if (!artifactManager) {
-            throw new Error("Artifact manager not provided or is null");
         }
 
         if (!loadingOverlayService) {
@@ -31,9 +31,8 @@ export class RefreshAction extends BPButtonAction {
             (): void => {
                 //loading overlay
                 const overlayId = loadingOverlayService.beginLoading();
-                const currentArtifact = artifactManager.selection.getArtifact();
 
-                currentArtifact.refresh()
+                artifact.refresh()
                     .catch((error) => {
                         // We're not interested in the error type.
                         // sometimes this error is created by artifact.load(), which returns the statefulArtifact instead of an error object.
@@ -47,16 +46,15 @@ export class RefreshAction extends BPButtonAction {
                     });
             },
             (): boolean => {
-                const currentArtifact = artifactManager.selection.getArtifact();
-                if (!currentArtifact) {
+                if (!artifact) {
                     return false;
                 }
 
-                if (currentArtifact.artifactState.readonly) {
+                if (artifact.artifactState.readonly) {
                     return false;
                 }
 
-                if (currentArtifact.artifactState.dirty) {
+                if (artifact.artifactState.dirty) {
                     return false;
                 }
 

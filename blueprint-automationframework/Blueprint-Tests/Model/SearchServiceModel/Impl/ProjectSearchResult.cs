e5b13713c4703447a5bb7a4aeee02c4e710e564c
Bogdan Tkachenko <bogdan.tkachenko@blueprintsys.com>
2016-10-12 16:51:06 -0400

    add Path to ProjectSearchResult

--- blueprint-automationframework/Blueprint-Tests/Model/SearchServiceModel/Impl/ProjectSearchResult.cs
+++ blueprint-automationframework/Blueprint-Tests/Model/SearchServiceModel/Impl/ProjectSearchResult.cs
@@ -1,10 +1,15 @@
-﻿
+﻿using Newtonsoft.Json;
+
 namespace Model.SearchServiceModel.Impl
 {
     public class ProjectSearchResult
     {
+        [JsonProperty("id")]
         public int ProjectId { get; set; }
 
+        [JsonProperty("name")]
         public string ProjectName { get; set; }
+
+        public string Path { get; set; }
     }
 }

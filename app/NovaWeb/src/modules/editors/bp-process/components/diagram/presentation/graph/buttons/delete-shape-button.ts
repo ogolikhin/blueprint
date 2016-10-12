07f6cfb4b6b4399328d2d59c2184d3dd2628262f
munishsaini <munish.saini@blueprintsys.com>
2016-10-12 17:35:52 -0400

    Modify delete shapes. Added a Delete shape button and used it in decision and user task. Created unit test. Added shape for readonly delete

---
+++ app/NovaWeb/src/modules/editors/bp-process/components/diagram/presentation/graph/buttons/delete-shape-button.ts
@@ -0,0 +1,40 @@
+import {Button} from "../buttons/button";
+import {DiagramElement} from "../shapes/diagram-element";
+import {NodeFactorySettings} from "../shapes/node-factory-settings";
+
+export class DeleteShapeButton extends Button {
+    constructor(nodeId: string, 
+    width: number,
+    height: number, 
+    tooltip: string = null,     
+    nodeFactorySettings: NodeFactorySettings = null,
+    clickAction?: any){        
+        
+        super(`DS${nodeId}`, width, height, null);
+
+        const defaultClickAction = () => { };
+
+
+        this.setNeutralImage(this.getImageSource("delete-neutral.svg"))
+        this.setActiveImage(this.getImageSource("delete-active.svg"));
+        this.setHoverImage(this.getImageSource("delete-hover.svg"));
+        this.setDisabledImage(this.getImageSource("delete-mute.svg"));
+
+        if (nodeFactorySettings && nodeFactorySettings.isDeleteShapeEnabled) {
+            this.isEnabled = true;    
+            if (clickAction) {
+                this.setClickAction(clickAction);
+            }
+            else{
+                this.setClickAction(defaultClickAction);
+            }
+            
+        } else {            
+            this.disable();            
+            this.setClickAction(defaultClickAction);
+        }
+
+        this.setTooltip(tooltip);
+        
+    }
+}

95c15addec7338d836d240be883321adc29ada17
dhalexkim <dhalexkim@gmail.com>
2016-10-13 14:22:04 -0400

    Changes: - Fixed typos - Added different parameters for GetDiagram test cases

--- blueprint-automationframework/Blueprint-Tests/Tests/ArtifactStoreTests/LegacyArtifactHistoryTests.cs
+++ blueprint-automationframework/Blueprint-Tests/Tests/ArtifactStoreTests/LegacyArtifactHistoryTests.cs
@@ -39,13 +39,15 @@ namespace ArtifactStoreTests
 
         #region 200 OK Tests
 
-        [TestCase]
+        [TestCase(BaseArtifactType.DomainDiagram)]
+        [TestCase(BaseArtifactType.GenericDiagram)]
+        [TestCase(BaseArtifactType.UseCaseDiagram)]
         [TestRail(182934)]
         [Description("Create & publish a diagram artifact, Get DiagramArtifact.  Verify that the latest version of valid diagram artifact is returned.")]
-        public void GetDiagramArtifact_PublishAndGetDiagamArtifact_ReturnsLatestVersionOfDiagramArtifact()
+        public void GetDiagramArtifact_PublishAndGetDiagamArtifact_ReturnsLatestVersionOfDiagramArtifact(BaseArtifactType artifactType)
         {
             // Setup: Create and publish a diagram artifact
-            var publishedDiagramArtifact = Helper.CreateAndPublishArtifact(_project, _user, BaseArtifactType.GenericDiagram);
+            var publishedDiagramArtifact = Helper.CreateAndPublishArtifact(_project, _user, artifactType: artifactType);
             var retrievedArtifact = OpenApiArtifact.GetArtifact(publishedDiagramArtifact.Address, _project, publishedDiagramArtifact.Id, _user);
             NovaDiagramArtifact diagramArtifact = null;
 
@@ -64,11 +66,11 @@ namespace ArtifactStoreTests
         public void GetGlossaryArtifact_PublishAndGetGlossaryArtifact_ReturnsLatestVersionOfGlossaryArtifact()
         {
             // Setup: Create and publish a glossary artifact
-            var publishedGlossaryArtifact = Helper.CreateAndPublishArtifact(_project, _user, BaseArtifactType.Glossary);
+            var publishedGlossaryArtifact = Helper.CreateAndPublishArtifact(_project, _user, artifactType: BaseArtifactType.Glossary);
             var retrievedArtifact = OpenApiArtifact.GetArtifact(publishedGlossaryArtifact.Address, _project, publishedGlossaryArtifact.Id, _user);
             NovaGlossaryArtifact glossaryArtifact = null;
 
-            // Execute: Get the diagram artifact using GetGlossaryArtifact
+            // Execute: Get the glossary artifact using GetGlossaryArtifact
             Assert.DoesNotThrow(() => {
                 glossaryArtifact = Helper.ArtifactStore.GetGlossaryArtifact(_user, publishedGlossaryArtifact.Id);
             }, "'GET {0}' should return 200 OK when passed a valid artifact ID!", RestPaths.Svc.ArtifactStore.GLOSSARY_id_);
@@ -96,13 +98,15 @@ namespace ArtifactStoreTests
             ArtifactStoreHelper.AssertArtifactsEqual(usecaseArtifact, retrievedArtifact);
         }
 
-        [TestCase(4)]
+        [TestCase(2,BaseArtifactType.DomainDiagram)]
+        [TestCase(3,BaseArtifactType.GenericDiagram)]
+        [TestCase(4,BaseArtifactType.UseCaseDiagram)]
         [TestRail(183352)]
         [Description("Create & publish a diagram artifact multiple times to have multiple version of it, Get diagram artifact without version. Verify that latest version of artifact is returned.")]
-        public void GetDiagramArtifact_PublishAndGetDiagramArtifactWithoutSpecificVersion_ReturnsLatestVersionOfDiagramArtifact(int numberOfVersions)
+        public void GetDiagramArtifact_PublishAndGetDiagramArtifactWithoutSpecificVersion_ReturnsLatestVersionOfDiagramArtifact(int numberOfVersions, BaseArtifactType artifactType)
         {
             // Setup: Create and publish a diagram artifact multiple times to have multiple versions of it
-            var publishedDiagramArtifact = Helper.CreateAndPublishOpenApiArtifact(_project, _user, BaseArtifactType.GenericDiagram, numberOfVersions: numberOfVersions);
+            var publishedDiagramArtifact = Helper.CreateAndPublishOpenApiArtifact(_project, _user, artifactType: artifactType, numberOfVersions: numberOfVersions);
             // getting the latest version of the artifact using open API GetArtifact
             var retrievedArtifact = OpenApiArtifact.GetArtifact(publishedDiagramArtifact.Address, _project, publishedDiagramArtifact.Id, _user);
             NovaDiagramArtifact diagramArtifact = null;
@@ -147,7 +151,7 @@ namespace ArtifactStoreTests
             var retrievedArtifact = OpenApiArtifact.GetArtifact(publishedUseCaseArtifact.Address, _project, publishedUseCaseArtifact.Id, _user);
             NovaUseCaseArtifact usecaseArtifact = null;
 
-            // Execute: Get the diagram artifact using GetUseCaseArtifact without passing versionId parameter
+            // Execute: Get the use case artifact using GetUseCaseArtifact without passing versionId parameter
             Assert.DoesNotThrow(() => {
                 usecaseArtifact = Helper.ArtifactStore.GetUseCaseArtifact(_user, publishedUseCaseArtifact.Id);
             }, "'GET {0}' should return 200 OK when passed a valid artifact ID!", RestPaths.Svc.ArtifactStore.USECASE_id_);
@@ -156,13 +160,15 @@ namespace ArtifactStoreTests
             ArtifactStoreHelper.AssertArtifactsEqual(usecaseArtifact, retrievedArtifact);
         }
 
-        [TestCase]
+        [TestCase(BaseArtifactType.DomainDiagram)]
+        [TestCase(BaseArtifactType.GenericDiagram)]
+        [TestCase(BaseArtifactType.UseCaseDiagram)]
         [TestRail(183355)]
         [Description("Create & publish a diagram artifact, modify & publish it again, GetDiagramArtifact with versionId=1. Verify that first version of diagram artifact is returned.")]
-        public void GetDiagramArtifact_PublishAndGetDiagramArtifactWithVersion1_ReturnsFirstVersionOfDiagramArtifact()
+        public void GetDiagramArtifact_PublishAndGetDiagramArtifactWithVersion1_ReturnsFirstVersionOfDiagramArtifact(BaseArtifactType artifactType)
         {
             // Setup: Create and publish a diagram artifact two times to have two versions of it			
-            IArtifact publishedDiagramArtifact = Helper.CreateAndPublishArtifact(_project, _user, BaseArtifactType.GenericDiagram);
+            IArtifact publishedDiagramArtifact = Helper.CreateAndPublishArtifact(_project, _user, artifactType: artifactType);
             var retrievedArtifactVersion1 = OpenApiArtifact.GetArtifact(publishedDiagramArtifact.Address, _project, publishedDiagramArtifact.Id, _user);
 
             // These are internal properties used by automation, so OpenAPI doesn't set them for us.
@@ -256,14 +262,16 @@ namespace ArtifactStoreTests
 
         #region 401 Unauthorized Tests
 
-        [TestCase("")]
-        [TestCase("invalidTokenString")]
+
+        [TestCase("", BaseArtifactType.DomainDiagram)]
+        [TestCase("invalidTokenString", BaseArtifactType.GenericDiagram)]
+        [TestCase("0000000000000000-00", BaseArtifactType.UseCaseDiagram)]
         [TestRail(183033)]
         [Description("Create & publish a diagram artifact, Get DiagramArtifact with invalid token header.  Verify 401 Unauthorized.")]
-        public void GetDiagramArtifact_PublishAndGetDiagamArtifactWithInvalidTokenHeader_401Unauthorized(string token)
+        public void GetDiagramArtifact_PublishAndGetDiagamArtifactWithInvalidTokenHeader_401Unauthorized(string token, BaseArtifactType artifactType)
         {
             // Setup: Create and publish a diagram artifact
-            var publishedDiagramArtifact = Helper.CreateAndPublishArtifact(_project, _user, BaseArtifactType.GenericDiagram);
+            var publishedDiagramArtifact = Helper.CreateAndPublishArtifact(_project, _user, artifactType: artifactType);
             NovaDiagramArtifact diagramArtifact = null;
 
             IUser userWithBadOrMissingToken = UserFactory.CreateUserAndAddToDatabase();
@@ -317,15 +325,15 @@ namespace ArtifactStoreTests
 
         #region 404 Not Found Tests
 
-        [TestCase(0)]
-        [TestCase(-10)]
-        [TestCase(999)]
+        [TestCase(0, BaseArtifactType.DomainDiagram)]
+        [TestCase(-10, BaseArtifactType.GenericDiagram)]
+        [TestCase(999, BaseArtifactType.UseCaseDiagram)]
         [TestRail(183027)]
         [Description("Create & publish a diagram artifact, Get DiagramArtifact with invalid versionId.  Verify 404 NotFound.")]
-        public void GetDiagramArtifact_PublishAndGetDiagamArtifactWithInvalidVersionId_404NotFound(int versionId)
+        public void GetDiagramArtifact_PublishAndGetDiagamArtifactWithInvalidVersionId_404NotFound(int versionId, BaseArtifactType artifactType)
         {
             // Setup: Create and publish a diagram artifact
-            var publishedDiagramArtifact = Helper.CreateAndPublishArtifact(_project, _user, BaseArtifactType.GenericDiagram);
+            var publishedDiagramArtifact = Helper.CreateAndPublishArtifact(_project, _user, artifactType: artifactType);
             NovaDiagramArtifact diagramArtifact = null;
 
             // Execute: Get the diagram artifact with invalid versionId using GetDiagramArtifact

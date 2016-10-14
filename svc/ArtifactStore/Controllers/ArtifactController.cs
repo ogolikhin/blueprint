367abafeee8a0182bdacb8f563c637d1d968565e
Alex Folomechine <alex.folomechine@blueprintsys.com>
2016-10-14 12:53:05 -0400

    Extended the Artifact controller and repository for Get Artifact Navigation Path web call, added some unit tests, user story 2776

--- svc/ArtifactStore/Controllers/ArtifactController.cs
+++ svc/ArtifactStore/Controllers/ArtifactController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Web.Http;
 using ArtifactStore.Models;
@@ -130,6 +131,27 @@ namespace ArtifactStore.Controllers
 
             var session = Request.Properties[ServiceConstants.SessionProperty] as Session;
             return await ArtifactRepository.GetExpandedTreeToArtifactAsync(projectId, expandedToArtifactId, includeChildren, session.UserId);
+        }
+
+        /// <summary>
+        /// Get the artifact navigation path, basic information of the artifact ancestors including the project.
+        /// </summary>
+        /// <remarks>
+        /// Returns the artifact navigation path, basic information of the artifact ancestors including the project.
+        /// </remarks>
+        /// <response code="200">OK.</response>
+        /// <response code="400">Bad Request. The session token is missing or malformed.</response>
+        /// <response code="401">Unauthorized. The session token is invalid.</response>
+        /// <response code="403">Forbidden. The user does not have permissions for the artifact.</response>
+        /// <response code="404">Not found. A artifact for the specified id is not found, does not exist or is deleted.</response>
+        /// <response code="500">Internal Server Error. An error occurred.</response>
+        [HttpGet, NoCache]
+        [Route("artifacts/{artifactId:int:min(1)}/navigationPath"), SessionRequired]
+        [ActionName("GetArtifactNavigationPath")]
+        public async Task<List<Artifact>> GetArtifactNavigationPathAsync(int artifactId)
+        {
+            var session = Request.Properties[ServiceConstants.SessionProperty] as Session;
+            return await ArtifactRepository.GetArtifactNavigatioPathAsync(artifactId, session.UserId);
         }
     }
 }

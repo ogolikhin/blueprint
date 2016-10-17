15298057e6dc96f99d9098a567ef2586efc4fbe9
Pavlo Pochapsky <pavlo.pochapsky@blueprintsys.com>
2016-10-17 18:52:49 -0400

    US1217: added item-info ng service, added SCSS

---
+++ app/NovaWeb/src/modules/core/navigation/item-info.svc.ts
@@ -0,0 +1,79 @@
+import * as angular from "angular";
+import { IUserGroup } from "../../main/models/models";
+
+export interface IItemInfoService {
+    get(id: number): ng.IPromise<any>;
+    isProject(item: IItemInfoResult): boolean;
+    isSubArtifact(item: IItemInfoResult): boolean;
+    isArtifact(item: IItemInfoResult): boolean;
+}
+
+export interface IItemInfoResult {
+  id: number;
+  subArtifactId: number;
+  name: string;
+  projectId: number;
+  parentId: number;
+  itemTypeId: number;
+  prefix: string;
+  predefinedType: number;
+  version: number;
+  versionCount: number;
+  isDeleted: boolean;
+  hasChanges: boolean;
+  orderIndex: number;
+  permissions: number;
+  lockedByUser: IUserGroup;
+  lockedDateTime: Date;
+  deletedByUser: IUserGroup;
+  deletedDateTime: Date;
+}
+
+export class ItemInfoService implements IItemInfoService {
+    public static $inject: [string] = [
+        "$q",
+        "$http"
+    ];
+
+    constructor(private $q: ng.IQService,
+                private $http: ng.IHttpService) {
+    }
+
+    public get(id: number, timeout?: ng.IPromise<void>): ng.IPromise<IItemInfoResult> {
+        const defer = this.$q.defer<IItemInfoResult>();
+        const request: ng.IRequestConfig = {
+            url: `/svc/artifactstore/artifacts/versionControlInfo/${id}`,
+            method: "GET",
+            timeout: timeout
+        };
+
+        this.$http(request).then(
+            (result: ng.IHttpPromiseCallbackArg<IItemInfoResult>) => defer.resolve(result.data),
+            (errResult: ng.IHttpPromiseCallbackArg<any>) => {
+                if (!errResult) {
+                    defer.reject();
+                    return;
+                }
+                const error = {
+                    statusCode: errResult.status,
+                    message: (errResult.data ? errResult.data.message : "")
+                };
+                defer.reject(error);
+            }
+        );
+
+        return defer.promise;
+    }
+
+    public isProject(item: IItemInfoResult): boolean {
+        return item.id === item.projectId;
+    }
+
+    public isArtifact(item: IItemInfoResult): boolean {
+        return !item.subArtifactId && item.id !== item.projectId;
+    }
+
+    public isSubArtifact(item: IItemInfoResult): boolean {
+        return !!item.subArtifactId;
+    }
+}

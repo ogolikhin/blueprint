cdc81d9b73e9edd42b48b9d2da8457e86827b693
Anton Trinkunas <anton.trinkunas@blueprintsys.com>
2016-10-17 15:03:07 -0400

    US 2864: [Nova] [Collection] Limit artifact picker to current project. Implementation

--- app/NovaWeb/src/modules/editors/configuration/types/actor-inheritance/actor-inheritance.ts
+++ app/NovaWeb/src/modules/editors/configuration/types/actor-inheritance/actor-inheritance.ts
@@ -79,7 +79,8 @@ export class BPFieldInheritFromController extends BPFieldBaseController {
 
             const dialogData: IArtifactPickerOptions = {
                 selectableItemTypes: [Models.ItemTypePredefined.Actor],
-                showSubArtifacts: false
+                showSubArtifacts: false,
+                isOneProjectLevel: true
             };
 
             dialogService.open(dialogSettings, dialogData).then((items: Models.IItem[]) => {

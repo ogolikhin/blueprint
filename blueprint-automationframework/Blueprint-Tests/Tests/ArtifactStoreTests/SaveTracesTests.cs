223199e3e07159b855b6deab253f308cac3d8f75
Bogdan Tkachenko <bogdan.tkachenko@blueprintsys.com>
2016-10-19 16:59:26 -0400

    mark Explicit test with unknown error

--- blueprint-automationframework/Blueprint-Tests/Tests/ArtifactStoreTests/SaveTracesTests.cs
+++ blueprint-automationframework/Blueprint-Tests/Tests/ArtifactStoreTests/SaveTracesTests.cs
@@ -172,6 +172,7 @@ namespace ArtifactStoreTests
         }
 
         [TestCase]
+        [Explicit(IgnoreReasons.TestBug)]//it gives a bug now
         [TestRail(183605)]
         [Description("Create trace between 2 SubArtifact, Artifact and other Artifacts, check that operation throw no errors.")]
         public void AddTrace_Between2SubArtifactsArtifactAndArtifact_TraceHasExpectedValue()

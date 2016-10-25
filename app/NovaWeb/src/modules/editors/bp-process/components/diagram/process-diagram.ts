b33c94396530774e0f27ec44ca8d0aa2ce657f81
munishsaini <munish.saini@blueprintsys.com>
2016-10-25 17:55:57 -0400

    revert change for resetting id generator

--- app/NovaWeb/src/modules/editors/bp-process/components/diagram/process-diagram.ts
+++ app/NovaWeb/src/modules/editors/bp-process/components/diagram/process-diagram.ts
@@ -133,9 +133,6 @@ export class ProcessDiagram {
                                selectedNodeId: number = undefined) {
 
         try {
-            if (this.shapesFactory) {
-                this.shapesFactory.reset();
-            }
 
             this.graph = new ProcessGraph(
                 this.$rootScope,

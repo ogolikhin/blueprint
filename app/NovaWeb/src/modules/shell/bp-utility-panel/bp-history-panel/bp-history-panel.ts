ece259da14a31554327c5587d21aa47de9fb37e6
Cristian Poll <cristian@cristianpoll.com>
2016-10-27 17:13:54 -0400

    fix bad method return

--- app/NovaWeb/src/modules/shell/bp-utility-panel/bp-history-panel/bp-history-panel.ts
+++ app/NovaWeb/src/modules/shell/bp-utility-panel/bp-history-panel/bp-history-panel.ts
@@ -91,6 +91,8 @@ export class BPHistoryPanelController extends BPBaseUtilityPanelController {
                         this.onSelectionChangedHelper(artif, null, timeout);
                     }));
         }
+
+        return this.$q.resolve();
     }
 
     private onSelectionChangedHelper = (artifact: IStatefulArtifact, subArtifact: IStatefulSubArtifact, timeout: ng.IPromise<void>): ng.IPromise<any> => {

32860684db359378033478a9d0d3564b8d560e72
Bogdan Tkachenko <bogdan.tkachenko@blueprintsys.com>
2016-10-30 17:43:03 -0400

    update SearchItem

--- blueprint-automationframework/Blueprint-Tests/Model/SearchServiceModel/Impl/ItemSearchResult.cs
+++ blueprint-automationframework/Blueprint-Tests/Model/SearchServiceModel/Impl/ItemSearchResult.cs
@@ -12,28 +12,63 @@ namespace Model.SearchServiceModel.Impl
     public class SearchItem
     {
         public int Id { get; set; }
+
+        public bool ShouldSerializeId()
+        {
+            return (Id != 0);
+        }
+
         public int? ProjectId { get; set; }
+
         public int ParentId { get; set; }
+
+        public bool ShouldSerializeParentId()
+        {
+            return (ParentId != 0);
+        }
+
         public int ItemTypeId { get; set; }
-        public string Prefix { get; set; }
-        public int PredefinedType { get; set; }
-        public int Version { get; set; }
-        public double OrderIndex { get; set; }
-        public int ItemId { get; set; }
-
-        //public int? ArtifactId { get; set; }
-
-        
-        //public string TypeName { get; set; }
-
-        public string TypePrefix { get; set; }
-
-        public string Name { get; set; }
 
         public bool ShouldSerializeItemTypeId()
         {
             return (ItemTypeId != 0);
         }
+
+        public string Prefix { get; set; }
+
+        public int PredefinedType { get; set; }
+
+        public bool ShouldSerializePredefinedType()
+        {
+            return (PredefinedType != 0);
+        }
+
+        public int Version { get; set; }
+
+        public bool ShouldSerializeVersion()
+        {
+            return (Version != 0);
+        }
+
+        public double OrderIndex { get; set; }
+
+        public bool ShouldSerializeOrderIndex()
+        {
+            return (OrderIndex != 0);
+        }
+
+        public int Permissions { get; set; }
+
+        public bool ShouldSerializePermissions()
+        {
+            return (Permissions != 0);
+        }
+
+        public int ItemId { get; set; }
+
+        public string TypePrefix { get; set; }
+
+        public string Name { get; set; }
 
         public string Path { get; set; }
     }

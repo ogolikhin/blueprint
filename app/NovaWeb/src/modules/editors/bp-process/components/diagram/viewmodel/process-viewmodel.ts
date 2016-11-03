56b81cf5e4259788bcb0de50515227009dce6b27
CRichards <colin.richards@blueprintsys.com>
2016-11-03 13:41:23 -0400

    force $digest cycle to show message

--- app/NovaWeb/src/modules/editors/bp-process/components/diagram/viewmodel/process-viewmodel.ts
+++ app/NovaWeb/src/modules/editors/bp-process/components/diagram/viewmodel/process-viewmodel.ts
@@ -36,7 +36,7 @@ export class ProcessViewModel implements IProcessViewModel {
     private DEFAULT_SHAPE_LIMIT: number = 100;
 
     private _rootScope: any = null;
-    private _scope: any = null;
+    private _scope: ng.IScope = null;
     private _messageService: IMessageService = null;
     private processGraphModel: IProcessGraphModel = null;
     private _licenseType: Enums.LicenseTypeEnum;
@@ -200,8 +200,9 @@ export class ProcessViewModel implements IProcessViewModel {
         const message = new Message(messageType, messageText);
 
         if (message && this._messageService) {
-            //this._messageService.clearMessages();
             this._messageService.addMessage(message);
+            // force $digest cycle to show message
+            this._scope.$apply();
         }
     }
 

dae8504faf32e4c798f4e2e93e41439007f91135
David He <david.he@blueprintsys.com>
2016-11-04 16:21:48 -0400

    disregard last commit. added it to the wrong line

--- app/NovaWeb/src/modules/shell/bp-utility-panel/bp-relationships-panel/bp-artifact-relationship-item/bp-artifact-relationship-item.ts
+++ app/NovaWeb/src/modules/shell/bp-utility-panel/bp-relationships-panel/bp-artifact-relationship-item/bp-artifact-relationship-item.ts
@@ -75,7 +75,7 @@ export class BPArtifactRelationshipItemController implements IBPArtifactRelation
         if (!this.expanded) {
             this.getRelationshipDetails(this.artifact.artifactId)
                 .then(relationshipExtendedInfo => {
-                    if (relationshipExtendedInfo.pathToProject[0].parentId === null) {
+                    if (relationshipExtendedInfo.pathToProject.length > 0 && relationshipExtendedInfo.pathToProject[0].parentId === null) {
                         relationshipExtendedInfo.pathToProject.shift(); // do not show project in the path.
                     }
                     this.relationshipExtendedInfo = relationshipExtendedInfo;
@@ -137,7 +137,7 @@ export class BPArtifactRelationshipItemController implements IBPArtifactRelation
     private getRelationshipDetails(artifactId: number): ng.IPromise<Relationships.IRelationshipExtendedInfo> {
         return this.relationshipDetailsService.getRelationshipDetails(artifactId)
             .then((relationshipExtendedInfo: Relationships.IRelationshipExtendedInfo) => {
-                if (relationshipExtendedInfo.pathToProject.length > 0 && relationshipExtendedInfo.pathToProject[0].parentId === 0) {
+                if (relationshipExtendedInfo.pathToProject[0].parentId === 0) {
                     this.artifact.projectId = relationshipExtendedInfo.pathToProject[0].itemId;
                     this.artifact.projectName = relationshipExtendedInfo.pathToProject[0].itemName;
                 }

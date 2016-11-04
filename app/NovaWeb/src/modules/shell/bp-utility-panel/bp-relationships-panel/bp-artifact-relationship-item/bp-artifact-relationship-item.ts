e436b8876f58bc79fd5096b3c073801f5e612357
David He <david.he@blueprintsys.com>
2016-11-04 16:37:01 -0400

    double equals null check for parentId

--- app/NovaWeb/src/modules/shell/bp-utility-panel/bp-relationships-panel/bp-artifact-relationship-item/bp-artifact-relationship-item.ts
+++ app/NovaWeb/src/modules/shell/bp-utility-panel/bp-relationships-panel/bp-artifact-relationship-item/bp-artifact-relationship-item.ts
@@ -75,7 +75,7 @@ export class BPArtifactRelationshipItemController implements IBPArtifactRelation
         if (!this.expanded) {
             this.getRelationshipDetails(this.artifact.artifactId)
                 .then(relationshipExtendedInfo => {
-                    if (relationshipExtendedInfo.pathToProject.length > 0 && relationshipExtendedInfo.pathToProject[0].parentId === null) {
+                    if (relationshipExtendedInfo.pathToProject.length > 0 && relationshipExtendedInfo.pathToProject[0].parentId == null) {
                         relationshipExtendedInfo.pathToProject.shift(); // do not show project in the path.
                     }
                     this.relationshipExtendedInfo = relationshipExtendedInfo;

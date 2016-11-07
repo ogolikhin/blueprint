eb12170f1858f01a3a86970637386a4ac62653fb
David He <david.he@blueprintsys.com>
2016-11-07 14:25:46 -0500

    add logic to publish artifact and lauch create rapid review

--- app/NovaWeb/src/modules/editors/bp-collection/actions/rapid-review-action.ts
+++ app/NovaWeb/src/modules/editors/bp-collection/actions/rapid-review-action.ts
@@ -2,35 +2,40 @@ import {BPButtonAction} from "../../../shared";
 import {ILocalizationService} from "../../../core";
 import {ItemTypePredefined} from "../../../main/models/enums";
 import {IStatefulCollectionArtifact} from "../collection-artifact";
+import {IDialogService} from "../../../shared";
 
 export class RapidReviewAction extends BPButtonAction {
     constructor(artifact: IStatefulCollectionArtifact,
-        localization: ILocalizationService) {
+        localization: ILocalizationService,
+        dialogService: IDialogService
+        ) {
         if (!localization) {
             throw new Error("Localization service not provided or is null");
         }
 
         super(
             (): void => {
-                let a = 5;
+                if (!artifact.artifactState.published) {
+                    dialogService.confirm("Please publish your changes before entering the review. Would you like to proceed?").then( () => {
+                        artifact.publish().then(() => {
+                            let url = `Web/#/RapidReview/${artifact.id}/edit`;
+                            window.open(url);
+                        });
+                    });
+                } else {
+                    let url = `Web/#/RapidReview/${artifact.id}/edit`;
+                    window.open(url);
+                }
             },
             (): boolean => {
-                if (!artifact) {
-                    return false;
+                if (
+                    !artifact 
+                    || artifact.predefinedType !== ItemTypePredefined.ArtifactCollection
+                    || artifact.artifactState.readonly 
+                    || artifact.rapidReviewCreated
+                    ) {
+                        return false;
                 }
-
-                if (artifact.predefinedType !== ItemTypePredefined.ArtifactCollection) {
-                    return false;
-                }
-
-                if (artifact.artifactState.readonly) {
-                    return false;
-                }
-
-                if (artifact.rapidReviewCreated) {
-                    return false;
-                }
-
                 return true;
             },
             undefined,

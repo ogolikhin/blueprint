0f66a3f7286dfd4c1578d9db71531fb4e87183cc
Anton Trinkunas <anton.trinkunas@blueprintsys.com>
2016-11-07 10:49:54 -0500

    Trello bug fix: https://trello.com/c/a8Q593F6 Remove AP button from breadcrumb

--- app/NovaWeb/src/modules/main/components/bp-page-content/bp-page-content.ts
+++ app/NovaWeb/src/modules/main/components/bp-page-content/bp-page-content.ts
@@ -1,35 +1,3 @@
-import {ArtifactPickerDialogController, IArtifactPickerOptions} from "../../../main/components/bp-artifact-picker";
-import {IDialogService, IDialogSettings} from "../../../shared/widgets/bp-dialog/bp-dialog";
-
 export class PageContent implements ng.IComponentOptions {
-    public template: string = require("./bp-page-content.html");
-    public controller: ng.Injectable<ng.IControllerConstructor> = PageContentCtrl;
-}
-
-class PageContentCtrl {
-    private subscribers: Rx.IDisposable[];
-
-    public static $inject: [string] = [
-        "dialogService"
-    ];
-
-    constructor(private dialogService: IDialogService) {
-    }
-
-    public openArtifactPicker() {
-        const dialogSettings = <IDialogSettings>{
-            okButton: "Open",
-            template: require("../../../main/components/bp-artifact-picker/bp-artifact-picker-dialog.html"),
-            controller: ArtifactPickerDialogController,
-            css: "nova-open-project",
-            header: "Single project Artifact picker"
-        };
-
-        const dialogData: IArtifactPickerOptions = {
-            showSubArtifacts: false,
-            isOneProjectLevel: true
-        };
-
-        this.dialogService.open(dialogSettings, dialogData);
-    }
+    public template: string = require("./bp-page-content.html"); 
 }

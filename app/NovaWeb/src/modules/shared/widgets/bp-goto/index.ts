2f325e520034dba2cdf2999ea400456869d134f9
Jacob Harasimo <jacobharasimo@gmail.com>
2016-11-07 21:07:08 -0500

    tighten dependancy

--- app/NovaWeb/src/modules/shared/widgets/bp-goto/index.ts
+++ app/NovaWeb/src/modules/shared/widgets/bp-goto/index.ts
@@ -3,5 +3,5 @@ require("./bp-goto.scss");
 import * as angular from "angular";
 import { BPGotoComponent } from "./bp-goto";
 
-angular.module("bp.widgets.goto", ["bp.widgets.filtered-input", "bp.core"])
+angular.module("bp.widgets.goto", ["bp.widgets.filtered-input", "bp.core.navigation"])
     .component("bpGoto", new BPGotoComponent());

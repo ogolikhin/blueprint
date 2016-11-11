56bac6db0b8428eff904b7e3e85527b2c5de614e
munishsaini <munish.saini@blueprintsys.com>
2016-11-11 12:34:03 -0500

    Set saerch timeout to 2 minutes. Fix bug https://trello.com/c/nX59PSHI

---
+++ svc/lib/ServiceLibrary/Helpers/AppSettingsHelper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Configuration;
+using System.Globalization;
+
+namespace ServiceLibrary.Helpers
+{
+    public class AppSettingsHelper
+    {
+        public static T TryGetConfigurationValue<T>(string appSettingsKey, T defaultValue = default(T))
+        {
+
+            var appSettingValue = ConfigurationManager.AppSettings[appSettingsKey];
+
+            if (appSettingValue == null)
+            {
+                return defaultValue;
+            }
+
+            var value = (T)Convert.ChangeType(appSettingValue, typeof(T), CultureInfo.InvariantCulture);
+
+            if (value == null)
+            {
+                return defaultValue;
+            }
+            return value;
+        }
+    }
+}

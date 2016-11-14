3420be3a0dbf1a90a677d1333cebf67c6c59021d
Pavlo Pochapsky <Pavlo.Pochapsky@blueprintsys.com>
2016-11-14 17:35:48 -0500

    us784: adding favicon to the app

--- app/NovaWeb/webpack/loaders.js
+++ app/NovaWeb/webpack/loaders.js
@@ -39,6 +39,10 @@ module.exports = [
         exclude: /node_modules/,
         loader: 'url'
     }, {
+        test: /\.ico$/,
+        exclude: /node_modules/,
+        loader: 'url'
+    }, {
         test: require.resolve('tinymce/tinymce'),
         loaders: [
           'imports?this=>window',

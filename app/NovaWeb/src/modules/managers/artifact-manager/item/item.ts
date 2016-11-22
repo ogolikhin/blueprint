1f4e236adbe6908b28290a3cf0c493399712770c
Michael Talis <Michael.Talis@blueprintsys.com>
2016-11-21 23:26:35 -0500

    Fixed Bug 3663

--- app/NovaWeb/src/modules/managers/artifact-manager/item/item.ts
+++ app/NovaWeb/src/modules/managers/artifact-manager/item/item.ts
@@ -393,6 +393,11 @@ export abstract class StatefulItem implements IIStatefulItem {
                     if (propertyValue) {
                         value = propertyValue.value;
                     }
+
+                    if (!_.isBoolean(propertyType.isValidated)) {
+                        propertyType.isValidated = true;
+                    }
+                    
                     break;
                 case Enums.PropertyLookupEnum.Special:
                     propertyValue = this.specialProperties.get(propertyType.modelPropertyName as number);

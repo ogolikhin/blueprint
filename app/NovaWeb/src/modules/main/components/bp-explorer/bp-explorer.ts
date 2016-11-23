bde63d17c3ca5e6be454fd1a899e0991d2186e24
Anton Trinkunas <anton.trinkunas@blueprintsys.com>
2016-11-23 15:14:10 -0500

    US 3489: Open project implemented

--- app/NovaWeb/src/modules/main/components/bp-explorer/bp-explorer.ts
+++ app/NovaWeb/src/modules/main/components/bp-explorer/bp-explorer.ts
@@ -1,4 +1,4 @@
-import {TreeModels} from "../../models";
+import {TreeModels, AdminStoreModels} from "../../models";
 import {Helper} from "../../../shared";
 import {IProjectManager, IArtifactManager} from "../../../managers";
 import {IItemChangeSet} from "../../../managers/artifact-manager";
@@ -6,6 +6,9 @@ import {ISelectionManager} from "../../../managers/selection-manager";
 import {INavigationService} from "../../../core/navigation/navigation.svc";
 import {IMessageService} from "../../../core/messages/message.svc";
 import {IBPTreeViewControllerApi, IColumn, IColumnRendererParams} from "../../../shared/widgets/bp-tree-view";
+import {IProjectService} from "../../../managers/project-manager/project-service";
+import {ILoadingOverlayService} from "../../../core/loading-overlay/loading-overlay.svc";
+import {IAnalyticsProvider} from "../analytics/analyticsProvider";
 
 export class ProjectExplorer implements ng.IComponentOptions {
     public template: string = require("./bp-explorer.html");
@@ -34,7 +37,10 @@ export class ProjectExplorerController implements IProjectExplorerController {
         "artifactManager",
         "navigationService",
         "selectionManager",
-        "messageService"
+        "messageService",
+        "projectService",
+        "loadingOverlayService",
+        "analytics"
     ];
 
     constructor(private $q: ng.IQService,
@@ -42,7 +48,10 @@ export class ProjectExplorerController implements IProjectExplorerController {
                 private artifactManager: IArtifactManager,
                 private navigationService: INavigationService,
                 private selectionManager: ISelectionManager,
-                private messageService: IMessageService) {
+                private messageService: IMessageService,
+                private projectService: IProjectService,
+                private loadingOverlayService: ILoadingOverlayService,
+                private analytics: IAnalyticsProvider) {
     }
 
     //all subscribers need to be created here in order to unsubscribe (dispose) them later on component destroy life circle step
@@ -105,6 +114,42 @@ export class ProjectExplorerController implements IProjectExplorerController {
     private onLoadProject = (projects: TreeModels.StatefulArtifactNodeVM[]) => {
         this.isLoading = true;
         this.projects = projects.slice(0); // create a copy
+    }
+
+    public openProject(): void {
+        const selectedArtifact = this.selectionManager.getArtifact();
+        if (!selectedArtifact || !selectedArtifact.projectId) {
+            return;
+        }
+
+        const openProjectLoadingId = this.loadingOverlayService.beginLoading();
+        this.projectService.getProject(selectedArtifact.projectId).then((project: AdminStoreModels.IInstanceItem) => {
+            if (!project) {
+                this.loadingOverlayService.endLoading(openProjectLoadingId);
+                return;
+            }
+
+            let openProjects = _.map(this.projectManager.projectCollection.getValue(), "model.id");
+
+            try {
+                this.projectManager.add(project)
+                    .finally(() => {
+                        //(eventCollection, action, label?, value?, custom?, jQEvent?
+                        const label = _.includes(openProjects, project.id) ? "duplicate" : "new";
+                        this.analytics.trackEvent("open", "project", label, project.id, {
+                            openProjects: openProjects
+                        });
+                        this.loadingOverlayService.endLoading(openProjectLoadingId);
+                    });
+            } catch (err) {
+                this.loadingOverlayService.endLoading(openProjectLoadingId);
+                throw err;
+            }
+
+        }).catch(err => {        
+            this.loadingOverlayService.endLoading(openProjectLoadingId);
+            throw err;
+        });   
     }
 
     public onGridReset(isExpanding: boolean): void {

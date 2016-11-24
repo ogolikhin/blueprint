4abd850a32ac0126daf2fb0cc4207e2611e20b37
Bogdan Tkachenko <bogdan.tkachenko@blueprintsys.com>
2016-11-24 13:56:20 -0500

    rollback changes to ICollection

--- blueprint-automationframework/Blueprint-Tests/Model/ArtifactModel/ICollection.cs
+++ blueprint-automationframework/Blueprint-Tests/Model/ArtifactModel/ICollection.cs
@@ -1,15 +1,11 @@
 ﻿using System.Collections.Generic;
 using Model.ArtifactModel;
-using Model.ArtifactModel.Impl;
 
 namespace Model
 {
-    public interface ICollection //: INovaArtifactDetails
+    public interface ICollection : IArtifactBase
     {
-        string ReviewName { get; set; }
-        bool IsCreated { get; set; }
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
-        List<CollectionItem> Artifacts { get; set; }
-        void UpdateArtifacts(List<int> artifactsIdsToAdd = null, List<int> artifactsIdsToRemove = null);
+        IAuthorHistory AuthorHistory { get; set; }
+        List<IArtifact> Contents { get; }
     }
 }

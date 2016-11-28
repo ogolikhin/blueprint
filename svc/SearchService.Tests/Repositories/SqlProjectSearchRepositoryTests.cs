0f63d3b09ccadcd58b4df3dde80bdcd0cf7bd9a1
Mehdi Momeni <mehdi.momeni@blueprintsys.com>
2016-11-28 09:32:59 -0500

    Fixes TFS bug #3855

--- svc/SearchService.Tests/Repositories/SqlProjectSearchRepositoryTests.cs
+++ svc/SearchService.Tests/Repositories/SqlProjectSearchRepositoryTests.cs
@@ -4,6 +4,13 @@ using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SearchService.Models;
 using ServiceLibrary.Repositories;
+using SearchService.Helpers;
+using Moq;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+using ServiceLibrary.Exceptions;
+using ServiceLibrary.Helpers;
 
 namespace SearchService.Repositories
 {
@@ -49,6 +56,60 @@ namespace SearchService.Repositories
             CollectionAssert.AreEqual(queryResult, result.Items.ToList());
         }
 
+        [TestMethod]
+        public async Task SearchName_WithSqlTimeoutException_SqlTimeoutExceptionOccurs()
+        {
+            // Arrange
+            const int userId = 1;
+            var searchCriteria = new SearchCriteria { Query = "test" };
+            const int resultCount = 1;
+            const string separatorString = "/";
+            Exception sqlException = SqlExceptionCreator.NewSqlException(-2);
+            var projectSearchRepository = CreateRepositoryWithExceptionExpectation<SearchResult>(sqlException);
+            SqlTimeoutException sqlTimeoutException = null;
+
+            // Act
+            try
+            {
+                await projectSearchRepository.SearchName(userId, searchCriteria, resultCount, separatorString);
+            }
+            catch (SqlTimeoutException exception)
+            {
+                sqlTimeoutException = exception;
+            }
+
+            // Assert
+            Assert.IsNotNull(sqlTimeoutException, "sqlTimeoutException != null");
+            Assert.IsTrue(sqlTimeoutException.ErrorCode == ErrorCodes.Timeout, "Timeout exception should occur");
+        }
+
+        [TestMethod]
+        public async Task SearchName_WithSqlException_SqlExceptionOccurs()
+        {
+            // Arrange
+            const int userId = 1;
+            var searchCriteria = new SearchCriteria { Query = "test" };
+            const int resultCount = 1;
+            const string separatorString = "/";
+            Exception expectedException = SqlExceptionCreator.NewSqlException(-4);
+            var projectSearchRepository = CreateRepositoryWithExceptionExpectation<SearchResult>(expectedException);
+            SqlException sqlException = null;
+
+            // Act
+            try
+            {
+                await projectSearchRepository.SearchName(userId, searchCriteria, resultCount, separatorString);
+            }
+            catch (SqlException exception)
+            {
+                sqlException = exception;
+            }
+
+            // Assert
+            Assert.IsNotNull(sqlException, "sqlException != null");
+            Assert.IsTrue(sqlException.Number == -4, "exception should occur");
+        }
+
         #endregion SearchName
 
         private static SqlProjectSearchRepository CreateRepository(int userId, SearchCriteria searchCriteria, int resultCount, string separatorString, SearchResult[] result)
@@ -63,7 +124,21 @@ namespace SearchService.Repositories
                     { "separatorString", separatorString }
                 },
                 result);
-            return new SqlProjectSearchRepository(connectionWrapper.Object);
+
+            var configuration = new Mock<ISearchConfiguration>();
+            return new SqlProjectSearchRepository(connectionWrapper.Object, configuration.Object);
+        }
+
+        private static SqlProjectSearchRepository CreateRepositoryWithExceptionExpectation<T>(Exception exception)
+        {
+            var connectionWrapper = new SqlConnectionWrapperMock();
+
+            connectionWrapper.Setup(
+                t => t.QueryAsync<T>("GetProjectsByName", It.IsAny<object>(), It.IsAny<IDbTransaction>(),
+                It.IsAny<int?>(), It.IsAny<CommandType?>())).Throws(exception);
+
+            var configuration = new Mock<ISearchConfiguration>();
+            return new SqlProjectSearchRepository(connectionWrapper.Object, configuration.Object);
         }
     }
 }

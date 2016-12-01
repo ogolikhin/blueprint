3c8c4d0dd52fd36e30f3ede5a6d6c77c12c1333e
Chris Just <chris.just@blueprintsys.com>
2016-12-01 17:56:37 -0500

    Fixing build with TraceDetails model change.

--- blueprint-automationframework/Blueprint-Tests/Model/ArtifactModel/Impl/NovaTrace.cs
+++ blueprint-automationframework/Blueprint-Tests/Model/ArtifactModel/Impl/NovaTrace.cs
@@ -83,14 +83,18 @@ namespace Model.ArtifactModel.Impl
         public int PrimitiveItemTypePredefined { get; set; }
     }
 
+    // Found in:  blueprint/svc/ArtifactStore/Models/RelationshipResultSet.cs  as ItemIdItemNameParentId
     public class TracePathItem
     {
-        public int? ItemId { get; set; }
+        public int ItemId { get; set; }
+        public int VersionProjectId { get; set; }
         [JsonProperty(NullValueHandling = NullValueHandling.Include)]   // Dev always sends ParentId, even if it's null.
         public int? ParentId { get; set; }
-        public string ItemName { get; set; }
+        public string Name { get; set; }
+        public int ItemTypeId { get; set; }
     }
 
+    // Found in:  blueprint/svc/ArtifactStore/Models/RelationshipResultSet.cs  as RelationshipExtendedInfo
     public class TraceDetails
     {
         public int ArtifactId { get; set; }

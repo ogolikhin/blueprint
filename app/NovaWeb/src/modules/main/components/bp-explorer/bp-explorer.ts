8129687ea559b1c2e67b3a91887c7e11975e9c70
Pavlo Pochapsky <Pavlo.Pochapsky@blueprintsys.com>
2016-12-05 13:50:21 -0500

    us3488: discarding an unpublished artifact or refreshing All when unpublished artifacts are displayed should function correctly

--- app/NovaWeb/src/modules/main/components/bp-explorer/bp-explorer.ts
+++ app/NovaWeb/src/modules/main/components/bp-explorer/bp-explorer.ts
@@ -99,6 +99,7 @@ export class ProjectExplorerController implements IProjectExplorerController {
     private setSelectedNode(artifact: IStatefulArtifact) {
         if (!artifact) {
             if (this.treeApi) {
+                this.selected = undefined;
                 this.treeApi.deselectAll();
             }
             return;
@@ -172,7 +173,7 @@ export class ProjectExplorerController implements IProjectExplorerController {
                     // for historical artifact we do not need to change selection in main area US3489
                     navigateToId = selectedArtifactId;
                 }
-            } else if (!selectedArtifactId || this.numberOfProjectsOnLastLoad !== this.projects.length) {
+            } else if (this.$state.current.name !== "main.unpublished" && (!selectedArtifactId || this.numberOfProjectsOnLastLoad !== this.projects.length)) {
                 navigateToId = this.projects[0].model.id;
             } else if (this.projects.some(vm => Boolean(vm.getNode(model => model.id === selectedArtifactId)))) {
                 navigateToId = selectedArtifactId;

1d70eed381a05e147f491f2adaf365fa3a2da312
Dmitry Lopyrev <dmitry.lopyrev@blueprintsys.com>
2016-12-08 16:05:11 -0500

    Small change in text-validation (required field)

--- app/NovaWeb/src/modules/managers/artifact-manager/validation/text-validation.ts
+++ app/NovaWeb/src/modules/managers/artifact-manager/validation/text-validation.ts
@@ -12,15 +12,10 @@ export class TextValidation extends BaseValidation implements ITextValidation {
 
 export class TextRtfValidation extends BaseValidation implements ITextRtfValidation {
     public hasValueIfRequired(isRequired: boolean, newValue: any, oldValue: any, isValidated: boolean = true) {
-        if (!isValidated) {
-            return true;
-        }
-
+        
         if (isRequired) {
-            return Helper.tagsContainText(newValue) ||
-                Helper.tagsContainText(oldValue);
-        } else {
-            return true;
-        }
+            return Helper.tagsContainText(newValue);
+        } 
+        return true;
     }
 }

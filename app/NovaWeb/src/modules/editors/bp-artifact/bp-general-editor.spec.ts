a384876ab386e6ba7f5f5f63a36c289a3454851c
Cristian Poll <cristian@cristianpoll.com>
2016-12-09 13:47:49 -0500

    fix unit test

--- app/NovaWeb/src/modules/editors/bp-artifact/bp-general-editor.spec.ts
+++ app/NovaWeb/src/modules/editors/bp-artifact/bp-general-editor.spec.ts
@@ -1,5 +1,6 @@
 ﻿import "angular";
 import "angular-mocks";
+import "../";
 import {ComponentTest} from "../../util/component.test";
 import {BpGeneralArtifactEditorController} from "./bp-general-editor";
 import {MessageServiceMock} from "../../core/messages/message.mock";
@@ -7,13 +8,17 @@ import {ArtifactManagerMock} from "./../../managers/artifact-manager/artifact-ma
 import {WindowManagerMock} from "./../../main/services/window-manager.mock";
 import {LocalizationServiceMock} from "../../core/localization/localization.mock";
 import {PropertyDescriptorBuilderMock} from "./../configuration/property-descriptor-builder.mock";
+import {IArtifactManager} from "./../../managers/artifact-manager";
+import {ISelectionManager} from "./../../managers/selection-manager/selection-manager";
+import {IStatefulArtifact} from "./../../managers/artifact-manager/artifact/artifact";
+import {ValidationServiceMock} from "./../../managers/artifact-manager/validation/validation.mock";
 
 describe("Component BpGeneralEditorInfo", () => {
     let componentTest: ComponentTest<BpGeneralArtifactEditorController>;
     let template = `<bp-artifact-general-editor context="artifact"></bp-artifact-general-editor>`;
-    let bindings: any;
     let ctrl: BpGeneralArtifactEditorController;
 
+    beforeEach(angular.mock.module("bp.editors.details"));
 
     beforeEach(angular.mock.module(($provide: ng.auto.IProvideService) => {
         $provide.service("messageService", MessageServiceMock);
@@ -21,6 +26,7 @@ describe("Component BpGeneralEditorInfo", () => {
         $provide.service("windowManager", WindowManagerMock);
         $provide.service("localization", LocalizationServiceMock);
         $provide.service("propertyDescriptorBuilder", PropertyDescriptorBuilderMock);
+        $provide.service("validationService", ValidationServiceMock);
 
         // $provide.service("session", SessionSvcMock);
         // $provide.service("stateManager", StateManager);
@@ -28,11 +34,22 @@ describe("Component BpGeneralEditorInfo", () => {
         // $provide.service("projectManager", ProjectManager);
         // $provide.service("selectionManager", SelectionManager);
         // $provide.service("windowResize", WindowResize);
+    }));
 
+    beforeEach(inject((artifactManager: IArtifactManager) => {
+        //artifactManager.selection.getArtifact(); -> Needs to return an artifact that has getObservable()
+        artifactManager.selection = {
+            getArtifact: () => {
+                return {
+                    getObservable: () => new Rx.BehaviorSubject<IStatefulArtifact>(this).asObservable(),
+                    artifactState: {readonly: false} as any
+                };
+            }
+        } as ISelectionManager;
     }));
 
     beforeEach(() => {
-        componentTest = new ComponentTest<BpGeneralArtifactEditorController>(template, "bpGeneralEditor");
+        componentTest = new ComponentTest<BpGeneralArtifactEditorController>(template, "bp-artifact-general-editor");
     });
 
     afterEach(() => {
@@ -41,7 +58,7 @@ describe("Component BpGeneralEditorInfo", () => {
 
      it("should be visible by default", () => {
         // Arrange
-        bindings = {
+        const bindings = {
             artifact: {
                 id: 1
             }
@@ -50,7 +67,7 @@ describe("Component BpGeneralEditorInfo", () => {
         ctrl = componentTest.createComponent(bindings);
 
         //Assert
-        expect(componentTest.element.find(".artifact-overview").length).toBe(0);
+        expect(componentTest.element.find(".artifact-overview").length).toBe(1);
         expect(componentTest.element.find(".readonly-indicator").length).toBe(0);
         expect(componentTest.element.find(".lock-indicator").length).toBe(0);
         expect(componentTest.element.find(".dirty-indicator").length).toBe(0);

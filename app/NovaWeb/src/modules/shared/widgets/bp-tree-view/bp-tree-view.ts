75e4f633292f210723f129264e86471bc0f097e4
amalivanchuk <amalivanchuk@VM1091PC>
2016-12-14 14:06:53 -0500

    Bug 3376:[Nova] Collections content stays empty and UI blocked till populated with artifacts

--- app/NovaWeb/src/modules/shared/widgets/bp-tree-view/bp-tree-view.ts
+++ app/NovaWeb/src/modules/shared/widgets/bp-tree-view/bp-tree-view.ts
@@ -376,7 +376,7 @@ export class BPTreeViewController implements IBPTreeViewController {
 
     public updateScrollbars() {
         const viewport = this.$element[0].querySelector(".ag-body-viewport");
-        if (viewport) {
+        if (viewport && !this.sizeColumnsToFit) {
             this.options.columnApi.autoSizeAllColumns();
         }
     };

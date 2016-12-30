07515f0c195bd2d687be1f867921482677169a53
Albert Wong <albert.wong@blueprintsys.com>
2016-12-30 14:18:50 -0500

    4298 - added api to retrieve info for single job for single job refresh.

--- svc/AdminStore/Repositories/Jobs/IJobsRepository.cs
+++ svc/AdminStore/Repositories/Jobs/IJobsRepository.cs
@@ -7,9 +7,11 @@ namespace AdminStore.Repositories.Jobs
     public interface IJobsRepository
     {
         Task<IEnumerable<JobInfo>> GetVisibleJobs(
-            int? userId, 
-            int? offset = null, 
-            int? limit = null, 
+            int? userId,
+            int? offset = null,
+            int? limit = null,
             JobType? jobType = JobType.None);
+
+        Task<JobInfo> GetJob(int jobId, int? userId);
     }
 }

7fcf196859a580f81dfb2d94daf2ff0b46d682f4
akapsh <alex.kapsh@blueprintsys.com>
2016-12-30 16:48:30 -0500

    Fixed testcase description

--- blueprint-automationframework/Blueprint-Tests/Tests/ArtifactStoreTests/ImageTests.cs
+++ blueprint-automationframework/Blueprint-Tests/Tests/ArtifactStoreTests/ImageTests.cs
@@ -232,7 +232,7 @@ namespace ArtifactStoreTests
 
         [TestCase("abcd1234")]
         [TestRail(211550)]
-        [Description("Try to get an image with malformed ImageId GUID.  Verify it returns 404 Not Found.")]
+        [Description("Try to get an image with malformed ImageId GUID.  Verify it returns 400 Bad Request.")]
         public void GetImage_MalformedImageGuid_400BadRequest(string imageId)
         {
             // Execute:

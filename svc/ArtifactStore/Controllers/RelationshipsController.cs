d3f4b9f43b2cd187b7844edddc423af55e5455fd
David He <david.he@blueprintsys.com>
2017-01-03 15:42:56 -0500

    Try to get last published version if item not marked as deleted and getItemInfo returns null.

--- svc/ArtifactStore/Controllers/RelationshipsController.cs
+++ svc/ArtifactStore/Controllers/RelationshipsController.cs
@@ -116,7 +116,17 @@ namespace ArtifactStore.Controllers
                 await _artifactVersionsRepository.GetDeletedItemInfo(artifactId) :
                 await _artifactPermissionsRepository.GetItemInfo(artifactId, session.UserId);
             if (artifactInfo == null)
-                throw new HttpResponseException(HttpStatusCode.NotFound);
+            {
+                if (!isDeleted)
+                {
+                    // artifact may have been deleted in a draft
+                    artifactInfo = await _artifactPermissionsRepository.GetItemInfo(artifactId, session.UserId, false);
+                }
+                if (artifactInfo == null)
+                {
+                    throw new HttpResponseException(HttpStatusCode.NotFound);
+                }
+            }
 
             var itemIds = new List<int> { artifactId };
             var permissions = await _artifactPermissionsRepository.GetArtifactPermissions(itemIds, session.UserId);

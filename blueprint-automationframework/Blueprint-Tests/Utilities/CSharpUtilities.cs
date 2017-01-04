ca209e3a990ea1775b95e4e00f1215acf2215cfd
akapsh <alex.kapsh@blueprintsys.com>
2017-01-04 12:02:44 -0500

    Changed parameter name

--- blueprint-automationframework/Blueprint-Tests/Utilities/CSharpUtilities.cs
+++ blueprint-automationframework/Blueprint-Tests/Utilities/CSharpUtilities.cs
@@ -7,11 +7,11 @@
         /// </summary>
         /// <param name="propertyName">Name of the property in which value will be changed.</param>
         /// <param name="propertyValue">The value to set the property to.</param>
-        /// <param name="objectToUpdate">Object that contains the property to be changed.</param>
-        public static void SetProperty<T>(string propertyName, T propertyValue, object objectToUpdate)
+        /// <param name="obToUpdate">Object that contains the property to be changed.</param>
+        public static void SetProperty<T>(string propertyName, T propertyValue, object obToUpdate)
         {
-            ThrowIf.ArgumentNull(objectToUpdate, nameof(objectToUpdate));
-            objectToUpdate.GetType().GetProperty(propertyName).SetValue(objectToUpdate, propertyValue, null);
+            ThrowIf.ArgumentNull(obToUpdate, nameof(obToUpdate));
+            obToUpdate.GetType().GetProperty(propertyName).SetValue(obToUpdate, propertyValue, null);
         }
     }
 }

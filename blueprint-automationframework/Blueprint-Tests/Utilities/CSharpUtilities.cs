6d09f6c21392ba23696ed30aa10eed8b12b0b759
akapsh <alex.kapsh@blueprintsys.com>
2017-01-04 13:13:31 -0500

    Added error suppression

--- blueprint-automationframework/Blueprint-Tests/Utilities/CSharpUtilities.cs
+++ blueprint-automationframework/Blueprint-Tests/Utilities/CSharpUtilities.cs
@@ -1,5 +1,8 @@
-﻿namespace Utilities
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Utilities
 {
+
     public static class CSharpUtilities
     {
         /// <summary>
@@ -7,11 +10,12 @@
         /// </summary>
         /// <param name="propertyName">Name of the property in which value will be changed.</param>
         /// <param name="propertyValue">The value to set the property to.</param>
-        /// <param name="obToUpdate">Object that contains the property to be changed.</param>
-        public static void SetProperty<T>(string propertyName, T propertyValue, object obToUpdate)
+        /// <param name="objectToUpdate">Object that contains the property to be changed.</param>
+        [SuppressMessage("Microsoft.Naming", "CA1720:IdentifiersShouldNotContainTypeNames")]
+        public static void SetProperty<T>(string propertyName, T propertyValue, object objectToUpdate)
         {
-            ThrowIf.ArgumentNull(obToUpdate, nameof(obToUpdate));
-            obToUpdate.GetType().GetProperty(propertyName).SetValue(obToUpdate, propertyValue, null);
+            ThrowIf.ArgumentNull(objectToUpdate, nameof(objectToUpdate));
+            objectToUpdate.GetType().GetProperty(propertyName).SetValue(objectToUpdate, propertyValue, null);
         }
     }
 }

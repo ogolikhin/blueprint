13f34d7b018f23f4a0f30ff6bb362e4887e4df77
Albert Wong <albert.wong@blueprintsys.com>
2017-01-09 16:57:10 -0500

    4512 - changed refresh button to 'get jobs' button

--- svc/db/AdminStorage/AdminStorage_Migration.sql
+++ svc/db/AdminStorage/AdminStorage_Migration.sql
@@ -1464,6 +1464,7 @@ INSERT INTO #tempAppLabels ([Key], [Locale], [Text]) VALUES ('Jobs_SubmittedDate
 INSERT INTO #tempAppLabels ([Key], [Locale], [Text]) VALUES ('Jobs_StartDate', 'en-US', N'Started on')
 INSERT INTO #tempAppLabels ([Key], [Locale], [Text]) VALUES ('Jobs_EndDate', 'en-US', N'Completed on')
 INSERT INTO #tempAppLabels ([Key], [Locale], [Text]) VALUES ('Jobs_Error_Label', 'en-US', N'Error')
+INSERT INTO #tempAppLabels ([Key], [Locale], [Text]) VALUES ('Jobs_Button_Label_GetJobs', 'en-US', N'Get Jobs')
 INSERT INTO #tempAppLabels ([Key], [Locale], [Text]) VALUES ('App_Button_Yes', 'fr-CA', N'Oui')
 INSERT INTO #tempAppLabels ([Key], [Locale], [Text]) VALUES ('App_Button_No', 'fr-CA', N'Non')
 INSERT INTO #tempAppLabels ([Key], [Locale], [Text]) VALUES ('App_Button_Ok', 'fr-CA', N'D''accord')

72b447322b4535845baeb08dfb413d7f66384509
akapsh <alex.kapsh@blueprintsys.com>
2017-01-09 13:15:31 -0500

    Added test case for get license usage information

---
+++ blueprint-automationframework/Blueprint-Tests/Model/ILicenseUsageInfo.cs
@@ -0,0 +1,28 @@
+﻿namespace Model
+{
+    public interface ILicenseUsageInfo
+    {
+        #region Properties
+
+        int activityMonth { get; set; }
+        int activityYear { get; set; }
+        int authorsActive { get; set; }
+        int authorsActiveLoggedOn { get; set; }
+        int authorsCreatedtodate { get; set; }
+        int collaboratorsActive { get; set; }
+        int collaboratorsActiveLoggedOn { get; set; }
+        int collaboratorsActivetodate { get; set; }
+        int maxConCurrentAuthors { get; set; }
+        int maxConCurrentCollaborators { get; set; }
+        int maxConCurrentViewers { get; set; }
+        int registeredAuthorsCreated { get; set; }
+        int registeredCollaboratorsCreated { get; set; }
+        int uniqueAuthors { get; set; }
+        int uniqueCollaborators { get; set; }
+        int uniqueViewers { get; set; }
+        int usersFromAnalytics { get; set; }
+        int usersFromRestApi { get; set; }
+
+        #endregion Properties
+    }
+}
---
+++ blueprint-automationframework/Blueprint-Tests/Model/Impl/LicenseUsageInfo.cs
@@ -0,0 +1,29 @@
+﻿
+namespace Model.Impl
+{
+    class LicenseUsageInfo : ILicenseUsageInfo
+    {
+        #region Properties
+
+        public int activityMonth { get; set; }
+        public int activityYear { get; set; }
+        public int authorsActive { get; set; }
+        public int authorsActiveLoggedOn { get; set; }
+        public int authorsCreatedtodate { get; set; }
+        public int collaboratorsActive { get; set; }
+        public int collaboratorsActiveLoggedOn { get; set; }
+        public int collaboratorsActivetodate { get; set; }
+        public int maxConCurrentAuthors { get; set; }
+        public int maxConCurrentCollaborators { get; set; }
+        public int maxConCurrentViewers { get; set; }
+        public int registeredAuthorsCreated { get; set; }
+        public int registeredCollaboratorsCreated { get; set; }
+        public int uniqueAuthors { get; set; }
+        public int uniqueCollaborators { get; set; }
+        public int uniqueViewers { get; set; }
+        public int usersFromAnalytics { get; set; }
+        public int usersFromRestApi { get; set; }
+
+        #endregion Properties
+    }
+}

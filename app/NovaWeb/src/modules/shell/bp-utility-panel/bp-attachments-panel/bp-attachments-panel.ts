0953321de760ae7f452d199530d794576486eed5
Fan Gao <fan.gao@blueprintsys.com>
2017-01-11 10:56:22 -0500

    remove unnecessary artifactPicker option

--- app/NovaWeb/src/modules/shell/bp-utility-panel/bp-attachments-panel/bp-attachments-panel.ts
+++ app/NovaWeb/src/modules/shell/bp-utility-panel/bp-attachments-panel/bp-attachments-panel.ts
@@ -74,7 +74,6 @@ export class BPAttachmentsPanelController extends BPBaseUtilityPanelController {
 
         const dialogData: IArtifactPickerOptions = {
             selectableItemTypes: [Models.ItemTypePredefined.Document],
-            showSubArtifacts: false,
             isItemSelectable: (item: Models.IArtifact) => {
                 // only select document reference that has been published at least once
                 return item.version > 0;

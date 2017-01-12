e54969f2311dc8e18a9e692ff43dd93222a88fdc
Chris Just <chris.just@blueprintsys.com>
2017-01-12 17:41:43 -0500

    US3084: Updated DiscussionsTests with a test using a Viewer, and simplified the SetUp() logic. - Created an IDiscussionAdaptor interface with properties common to Nova and RapidReview discussions. - Renamed properties & variables to be consistent. - Added AssertAreEqual() functions to replace the Equals() functions.

--- blueprint-automationframework/Blueprint-Tests/Model/ArtifactModel/Impl/RaptorDiscussion.cs
+++ blueprint-automationframework/Blueprint-Tests/Model/ArtifactModel/Impl/RaptorDiscussion.cs
@@ -1,97 +1,252 @@
 ﻿using System;
 using System.Collections.Generic;
+using Model.ArtifactModel.Adaptors;
 using Newtonsoft.Json;
-using Model.ArtifactModel.Impl;
+using NUnit.Framework;
 
-namespace Model.Impl
+namespace Model.ArtifactModel.Impl
 {
+    // Found in:  blueprint-current/Source/BluePrintSys.RC.Business.Internal/Components/RapidReview/Models/DiscussionsInfo.cs
+    public class RaptorDiscussionsInfo : IRaptorDiscussionsInfo
+    {
+        #region Serialized JSON properties
+
+        public int ArtifactId { get; set; }
+        public int SubArtifactId { get; set; }
+        public bool CanCreate { get; set; }
+        public bool AreEmailDiscusssionsEnabled { get; set; }
+        public List<IRaptorDiscussion> Discussions { get; } = new List<IRaptorDiscussion>();
+
+        #endregion Serialized JSON properties
+
+        public RaptorDiscussionsInfo()
+        { }
+
+        /// <summary>
+        /// Asserts that all the properties of the two DiscussionsInfo are equal.
+        /// </summary>
+        /// <param name="expectedDiscussionInfo">The expected comment.</param>
+        /// <param name="actualDiscussionInfo">The actual comment.</param>
+        public static void AssertAreEqual(IRaptorDiscussionsInfo expectedDiscussionInfo, IRaptorDiscussionsInfo actualDiscussionInfo)
+        {
+            if ((expectedDiscussionInfo == null) || (actualDiscussionInfo == null))
+            {
+                Assert.AreEqual(expectedDiscussionInfo, actualDiscussionInfo, "One Discussion was null but the other wasn't!");
+            }
+            else
+            {
+                const string MESSAGE = "The {0} properties don't match!";
+
+                Assert.AreEqual(expectedDiscussionInfo.AreEmailDiscusssionsEnabled, actualDiscussionInfo.AreEmailDiscusssionsEnabled, MESSAGE, nameof(IRaptorDiscussionsInfo.AreEmailDiscusssionsEnabled));
+                Assert.AreEqual(expectedDiscussionInfo.ArtifactId, actualDiscussionInfo.ArtifactId, MESSAGE, nameof(IRaptorDiscussionsInfo.ArtifactId));
+                Assert.AreEqual(expectedDiscussionInfo.CanCreate, actualDiscussionInfo.CanCreate, MESSAGE, nameof(IRaptorDiscussionsInfo.CanCreate));
+                Assert.AreEqual(expectedDiscussionInfo.SubArtifactId, actualDiscussionInfo.SubArtifactId, MESSAGE, nameof(IRaptorDiscussionsInfo.SubArtifactId));
+
+                // Compare Discussions.
+                if ((expectedDiscussionInfo.Discussions == null) || (actualDiscussionInfo.Discussions == null))
+                {
+                    Assert.AreEqual(expectedDiscussionInfo.Discussions, actualDiscussionInfo.Discussions,
+                        "One Discussions property was null but the other wasn't!");
+                }
+                else
+                {
+                    Assert.AreEqual(expectedDiscussionInfo.Discussions.Count, actualDiscussionInfo.Discussions.Count, "The number of Discussions are different!");
+
+                    foreach (var expectedDiscussion in expectedDiscussionInfo.Discussions)
+                    {
+                        var actualDiscussion =
+                            actualDiscussionInfo.Discussions.Find(
+                                d => d.DiscussionId.Equals(expectedDiscussion.DiscussionId) && d.ItemId.Equals(expectedDiscussion.ItemId));
+
+                        Assert.NotNull(actualDiscussion, "Couldn't find a Discussion with DiscussionId: {0} and ItemId: {1}",
+                            expectedDiscussion.DiscussionId, expectedDiscussion.ItemId);
+
+                        RaptorDiscussion.AssertAreEqual(expectedDiscussion, actualDiscussion);
+                    }
+                }
+            }
+        }
+    }
+
+    // Found in:  blueprint-current/Source/BluePrintSys.RC.Business.Internal/Components/RapidReview/Models/DiscussionsInfo.cs
     public class RaptorDiscussion : IRaptorDiscussion
     {
-        #region
-        public int ArtifactId { get; set; }
+        #region Serialized JSON properties
 
-        public int SubArtifactId { get; set; }
+        public List<IRaptorReply> Replies { get; set; }
+        public bool IsClosed { get; set; }
+        public int ItemId { get; set; }
+        public int DiscussionId { get; set; }
+        public int Version { get; set; }
+        public int UserId { get; set; }
 
-        public bool CanCreate { get; set; }
+        [JsonProperty("LastEditedOnUtc")]
+        public DateTime LastEditedOn { get; set; }
 
-        public bool AreEmailDiscusssionsEnabled { get; set; }
+        public string Username { get; set; }
+        public bool IsGuest { get; set; }
+        public string Comment { get; set; }
+        public bool CanEdit { get; set; }
+        public bool CanDelete { get; set; }
 
-        public List<IRaptorComment> Comments { get; } = new List<IRaptorComment>();
-        #endregion
+        #endregion Serialized JSON properties
 
         public RaptorDiscussion()
         { }
-    }
 
-    public class RaptorComment : IRaptorComment
-    {
-        public bool IsClosed { get; set; }
+        /// <summary>
+        /// Asserts that all the properties of the two Discussions are equal.
+        /// </summary>
+        /// <param name="expectedDiscussion">The expected comment.</param>
+        /// <param name="actualDiscussion">The actual comment.</param>
+        public static void AssertAreEqual(IRaptorDiscussion expectedDiscussion, IRaptorDiscussion actualDiscussion)
+        {
+            AssertAreEqual((IDiscussionAdaptor)expectedDiscussion, (IDiscussionAdaptor)actualDiscussion);
 
-        public int ItemId { get; set; }
+            if ((expectedDiscussion != null) && (actualDiscussion != null))
+            {
+                // Compare Replies.
+                if ((expectedDiscussion.Replies == null) || (actualDiscussion.Replies == null))
+                {
+                    Assert.AreEqual(expectedDiscussion.Replies, actualDiscussion.Replies,
+                        "One Replies property was null but the other wasn't!");
+                }
+                else
+                {
+                    Assert.AreEqual(expectedDiscussion.Replies.Count, actualDiscussion.Replies.Count, "The number of Replies are different!");
 
-        public int DiscussionId { get; set; }
+                    foreach (var expectedReply in expectedDiscussion.Replies)
+                    {
+                        var actualReply =
+                            actualDiscussion.Replies.Find(
+                                r => r.DiscussionId.Equals(expectedReply.DiscussionId) && r.ItemId.Equals(expectedReply.ItemId));
 
-        public int Version { get; set; }
+                        Assert.NotNull(actualReply, "Couldn't find a Reply with DiscussionId: {0} and ItemId: {1}",
+                            expectedReply.DiscussionId, expectedReply.ItemId);
 
-        public int UserId { get; set; }
+                        RaptorReply.AssertAreEqual(expectedReply, actualReply);
+                    }
+                }
+            }
+        }
 
-        public DateTime LastEditedOnUtc { get; set; }
+        /// <summary>
+        /// Asserts that all the properties of the two Discussions are equal.
+        /// </summary>
+        /// <param name="expectedDiscussion">The expected comment.</param>
+        /// <param name="actualDiscussion">The actual comment.</param>
+        /// <param name="skipCanDelete">(optional) Pass true to skip comparison of the CanDelete properties.</param>
+        /// <param name="skipCanEdit">(optional) Pass true to skip comparison of the CanEdit properties.</param>
+        public static void AssertAreEqual(IDiscussionAdaptor expectedDiscussion, IDiscussionAdaptor actualDiscussion,
+            bool skipCanDelete = false, bool skipCanEdit = false)
+        {
+            if ((expectedDiscussion == null) || (actualDiscussion == null))
+            {
+                Assert.AreEqual(expectedDiscussion, actualDiscussion, "One Discussion was null but the other wasn't!");
+            }
+            else
+            {
+                const string MESSAGE = "The {0} properties don't match!";
 
-        public string Username { get; set; }
+                if (!skipCanDelete)
+                {
+                    Assert.AreEqual(expectedDiscussion.CanDelete, actualDiscussion.CanDelete, MESSAGE, nameof(IDiscussionAdaptor.CanDelete));
+                }
 
-        public bool IsGuest { get; set; }
+                if (!skipCanEdit)
+                {
+                    Assert.AreEqual(expectedDiscussion.CanEdit, actualDiscussion.CanEdit, MESSAGE, nameof(IDiscussionAdaptor.CanEdit));
+                }
 
-        [JsonProperty("comment")]
-        public string CommentValue { get; set; }
+                // TFS Bug: 4706  Milliseconds are different.
+                // We need to remove the milliseconds when comparing the dates because RapidReview and Nova return different milliseconds for some reason.
+                var expectedLastEditedOn = expectedDiscussion.LastEditedOn;
+                var actualLastEditedOn = actualDiscussion.LastEditedOn;
 
-        public bool CanEdit { get; set; }
+                expectedLastEditedOn = new DateTime(expectedLastEditedOn.Year, expectedLastEditedOn.Month, expectedLastEditedOn.Day,
+                    expectedLastEditedOn.Hour, expectedLastEditedOn.Minute, expectedLastEditedOn.Second, expectedLastEditedOn.Kind);
 
-        public bool CanDelete { get; set; }
+                actualLastEditedOn = new DateTime(actualLastEditedOn.Year, actualLastEditedOn.Month, actualLastEditedOn.Day,
+                    actualLastEditedOn.Hour, actualLastEditedOn.Minute, actualLastEditedOn.Second, actualLastEditedOn.Kind);
 
-        public RaptorComment()
-        { }
+                Assert.AreEqual(expectedLastEditedOn, actualLastEditedOn, MESSAGE, nameof(IDiscussionAdaptor.LastEditedOn));
 
-        public bool Equals(Comment comment)
+                Assert.AreEqual(expectedDiscussion.Comment, actualDiscussion.Comment, MESSAGE, nameof(IDiscussionAdaptor.Comment));
+                Assert.AreEqual(expectedDiscussion.DiscussionId, actualDiscussion.DiscussionId, MESSAGE, nameof(IDiscussionAdaptor.DiscussionId));
+                Assert.AreEqual(expectedDiscussion.IsClosed, actualDiscussion.IsClosed, MESSAGE, nameof(IDiscussionAdaptor.IsClosed));
+                Assert.AreEqual(expectedDiscussion.IsGuest, actualDiscussion.IsGuest, MESSAGE, nameof(IDiscussionAdaptor.IsGuest));
+                Assert.AreEqual(expectedDiscussion.ItemId, actualDiscussion.ItemId, MESSAGE, nameof(IDiscussionAdaptor.ItemId));
+//                Assert.AreEqual(expectedDiscussion.LastEditedOn, actualDiscussion.LastEditedOn, MESSAGE, nameof(IDiscussionAdaptor.LastEditedOn));
+                Assert.AreEqual(expectedDiscussion.UserId, actualDiscussion.UserId, MESSAGE, nameof(IDiscussionAdaptor.UserId));
+                Assert.AreEqual(expectedDiscussion.Username, actualDiscussion.Username, MESSAGE, nameof(IDiscussionAdaptor.Username));
+                Assert.AreEqual(expectedDiscussion.Version, actualDiscussion.Version, MESSAGE, nameof(IDiscussionAdaptor.Version));
+            }
+        }
+
+        public bool Equals(Discussion comment)
         {
             if (comment == null)
             { return false; }
             else
             {
-                return (string.Equals(comment.CommentText, CommentValue)) &&
+                return (string.Equals(comment.Comment, Comment)) &&
                     (comment.DiscussionId == DiscussionId) && (comment.IsClosed == IsClosed) &&
                     (comment.IsGuest == IsGuest) && (comment.ItemId == ItemId) &&
                     //(DateTime.Equals(comment.LastEditedOn, LastEditedOnUtc)) && //microseconds are different
-                    (comment.UserId == UserId) && (string.Equals(comment.UserName, Username)) &&
+                    (comment.UserId == UserId) && (string.Equals(comment.Username, Username)) &&
                     (comment.Version == Version) && (comment.CanDelete == CanDelete) &&
                     (comment.CanEdit == CanEdit);
             }
         }
     }
 
+    // Found in:  blueprint-current/Source/BluePrintSys.RC.Business.Internal/Components/RapidReview/Models/DiscussionsInfo.cs
     public class RaptorReply : IRaptorReply
     {
+        #region Serialized JSON properties
+
         public int ReplyId { get; set; }
-
         public int ItemId { get; set; }
-
         public int DiscussionId { get; set; }
-
         public int Version { get; set; }
-
         public int UserId { get; set; }
-
         public DateTime LastEditedOnUtc { get; set; }
-
         public string UserName { get; set; }
-
         public bool IsGuest { get; set; }
-
-        [JsonProperty("comment")]
-        public string ReplyText { get; set; }
-
+        public string Comment { get; set; }
         public bool CanEdit { get; set; }
-
         public bool CanDelete { get; set; }
+
+        #endregion Serialized JSON properties
+
+        /// <summary>
+        /// Asserts that all the properties of the two Replies are equal.
+        /// </summary>
+        /// <param name="expectedReply">The expected Reply.</param>
+        /// <param name="actualReply">The actual Reply.</param>
+        public static void AssertAreEqual(IRaptorReply expectedReply, IRaptorReply actualReply)
+        {
+            if ((expectedReply == null) || (actualReply == null))
+            {
+                Assert.AreEqual(expectedReply, actualReply, "One Reply was null but the other wasn't!");
+            }
+            else
+            {
+                const string MESSAGE = "The {0} properties don't match!";
+
+                Assert.AreEqual(expectedReply.CanDelete, actualReply.CanDelete, MESSAGE, nameof(IRaptorReply.CanDelete));
+                Assert.AreEqual(expectedReply.CanEdit, actualReply.CanEdit, MESSAGE, nameof(IRaptorReply.CanEdit));
+                Assert.AreEqual(expectedReply.Comment, actualReply.Comment, MESSAGE, nameof(IRaptorReply.Comment));
+                Assert.AreEqual(expectedReply.DiscussionId, actualReply.DiscussionId, MESSAGE, nameof(IRaptorReply.DiscussionId));
+                Assert.AreEqual(expectedReply.IsGuest, actualReply.IsGuest, MESSAGE, nameof(IRaptorReply.IsGuest));
+                Assert.AreEqual(expectedReply.ItemId, actualReply.ItemId, MESSAGE, nameof(IRaptorReply.ItemId));
+                Assert.AreEqual(expectedReply.LastEditedOnUtc, actualReply.LastEditedOnUtc, MESSAGE, nameof(IRaptorReply.LastEditedOnUtc));
+                Assert.AreEqual(expectedReply.ReplyId, actualReply.ReplyId, MESSAGE, nameof(IRaptorReply.ReplyId));
+                Assert.AreEqual(expectedReply.UserId, actualReply.UserId, MESSAGE, nameof(IRaptorReply.UserId));
+                Assert.AreEqual(expectedReply.UserName, actualReply.UserName, MESSAGE, nameof(IRaptorReply.UserName));
+                Assert.AreEqual(expectedReply.Version, actualReply.Version, MESSAGE, nameof(IRaptorReply.Version));
+            }
+        }
 
         public bool Equals(Reply reply)
         {
@@ -99,7 +254,7 @@ namespace Model.Impl
             { return false; }
             else
             {
-                if ((string.Equals(reply.ReplyText, ReplyText)) &&
+                if ((string.Equals(reply.ReplyText, Comment)) &&
                     (reply.DiscussionId == DiscussionId) && (reply.IsGuest == IsGuest) &&
                     (reply.ItemId == ItemId) && (reply.ReplyId == ReplyId) &&
                     //(DateTime.Equals(comment.LastEditedOn, LastEditedOnUtc)) && //microseconds are different

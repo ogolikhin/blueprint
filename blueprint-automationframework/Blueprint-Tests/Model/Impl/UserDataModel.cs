26b107e82348f86a5d0d91fcda5249bcdedaca61
Chris Just <chris.just@blueprintsys.com>
2017-02-13 11:12:04 -0500

    Removed commented out code.

--- blueprint-automationframework/Blueprint-Tests/Model/Impl/UserDataModel.cs
+++ blueprint-automationframework/Blueprint-Tests/Model/Impl/UserDataModel.cs
@@ -20,30 +20,6 @@ namespace Model.Impl
         public bool? ExpirePassword { get; set; }
         public bool Enabled { get; set; }
 
-        /*
-        public bool IsDeleted { get { return (!IsDeletedFromDatabase && (EndTimestamp != null)); } }
-
-        // All User table fields are as follows:
-        // [AllowFallback],[CurrentVersion],[Department],[DisplayName],[Email],[Enabled],[EndTimestamp],[EULAccepted],[ExpirePassword],[FirstName],[Guest],[Image_ImageId],[InstanceAdminRoleId],
-        // [InvalidLogonAttemptsNumber],[LastInvalidLogonTimeStamp],[LastName],[LastPasswordChangeTimestamp],[Login],[Password],[Source],[StartTimestamp],[Title],[UserId],[UserSALT]
-        public LicenseType License { get; set; }
-        public IEnumerable<byte> Picture { get; set; }
-        public virtual UserSource Source { get { return UserSource.Unknown; } }
-        public IBlueprintToken Token { get; set; } = new BlueprintToken();
-
-        // These are fields not included by IUser:
-        public bool? AllowFallback { get; set; }
-        public int CurrentVersion { get; set; }
-        public string EncryptedPassword { get; set; }
-        public DateTime? EndTimestamp { get; set; }
-        public bool EULAccepted { get; set; }
-        public bool Guest { get; set; }
-        public int InvalidLogonAttemptsNumber { get; set; }
-        public DateTime? LastInvalidLogonTimeStamp { get; set; }
-        public DateTime? LastPasswordChangeTimestamp { get; set; }
-        public DateTime StartTimestamp { get; set; }
-        public Guid UserSALT { get; set; }
-        */
         #endregion Properties
     }
 }

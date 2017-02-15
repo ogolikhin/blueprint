f2a6c681bfd5ba5ab653bfb68026170d848cbd91
Chris Just <chris.just@blueprintsys.com>
2017-02-15 18:14:49 -0500

    US4967: Implementing a positive DeleteUser test.  Added GetUser() to OpenApi class.

---
+++ blueprint-automationframework/Blueprint-Tests/Model/OpenApiModel/UserModel/GetGroupResultSet.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Model.OpenApiModel.UserModel
+{
+    public class GetGroupResultSet
+    {
+        public List<GetGroupResult> Groups { get; set; }
+    }
+
+    public class GetGroupResult
+    {
+        #region Serialized properties
+
+        public int? ProjectId { get; set; }
+
+        [JsonProperty("Type")]
+        public string UserOrGroupType { get; set; }
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        #endregion Serialized properties
+    }
+}

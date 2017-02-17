d530677c9e01db687b67ecbe450c656b52473b13
Chris Just <chris.just@blueprintsys.com>
2017-02-17 13:39:55 -0500

    US4967: Added 401, 403 & 409 tests.  Dev also changed the returned JSON.

---
+++ blueprint-automationframework/Blueprint-Tests/Model/OpenApiModel/UserModel/Enums/ResultStatusEnum.cs
@@ -0,0 +1,11 @@
+﻿namespace Model.OpenApiModel.UserModel.Enums
+{
+    // Taken from:  blueprint-current/Source/BluePrintSys.RC.Api.Business/Models/Enums/ResultStatusEnum.cs
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1008:EnumsShouldHaveZeroValue")]    // This is what Dev has.
+    public enum ResultStatusEnum
+    {
+        Success = 201,
+        PartialSuccess = 207,
+        Failed = 409
+    }
+}

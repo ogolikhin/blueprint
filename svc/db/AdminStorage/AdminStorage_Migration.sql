9bacacaf9dab64525b665585c4d4f5d498a633bb
Albert Wong <albert.wong@blueprintsys.com>
2017-02-28 16:58:39 -0500

    US5217 - added labels for walkthrough toggle buttons.

--- svc/db/AdminStorage/AdminStorage_Migration.sql
+++ svc/db/AdminStorage/AdminStorage_Migration.sql
@@ -1513,6 +1513,8 @@ INSERT INTO #tempAppLabels ([Key], [Locale], [Text]) VALUES ('Jobs_EndDate', 'en
 INSERT INTO #tempAppLabels ([Key], [Locale], [Text]) VALUES ('Jobs_Error_Label', 'en-US', N'Error')
 INSERT INTO #tempAppLabels ([Key], [Locale], [Text]) VALUES ('Jobs_Button_Label_GetJobs', 'en-US', N'Get Jobs')
 INSERT INTO #tempAppLabels ([Key], [Locale], [Text]) VALUES ('Walkthrough_Button_Label', 'en-US', N'Walkthrough')
+INSERT INTO #tempAppLabels ([Key], [Locale], [Text]) VALUES ('Process_Walkthrough_Mode_Tooltip', 'en-US', N'Walkthrough Mode')
+INSERT INTO #tempAppLabels ([Key], [Locale], [Text]) VALUES ('Process_Diagram_Mode_Tooltip', 'en-US', N'Diagram Mode')
 INSERT INTO #tempAppLabels ([Key], [Locale], [Text]) VALUES ('App_Button_Yes', 'fr-CA', N'Oui')
 INSERT INTO #tempAppLabels ([Key], [Locale], [Text]) VALUES ('App_Button_No', 'fr-CA', N'Non')
 INSERT INTO #tempAppLabels ([Key], [Locale], [Text]) VALUES ('App_Button_Ok', 'fr-CA', N'D''accord')

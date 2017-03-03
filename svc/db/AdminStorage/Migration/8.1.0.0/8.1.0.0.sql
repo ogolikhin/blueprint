9cc7ae95d71198e50863a4e193fe568a5c5d32e6
Cristian Poll <cristian@cristianpoll.com>
2017-03-03 11:29:42 -0500

    add password recovery tokens table

--- svc/db/AdminStorage/Migration/8.1.0.0/8.1.0.0.sql
+++ svc/db/AdminStorage/Migration/8.1.0.0/8.1.0.0.sql
@@ -7,6 +7,20 @@ IF NOT ([dbo].[IsSchemaVersionLessOrEqual](N'8.1.0') <> 0)
 Print 'Migrating 8.1.0.0 ...'
 -- -----------------------------------------------------------------------------------------------
 
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[PasswordRecoveryTokens]') AND type in (N'U'))
+DROP TABLE [dbo].[PasswordRecoveryTokens]
+GO
+
+CREATE TABLE [dbo].[PasswordRecoveryTokens](
+    [UserName] [nvarchar](max),
+    [CreationTime] [datetime] NOT NULL,
+    [RecoveryToken] [uniqueidentifier] NOT NULL,
+
+	 CONSTRAINT [PK_PasswordRecoveryTokens] PRIMARY KEY CLUSTERED 
+(
+	[RecoveryToken] ASC
+)) ON [PRIMARY]
+GO
 
 -- -----------------------------------------------------------------------------------------------
 -- Always add your code just above this comment block

488f4fb5f4f144111770e7d121bab1fee1851746
Alexander Utkin <alexander.utkin@blueprintsys.com>
2017-03-09 09:22:23 -0500

    adds powershell script to set up environment

--- doc/environment/Create_Blueprint_DataBases.ps1
+++ doc/environment/Create_Blueprint_DataBases.ps1
@@ -6,16 +6,19 @@ Write-Host 'Creating Raptor database'
 Invoke-Sqlcmd -ServerInstance BlueprintDevDB -InputFile $blueprintCurrentDir\Source\BluePrintSys.RC.Data.EntityModel\RecreateRaptor.sql
 Invoke-Sqlcmd -ServerInstance BlueprintDevDB -InputFile $blueprintCurrentDir\Source\BluePrintSys.RC.Data.EntityModel\Instance.sql
 
-#Write-Host 'Creating AdminStorage database'
-#Invoke-Sqlcmd -ServerInstance BlueprintDevDB -InputFile $blueprintDir\svc\db\AdminStorage\RecreateAdminStorage.sql
-#Invoke-Sqlcmd -ServerInstance BlueprintDevDB -InputFile $blueprintDir\svc\db\AdminStorage\AdminStorage_Instance.sql
-#Write-Host 'Creating ArtifactStorage database'
-#Invoke-Sqlcmd -ServerInstance BlueprintDevDB -InputFile $blueprintDir\svc\db\ArtifactStorage\RecreateArtifactStorage.sql
-#Invoke-Sqlcmd -ServerInstance BlueprintDevDB -InputFile $blueprintDir\svc\db\ArtifactStorage\ArtifactStorage_Instance.sql
-#Write-Host 'Creating FileStorage database'
-#Invoke-Sqlcmd -ServerInstance BlueprintDevDB -InputFile $blueprintDir\svc\db\FileStorage\RecreateFileStorage.sql
-#Invoke-Sqlcmd -ServerInstance BlueprintDevDB -InputFile $blueprintDir\svc\db\FileStorage\FileStorage_Instance.sql
-#Write-Host 'Creating AppPool logins'
-#Invoke-Sqlcmd -ServerInstance BlueprintDevDB -InputFile $scriptDir\CreateBlueprint_AuxiliaryServicesAppPoolLoginForDev.sql
-#Invoke-Sqlcmd -ServerInstance BlueprintDevDB -InputFile $scriptDir\CreateBlueprint_PrimaryServicesAppPoolLoginForDev.sql
-#Invoke-Sqlcmd -ServerInstance BlueprintDevDB -InputFile $scriptDir\CreateBlueprintAppPoolLoginForDev.sql
+Write-Host 'Creating AdminStorage database'
+Invoke-Sqlcmd -ServerInstance BlueprintDevDB -InputFile $blueprintDir\svc\db\AdminStorage\RecreateAdminStorage.sql
+Invoke-Sqlcmd -ServerInstance BlueprintDevDB -InputFile $blueprintDir\svc\db\AdminStorage\AdminStorage_Instance.sql
+
+Write-Host 'Creating ArtifactStorage database'
+Invoke-Sqlcmd -ServerInstance BlueprintDevDB -InputFile $blueprintDir\svc\db\ArtifactStorage\RecreateArtifactStorage.sql
+Invoke-Sqlcmd -ServerInstance BlueprintDevDB -InputFile $blueprintDir\svc\db\ArtifactStorage\ArtifactStorage_Instance.sql
+
+Write-Host 'Creating FileStorage database'
+Invoke-Sqlcmd -ServerInstance BlueprintDevDB -InputFile $blueprintDir\svc\db\FileStorage\RecreateFileStorage.sql
+Invoke-Sqlcmd -ServerInstance BlueprintDevDB -InputFile $blueprintDir\svc\db\FileStorage\FileStorage_Instance.sql
+
+Write-Host 'Creating AppPool logins'
+Invoke-Sqlcmd -ServerInstance BlueprintDevDB -InputFile $scriptDir\CreateBlueprint_AuxiliaryServicesAppPoolLoginForDev.sql
+Invoke-Sqlcmd -ServerInstance BlueprintDevDB -InputFile $scriptDir\CreateBlueprint_PrimaryServicesAppPoolLoginForDev.sql
+Invoke-Sqlcmd -ServerInstance BlueprintDevDB -InputFile $scriptDir\CreateBlueprintAppPoolLoginForDev.sql

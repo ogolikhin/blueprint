175773a1e8fede0100a5358a7f1e24b1df30a58b
Nikita Pliassounov <nikita.pliassounov@blueprintsys.com>
2017-03-13 11:44:50 -0400

    Fixing unit test

--- svc/AccessControl.Tests/App_Start/WebApiConfigTests.cs
+++ svc/AccessControl.Tests/App_Start/WebApiConfigTests.cs
@@ -22,7 +22,7 @@ namespace AccessControl
             config.EnsureInitialized();
 
             // Assert
-            config.AssertTotalRoutes(11, "Please update asserts in WebApiConfigTests when changing routes.");
+            config.AssertTotalRoutes(12, "Please update asserts in WebApiConfigTests when changing routes.");
             config.AssertAction<LicensesController>("GetActiveLicenses", HttpMethod.Get, "licenses/active");
             config.AssertAction<LicensesController>("GetLockedLicenses", HttpMethod.Get, "licenses/locked");
             config.AssertAction<LicensesController>("GetLicenseTransactions", HttpMethod.Get, "licenses/transactions?days=1&consumerType=1");
@@ -35,6 +35,7 @@ namespace AccessControl
             config.AssertAction<SessionsController>("PutSession", HttpMethod.Put, "sessions");
             config.AssertAction<SessionsController>("PutSession", HttpMethod.Put, "sessions?op=op&aid=1");
             config.AssertAction<SessionsController>("DeleteSession", HttpMethod.Delete, "sessions");
+            config.AssertAction<SessionsController>("DeleteSession", HttpMethod.Delete, "sessions/1");
             config.AssertAction<StatusController>("GetStatus", HttpMethod.Get, "status");
             config.AssertAction<StatusController>("GetStatusUpCheck", HttpMethod.Get, "status/upcheck");
         }

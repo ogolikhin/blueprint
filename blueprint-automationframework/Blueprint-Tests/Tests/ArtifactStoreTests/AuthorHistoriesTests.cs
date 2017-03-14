2a182bcf3316b6d3b6dfe4907a6b20b641fb5899
Bogdan Tkachenko <bogdan.tkachenko@blueprintsys.com>
2017-03-14 17:35:41 -0400

    improve test

--- blueprint-automationframework/Blueprint-Tests/Tests/ArtifactStoreTests/AuthorHistoriesTests.cs
+++ blueprint-automationframework/Blueprint-Tests/Tests/ArtifactStoreTests/AuthorHistoriesTests.cs
@@ -1,15 +1,12 @@
-﻿using System;
-using Helper;
+﻿using Helper;
 using Model;
 using Model.Impl;
 using NUnit.Framework;
 using CustomAttributes;
 using System.Collections.Generic;
 using TestCommon;
-using Model.ArtifactModel;
 using Model.Factories;
 using Model.ArtifactModel.Impl;
-using Utilities;
 using Model.ArtifactModel.Enums;
 
 namespace ArtifactStoreTests
@@ -35,13 +32,14 @@ namespace ArtifactStoreTests
             Helper?.Dispose();
         }
 
+        [Explicit(IgnoreReasons.UnderDevelopmentQaDev)]
         [TestCase]
-        [TestRail(7)]
+        [TestRail(267032)]
         [Description("Create artifact, publish it, get history.  Verify 1 published artifact history is returned with the expected values.")]
-        public void GetArtifactAuthorHistory_PublishedWithDraft_VerifyHistoryHasExpectedValue()
+        public void GetArtifactAuthorHistory_PublishedArtifact_VerifyHistory()
         {
             // Setup:
-            Helper.CreateNovaArtifactInSpecificState(_adminUser, _project, 
+            var artifact = Helper.CreateNovaArtifactInSpecificState(_adminUser, _project, 
                 TestHelper.TestArtifactState.ScheduledToDelete, ItemTypePredefined.Actor, _project.Id);
 
             List<AuthorHistoryItem> artifactHistory = null;
@@ -49,11 +47,13 @@ namespace ArtifactStoreTests
             // Execute:
             Assert.DoesNotThrow(() =>
             {
-                artifactHistory = ArtifactStore.GetArtifactsAuthorHistory(Helper.ArtifactStore.Address, Helper.NovaArtifacts[_adminUser], _viewerUser);
+                artifactHistory = ArtifactStore.GetArtifactsAuthorHistory(Helper.ArtifactStore.Address, new List<int>{artifact.Id}, _viewerUser);
             }, "GetArtifactsAuthorHistory shouldn return 200 OK when sent with valid parameters!");
 
             // Verify:
             Assert.AreEqual(1, artifactHistory.Count, "Artifacts Author History history must have 1 item, but it has {0} items", artifactHistory.Count);
+            Assert.AreEqual(artifact.CreatedBy.Id, artifactHistory[0].CreatedByUserId, "Artifacts Author History item should have expected CreatedByUserId");
+            Assert.AreEqual(artifact.Id, artifactHistory[0].ItemId, "Artifacts Author History item should have expected Id");
         }
     }
 }

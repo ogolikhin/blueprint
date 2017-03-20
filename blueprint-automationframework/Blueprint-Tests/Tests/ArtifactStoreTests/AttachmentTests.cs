5bf0d01eef009a991b2f21c77089731f497fed80
akapsh <alex.kapsh@blueprintsys.com>
2017-03-20 17:05:10 -0400

    Added IndicatorFlags verification to some tests

--- blueprint-automationframework/Blueprint-Tests/Tests/ArtifactStoreTests/AttachmentTests.cs
+++ blueprint-automationframework/Blueprint-Tests/Tests/ArtifactStoreTests/AttachmentTests.cs
@@ -125,6 +125,9 @@ namespace ArtifactStoreTests
             var artifactDetails = Helper.ArtifactStore.GetArtifactDetails(_adminUser, artifact.Id);
             Assert.AreEqual(attachment.AttachedFiles[0].UploadedDate, artifactDetails.LastEditedOn,
                 "UploadedDate for published artifact's attachment should be equal to LastEditedOn date of artifact");
+
+            ArtifactStoreHelper.VerifyIndicatorFlags(Helper, _adminUser, artifact.Id, (int)ItemIndicatorFlags.HasAttachmentsOrDocumentRefs);
+            ArtifactStoreHelper.VerifyIndicatorFlags(Helper, _adminUser, artifact.Id, (int)ItemIndicatorFlags.HasAttachmentsOrDocumentRefs, userTask.Id);
         }
 
         [TestCase]
@@ -159,6 +162,9 @@ namespace ArtifactStoreTests
             var artifactDetails = Helper.ArtifactStore.GetArtifactDetails(_adminUser, artifact.Id);
             Assert.AreEqual(attachment.AttachedFiles[0].UploadedDate, artifactDetails.LastEditedOn,
                 "UploadedDate for published artifact's attachment should be equal to LastEditedOn date of artifact");
+
+            ArtifactStoreHelper.VerifyIndicatorFlags(Helper, _adminUser, artifact.Id, (int)ItemIndicatorFlags.HasAttachmentsOrDocumentRefs);
+            ArtifactStoreHelper.VerifyIndicatorFlags(Helper, _adminUser, artifact.Id, (int)ItemIndicatorFlags.HasAttachmentsOrDocumentRefs, userTask.Id);
         }
 
         [TestCase]
@@ -199,6 +205,9 @@ namespace ArtifactStoreTests
             var artifactDetails = Helper.ArtifactStore.GetArtifactDetails(_adminUser, artifact.Id);
             Assert.AreEqual(attachment.AttachedFiles[0].UploadedDate, artifactDetails.LastEditedOn,
                 "UploadedDate for published artifact's attachment should be equal to LastEditedOn date of artifact");
+
+            ArtifactStoreHelper.VerifyIndicatorFlags(Helper, _adminUser, artifact.Id, (int)ItemIndicatorFlags.HasAttachmentsOrDocumentRefs);
+            ArtifactStoreHelper.VerifyIndicatorFlags(Helper, _adminUser, artifact.Id, (int)ItemIndicatorFlags.HasAttachmentsOrDocumentRefs, (int)subArtifact.Id);
         }
 
         [TestCase]

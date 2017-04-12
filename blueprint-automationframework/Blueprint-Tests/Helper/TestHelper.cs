9d4ff14d93ae159cacf5d2e91ce18dbcde002c40
Bogdan Tkachenko <bogdan.tkachenko@blueprintsys.com>
2017-04-12 11:47:21 -0400

    address PR comments

--- blueprint-automationframework/Blueprint-Tests/Helper/TestHelper.cs
+++ blueprint-automationframework/Blueprint-Tests/Helper/TestHelper.cs
@@ -799,7 +799,7 @@ namespace Helper
         /// <param name="parentId">(optional) The id of the parent artifact where Baseline should be created. By default it will be created in the Baselines and Reviews folder.</param>
         /// <param name="artifactToAddId">(optional) Artifact ID to be added to baseline.  By default empty baseline will be created.</param>
         /// <returns>The Baseline artifact.</returns>
-        public INovaArtifactDetails CreateBaseline(IUser user, IProject project, string name = null, int? parentId = null,
+        public ArtifactWrapper CreateBaseline(IUser user, IProject project, string name = null, int? parentId = null,
             int? artifactToAddId = null)
         {
             ThrowIf.ArgumentNull(user, nameof(user));

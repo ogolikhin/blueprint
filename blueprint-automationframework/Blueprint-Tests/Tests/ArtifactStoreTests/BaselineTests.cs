98c33bb09bc6fc2995d0ffed159fc2d8fdb04b44
Alex Kim <dhalexkim@gmail.com>
2017-04-13 17:38:26 -0400

    Changes for US 6066: - Added two integration tests:     - GetBaseline_CreateDefaultBaseline_ValidateMinimalUtcTimestamp     - GetBaseline_SaveBaselineWithInvalidBaselineTimestamp_Check409

--- blueprint-automationframework/Blueprint-Tests/Tests/ArtifactStoreTests/BaselineTests.cs
+++ blueprint-automationframework/Blueprint-Tests/Tests/ArtifactStoreTests/BaselineTests.cs
@@ -1,20 +1,24 @@
-﻿using System;
-using CustomAttributes;
+﻿using CustomAttributes;
 using Helper;
 using Model;
 using Model.ArtifactModel;
 using Model.ArtifactModel.Enums;
 using Model.ArtifactModel.Impl;
-using Model.NovaModel.Impl;
 using Model.Factories;
 using Model.Impl;
+using Model.NovaModel.Impl;
 using NUnit.Framework;
+using System;
 using System.Collections.Generic;
+using System.Globalization;
 using TestCommon;
 using Utilities;
 
 namespace ArtifactStoreTests
 {
+    [TestFixture]
+    [Category(Categories.ArtifactStore)]
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Maintainability", "CA1506:AvoidExcessiveClassCoupling")]
     public class BaselineTests : TestBase
     {
         private IUser _adminUser = null;
@@ -27,6 +31,17 @@ namespace ArtifactStoreTests
 
         private const string expectedInternalExceptionMessage = "Exception of type 'BluePrintSys.RC.Business.Internal.Models.InternalApiBusinessException' was thrown.";
 
+        private static Dictionary<int, string> projectExpectedCreationDateMap { get; } = new Dictionary<int, string>
+        {
+            { 1, "2016-09-20 17:04:14.787" },
+            { 4, "2016-09-20 17:04:35.690" },
+            { 212, "2016-10-28 19:37:50.410" },
+            { 382, "2016-12-06 18:56:49.993" },
+            { 385, "2016-12-06 21:33:18.493" },
+            { 388, "2017-01-09 21:19:27.043" },
+            { 392, "2017-01-23 15:34:42.550" }
+        };
+        
         [SetUp]
         public void SetUp()
         {
@@ -494,6 +509,25 @@ namespace ArtifactStoreTests
 
         #region Edit Baseline Properties
 
+        [Category(Categories.GoldenData)]
+        [TestCase]
+        [TestRail(290069)]
+        [Description("Create Baseline, get Baseline and check that MinimalUtcTimestamp is equal to the CreatedDateTime value of the project it is based on.")]
+        public void GetBaseline_CreateDefaultBaseline_ValidateMinimalUtcTimestamp()
+        {
+            // Setup:
+            var baselineArtifact = Helper.CreateBaseline(_user, _project);
+
+            // Execute:
+            Baseline receivedBaseline = null;
+            Assert.DoesNotThrow(() => {
+                receivedBaseline = Helper.ArtifactStore.GetBaseline(_user, baselineArtifact.Id);
+            }, "Getting Baseline shouldn't throw an error.");
+
+            // Verify:
+            ValidateMinimalUtctTimestamp(_project, receivedBaseline, projectExpectedCreationDateMap);
+        }
+
         [TestCase(true)]
         [TestCase(false)]
         [TestRail(267068)]
@@ -750,6 +784,37 @@ namespace ArtifactStoreTests
             TestHelper.ValidateServiceError(ex.RestResponse, InternalApiErrorCodes.SealedBaseline, expectedInternalExceptionMessage);
         }
 
+        [Category(Categories.GoldenData)]
+        [TestCase]
+        [TestRail(290070)]
+        [Description("Create Baseline, get Baseline and update its baselinetimestamp with invalid value, which is less than MinimalUtcTimestamp. Verify that 409 Conflict.")]
+        public void GetBaseline_SaveBaselineWithInvalidBaselineTimestamp_Check409()
+        {
+            // Setup:
+            var baselineArtifact = Helper.CreateBaseline(_user, _project);
+            var baseline = Helper.ArtifactStore.GetBaseline(_user, baselineArtifact.Id);
+            var myDate = DateTime.ParseExact(
+                projectExpectedCreationDateMap[_project.Id],
+                "yyyy-MM-dd HH:mm:ss.fff",
+                CultureInfo.InvariantCulture);
+
+            baseline.UtcTimestamp = myDate.AddMinutes(-1);
+
+            // Execute:
+            var ex = Assert.Throws<Http409ConflictException>(() => {
+                Helper.ArtifactStore.UpdateArtifact(_user, baseline);
+            }, "Adding artifact to Baseline shouldn't throw an error.");
+
+            // Verify:
+            const string expectedErrorMessage = "Baseline timestamp should be between project creation date and current time.";
+
+            TestHelper.ValidateServiceError(ex.RestResponse,
+                InternalApiErrorCodes.CannotSaveBaselineBecauseOfFutureTimestamp,
+                expectedErrorMessage);
+
+            ValidateMinimalUtctTimestamp(_project, baseline, projectExpectedCreationDateMap);
+        }
+
         [TestCase]
         [TestRail(267154)]
         [Description("Try to add default Collection folder to the Baseline, check 404 error message.")]
@@ -958,6 +1023,30 @@ namespace ArtifactStoreTests
                 Assert.AreEqual(utcTimestamp, baseline.UtcTimestamp, "UtcTimestamp should have expected value.");
             }
             return baseline;
+        }
+
+        /// TODO: Move it to Helper class
+        /// <summary>
+        /// Validate MinimalUtcTimestamp
+        /// </summary>
+        /// <param name="project">The project that baseline artifact gets created.</param>
+        /// <param name="baseline">Baseline to validate</param>
+        /// <param name="projectExpectedCreationDateMap">Map contains list of project Id with project creation time.</param>
+        private static void ValidateMinimalUtctTimestamp(IProject project, Baseline baseline, Dictionary<int, string> projectExpectedCreationDateMap)
+        {
+            ThrowIf.ArgumentNull(project, nameof(project));
+            ThrowIf.ArgumentNull(baseline, nameof(baseline));
+            ThrowIf.ArgumentNull(projectExpectedCreationDateMap, nameof(projectExpectedCreationDateMap));
+
+            Assert.NotNull(baseline.MinimalUtcTimestamp, "MinimalUtcTimestamp from baseline artifact is null!");
+            var formattedReceivedMinimalUtcTimestamp = ((DateTime)baseline.MinimalUtcTimestamp).ToString(
+                "yyyy-MM-dd HH:mm:ss.fff", CultureInfo.InvariantCulture);
+
+            Assert.AreEqual(
+                projectExpectedCreationDateMap[project.Id],
+                formattedReceivedMinimalUtcTimestamp,
+                "{0} was expected from MinimalUtcTimestamp but {1} was returned from the receievedBaseline",
+                projectExpectedCreationDateMap[project.Id], formattedReceivedMinimalUtcTimestamp);
         }
 
         #endregion Private Functions

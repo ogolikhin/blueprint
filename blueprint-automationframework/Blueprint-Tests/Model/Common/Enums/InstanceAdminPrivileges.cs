77b4dad87609efd723b1d08e0ebe0d7816159663
Darren Martell <darren.martell@blueprintsys.com>
2017-04-18 16:17:44 -0400

    Change AdminStoreUser to LoginUser to prepare for AdminPortal

---
+++ blueprint-automationframework/Blueprint-Tests/Model/Common/Enums/InstanceAdminPrivileges.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Model.Common.Enums
+{
+    [Flags]
+    public enum InstanceAdminPrivileges
+    {
+        None = 0x00,
+
+        AccessMainExperience = 0x01,
+
+        AccessAllProjectData = 0x02 | AccessMainExperience | ViewProjects | ManageProjects | AccessAllProjectsAdmin,
+
+        ViewInstanceSettings = 0x04,
+
+        ManageInstanceSettings = 0x08 | ViewInstanceSettings,
+
+        ViewAdminRoles = 0x10,
+
+        ManageAdminRoles = 0x20 | ViewAdminRoles,
+
+        ViewProjects = 0x40,
+
+        ManageProjects = 0x80 | ViewProjects,
+
+        DeleteProjects = 0x100 | ViewProjects | ManageProjects,
+
+        AccessAllProjectsAdmin = 0x200 | ViewProjects | ManageProjects,
+
+        ViewUsers = 0x400,
+
+        ManageUsers = 0x800 | ViewUsers,
+
+        ViewGroups = 0x1000,
+
+        ManageGroups = 0x2000 | ViewGroups,
+
+        AssignAdminRoles = 0x4000 | ManageUsers | ManageGroups,
+
+        CanReportOnAllProjects = 0x8000,
+
+        ViewStandardPropertiesAndArtifactTypes = 0x10000,
+
+        ManageStandardPropertiesAndArtifactTypes = 0x20000 | ViewStandardPropertiesAndArtifactTypes | AccessAllProjectsAdmin,
+
+        CanManageAllRunningJobs = 0x40000
+    }
+}
+
+

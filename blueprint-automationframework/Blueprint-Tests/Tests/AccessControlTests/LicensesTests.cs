793ca5ef62578c49d9c915ee9f5920c6b409404d
Valentina <valentina.alekseeva@blueprintsys.com>
2017-04-19 13:42:26 -0400

    Move private functions section to the bottom.

--- blueprint-automationframework/Blueprint-Tests/Tests/AccessControlTests/LicensesTests.cs
+++ blueprint-automationframework/Blueprint-Tests/Tests/AccessControlTests/LicensesTests.cs
@@ -58,149 +58,6 @@ namespace AccessControlTests
             Helper?.Dispose();
         }
 
-        #region Private functions
-
-        /// <summary>
-        /// Adds (POST's) the specified session to the AccessControl service.
-        /// </summary>
-        /// <param name="session">The session to add.</param>
-        /// <returns>The session that was added including the session token.</returns>
-        private ISession AddSessionToAccessControl(ISession session)
-        {
-            // POST the new session.
-            var createdSession = Helper.AccessControl.AddSession(session);
-
-            // Verify that the POST returned the expected session.
-            Assert.True(session.Equals(createdSession),
-                "POST returned a different session than expected!  Got '{0}', but expected '{1}'.",
-                createdSession.SessionId, session.SessionId);
-
-            return createdSession;
-        }
-
-        /// <summary>
-        /// Creates a random session and adds (POST's) it to the AccessControl service.
-        /// </summary>
-        /// <param name="sessionUser">(optional) The user for whom the session is created.</param>
-        /// <returns>The session that was added including the session token.</returns>
-        private ISession CreateAndAddSessionToAccessControl(IUser sessionUser = null)
-        {
-            sessionUser = sessionUser ?? _user;
-            var session = SessionFactory.CreateSession(sessionUser);
-
-            // POST the new session.
-            return AddSessionToAccessControl(session);
-        }
-
-        /// <summary>
-        /// Verifying couple of elements in the response list
-        /// </summary>
-        /// <param name="licenseUsageInfo">License usage information broken down by months</param>
-        /// <param name="expectedEmptyResponse">Pass true if the response should be empty or false if it should contain data.</param>
-        /// <param name="year">The year requested in the REST call.</param>
-        /// <param name="month">The month requested in the REST call.</param>
-        private static void VerifySomeProperties(LicenseUsage licenseUsageInfo, bool expectedEmptyResponse, int? year, int? month)
-        {
-            Assert.IsNotNull(licenseUsageInfo, "License usage information should never be null!");
-
-            if (!expectedEmptyResponse)
-            {
-                Assert.IsNotEmpty(licenseUsageInfo.Summary, "Summary has no values!");
-                Assert.IsNotEmpty(licenseUsageInfo.UserActivities, "UserActivities has no values!");
-
-                const int FIRST_YEAR_IN_DB = 2016;
-                const int FIRST_MONTH_IN_DB = 9;
-                const int JANUARY_2017 = 201701;
-                const int NOVEMBER_2016 = 201611;
-
-                // If we passed null month & year, default to the first month & year in the golden data.
-                year = year ?? FIRST_YEAR_IN_DB;
-                month = month ?? FIRST_MONTH_IN_DB;
-
-                // If the year is before our golden database was created, set month & year to the first ones in the golden DB.
-                if (year < FIRST_YEAR_IN_DB)
-                {
-                    year = FIRST_YEAR_IN_DB;
-                    month = FIRST_MONTH_IN_DB;
-                }
-
-                // First verify that the license usage starts from the month & year we requested.
-                int yearMonth = (int)(year * 100 + month);
-
-                if (yearMonth < JANUARY_2017)
-                {
-                    var licenseUsageSummary = licenseUsageInfo.Summary.Find(u => u.YearMonth.Equals(yearMonth));
-                    var licenseUserActivity = licenseUsageInfo.UserActivities.Find(u => u.YearMonth.Equals(yearMonth));
-
-                    if (yearMonth == NOVEMBER_2016)
-                    {
-                        VerifyLicenseUsageValues(licenseUsageSummary, yearMonth, uniqueAuthors: 2);
-                    }
-                    else
-                    {
-                        VerifyLicenseUsageValues(licenseUsageSummary, yearMonth, uniqueAuthors: 1);
-                    }
-                    VerifyLicenseUserActivityValues(licenseUserActivity, userId: 1, licenseType: 3, yearMonth: yearMonth);
-
-                    licenseUsageSummary = licenseUsageInfo.Summary.Find(u => u.YearMonth.Equals(JANUARY_2017));
-                    licenseUserActivity = licenseUsageInfo.UserActivities.Find(u => u.YearMonth.Equals(JANUARY_2017));
-
-                    VerifyLicenseUsageValues(licenseUsageSummary, yearMonth: 201701, uniqueAuthors: 4, uniqueCollaborators: 3, uniqueViewers: 6, usersFromRestApi: 0,
-                        usersFromAnalytics: 0, maxConcurrentAuthors: 2, maxConcurrentCollaborators: 2, maxConcurrentViewers: 3);
-                    VerifyLicenseUserActivityValues(licenseUserActivity, userId: 1, licenseType: 3, yearMonth: JANUARY_2017);
-                }
-            }
-            else
-            {
-                Assert.IsEmpty(licenseUsageInfo.Summary);
-                Assert.IsEmpty(licenseUsageInfo.UserActivities);
-            }
-        }
-
-        /// <summary>
-        /// Verifies that the specified LicenseUsageSummary contains the specified values.
-        /// </summary>
-        /// <param name="licenseUsageSummary">The LicenseUsageSummary to verify.</param>
-        /// <param name="yearMonth">The expected year and month.</param>
-        /// <param name="uniqueAuthors">The expected uniqueAuthors.</param>
-        /// <param name="uniqueCollaborators">(optional) The expected uniqueCollaborators.</param>
-        /// <param name="uniqueViewers">(optional) The expected uniqueViewers.</param>
-        /// <param name="usersFromAnalytics">(optional) The expected usersFromAnalytics.</param>
-        /// <param name="usersFromRestApi">(optional) The expected usersFromRestApi.</param>
-        /// <param name="maxConcurrentAuthors">(optional) The expected maxConcurrentAuthors.</param>
-        /// <param name="maxConcurrentCollaborators">(optional) The expected maxConcurrentCollaborators.</param>
-        /// <param name="maxConcurrentViewers">(optional) The expected maxConcurrentViewers.</param>
-        private static void VerifyLicenseUsageValues(LicenseUsageSummary licenseUsageSummary, int yearMonth, int uniqueAuthors, int uniqueCollaborators = 0,
-            int uniqueViewers = 0, int usersFromAnalytics = 0, int usersFromRestApi = 0, int maxConcurrentAuthors = 1, int maxConcurrentCollaborators = 0,
-            int maxConcurrentViewers = 0)
-        {
-            Assert.AreEqual(yearMonth, licenseUsageSummary.YearMonth, "The yearMonth should be {0}!", yearMonth);
-            Assert.AreEqual(uniqueAuthors, licenseUsageSummary.UniqueAuthors, "UniqueAuthors should be {0}!", uniqueAuthors);
-            Assert.AreEqual(uniqueCollaborators, licenseUsageSummary.UniqueCollaborators, "UniqueCollaborators should be {0}!", uniqueCollaborators);
-            Assert.AreEqual(uniqueViewers, licenseUsageSummary.UniqueViewers, "UniqueCollaborators should be {0}!", uniqueViewers);
-            Assert.AreEqual(maxConcurrentAuthors, licenseUsageSummary.MaxConcurrentAuthors, "MaxConcurrentAuthors should be {0}!", maxConcurrentAuthors);
-            Assert.AreEqual(maxConcurrentCollaborators, licenseUsageSummary.MaxConcurrentCollaborators, "MaxConcurrentCollaborators should be {0}!", maxConcurrentCollaborators);
-            Assert.AreEqual(maxConcurrentViewers, licenseUsageSummary.MaxConcurrentViewers, "MaxConcurrentViewers should be {0}!", maxConcurrentViewers);
-            Assert.AreEqual(usersFromAnalytics, licenseUsageSummary.UsersFromAnalytics, "UsersFromAnalytics should be {0}!", usersFromAnalytics);
-            Assert.AreEqual(usersFromRestApi, licenseUsageSummary.UsersFromRestApi, "UsersFromRestApi should be {0}!", usersFromRestApi);
-        }
-
-        /// <summary>
-        /// Verifies that the specified LicenseUserActivity contains the specified values.
-        /// </summary>
-        /// <param name="licenseUserActivity">The LicenseUserActivity to verify</param>
-        /// <param name="userId">The expected user id</param>
-        /// <param name="licenseType">The expected license type</param>
-        /// <param name="yearMonth">The expected year & month</param>
-        private static void VerifyLicenseUserActivityValues(LicenseUserActivity licenseUserActivity, int userId, int licenseType, int yearMonth)
-        {
-            Assert.AreEqual(userId, licenseUserActivity.UserId, "The User Id should be {0}!", userId);
-            Assert.AreEqual(licenseType, licenseUserActivity.LicenseType, "The User Id should be {0}!", licenseType);
-            Assert.AreEqual(yearMonth, licenseUserActivity.YearMonth, "The User Id should be {0}!", yearMonth);
-        }
-
-        #endregion Private functions
-
         #region GetLicensesInfo tests
 
         [Category(Categories.CannotRunInParallel)] // This test may fail if sessions are created or deleted while it's in progress. 
@@ -382,5 +239,148 @@ namespace AccessControlTests
         }
 
         #endregion GetLicenseUsage tests
+
+        #region Private functions
+
+        /// <summary>
+        /// Adds (POST's) the specified session to the AccessControl service.
+        /// </summary>
+        /// <param name="session">The session to add.</param>
+        /// <returns>The session that was added including the session token.</returns>
+        private ISession AddSessionToAccessControl(ISession session)
+        {
+            // POST the new session.
+            var createdSession = Helper.AccessControl.AddSession(session);
+
+            // Verify that the POST returned the expected session.
+            Assert.True(session.Equals(createdSession),
+                "POST returned a different session than expected!  Got '{0}', but expected '{1}'.",
+                createdSession.SessionId, session.SessionId);
+
+            return createdSession;
+        }
+
+        /// <summary>
+        /// Creates a random session and adds (POST's) it to the AccessControl service.
+        /// </summary>
+        /// <param name="sessionUser">(optional) The user for whom the session is created.</param>
+        /// <returns>The session that was added including the session token.</returns>
+        private ISession CreateAndAddSessionToAccessControl(IUser sessionUser = null)
+        {
+            sessionUser = sessionUser ?? _user;
+            var session = SessionFactory.CreateSession(sessionUser);
+
+            // POST the new session.
+            return AddSessionToAccessControl(session);
+        }
+
+        /// <summary>
+        /// Verifying couple of elements in the response list
+        /// </summary>
+        /// <param name="licenseUsageInfo">License usage information broken down by months</param>
+        /// <param name="expectedEmptyResponse">Pass true if the response should be empty or false if it should contain data.</param>
+        /// <param name="year">The year requested in the REST call.</param>
+        /// <param name="month">The month requested in the REST call.</param>
+        private static void VerifySomeProperties(LicenseUsage licenseUsageInfo, bool expectedEmptyResponse, int? year, int? month)
+        {
+            Assert.IsNotNull(licenseUsageInfo, "License usage information should never be null!");
+
+            if (!expectedEmptyResponse)
+            {
+                Assert.IsNotEmpty(licenseUsageInfo.Summary, "Summary has no values!");
+                Assert.IsNotEmpty(licenseUsageInfo.UserActivities, "UserActivities has no values!");
+
+                const int FIRST_YEAR_IN_DB = 2016;
+                const int FIRST_MONTH_IN_DB = 9;
+                const int JANUARY_2017 = 201701;
+                const int NOVEMBER_2016 = 201611;
+
+                // If we passed null month & year, default to the first month & year in the golden data.
+                year = year ?? FIRST_YEAR_IN_DB;
+                month = month ?? FIRST_MONTH_IN_DB;
+
+                // If the year is before our golden database was created, set month & year to the first ones in the golden DB.
+                if (year < FIRST_YEAR_IN_DB)
+                {
+                    year = FIRST_YEAR_IN_DB;
+                    month = FIRST_MONTH_IN_DB;
+                }
+
+                // First verify that the license usage starts from the month & year we requested.
+                int yearMonth = (int)(year * 100 + month);
+
+                if (yearMonth < JANUARY_2017)
+                {
+                    var licenseUsageSummary = licenseUsageInfo.Summary.Find(u => u.YearMonth.Equals(yearMonth));
+                    var licenseUserActivity = licenseUsageInfo.UserActivities.Find(u => u.YearMonth.Equals(yearMonth));
+
+                    if (yearMonth == NOVEMBER_2016)
+                    {
+                        VerifyLicenseUsageValues(licenseUsageSummary, yearMonth, uniqueAuthors: 2);
+                    }
+                    else
+                    {
+                        VerifyLicenseUsageValues(licenseUsageSummary, yearMonth, uniqueAuthors: 1);
+                    }
+                    VerifyLicenseUserActivityValues(licenseUserActivity, userId: 1, licenseType: 3, yearMonth: yearMonth);
+
+                    licenseUsageSummary = licenseUsageInfo.Summary.Find(u => u.YearMonth.Equals(JANUARY_2017));
+                    licenseUserActivity = licenseUsageInfo.UserActivities.Find(u => u.YearMonth.Equals(JANUARY_2017));
+
+                    VerifyLicenseUsageValues(licenseUsageSummary, yearMonth: 201701, uniqueAuthors: 4, uniqueCollaborators: 3, uniqueViewers: 6, usersFromRestApi: 0,
+                        usersFromAnalytics: 0, maxConcurrentAuthors: 2, maxConcurrentCollaborators: 2, maxConcurrentViewers: 3);
+                    VerifyLicenseUserActivityValues(licenseUserActivity, userId: 1, licenseType: 3, yearMonth: JANUARY_2017);
+                }
+            }
+            else
+            {
+                Assert.IsEmpty(licenseUsageInfo.Summary);
+                Assert.IsEmpty(licenseUsageInfo.UserActivities);
+            }
+        }
+
+        /// <summary>
+        /// Verifies that the specified LicenseUsageSummary contains the specified values.
+        /// </summary>
+        /// <param name="licenseUsageSummary">The LicenseUsageSummary to verify.</param>
+        /// <param name="yearMonth">The expected year and month.</param>
+        /// <param name="uniqueAuthors">The expected uniqueAuthors.</param>
+        /// <param name="uniqueCollaborators">(optional) The expected uniqueCollaborators.</param>
+        /// <param name="uniqueViewers">(optional) The expected uniqueViewers.</param>
+        /// <param name="usersFromAnalytics">(optional) The expected usersFromAnalytics.</param>
+        /// <param name="usersFromRestApi">(optional) The expected usersFromRestApi.</param>
+        /// <param name="maxConcurrentAuthors">(optional) The expected maxConcurrentAuthors.</param>
+        /// <param name="maxConcurrentCollaborators">(optional) The expected maxConcurrentCollaborators.</param>
+        /// <param name="maxConcurrentViewers">(optional) The expected maxConcurrentViewers.</param>
+        private static void VerifyLicenseUsageValues(LicenseUsageSummary licenseUsageSummary, int yearMonth, int uniqueAuthors, int uniqueCollaborators = 0,
+            int uniqueViewers = 0, int usersFromAnalytics = 0, int usersFromRestApi = 0, int maxConcurrentAuthors = 1, int maxConcurrentCollaborators = 0,
+            int maxConcurrentViewers = 0)
+        {
+            Assert.AreEqual(yearMonth, licenseUsageSummary.YearMonth, "The yearMonth should be {0}!", yearMonth);
+            Assert.AreEqual(uniqueAuthors, licenseUsageSummary.UniqueAuthors, "UniqueAuthors should be {0}!", uniqueAuthors);
+            Assert.AreEqual(uniqueCollaborators, licenseUsageSummary.UniqueCollaborators, "UniqueCollaborators should be {0}!", uniqueCollaborators);
+            Assert.AreEqual(uniqueViewers, licenseUsageSummary.UniqueViewers, "UniqueCollaborators should be {0}!", uniqueViewers);
+            Assert.AreEqual(maxConcurrentAuthors, licenseUsageSummary.MaxConcurrentAuthors, "MaxConcurrentAuthors should be {0}!", maxConcurrentAuthors);
+            Assert.AreEqual(maxConcurrentCollaborators, licenseUsageSummary.MaxConcurrentCollaborators, "MaxConcurrentCollaborators should be {0}!", maxConcurrentCollaborators);
+            Assert.AreEqual(maxConcurrentViewers, licenseUsageSummary.MaxConcurrentViewers, "MaxConcurrentViewers should be {0}!", maxConcurrentViewers);
+            Assert.AreEqual(usersFromAnalytics, licenseUsageSummary.UsersFromAnalytics, "UsersFromAnalytics should be {0}!", usersFromAnalytics);
+            Assert.AreEqual(usersFromRestApi, licenseUsageSummary.UsersFromRestApi, "UsersFromRestApi should be {0}!", usersFromRestApi);
+        }
+
+        /// <summary>
+        /// Verifies that the specified LicenseUserActivity contains the specified values.
+        /// </summary>
+        /// <param name="licenseUserActivity">The LicenseUserActivity to verify</param>
+        /// <param name="userId">The expected user id</param>
+        /// <param name="licenseType">The expected license type</param>
+        /// <param name="yearMonth">The expected year & month</param>
+        private static void VerifyLicenseUserActivityValues(LicenseUserActivity licenseUserActivity, int userId, int licenseType, int yearMonth)
+        {
+            Assert.AreEqual(userId, licenseUserActivity.UserId, "The User Id should be {0}!", userId);
+            Assert.AreEqual(licenseType, licenseUserActivity.LicenseType, "The User Id should be {0}!", licenseType);
+            Assert.AreEqual(yearMonth, licenseUserActivity.YearMonth, "The User Id should be {0}!", yearMonth);
+        }
+
+        #endregion Private functions
     }
 }

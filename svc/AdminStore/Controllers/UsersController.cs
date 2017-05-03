df8c3971ff5d3bdbb2b13e11677658e52e20a7a6
Evgeniy Dubovsky <e.dubovsky@softteco.com>
2017-05-03 14:32:33 +0300

    refactoring

--- svc/AdminStore/Controllers/UsersController.cs
+++ svc/AdminStore/Controllers/UsersController.cs
@@ -137,7 +137,7 @@ namespace AdminStore.Controllers
         [SessionRequired]
         [Route("delete")]
         [ResponseType(typeof(IEnumerable<int>))]
-        public async Task<IHttpActionResult> DeleteUsers([FromBody]OperationScope body,[FromUri]string search = null)
+        public async Task<IHttpActionResult> DeleteUsers([FromBody]OperationScope body, string search = null)
         {
             if (body == null || ((body.Ids == null || !body.Ids.Any()) && body.SelectAll == false))
             {

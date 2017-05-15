9994eece45072f7ca322bfd6778f163bfb55b04d
Alex Kim <dhalexkim@gmail.com>
2017-05-15 11:48:49 -0400

    Changes: - updated ConcurrentRestCallTests to use ArtifactWrapper

--- blueprint-automationframework/Blueprint-Tests/Tests/CommonServiceTests/ConcurrentRestCallTests.cs
+++ blueprint-automationframework/Blueprint-Tests/Tests/CommonServiceTests/ConcurrentRestCallTests.cs
@@ -1,11 +1,15 @@
 ﻿using CustomAttributes;
 using Helper;
 using Model;
-using Model.ArtifactModel;
+using Model.ArtifactModel.Enums;
 using Model.ArtifactModel.Impl;
 using Model.Factories;
+using Model.ModelHelpers;
 using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
 using TestCommon;
+using Utilities;
 
 namespace CommonServiceTests
 {
@@ -43,7 +47,7 @@ namespace CommonServiceTests
         [TestCase(50, 50)]
         [Explicit(IgnoreReasons.ManualOnly)]    // Often fails with a deadlock error.  Run manually only.
         [TestRail(154502)]
-        [Description("Tries to lock, discard, save, publish, get artifact info, get properties for Rapid Review, get version, post discussion and delete an artifact.  " +
+        [Description("Tries to lock, discard, save, publish, get artifact info, get properties for Rapid Review, get artifact details, post discussion and delete an artifact.  " +
             "Verifies all REST calls succeeded.")]
         public void MultipleRestCallsForArtifact_ValidParams_Success(int numThreads, int iterations)
         {
@@ -54,12 +58,13 @@ namespace CommonServiceTests
             for (int i = 0; i < numThreads; ++i)
             {
                 var user = Helper.CreateUserAndAuthenticate(TestHelper.AuthenticationTokenTypes.BothAccessControlAndOpenApiTokens);
-                var artifact = Helper.CreateAndPublishArtifact(_project, user, BaseArtifactType.Process);
+                var artifact = Helper.CreateAndPublishNovaArtifact(user, _project, ItemTypePredefined.Actor);
 
                 threadHelper.AddTestFunctionToThread(() =>
                 {
                     LockDiscardSavePublishGetArtifactInfoGetPropertiesForRapidReviewGetVersionPostDiscussionsAndDelete(
-                        user, artifact, Helper.AdminStore);
+                        Helper,
+                        user, artifact);
                 }, iterations);
             }
 
@@ -69,41 +74,43 @@ namespace CommonServiceTests
 
         /// <summary>
         /// Tries to lock, discard, save, publish, get artifact info, get properties for Rapid Review,
-        /// get version, post discussion and delete an artifact.
+        /// get artifact details, post discussion and delete an artifact.
         /// </summary>
+        /// <param name="testHelper">TestHelper reference to which contains adminstore to re-authenticate the user to prevent a long
+        /// running thread's session from expiring.</param>
         /// <param name="user">The user to perform all the REST calls with.</param>
         /// <param name="artifact">The artifact to perform the operations against.</param>
-        /// <param name="adminStore">AdminStore reference to re-authenticate the user to prevent a long running thread's session from expiring.</param>
         /// <exception cref="NUnit.Framework.AssertionException">If any of the REST calls fail.</exception>
         private static void LockDiscardSavePublishGetArtifactInfoGetPropertiesForRapidReviewGetVersionPostDiscussionsAndDelete(
+            TestHelper testHelper,
             IUser user,
-            IArtifact artifact,
-            IAdminStore adminStore)
+            ArtifactWrapper artifact)
         {
-            LockResultInfo lockResultInfo = null;
+            ThrowIf.ArgumentNull(testHelper, nameof(testHelper));
+            ThrowIf.ArgumentNull(user, nameof(user));
+            ThrowIf.ArgumentNull(artifact, nameof(artifact));
 
-            Assert.DoesNotThrow(() => { lockResultInfo = artifact.Lock(user); });
-            Assert.AreEqual(LockResult.Success, lockResultInfo.Result, "The lockResultInfo.Result should be Success!");
+            List<LockResultInfo> lockResultInfo = null;
 
-            Assert.DoesNotThrow(() => { artifact.Discard(user); });
-            Assert.DoesNotThrow(() => { artifact.Save(user); });
-            Assert.DoesNotThrow(() => { artifact.Publish(user); });
+            Assert.DoesNotThrow(() => lockResultInfo = artifact.Lock(user));
+            Assert.AreEqual(LockResult.Success, lockResultInfo.First().Result,
+                "The lockResultInfo.Result should be Success!");
 
-            Assert.DoesNotThrow(() => { artifact.GetArtifactInfo(user); });
-            Assert.DoesNotThrow(() => { artifact.GetRapidReviewArtifactProperties(user); });
-            Assert.DoesNotThrow(() => { artifact.GetVersion(user); });
-            Assert.DoesNotThrow(() => { artifact.PostRapidReviewArtifactDiscussion("Discussion text", user); });
+            Assert.DoesNotThrow(() => artifact.Discard(user));
+            Assert.DoesNotThrow(() => artifact.Lock(user));
+            Assert.DoesNotThrow(() => artifact.SaveWithNewDescription(user));
+            Assert.DoesNotThrow(() => artifact.Publish(user));
 
-            Assert.DoesNotThrow(() => { artifact.Save(user); });
-            NovaPublishArtifactResult publishResult = null;
-            Assert.DoesNotThrow(() => { publishResult = artifact.StorytellerPublish(user); });
-            Assert.AreEqual(NovaPublishArtifactResult.Result.Success, publishResult.StatusCode, "NovaPublish failed!");
+            Assert.DoesNotThrow(() => testHelper.SvcComponents.GetArtifactInfo(artifact.Id, user));
+            Assert.DoesNotThrow(() => testHelper.SvcComponents.GetRapidReviewArtifactsProperties(user, new List<int> { artifact.Id }));
+            Assert.DoesNotThrow(() => testHelper.ArtifactStore.GetArtifactDetails(user, artifact.Id));
+            Assert.DoesNotThrow(() => testHelper.SvcComponents.PostRapidReviewDiscussion(user, artifact.Id, "Discussion text"));
 
-            Assert.DoesNotThrow(() => { artifact.Delete(user); });
-            Assert.DoesNotThrow(() => { artifact.NovaDiscard(user); });
+            Assert.DoesNotThrow(() => artifact.Delete(user));
+            Assert.DoesNotThrow(() => artifact.Discard(user));
 
             // Refresh the token.
-            adminStore.AddSession(user, force: true);
+            testHelper.AdminStore.AddSession(user, force: true);
         }
     }
 }

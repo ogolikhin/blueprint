09689d2ea84fb6f96fe0457025d2ea4bf8a8098c
Darren Martell <darren.martell@blueprintsys.com>
2017-05-24 12:03:55 -0300

    First commit for GetInstanceUsers

--- blueprint-automationframework/Blueprint-Tests/Model/IAdminStore.cs
+++ blueprint-automationframework/Blueprint-Tests/Model/IAdminStore.cs
@@ -168,19 +168,19 @@ namespace Model
         /// property1={value}..., propertyN={value}, search={string}
         /// </summary>
         /// <param name="adminUser">The admin user getting the users.</param>
-        /// <param name="offset">(optional) 0-based index of the first item to return. The default is 0.</param>
+        /// <param name="offset">(optional) 0-based index of the first item to return (Default: null).</param>
         /// <param name="limit">(optional) Maximum number of items to return (if any). 
-        /// The server may return fewer items than requested.</param>
+        /// The server may return fewer items than requested.  (Default: null)</param>
         /// <param name="sort">(optional) Property name by which to sort results.</param>
-        /// <param name="order">(optional) "asc" sorts in ascending order; "desc" sorts in descending order. 
+        /// <param name="order">(optional) "asc" sorts in ascending order; "desc" sorts in descending order (Default: Desc). 
         /// The default order depends on the particular property.</param>
         /// <param name="search">(optional) Search query that would be applied on predefined properties specific to data.</param>
-        /// <returns>A list of InstanceUser objects</returns>
-        List<InstanceUser> GetUsers(IUser adminUser,
+        /// <returns>A QueryResult object of InstanceUser objects</returns>
+        QueryResult<InstanceUser> GetUsers(IUser adminUser,
             int? offset = null,
             int? limit = null,
             string sort = null,
-            SortOrder? order = null,
+            SortOrder? order = SortOrder.Desc,
             string search = null);
 
         /// <summary>

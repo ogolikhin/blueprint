fbb6fe81bdd797fcbb8aa2f6ac50659ab8cc992d
Roman Dolgov <roman.dolgov@blueprintsys.com>
2017-05-25 17:14:45 -0400

    Updates message for missing pagination

--- svc/lib/ServiceLibrary/Helpers/ErrorMessages.cs
+++ svc/lib/ServiceLibrary/Helpers/ErrorMessages.cs
@@ -3,7 +3,7 @@
     public class ErrorMessages
     {
         //Users
-        public const string InvalidPagination = "Pagination object is invalid.";
+        public const string InvalidPagination = "The \"offset\" and/or \"limit\" parameters are required.";
         public static readonly string LoginRequired = "The \"Login\" field is required.";
         public static readonly string DisplayNameRequired = "The \"Display name\" field is required.";
         public static readonly string FirstNameRequired = "The \"First name\" field is required.";

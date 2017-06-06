d3c0931e51fe45df0baa877828f215daa70fd78c
Eugen Goroluov <eugen@ea-code.com>
2017-06-06 14:11:13 +0300

    Added Id in GrouypUserModel.

--- svc/AdminStore/Models/GroupUser.cs
+++ svc/AdminStore/Models/GroupUser.cs
@@ -4,6 +4,7 @@ namespace AdminStore.Models
 {
     public class GroupUser
     {
+        public int Id { get; set; }
         public string DisplayName { get; set; }
         public string Email { get; set; }
         public string UserName { get; set; }

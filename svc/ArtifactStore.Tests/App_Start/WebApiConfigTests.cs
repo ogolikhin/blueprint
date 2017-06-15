9f42a138a1304687caa1f553e6252e1a45c0222b
Anton Trinkunas <anton.trinkunas@blueprintsys.com>
2017-06-15 10:49:00 -0400

    Build fix. Unit tests updated

--- svc/ArtifactStore.Tests/App_Start/WebApiConfigTests.cs
+++ svc/ArtifactStore.Tests/App_Start/WebApiConfigTests.cs
@@ -22,7 +22,7 @@ namespace ArtifactStore
             config.EnsureInitialized();
 
             // Assert
-            config.AssertTotalRoutes(29, "Please update asserts in WebApiConfigTests when changing routes.");
+            config.AssertTotalRoutes(30, "Please update asserts in WebApiConfigTests when changing routes.");
             config.AssertAction<StatusController>("GetStatus", HttpMethod.Get, "status");
             config.AssertAction<StatusController>("GetStatusUpCheck", HttpMethod.Get, "status/upcheck");
             config.AssertAction<ArtifactController>("GetProjectChildren", HttpMethod.Get, "projects/1/children");
@@ -50,6 +50,7 @@ namespace ArtifactStore
             config.AssertAction<ReviewContainersController>("GetTableOfContentAsync", HttpMethod.Get, "containers/1/toc/1");
             config.AssertAction<ReviewContainersController>("AddArtifactsToReview", HttpMethod.Put, "containers/1/content");
             config.AssertAction<ReviewContainersController>("GetReviewedArtifacts", HttpMethod.Get, "containers/1/artifacts");
+            config.AssertAction<ReviewContainersController>("AssignApprovalRequiredToArtifacts", HttpMethod.Put, "containers/1/artifacts/approval");
         }
 
         [TestMethod]

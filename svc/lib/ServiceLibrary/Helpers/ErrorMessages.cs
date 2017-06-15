d07fff5a3f5a5c19921609be9217197c343bcf2e
Roman Dolgov <roman.dolgov@blueprintsys.com>
2017-06-15 14:45:04 -0400

    Validates GroupName length

--- svc/lib/ServiceLibrary/Helpers/ErrorMessages.cs
+++ svc/lib/ServiceLibrary/Helpers/ErrorMessages.cs
@@ -48,8 +48,8 @@
         public static readonly string IncorrectBase64FormatPasswordField = "The password is not a valid Base-64 string as it contains a non-base 64 character, more than two padding characters, or an illegal character among the padding characters";
         //Groups
         public static readonly string GroupModelIsEmpty = "The group model is empty.";
-        public static readonly string GroupName = "Please enter a Group Name.";
-        public static readonly string GroupNameFieldLimitation = "The length of the \"Group Name\" field must be between 4 and 255 characters.";
+        public static readonly string GroupNameRequired = "Please enter a Group Name.";
+        public static readonly string GroupNameFieldLimitation = "The length of the \"Group Name\" field must be between 1 and 255 characters.";
         public static readonly string GroupEmailFieldLimitation = "The length of the \"Group Email\" field must be between 4 and 255 characters.";
         public static readonly string GeneralErrorOfCreatingGroup = "An error has occurred when the operation was being performed at SQL level.";
         public static readonly string CreationOnlyDatabaseGroup = "You can create only database group.";

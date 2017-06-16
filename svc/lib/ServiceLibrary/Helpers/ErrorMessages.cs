a555933bb9e97c0bd09760590107b81bc7d0cfdb
Eugen Goroluov <eugen@ea-code.com>
2017-06-16 11:59:21 +0300

    Trello bug

--- svc/lib/ServiceLibrary/Helpers/ErrorMessages.cs
+++ svc/lib/ServiceLibrary/Helpers/ErrorMessages.cs
@@ -56,7 +56,6 @@
         public static readonly string CreationGroupsOnlyWithCollaboratorOrAuthorOrNoneLicenses = "You can create groups only with the \"collaborator\", the \"author\" or empty license.";
         public static readonly string CreationGroupWithScopeAndLicenseIdSimultaneously = "You can not create the \"Access Group\" and \"License Group\" simultaneously.";
         public static readonly string GroupAlreadyExist = "An existing group has the same name. Please try another.";
-        public static readonly string GroupDoesNotExist = "The group with this Id does not exist or removed from the system.";
         public static readonly string SourceFieldValueForGroupsShouldBeOnlyDatabase = "You can modify only database groups.";
         public static readonly string TheScopeCannotBeChanged = "The scope cannot be changed.";
         public static readonly string UpdateGroupsOnlyWithCollaboratorOrAuthorOrNoneLicenses = "You can modify groups only with the \"collaborator\", the \"author\" or empty license values.";

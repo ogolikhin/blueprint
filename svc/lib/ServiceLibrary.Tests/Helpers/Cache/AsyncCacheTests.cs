f18fb3184772ff3e1cbcf2ea2af532020a4c98f5
Dmitry Agapitov <dmitry.agapitov@blueprintsys.com>
2017-06-19 14:58:45 -0400

    Add async cache utility
    This is just an utility class that will be used to cache sessions in the web application.

---
+++ svc/lib/ServiceLibrary.Tests/Helpers/Cache/AsyncCacheTests.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Runtime.Caching;
+using System.Threading.Tasks;
+
+namespace ServiceLibrary.Helpers.Cache
+{
+    [TestClass]
+    public class AsyncCacheTests
+    {
+        private Mock<ObjectCache> _cacheMock;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _cacheMock = new Mock<ObjectCache>();
+        }
+
+        [TestMethod]
+        public async Task AddOrGetExystingAsync_ItemNotCached_ItemFactoryCalledAndResultCached()
+        {
+            // Arrange
+            var key = "this-is-the-key";
+            var value = "this-is-the-value";
+            var cacheItemPolicy = new CacheItemPolicy() { };
+            var isFactoryCalled = false;
+            Func<Task<string>> func = () =>
+            {
+                isFactoryCalled = true;
+                return Task.FromResult(value);
+            };
+
+            _cacheMock
+                //.Setup(c => c.AddOrGetExisting(It.IsAny<string>(), It.IsAny<object>(), It.IsAny<CacheItemPolicy>(), null))
+                .Setup(c => c.AddOrGetExisting(key, It.IsAny<AsyncLazy<string>>(), cacheItemPolicy, null))
+                .Returns(null);
+
+            var cache = new AsyncCache(_cacheMock.Object);
+
+            // Act
+            var result = await cache.AddOrGetExystingAsync(key, func, cacheItemPolicy);
+
+            // Asserts
+            Assert.AreEqual(value, result);
+            Assert.IsTrue(isFactoryCalled);
+            _cacheMock.VerifyAll();
+        }
+
+        [TestMethod]
+        public async Task AddOrGetExystingAsync_AbsoluteExpiration_ItemNotCached_ItemFactoryCalledAndResultCached()
+        {
+            // Arrange
+            var key = "this-is-the-key";
+            var value = "this-is-the-value";
+            var absoluteExpiration = DateTime.UtcNow.AddSeconds(30);
+            Func<Task<string>> func = () => Task.FromResult(value);
+
+            _cacheMock
+                .Setup(c => c.AddOrGetExisting(key, It.IsAny<AsyncLazy<string>>(), It.Is<CacheItemPolicy>(policy => policy.AbsoluteExpiration == absoluteExpiration), null))
+                .Returns(null);
+
+            var cache = new AsyncCache(_cacheMock.Object);
+
+            // Act
+            var result = await cache.AddOrGetExystingAsync(key, func, absoluteExpiration);
+
+            // Asserts
+            Assert.AreEqual(value, result);
+            _cacheMock.VerifyAll();
+        }
+
+        [TestMethod]
+        public async Task AddOrGetExystingAsync_ItemCached_ItemFactoryNotCalled()
+        {
+            // Arrange
+            var key = "this-is-the-key";
+            var value = "this-is-the-value";
+            var cacheItemPolicy = new CacheItemPolicy() { };
+            var isFactoryCalled = false;
+            var cachedLazy = new AsyncLazy<string>(() => Task.FromResult(value));
+            Func<Task<string>> func = () =>
+            {
+                isFactoryCalled = true;
+                return Task.FromResult(value);
+            };
+
+            _cacheMock
+                .Setup(c => c.AddOrGetExisting(key, It.IsAny<AsyncLazy<string>>(), cacheItemPolicy, null))
+                .Returns(cachedLazy);
+
+            var cache = new AsyncCache(_cacheMock.Object);
+
+            // Act
+            var result = await cache.AddOrGetExystingAsync(key, func, cacheItemPolicy);
+
+            // Asserts
+            Assert.AreEqual(value, result);
+            Assert.IsFalse(isFactoryCalled);
+            _cacheMock.VerifyAll();
+        }
+
+        [TestMethod]        
+        public async Task AddOrGetExystingAsync_ItemNotCached_ItemFactoryFailed_ExceptionRetrown()
+        {
+            // Arrange
+            var key = "this-is-the-key";
+            var cacheItemPolicy = new CacheItemPolicy() { };
+            Func<Task<string>> func = async () => {
+                await Task.Delay(1);
+                throw new ArgumentNullException();
+            };
+
+            _cacheMock
+                .Setup(c => c.AddOrGetExisting(key, It.IsAny<AsyncLazy<string>>(), cacheItemPolicy, null))
+                .Returns(null);
+
+            var cache = new AsyncCache(_cacheMock.Object);
+
+            // Act
+            var result = cache.AddOrGetExystingAsync(key, func, cacheItemPolicy);
+
+            // Asserts
+            try
+            {
+                await result;
+
+                Assert.Fail("ItemFactory fail is not raised");
+            }
+            catch (ArgumentNullException)
+            {
+
+            }
+            _cacheMock.Verify(c => c.Remove(key, null), Times.Once);
+        }
+    }
+}
---
+++ svc/lib/ServiceLibrary/Helpers/Cache/AsyncCache.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Runtime.Caching;
+using System.Threading.Tasks;
+
+namespace ServiceLibrary.Helpers.Cache
+{
+    public class AsyncCache: IAsyncCache
+    {
+        protected ObjectCache Cache { get; }
+
+        public AsyncCache(ObjectCache cache)
+        {
+            if (cache == null)
+            {
+                throw new ArgumentNullException(nameof(cache));
+            }
+
+            Cache = cache;
+        }
+
+        public async Task<T> AddOrGetExystingAsync<T>(string key, Func<Task<T>> asyncValueFactory, CacheItemPolicy policy)
+        {
+            ValidateKey(key);
+
+            var newLazyCacheItem = new AsyncLazy<T>(asyncValueFactory);
+
+            EnsureRemovedCallbackDoesNotReturnTheAsyncLazy<T>(policy);
+
+            var existingCacheItem = Cache.AddOrGetExisting(key, newLazyCacheItem, policy);
+
+            if (existingCacheItem != null)
+            {
+                return await UnwrapAsyncLazys<T>(existingCacheItem);
+            }
+
+            try
+            {
+                var result = newLazyCacheItem.Value;
+
+                if (result.IsCanceled || result.IsFaulted)
+                {
+                    Cache.Remove(key);
+                }
+
+                return await result;
+            }
+            catch
+            {
+                Cache.Remove(key);
+                throw;
+            }
+        }
+
+        public Task<T> AddOrGetExystingAsync<T>(string key, Func<Task<T>> asyncValueFactory, DateTimeOffset absoluteExpiration)
+        {
+            return AddOrGetExystingAsync(key, asyncValueFactory, new CacheItemPolicy { AbsoluteExpiration = absoluteExpiration });
+        }
+
+        public Task<T> AddOrGetExystingAsync<T>(string key, Func<Task<T>> asyncValueFactory, TimeSpan slidingExpiration)
+        {
+            return AddOrGetExystingAsync(key, asyncValueFactory, new CacheItemPolicy { SlidingExpiration = slidingExpiration });
+        }
+
+        public void Remove(string key)
+        {
+            ValidateKey(key);
+            Cache.Remove(key);
+        }
+
+        private static async Task<T> UnwrapAsyncLazys<T>(object item)
+        {
+            var asyncLazy = item as AsyncLazy<T>;
+            if (asyncLazy != null)
+            {
+                return await asyncLazy.Value;
+            }
+
+            var task = item as Task<T>;
+            if (task != null)
+            {
+                return await task;
+            }
+
+            var lazy = item as Lazy<T>;
+            if (lazy != null)
+            {
+                return lazy.Value;
+            }
+
+            if (item is T)
+            {
+                return (T)item;
+            }
+
+            return default(T);
+        }
+
+        private static void EnsureRemovedCallbackDoesNotReturnTheAsyncLazy<T>(CacheItemPolicy policy)
+        {
+            if (policy?.RemovedCallback != null)
+            {
+                var originallCallback = policy.RemovedCallback;
+                policy.RemovedCallback = args =>
+                {
+                    //unwrap the cache item in a callback given one is specified
+                    var item = args?.CacheItem?.Value as AsyncLazy<T>;
+                    if (item != null)
+                    {
+                        args.CacheItem.Value = item.IsValueCreated ? item.Value : Task.FromResult(default(T));
+                    }
+                    originallCallback(args);
+                };
+            }
+        }
+
+        private void ValidateKey(string key)
+        {
+            if (string.IsNullOrWhiteSpace(key))
+            {
+                throw new ArgumentOutOfRangeException(nameof(key), "Cache keys cannot be null, empty or whitespace");
+            }
+        }
+    }
+}

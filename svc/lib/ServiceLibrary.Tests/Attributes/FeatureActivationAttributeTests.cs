e2e406c4afd4b6edff733c1dbc6cdcf15c9905fa
ssnowdon <ssnowdon@VM1102PC>
2017-06-21 17:59:41 -0400

    us7551 unit tests

---
+++ svc/lib/ServiceLibrary.Tests/Attributes/FeatureActivationAttributeTests.cs
@@ -0,0 +1,117 @@
+﻿using System.Net;
+using System.Web.Http.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using ServiceLibrary.Helpers;
+using ServiceLibrary.Models.Enums;
+
+namespace ServiceLibrary.Attributes
+{
+    /// <summary>
+    /// Summary description for FeatureActivationAttributeTests
+    /// </summary>
+    [TestClass]
+    public class FeatureActivationAttributeTests
+    {
+        private Mock<IFeatureLicenseHelper> _featureLicenseHelperMock;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _featureLicenseHelperMock = new Mock<IFeatureLicenseHelper>(MockBehavior.Strict);
+        }
+
+        [TestMethod]
+        public void IfRequiredLicenseIsNotFoundThenResponseCodeIsForbidden()
+        {
+            //Arrange
+            var requiredLicense = FeatureTypes.Workflow;
+            var validLicense = FeatureTypes.Storyteller;
+            _featureLicenseHelperMock.Setup(i => i.GetValidBlueprintLicenseFeatures()).Returns(validLicense);
+            var attribute = new FeatureActivationAttribute(requiredLicense, _featureLicenseHelperMock.Object);
+            var context = new HttpActionContext();
+            //Act
+            attribute.OnActionExecuting(context);
+            //Assert
+            Assert.IsNotNull(context.Response);
+            Assert.AreEqual(HttpStatusCode.Forbidden, context.Response.StatusCode);
+        }
+
+        [TestMethod]
+        public void IfOneOfTheRequiredLicensesIsNotFoundThenResponseCodeIsForbidden()
+        {
+            //Arrange
+            var requiredLicenses = FeatureTypes.Workflow | FeatureTypes.Storyteller;
+            var validLicenses = FeatureTypes.Workflow | FeatureTypes.Blueprint;
+            _featureLicenseHelperMock.Setup(i => i.GetValidBlueprintLicenseFeatures()).Returns(validLicenses);
+            var attribute = new FeatureActivationAttribute(requiredLicenses, _featureLicenseHelperMock.Object);
+            var context = new HttpActionContext();
+            //Act
+            attribute.OnActionExecuting(context);
+            //Assert
+            Assert.IsNotNull(context.Response);
+            Assert.AreEqual(HttpStatusCode.Forbidden, context.Response.StatusCode);
+        }
+
+        [TestMethod]
+        public void IfAllRequiredLicensesAreNotFoundThenResponseCodeIsForbidden()
+        {
+            //Arrange
+            var requiredLicenses = FeatureTypes.Workflow | FeatureTypes.Storyteller;
+            var validLicenses = FeatureTypes.BlueprintOpenApi | FeatureTypes.Blueprint;
+            _featureLicenseHelperMock.Setup(i => i.GetValidBlueprintLicenseFeatures()).Returns(validLicenses);
+            var attribute = new FeatureActivationAttribute(requiredLicenses, _featureLicenseHelperMock.Object);
+            var context = new HttpActionContext();
+            //Act
+            attribute.OnActionExecuting(context);
+            //Assert
+            Assert.IsNotNull(context.Response);
+            Assert.AreEqual(HttpStatusCode.Forbidden, context.Response.StatusCode);
+        }
+
+        [TestMethod]
+        public void IfRequiredLicenseIsFoundThenResponseIsNull()
+        {
+            //Arrange
+            var requiredLicense = FeatureTypes.Workflow;
+            var validLicense = FeatureTypes.Workflow;
+            _featureLicenseHelperMock.Setup(i => i.GetValidBlueprintLicenseFeatures()).Returns(validLicense);
+            var attribute = new FeatureActivationAttribute(requiredLicense, _featureLicenseHelperMock.Object);
+            var context = new HttpActionContext();
+            //Act
+            attribute.OnActionExecuting(context);
+            //Assert
+            Assert.IsNull(context.Response);
+        }
+
+        [TestMethod]
+        public void IfAllRequiredLicensesAreFoundThenResponseIsNull()
+        {
+            //Arrange
+            var requiredLicenses = FeatureTypes.Workflow | FeatureTypes.Storyteller;
+            var validLicenses = FeatureTypes.Workflow | FeatureTypes.Storyteller;
+            _featureLicenseHelperMock.Setup(i => i.GetValidBlueprintLicenseFeatures()).Returns(validLicenses);
+            var attribute = new FeatureActivationAttribute(requiredLicenses, _featureLicenseHelperMock.Object);
+            var context = new HttpActionContext();
+            //Act
+            attribute.OnActionExecuting(context);
+            //Assert
+            Assert.IsNull(context.Response);
+        }
+
+        [TestMethod]
+        public void IfMoreThanAllRequiredLicensesAreFoundThenResponseIsNull()
+        {
+            //Arrange
+            var requiredLicenses = FeatureTypes.Workflow | FeatureTypes.Storyteller;
+            var validLicenses = FeatureTypes.Workflow | FeatureTypes.Storyteller | FeatureTypes.Blueprint;
+            _featureLicenseHelperMock.Setup(i => i.GetValidBlueprintLicenseFeatures()).Returns(validLicenses);
+            var attribute = new FeatureActivationAttribute(requiredLicenses, _featureLicenseHelperMock.Object);
+            var context = new HttpActionContext();
+            //Act
+            attribute.OnActionExecuting(context);
+            //Assert
+            Assert.IsNull(context.Response);
+        }
+    }
+}
---
+++ svc/lib/ServiceLibrary.Tests/Models/Licenses/FeatureInformationTests.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using ServiceLibrary.Helpers;
+using ServiceLibrary.Models.Enums;
+
+namespace ServiceLibrary.Models.Licenses
+{
+    /// <summary>
+    /// Summary description for FeatureInformationTests
+    /// </summary>
+    [TestClass]
+    public class FeatureInformationTests
+    {
+        private const string WorkflowFeatureName = "Workflow";
+        private Mock<ITimeProvider> _timeProviderMock;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _timeProviderMock = new Mock<ITimeProvider>(MockBehavior.Strict);
+        }
+
+        [TestMethod]
+        public void StatusIsActiveWhenExpirationDateIsUpcoming()
+        {
+            //Arrange
+            var expirationDate = DateTime.MaxValue;
+            var featureInformation = new FeatureInformation(WorkflowFeatureName, expirationDate);
+            //Act
+            var status = featureInformation.GetStatus();
+            //Assert
+            Assert.AreEqual(FeatureLicenseStatus.Active, status);
+        }
+
+        [TestMethod]
+        public void StatusIsActiveWhenExpirationDateIsEqual()
+        {
+            //Arrange
+            var expirationDate = new DateTime(2000, 1, 1);
+            _timeProviderMock.Setup(i => i.CurrentUniversalTime).Returns(expirationDate);
+            var featureInformation = new FeatureInformation(WorkflowFeatureName, expirationDate, _timeProviderMock.Object);
+            //Act
+            var status = featureInformation.GetStatus();
+            //Assert
+            Assert.AreEqual(FeatureLicenseStatus.Active, status);
+        }
+
+        [TestMethod]
+        public void StatusIsExpiredWhenExpirationDateHasPassed()
+        {
+            //Arrange
+            var expirationDate = DateTime.MinValue;
+            var featureInformation = new FeatureInformation(WorkflowFeatureName, expirationDate);
+            //Act
+            var status = featureInformation.GetStatus();
+            //Assert
+            Assert.AreEqual(FeatureLicenseStatus.Expired, status);
+        }
+
+        [TestMethod]
+        public void GetFeatureTypeReturnsProperFeatureType()
+        {
+            //Arrange
+            var featureInformation = new FeatureInformation(WorkflowFeatureName, DateTime.UtcNow);
+            //Act
+            var featureType = featureInformation.GetFeatureType();
+            //Assert
+            Assert.AreEqual(FeatureTypes.Workflow, featureType);
+        }
+    }
+}

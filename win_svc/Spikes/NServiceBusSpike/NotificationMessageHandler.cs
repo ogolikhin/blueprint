c04824705678c21cb7a0b7843ffb752c2a5d3edd
munishsaini <munish.saini@blueprintsys.com>
2017-07-10 21:57:40 -0400

    [dev-399] adding nservicebus spike to win_svc

---
+++ win_svc/Spikes/NServiceBusSpike/NotificationMessageHandler.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using NServiceBus;
+
+namespace NServiceBusSpike
+{
+    public class NotificationMessage : IMessage
+    {
+        public Guid NotificationId { get; set; }
+
+        public override string ToString()
+        {
+            return NotificationId.ToString();
+        }
+    }
+
+    public class NotificationMessageHandler : IHandleMessages<NotificationMessage>
+    {
+        private static int _messagesHandled;
+        public Task Handle(NotificationMessage message, IMessageHandlerContext context)
+        {
+            Console.WriteLine(message);
+            _messagesHandled++;
+
+            var foreColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"Handled {_messagesHandled} notification messages");
+
+            Interlocked.Increment(ref Program.TotalMessagesHandled);
+            Console.WriteLine($"Handled {Program.TotalMessagesHandled} total messages");
+
+            Console.ForegroundColor = foreColor;
+
+            var task = Task.Factory.StartNew(() => { });
+
+            Program.StateChangeMessageScheduler.Request(new StateChangeMessage()
+            {
+                ArtifactId = Guid.NewGuid()
+            });
+
+            return task;
+        }
+    }
+}
---
+++ win_svc/Spikes/NServiceBusSpike/StateChangeMessageHandler.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using NServiceBus;
+
+namespace NServiceBusSpike
+{
+    public class StateChangeMessage : IMessage
+    {
+        public Guid ArtifactId { get; set; }
+
+        public override string ToString()
+        {
+            return ArtifactId.ToString();
+        }
+    }
+
+    public class StateChangeMessageHandler : IHandleMessages<StateChangeMessage>
+    {
+        private static int _messagesHandled;
+
+        public Task Handle(StateChangeMessage message, IMessageHandlerContext context)
+        {
+            Console.WriteLine(message);
+            _messagesHandled++;
+
+            var foreColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"Handled {_messagesHandled} Statechange messages");
+
+            Interlocked.Increment(ref Program.TotalMessagesHandled);
+            Console.WriteLine($"Handled {Program.TotalMessagesHandled} total messages");
+
+            Console.ForegroundColor = foreColor;
+
+            var task = Task.Factory.StartNew(() => { });
+
+            Program.NotificationMessageScheduler.Request(new NotificationMessage
+            {
+                NotificationId = Guid.NewGuid()
+            });
+
+            return task;
+        }
+    }
+}

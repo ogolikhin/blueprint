31c23d7187072255d835ea4a0579e7a6b9524e2d
Evgeniy Dubovsky <e.dubovsky@softteco.com>
2017-07-26 12:01:25 +0300

    improvements

--- svc/AdminStore/Controllers/InstanceController.cs
+++ svc/AdminStore/Controllers/InstanceController.cs
@@ -91,8 +91,14 @@ namespace AdminStore.Controllers
             return await _instanceRepository.GetInstanceFolderChildrenAsync(id, Session.UserId);
         }
 
-        [HttpGet]
-        [NoCache]
+        /// <summary>
+        /// Search folder by name
+        /// </summary>
+        /// <param name="name">name of folder</param>
+        /// <response code="200">OK.</response>
+        /// <response code="401">Unauthorized. The session token is invalid, missing or malformed.</response>
+        /// <response code="500">Internal Server Error. An error occurred.</response>
+        [HttpGet,NoCache]
         [Route("foldersearch"), SessionRequired]
         [ResponseType(typeof(IEnumerable<FolderDto>))]
         public async Task<IHttpActionResult> SearchFolderByName(string name)

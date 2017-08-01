4ac4b96f4af51418097b3687458023772472529e
ssnowdon <ssnowdon@VM1102PC>
2017-08-01 17:13:07 -0400

    DEV-552 added unit tests for the FeatureActivationAttribute

--- svc/lib/ServiceLibrary.Tests/Attributes/FeatureActivationAttributeTests.cs
+++ svc/lib/ServiceLibrary.Tests/Attributes/FeatureActivationAttributeTests.cs
@@ -14,60 +14,165 @@ namespace ServiceLibrary.Attributes
     [TestClass]
     public class FeatureActivationAttributeTests
     {
-        private Mock<IFeatureLicenseHelper> _featureLicenseHelperMock;
+        private HttpActionContext _context;
 
         [TestInitialize]
         public void TestInitialize()
         {
-            _featureLicenseHelperMock = new Mock<IFeatureLicenseHelper>(MockBehavior.Strict);
+            _context = HttpFilterHelper.CreateHttpActionContext(new HttpRequestMessage(HttpMethod.Get, string.Empty));
         }
 
         [TestMethod]
-        public void OnActionExecuting_WhenRequiredLicenseIsNotFound_ResponseCodeIsForbidden()
+        public void FeatureActivationAttribute_InstantiatesSuccessfully()
+        {
+            var attribute = new FeatureActivationAttribute(FeatureTypes.Workflow);
+            Assert.IsNotNull(attribute);
+        }
+
+        [TestMethod]
+        public void OnActionExecuting_WhenRequiredWorkflowLicenseIsNotFound_ContentContainsWorkflowLicenseErrorCode()
         {
             //Arrange
             var requiredLicense = FeatureTypes.Workflow;
+            var licenseErrorCode = ErrorCodes.WorkflowLicenseUnavailable;
             var validLicense = FeatureTypes.Storyteller;
-            _featureLicenseHelperMock.Setup(i => i.GetValidBlueprintLicenseFeatures()).Returns(validLicense);
-            var attribute = new FeatureActivationAttribute(requiredLicense, _featureLicenseHelperMock.Object);
-            var context = HttpFilterHelper.CreateHttpActionContext(new HttpRequestMessage(HttpMethod.Get, ""));
+            var attribute = CreateAttribute(validLicense, requiredLicense);
+
             //Act
-            attribute.OnActionExecuting(context);
+            attribute.OnActionExecuting(_context);
+
             //Assert
-            Assert.IsNotNull(context.Response);
-            Assert.AreEqual(HttpStatusCode.Forbidden, context.Response.StatusCode);
+            ValidateResponse(licenseErrorCode);
+        }
+
+        private FeatureActivationAttribute CreateAttribute(FeatureTypes validLicense, FeatureTypes requiredLicense)
+        {
+            var featureLicenseHelperMock = new Mock<IFeatureLicenseHelper>(MockBehavior.Strict);
+            featureLicenseHelperMock.Setup(i => i.GetValidBlueprintLicenseFeatures()).Returns(validLicense);
+            return new FeatureActivationAttribute(requiredLicense, featureLicenseHelperMock.Object);
+        }
+
+        private void ValidateResponse(int licenseErrorCode)
+        {
+            //Response should not be null
+            Assert.IsNotNull(_context.Response);
+            //Status Code should be Forbidden
+            Assert.AreEqual(HttpStatusCode.Forbidden, _context.Response.StatusCode);
+            //Content should contain the Error Code
+            var contentResult = _context.Response.Content.ReadAsStringAsync().Result;
+            Assert.IsTrue(contentResult.Contains(licenseErrorCode.ToStringInvariant()));
         }
 
         [TestMethod]
-        public void OnActionExecuting_WhenOneOfTheRequiredLicensesIsNotFound_ResponseCodeIsForbidden()
+        public void OnActionExecuting_WhenRequiredStorytellerLicenseIsNotFound_ContentContainsLicenseErrorCode()
         {
             //Arrange
-            var requiredLicenses = FeatureTypes.Workflow | FeatureTypes.Storyteller;
-            var validLicenses = FeatureTypes.Workflow | FeatureTypes.Blueprint;
-            _featureLicenseHelperMock.Setup(i => i.GetValidBlueprintLicenseFeatures()).Returns(validLicenses);
-            var attribute = new FeatureActivationAttribute(requiredLicenses, _featureLicenseHelperMock.Object);
-            var context = HttpFilterHelper.CreateHttpActionContext(new HttpRequestMessage(HttpMethod.Get, ""));
+            var requiredLicense = FeatureTypes.Storyteller;
+            var licenseErrorCode = ErrorCodes.LicenseUnavailable;
+            var validLicense = FeatureTypes.Blueprint;
+            var attribute = CreateAttribute(validLicense, requiredLicense);
+
             //Act
-            attribute.OnActionExecuting(context);
+            attribute.OnActionExecuting(_context);
+
             //Assert
-            Assert.IsNotNull(context.Response);
-            Assert.AreEqual(HttpStatusCode.Forbidden, context.Response.StatusCode);
+            ValidateResponse(licenseErrorCode);
         }
 
         [TestMethod]
-        public void OnActionExecuting_WhenAllRequiredLicensesAreNotFound_ResponseCodeIsForbidden()
+        public void OnActionExecuting_WhenRequiredBlueprintLicenseIsNotFound_ContentContainsLicenseErrorCode()
         {
             //Arrange
-            var requiredLicenses = FeatureTypes.Workflow | FeatureTypes.Storyteller;
-            var validLicenses = FeatureTypes.BlueprintOpenApi | FeatureTypes.Blueprint;
-            _featureLicenseHelperMock.Setup(i => i.GetValidBlueprintLicenseFeatures()).Returns(validLicenses);
-            var attribute = new FeatureActivationAttribute(requiredLicenses, _featureLicenseHelperMock.Object);
-            var context = HttpFilterHelper.CreateHttpActionContext(new HttpRequestMessage(HttpMethod.Get, ""));
+            var requiredLicense = FeatureTypes.Blueprint;
+            var licenseErrorCode = ErrorCodes.LicenseUnavailable;
+            var validLicense = FeatureTypes.Storyteller;
+            var attribute = CreateAttribute(validLicense, requiredLicense);
+
             //Act
-            attribute.OnActionExecuting(context);
+            attribute.OnActionExecuting(_context);
+
             //Assert
-            Assert.IsNotNull(context.Response);
-            Assert.AreEqual(HttpStatusCode.Forbidden, context.Response.StatusCode);
+            ValidateResponse(licenseErrorCode);
+        }
+
+        [TestMethod]
+        public void OnActionExecuting_WhenRequiredBlueprintOpenApiLicenseIsNotFound_ContentContainsLicenseErrorCode()
+        {
+            //Arrange
+            var requiredLicense = FeatureTypes.BlueprintOpenApi;
+            var licenseErrorCode = ErrorCodes.LicenseUnavailable;
+            var validLicense = FeatureTypes.Storyteller;
+            var attribute = CreateAttribute(validLicense, requiredLicense);
+
+            //Act
+            attribute.OnActionExecuting(_context);
+
+            //Assert
+            ValidateResponse(licenseErrorCode);
+        }
+
+        [TestMethod]
+        public void OnActionExecuting_WhenRequiredHewlettPackardQCIntegrationLicenseIsNotFound_ContentContainsLicenseErrorCode()
+        {
+            //Arrange
+            var requiredLicense = FeatureTypes.HewlettPackardQCIntegration;
+            var licenseErrorCode = ErrorCodes.LicenseUnavailable;
+            var validLicense = FeatureTypes.Storyteller;
+            var attribute = CreateAttribute(validLicense, requiredLicense);
+
+            //Act
+            attribute.OnActionExecuting(_context);
+
+            //Assert
+            ValidateResponse(licenseErrorCode);
+        }
+
+        [TestMethod]
+        public void OnActionExecuting_WhenRequiredMicrosoftTfsIntegrationLicenseIsNotFound_ContentContainsLicenseErrorCode()
+        {
+            //Arrange
+            var requiredLicense = FeatureTypes.MicrosoftTfsIntegration;
+            var licenseErrorCode = ErrorCodes.LicenseUnavailable;
+            var validLicense = FeatureTypes.Storyteller;
+            var attribute = CreateAttribute(validLicense, requiredLicense);
+
+            //Act
+            attribute.OnActionExecuting(_context);
+
+            //Assert
+            ValidateResponse(licenseErrorCode);
+        }
+
+        [TestMethod]
+        public void OnActionExecuting_WhenOneOfTheRequiredLicensesIsNotFound_ContentContainsLicenseErrorCode()
+        {
+            //Arrange
+            var requiredLicense = FeatureTypes.Workflow | FeatureTypes.Storyteller;
+            var licenseErrorCode = ErrorCodes.LicenseUnavailable;
+            var validLicense = FeatureTypes.Workflow | FeatureTypes.Blueprint;
+            var attribute = CreateAttribute(validLicense, requiredLicense);
+
+            //Act
+            attribute.OnActionExecuting(_context);
+
+            //Assert
+            ValidateResponse(licenseErrorCode);
+        }
+
+        [TestMethod]
+        public void OnActionExecuting_WhenAllRequiredLicensesAreNotFound_ContentContainsLicenseErrorCode()
+        {
+            //Arrange
+            var requiredLicense = FeatureTypes.Workflow | FeatureTypes.Storyteller;
+            var licenseErrorCode = ErrorCodes.LicenseUnavailable;
+            var validLicense = FeatureTypes.BlueprintOpenApi | FeatureTypes.Blueprint;
+            var attribute = CreateAttribute(validLicense, requiredLicense);
+
+            //Act
+            attribute.OnActionExecuting(_context);
+
+            //Assert
+            ValidateResponse(licenseErrorCode);
         }
 
         [TestMethod]
@@ -76,43 +181,58 @@ namespace ServiceLibrary.Attributes
             //Arrange
             var requiredLicense = FeatureTypes.Workflow;
             var validLicense = FeatureTypes.Workflow;
-            _featureLicenseHelperMock.Setup(i => i.GetValidBlueprintLicenseFeatures()).Returns(validLicense);
-            var attribute = new FeatureActivationAttribute(requiredLicense, _featureLicenseHelperMock.Object);
-            var context = new HttpActionContext();
+            var attribute = CreateAttribute(validLicense, requiredLicense);
+
             //Act
-            attribute.OnActionExecuting(context);
+            attribute.OnActionExecuting(_context);
+
             //Assert
-            Assert.IsNull(context.Response);
+            Assert.IsNull(_context.Response);
         }
 
         [TestMethod]
         public void OnActionExecuting_WhenAllRequiredLicensesAreFound_ResponseIsNull()
         {
             //Arrange
-            var requiredLicenses = FeatureTypes.Workflow | FeatureTypes.Storyteller;
-            var validLicenses = FeatureTypes.Workflow | FeatureTypes.Storyteller;
-            _featureLicenseHelperMock.Setup(i => i.GetValidBlueprintLicenseFeatures()).Returns(validLicenses);
-            var attribute = new FeatureActivationAttribute(requiredLicenses, _featureLicenseHelperMock.Object);
-            var context = new HttpActionContext();
+            var requiredLicense = FeatureTypes.Workflow | FeatureTypes.Storyteller;
+            var validLicense = FeatureTypes.Workflow | FeatureTypes.Storyteller;
+            var attribute = CreateAttribute(validLicense, requiredLicense);
+
             //Act
-            attribute.OnActionExecuting(context);
+            attribute.OnActionExecuting(_context);
+
             //Assert
-            Assert.IsNull(context.Response);
+            Assert.IsNull(_context.Response);
         }
 
         [TestMethod]
         public void OnActionExecuting_WhenMoreThanAllRequiredLicensesAreFound_ResponseIsNull()
         {
             //Arrange
-            var requiredLicenses = FeatureTypes.Workflow | FeatureTypes.Storyteller;
-            var validLicenses = FeatureTypes.Workflow | FeatureTypes.Storyteller | FeatureTypes.Blueprint;
-            _featureLicenseHelperMock.Setup(i => i.GetValidBlueprintLicenseFeatures()).Returns(validLicenses);
-            var attribute = new FeatureActivationAttribute(requiredLicenses, _featureLicenseHelperMock.Object);
-            var context = new HttpActionContext();
+            var requiredLicense = FeatureTypes.Workflow | FeatureTypes.Storyteller;
+            var validLicense = FeatureTypes.Workflow | FeatureTypes.Storyteller | FeatureTypes.Blueprint;
+            var attribute = CreateAttribute(validLicense, requiredLicense);
+
             //Act
-            attribute.OnActionExecuting(context);
+            attribute.OnActionExecuting(_context);
+
             //Assert
-            Assert.IsNull(context.Response);
+            Assert.IsNull(_context.Response);
+        }
+
+        [TestMethod]
+        public void OnActionExecuting_WhenNoLicenseIsRequired_ResponseIsNull()
+        {
+            //Arrange
+            var requiredLicense = FeatureTypes.None;
+            var validLicense = FeatureTypes.Storyteller;
+            var attribute = CreateAttribute(validLicense, requiredLicense);
+
+            //Act
+            attribute.OnActionExecuting(_context);
+
+            //Assert
+            Assert.IsNull(_context.Response);
         }
     }
 }

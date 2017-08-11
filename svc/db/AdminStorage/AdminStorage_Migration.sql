9804a2e9ac68475631de6ec7ad2c2d6fadec1330
Alexander Groman <alexander.groman@blueprintsys.com>
2017-08-11 16:22:11 -0400

    DEV-1726: updates `AdminStore` migration script to support single database installation

--- svc/db/AdminStorage/AdminStorage_Migration.sql
+++ svc/db/AdminStorage/AdminStorage_Migration.sql
@@ -1192,40 +1192,51 @@ Print 'Migrating 8.2.0.0 ...'
 -- -----------------------------------------------------------------------------------------------
 
 -- Drop all functions associated with dbo schema
-IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[IsSchemaVersionLessOrEqual]') AND type in (N'FN', N'IF', N'TF', N'FS', N'FT'))
-DROP FUNCTION [dbo].[IsSchemaVersionLessOrEqual]
+IF ([AdminStore].[IsSchemaVersionLessOrEqual](N'8.2.0') <> 0)
+	-- Distributed database installation only
+	AND (OBJECT_ID(N'[dbo].[Instances]', 'U') IS NULL)
+	AND (OBJECT_ID(N'[dbo].[IsSchemaVersionLessOrEqual]', 'FN') IS NOT NULL)
+	DROP FUNCTION [dbo].[IsSchemaVersionLessOrEqual];
 GO
 
 -- Drop all procedures associated with dbo schema
-DECLARE @name AS sysname, @tsql AS nvarchar(max), @beginCount AS int, @endCount AS int;
-DECLARE NameCursor CURSOR FOR
-SELECT o.[name] FROM [sys].[objects] AS o INNER JOIN [sys].[schemas] AS s ON (o.[schema_id] = s.[schema_id]) WHERE (s.[name] = N'dbo') AND (o.[type] = 'P');
-WHILE (0 = 0)
+IF ([AdminStore].[IsSchemaVersionLessOrEqual](N'8.2.0') <> 0)
+	-- Distributed database installation only
+	AND (OBJECT_ID(N'[dbo].[Instances]', 'U') IS NULL)
 BEGIN
-	SELECT @beginCount = COUNT(*) FROM [sys].[objects] AS o INNER JOIN [sys].[schemas] AS s ON (o.[schema_id] = s.[schema_id]) WHERE (s.[name] = N'dbo') AND (o.[type] = 'P');
-	OPEN NameCursor;
-	FETCH NEXT FROM NameCursor INTO @name;
-	WHILE (@@FETCH_STATUS = 0)
+	DECLARE @name AS sysname, @tsql AS nvarchar(max), @beginCount AS int, @endCount AS int;
+	DECLARE NameCursor CURSOR FOR
+	SELECT o.[name] FROM [sys].[objects] AS o INNER JOIN [sys].[schemas] AS s ON (o.[schema_id] = s.[schema_id]) WHERE (s.[name] = N'dbo') AND (o.[type] = 'P');
+	WHILE (0 = 0)
 	BEGIN
-		SET @tsql = N'DROP PROCEDURE [dbo].[' + CAST(@name AS nvarchar(256)) + N'];';
-		BEGIN TRY
-			EXEC(@tsql);
-		END TRY
-		BEGIN CATCH
-		END CATCH
+		SELECT @beginCount = COUNT(*) FROM [sys].[objects] AS o INNER JOIN [sys].[schemas] AS s ON (o.[schema_id] = s.[schema_id]) WHERE (s.[name] = N'dbo') AND (o.[type] = 'P');
+		OPEN NameCursor;
 		FETCH NEXT FROM NameCursor INTO @name;
+		WHILE (@@FETCH_STATUS = 0)
+		BEGIN
+			SET @tsql = N'DROP PROCEDURE [dbo].[' + CAST(@name AS nvarchar(256)) + N'];';
+			BEGIN TRY
+				EXEC(@tsql);
+			END TRY
+			BEGIN CATCH
+			END CATCH
+			FETCH NEXT FROM NameCursor INTO @name;
+		END
+		CLOSE NameCursor;
+		SELECT @endCount = COUNT(*) FROM [sys].[objects] AS o INNER JOIN [sys].[schemas] AS s ON (o.[schema_id] = s.[schema_id]) WHERE (s.[name] = N'dbo') AND (o.[type] = 'P');
+		IF ((@endCount = 0) OR (@endCount = @beginCount))
+			BREAK;
 	END
-	CLOSE NameCursor;
-	SELECT @endCount = COUNT(*) FROM [sys].[objects] AS o INNER JOIN [sys].[schemas] AS s ON (o.[schema_id] = s.[schema_id]) WHERE (s.[name] = N'dbo') AND (o.[type] = 'P');
-	IF ((@endCount = 0) OR (@endCount = @beginCount))
-		BREAK;
+	DEALLOCATE NameCursor;
 END
-DEALLOCATE NameCursor;
 GO
 
 -- Drop all types associated with dbo schema
-IF  EXISTS (SELECT * FROM sys.types st JOIN sys.schemas ss ON st.schema_id = ss.schema_id WHERE st.name = N'LogsType' AND ss.name = N'dbo')
-DROP TYPE [dbo].[LogsType]
+IF ([AdminStore].[IsSchemaVersionLessOrEqual](N'8.2.0') <> 0)
+	-- Distributed database installation only
+	AND (OBJECT_ID(N'[dbo].[Instances]', 'U') IS NULL)
+	AND (TYPE_ID(N'[dbo].[LogsType]') IS NOT NULL)
+	DROP TYPE [dbo].[LogsType];
 GO
 
 DECLARE @blueprintDB SYSNAME, @jobname SYSNAME, @schedulename SYSNAME

ce16e30576f2f12065ae05c8c72fb4da9f9bf952
Aleksey Glushchenko <tuktonchik@gmail.com>
2017-08-14 19:35:31 +0300

    DEV-1931 [Instance Folders]: [Update] 400 BadRequest when updating an instance folder

--- svc/AdminStore/Controllers/InstanceController.cs
+++ svc/AdminStore/Controllers/InstanceController.cs
@@ -280,11 +280,6 @@ namespace AdminStore.Controllers
                 throw new BadRequestException(ErrorMessages.ModelIsEmpty, ErrorCodes.BadRequest);
             }
 
-            if (!folderDto.ParentFolderId.HasValue)
-            {
-                throw new BadRequestException(ErrorMessages.EditRootFolderIsForbidden, ErrorCodes.BadRequest);
-            }
-
             await _privilegesManager.Demand(Session.UserId, InstanceAdminPrivileges.ManageProjects);
 
             FolderValidator.ValidateModel(folderDto);

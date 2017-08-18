34a3fcf219b9091238dc9aabbdb97c7142302116
Matthew Ellwood <Seaal@users.noreply.github.com>
2017-08-18 13:30:56 -0400

    [DEV-1921] Removed unnecessary usings

--- svc/AdminStore/Models/Emails/EmailOutgoingSettings.cs
+++ svc/AdminStore/Models/Emails/EmailOutgoingSettings.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AdminStore.Models.Emails
+﻿namespace AdminStore.Models.Emails
 {
     public class EmailOutgoingSettings
     {

cb3800b537e41eb45ec7ff2ad7a9ef3bd8dad234
Nikita Pliassounov <nikita.pliassounov@blueprintsys.com>
2017-08-24 15:08:02 -0400

    dev-528 refactoring as per code review

--- win_svc/ActionHandlerService/MessageHandlers/GenerateUserStories/GenerateUserStoriesActionHelper.cs
+++ win_svc/ActionHandlerService/MessageHandlers/GenerateUserStories/GenerateUserStoriesActionHelper.cs
@@ -2,9 +2,19 @@ using System.Threading.Tasks;
 using ActionHandlerService.Models;
 using ActionHandlerService.Repositories;
 using BluePrintSys.Messaging.Models.Actions;
+using ServiceLibrary.Helpers;
+using ServiceLibrary.Models.Jobs;
+using ServiceLibrary.Repositories.Jobs;
 
 namespace ActionHandlerService.MessageHandlers.GenerateUserStories
 {
+    public class GenerateUserStoryInfo
+    {
+        public int ProcessId { get; set; }
+
+        public int? TaskId { get; set; }
+    }
+
     public class GenerateUserStoriesActionHelper : IActionHelper
     {
         public async Task<bool> HandleAction(TenantInformation tenant, ActionMessage actionMessage, IActionHandlerServiceRepository actionHandlerServiceRepository)
@@ -12,14 +22,19 @@ namespace ActionHandlerService.MessageHandlers.GenerateUserStories
             var generateUserStoriesMessage = actionMessage as GenerateUserStoriesMessage;
             if (generateUserStoriesMessage == null)
             {
-                return await Task.FromResult(false);
+                return false;
             }
-            return await UserStoryGenerationRepository.GenerateUserStories(generateUserStoriesMessage.ProjectId, 
-                generateUserStoriesMessage.ArtifactId,
-                generateUserStoriesMessage.ProjectName,
-                generateUserStoriesMessage.UserName,
-                generateUserStoriesMessage.UserId,
-                generateUserStoriesMessage.BaseHostUri);
+
+            var payload = new GenerateUserStoryInfo { ProcessId = generateUserStoriesMessage.ArtifactId, TaskId = null };
+            var parameters = SerializationHelper.ToXml(payload);
+            var jobsRepository = new JobsRepository();
+            var jobId = await jobsRepository.AddJobMessage(JobType.GenerateUserStories,
+                false, parameters, null, generateUserStoriesMessage.ProjectId, 
+                generateUserStoriesMessage.ProjectName, generateUserStoriesMessage.UserId, 
+                generateUserStoriesMessage.UserName, generateUserStoriesMessage.BaseHostUri);
+
+            return jobId > 0;
+
         }
     }
 }

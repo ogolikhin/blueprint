38705fa3b86ea88b26e25fd461f714fd2a6a9c83
MikitaBelikau <mikita87@tut.by>
2017-09-06 14:10:34 +0300

    removed unused directives

--- svc/AdminStore/Models/ProjectRole.cs
+++ svc/AdminStore/Models/ProjectRole.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace AdminStore.Models
+﻿namespace AdminStore.Models
 {
     public class ProjectRole
     {

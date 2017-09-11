3de30dda2985d32122e38d467326f7875d9a597f
Evgeniy Dubovsky <e.dubovsky@softteco.com>
2017-09-11 16:16:51 +0300

    https://jira.blueprintsys.net/browse/STOR-892

---
+++ svc/AdminStore/Models/Workflow/WorkflowDetailsDto.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace AdminStore.Models.Workflow
+{
+    public class WorkflowDetailsDto
+    {
+        public int WorkflowId { get; set; }
+        public string Name { get; set; }
+        public bool Active { get; set; }
+        public int VersionId { get; set; }
+        public string Description { get; set; }
+        public IEnumerable<WorkflowProjectDto> Projects { get; set; }
+        public IEnumerable<WorkflowArtifactTypeDto> ArtifactTypes { get; set; }
+    }
+}

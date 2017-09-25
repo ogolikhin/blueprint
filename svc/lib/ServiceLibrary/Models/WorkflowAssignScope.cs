b44214305a8c3682c27032aa00e5032bce0389b6
maxyak888 <31539775+maxyak888@users.noreply.github.com>
2017-09-25 16:14:22 +0300

    add WorkflowAssignScope class

---
+++ svc/lib/ServiceLibrary/Models/WorkflowAssignScope.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AdminStore.Models
+{
+    public class WorkflowAssignScope
+    {
+        public bool AllProjects { get; set; }
+
+        public IEnumerable<int> ProjectIds { get; set; }
+
+        public bool AllArtifacts { get; set; }
+
+        public IEnumerable<int> ArtifactIds { get; set; }
+
+        public bool IsEmpty()
+        {
+            if (AllProjects && (ArtifactIds == null || !ArtifactIds.Any()))
+                return true;
+            else if (AllArtifacts && (ProjectIds == null || !ProjectIds.Any()))
+                return true;
+            else if ((ArtifactIds == null || !ArtifactIds.Any()) && (AllArtifacts && (ProjectIds == null || !ProjectIds.Any())))
+                return true;
+            else return false;
+        }
+    }
+}

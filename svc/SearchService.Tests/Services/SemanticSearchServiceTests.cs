e3f5b33e1bde3a7430426b6b8f893cc0c64acd75
Albert Wong <albert.wong@blueprintsys.com>
2017-10-02 13:52:38 -0400

    stor-955: fixed issue where it does not throw an artifact not found exception when id could not be found.

--- svc/SearchService.Tests/Services/SemanticSearchServiceTests.cs
+++ svc/SearchService.Tests/Services/SemanticSearchServiceTests.cs
@@ -104,5 +104,19 @@ namespace SearchService.Services
             //act
             await _semanticSearchService.GetSemanticSearchSuggestions(parameters);
         }
+
+        [TestMethod]
+        [ExpectedException(typeof(ResourceNotFoundException))]
+        public async Task GetSemanticSearchSuggestions_WhenArtifactNotFound_ThrowsNotFoundException()
+        {
+            //arrange
+            var parameters = new SemanticSearchSuggestionParameters(1, 1);
+
+            _artifactRepository.Setup(a => a.GetArtifactBasicDetails(It.IsAny<int>(), It.IsAny<int>()))
+                .ReturnsAsync((ArtifactBasicDetails)null);
+
+            //act
+            await _semanticSearchService.GetSemanticSearchSuggestions(parameters);
+        }
     }
 }

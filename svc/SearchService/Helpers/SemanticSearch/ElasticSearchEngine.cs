5cab129b6397aac2f1f94890b856db410efb2d88
Albert Wong <albert.wong@blueprintsys.com>
2017-10-03 12:59:49 -0400

    stor-1337: temp checkin

--- svc/SearchService/Helpers/SemanticSearch/ElasticSearchEngine.cs
+++ svc/SearchService/Helpers/SemanticSearch/ElasticSearchEngine.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using Nest;
 using SearchService.Models;
 using SearchService.Repositories;
 
@@ -9,13 +11,18 @@ namespace SearchService.Helpers.SemanticSearch
     {
         private const string IndexPrefix = "semanticsdb_";
         private string IndexName { get; }
-        
+
+        private IElasticClient _elasticClient;
 
         public ElasticSearchEngine(string connectionString, string tenantId, ISemanticSearchRepository semanticSearchRepository) 
             : base(semanticSearchRepository)
         {
             IndexName = IndexPrefix + tenantId;
+
             // create ElasticClient using conneciton string
+            var connectionSettings = new ConnectionSettings(new Uri(connectionString)).DefaultIndex(IndexName);
+            _elasticClient = new ElasticClient(connectionSettings);
+            
         }
 
         public override async Task<IEnumerable<ArtifactSearchResult>> GetSemanticSearchSuggestions(SearchEngineParameters searchEngineParameters)
@@ -24,6 +31,13 @@ namespace SearchService.Helpers.SemanticSearch
                 searchEngineParameters.UserId);
             // use searchText to execute Elasticsearch query
             var itemIds = new List<int>();
+            
+            _elasticClient.Search<object>(
+                s => s.From(0).Size(10).Query(
+                    q => q.MoreLikeThis(
+                        m => m.Fields(
+                            f => f.Field("SearchColumn")).Like( l => l.Text(searchText)))));
+
             // parse the artifact ids into a artifactsearchresult to return to the caller
             return await GetArtifactSearchResultsFromItemIds(itemIds, searchEngineParameters.UserId, searchEngineParameters.ArtifactId);
         }

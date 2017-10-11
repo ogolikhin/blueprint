0dedeeb9697a6a3464d157eee0d088f7325bcc1c
Albert Wong <albert.wong@blueprintsys.com>
2017-10-11 17:03:51 -0400

    stor-1337: fixed some tests for elastic search responses.

--- svc/SearchService.Tests/Helpers/SemanticSearch/ElasticSearchEngineTests.cs
+++ svc/SearchService.Tests/Helpers/SemanticSearch/ElasticSearchEngineTests.cs
@@ -1,7 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using Nest;
+using SearchService.Models;
 using SearchService.Repositories;
 using ServiceLibrary.Exceptions;
 
@@ -49,7 +54,7 @@ namespace SearchService.Helpers.SemanticSearch
 
             _elasticClient.Setup(e => e.Ping(It.IsAny<Func<PingDescriptor, IPingRequest>>()))
                 .Returns(mockResponse.Object);
-            _elasticClient.Setup(e => e.IndexExists(It.IsAny<string>(),It.IsAny<Func<IndexExistsDescriptor, IIndexExistsRequest>>()))
+            _elasticClient.Setup(e => e.IndexExists(It.IsAny<Indices>(),It.IsAny<Func<IndexExistsDescriptor, IIndexExistsRequest>>()))
                 .Returns(mockIndexResponse.Object);
 
             _elasticSearchEngine = new ElasticSearchEngine(_fakeTenantId, _elasticClient.Object, _semanticSearchRepository.Object);
@@ -66,14 +71,66 @@ namespace SearchService.Helpers.SemanticSearch
             mockExistsTrueResponse.SetupGet(m => m.Exists).Returns(true);
 
             var mockExistsFalseResponse = new Mock<IExistsResponse>();
-            mockExistsTrueResponse.SetupGet(m => m.Exists).Returns(false);
+            mockExistsFalseResponse.SetupGet(m => m.Exists).Returns(false);
 
             _elasticClient.Setup(e => e.Ping(It.IsAny<Func<PingDescriptor, IPingRequest>>()))
                 .Returns(mockResponse.Object);
-            _elasticClient.Setup(e => e.IndexExists(It.IsAny<string>(), It.IsAny<Func<IndexExistsDescriptor, IIndexExistsRequest>>()))
+            _elasticClient.Setup(e => e.IndexExists(It.IsAny<Indices>(), It.IsAny<Func<IndexExistsDescriptor, IIndexExistsRequest>>()))
                 .Returns(mockExistsTrueResponse.Object);
 
-            _elasticClient.Setup(e => e.TypeExists(It.IsAny<string>(), It.IsAny<Type>(), It.IsAny<Func<TypeExistsDescriptor, ITypeExistsRequest>>()))
+            _elasticClient.Setup(e => e.TypeExists(It.IsAny<Indices>(), It.IsAny<Types>(), It.IsAny<Func<TypeExistsDescriptor, ITypeExistsRequest>>()))
+                .Returns(mockExistsFalseResponse.Object);
+
+            _elasticSearchEngine = new ElasticSearchEngine(_fakeTenantId, _elasticClient.Object, _semanticSearchRepository.Object);
+        }
+
+        [TestMethod]
+        public async Task GetSemanticSearchSuggestions_WhenQuerying_ReturnsArtifactDetails()
+        {
+            InitializeValidElasticClient();
+
+            var searchParameters = new SearchEngineParameters(1, 1, true, new HashSet<int>());
+            _semanticSearchRepository.Setup(s => s.GetSemanticSearchText(It.IsAny<int>(), It.IsAny<int>()))
+                .ReturnsAsync(new SemanticSearchText()
+                {
+                    Name = "test",
+                    SearchText = "searchTest"
+                });
+
+            var searchItem = new SemanticSearchItem() {ItemId = 2};
+            var searchResponse = new Mock<ISearchResponse<SemanticSearchItem>>();
+            searchResponse.SetupGet(s => s.Documents).Returns(new List<SemanticSearchItem>() {searchItem});
+
+            _elasticClient.Setup(
+                e => e.SearchAsync<SemanticSearchItem>(It.IsAny<ISearchRequest>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(searchResponse.Object);
+
+            _semanticSearchRepository.Setup(s => s.GetSuggestedArtifactDetails(It.IsAny<List<int>>(), It.IsAny<int>()))
+                .ReturnsAsync(new List<ArtifactSearchResult>() { new ArtifactSearchResult() {ItemId = 2}});
+
+            var result = await _elasticSearchEngine.GetSemanticSearchSuggestions(searchParameters);
+
+            Assert.IsTrue(result.Count() == 1);
+            Assert.IsTrue(result.FirstOrDefault().Id == 2);
+        }
+
+        private void InitializeValidElasticClient()
+        {
+            var mockResponse = new Mock<IPingResponse>();
+            mockResponse.SetupGet(m => m.IsValid).Returns(true);
+
+            var mockExistsTrueResponse = new Mock<IExistsResponse>();
+            mockExistsTrueResponse.SetupGet(m => m.Exists).Returns(true);
+
+            var mockExistsFalseResponse = new Mock<IExistsResponse>();
+            mockExistsFalseResponse.SetupGet(m => m.Exists).Returns(true);
+
+            _elasticClient.Setup(e => e.Ping(It.IsAny<Func<PingDescriptor, IPingRequest>>()))
+                .Returns(mockResponse.Object);
+            _elasticClient.Setup(e => e.IndexExists(It.IsAny<Indices>(), It.IsAny<Func<IndexExistsDescriptor, IIndexExistsRequest>>()))
+                .Returns(mockExistsTrueResponse.Object);
+
+            _elasticClient.Setup(e => e.TypeExists(It.IsAny<Indices>(), It.IsAny<Types>(), It.IsAny<Func<TypeExistsDescriptor, ITypeExistsRequest>>()))
                 .Returns(mockExistsFalseResponse.Object);
 
             _elasticSearchEngine = new ElasticSearchEngine(_fakeTenantId, _elasticClient.Object, _semanticSearchRepository.Object);

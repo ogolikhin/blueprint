97a14d503e26f0e90b3d5b788cfea4eb3e98aca9
Roman Dolgov <roman.dolgov@blueprintsys.com>
2017-11-07 09:56:07 -0500

    Updates API response Http status code for UpdateReviewSettingsAsync

--- svc/ArtifactStore/Controllers/ReviewContainersController.cs
+++ svc/ArtifactStore/Controllers/ReviewContainersController.cs
@@ -432,7 +432,7 @@ namespace ArtifactStore.Controllers
         /// </summary>
         /// <param name="reviewId">The review id.</param>
         /// <param name="reviewSettings">The updated review settings.</param>
-        /// <response code="200">OK. The review settings were successfully updated.</response>
+        /// <response code="204">No Content. The review settings were successfully updated.</response>
         /// <response code="400">Bad Request. The request parameters are invalid.</response>
         /// <response code="401">Unauthorized. The session token is invalid.</response>
         /// <response code="403">Forbidden. The user does not have permissions to access review.</response>

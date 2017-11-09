3750e59c1c776f71f7f128334cc59f6eeca74225
BLUEPRINT\atkachev <alex.tkachev@blueprintsys.com>
2017-11-09 15:14:09 -0500

    STOR-1715: add new property "HasReviewComments" to Review Content

--- svc/ArtifactStore/Repositories/SqlReviewsRepository.cs
+++ svc/ArtifactStore/Repositories/SqlReviewsRepository.cs
@@ -295,6 +295,7 @@ namespace ArtifactStore.Repositories
                         reviewArtifact.Disapproved = reviewArtifactStatus.Disapproved;
                         reviewArtifact.Viewed = reviewArtifactStatus.Viewed;
                         reviewArtifact.Unviewed = reviewArtifactStatus.Unviewed;
+                        reviewArtifact.HasReviewComments = reviewArtifactStatus.HasComments;
                     }
                     else
                     {

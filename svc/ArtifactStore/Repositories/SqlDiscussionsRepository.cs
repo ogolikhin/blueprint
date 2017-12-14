03d4b1cf7af9149f87d9dd61b1fa9907f597531f
Areag Osman <areag.osman@blueprintsys.com>
2017-12-14 09:54:03 -0500

    STOR-5704 - Do not overwrite the comment's user display name with the information found in the [dbo].[Users] table

--- svc/ArtifactStore/Repositories/SqlDiscussionsRepository.cs
+++ svc/ArtifactStore/Repositories/SqlDiscussionsRepository.cs
@@ -128,10 +128,16 @@ namespace ArtifactStore.Repositories
                 var userInfo = (UserInfo)null;
                 if (userInfos.TryGetValue(comment.UserId, out userInfo))
                 {
-                    comment.DisplayName = userInfo.DisplayName;
+                    // During project import, we perserve the display name of the user on comment but we do not create
+                    // any user records. Thus we may have situations where the display name on the comment does not
+                    // correspond to any user records within the database. In which case the user Id on the comment is the user id
+                    // of the user who performed the project import and we should not be using this to determine the display name of
+                    // the author on the comment. The comment's user's display name is populated by the GetItemDiscussions SP. 
+                    // Here we just indicate if the user is a guest or not. STOR-5704
+                    bool userDoesNotExist = userInfo.DisplayName != comment.DisplayName;
+                    comment.IsGuest = userDoesNotExist || userInfo.IsGuest;
                 }
                 comment.LastEditedOn = DateTime.SpecifyKind(comment.LastEditedOn, DateTimeKind.Utc);
-                comment.IsGuest = userInfo.IsGuest;
                 comment.Comment = await _mentionHelper.ProcessComment(comment.Comment, areEmailDiscussionsEnabled);
 
                 onCommentInit?.Invoke(comment);

30f4dc8d3d9eba707fac5df992037680e5ffe61e
MikitaBelikau <mikita87@tut.by>
2017-12-21 18:12:15 +0300

    - created basic infrustructure for Collections controller and repository (including unit tests)

---
+++ svc/ArtifactStore.Tests/Controllers/CollectionsControllerTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using ServiceLibrary.Helpers;
+using ServiceLibrary.Models;
+using ServiceLibrary.Repositories;
+using ServiceLibrary.Repositories.ConfigControl;
+
+namespace ArtifactStore.Controllers
+{
+    [TestClass]
+    public class CollectionsControllerTests
+    {
+        private Mock<ICollectionsRepository> mockCollectionsRepository;
+        private Mock<IServiceLogRepository> mockServiceLogRepository;
+        private Mock<IPrivilegesRepository> _mockSqlPrivilegesRepository;
+        private CollectionsController collectionsController;
+
+        private const int userId = 1;
+        private Session session;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            session = new Session { UserId = userId };
+
+            mockServiceLogRepository = new Mock<IServiceLogRepository>();
+            _mockSqlPrivilegesRepository = new Mock<IPrivilegesRepository>();
+            mockCollectionsRepository = new Mock<ICollectionsRepository>();
+
+            collectionsController = new CollectionsController(mockCollectionsRepository.Object, _mockSqlPrivilegesRepository.Object, mockServiceLogRepository.Object)
+            {
+                Request = new HttpRequestMessage()
+            };
+
+            collectionsController.Request.Properties[ServiceConstants.SessionProperty] = session;
+        }
+    }
+}
---
+++ svc/lib/ServiceLibrary/Repositories/CollectionsRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ServiceLibrary.Helpers;
+
+namespace ServiceLibrary.Repositories
+{
+    public class CollectionsRepository : ICollectionsRepository
+    {
+        private readonly ISqlConnectionWrapper _connectionWrapper;
+
+        public CollectionsRepository()
+            : this(new SqlConnectionWrapper(ServiceConstants.RaptorMain))
+        {
+        }
+
+        public CollectionsRepository(ISqlConnectionWrapper connectionWrapper)
+        {
+            _connectionWrapper = connectionWrapper;
+        }
+    }
+}

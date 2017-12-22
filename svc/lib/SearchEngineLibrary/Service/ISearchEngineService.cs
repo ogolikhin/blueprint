fefe6ee9d4b5bed767e3c56dabbc97f4fb9d0da7
Roman Dolgov <roman.dolgov@blueprintsys.com>
2017-12-22 10:14:42 -0500

    Moves SearchEngineLibrary to lib folder.

--- svc/lib/SearchEngineLibrary/Service/ISearchEngineService.cs
+++ svc/lib/SearchEngineLibrary/Service/ISearchEngineService.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace SearchEngineLibrary.Service
 {
-    interface ISearchEngineService
+    public interface ISearchEngineService
     {
         Task<IEnumerable<int>> GetArtifactIds();
     }
--- svc/lib/SearchEngineLibrary/Service/SearchEngineService.cs
+++ svc/lib/SearchEngineLibrary/Service/SearchEngineService.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 
 using SearchEngineLibrary.Repository;
@@ -14,7 +11,6 @@ namespace SearchEngineLibrary.Service
 
         public SearchEngineService() : this(new SearchEngineRepository())
         {
-            
         }
 
         internal SearchEngineService(ISearchEngineRepository searchEngineRepository)

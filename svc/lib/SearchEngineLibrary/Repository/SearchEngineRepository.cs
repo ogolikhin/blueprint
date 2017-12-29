f72e660f81d265a2b4e3d535e8dd140d4994e02b
maxyak888 <maxyak88@gmail.com>
2017-12-29 17:34:19 +0300

    improve dynamic sql

--- svc/lib/SearchEngineLibrary/Repository/SearchEngineRepository.cs
+++ svc/lib/SearchEngineLibrary/Repository/SearchEngineRepository.cs
@@ -55,9 +55,9 @@ namespace SearchEngineLibrary.Repository
             var query = String.Format("DECLARE @Offset INT = {0} DECLARE   @Limit INT = {1} DECLARE @includeDraft BIT = {2} DECLARE @scopeId INT = {3} DECLARE @infinityRevision INT = 2147483647 ", pagination.Offset, pagination.Limit, includeDraft ? 1 : 0, scopeId);
             query += "CREATE TABLE #VersionArtifactId (id int identity(1,1), VersionArtifactId int) ";
             query += "INSERT INTO #VersionArtifactId SELECT [VersionArtifactId] FROM [dbo].[CollectionAssignmentVersions] WHERE [VersionCollectionId] = @scopeId AND EndRevision = @infinityRevision ";
-            query += "DELETE FROM #VersionArtifactId WHERE VersionArtifactId IN (SELECT [VersionArtifactId] FROM #VersionArtifactId JOIN [dbo].[Items] as it ON VersionArtifactId = it.ItemId WHERE it.DraftVersion_VersionId IS NULL AND it.LatestVersion_VersionId IS NULL) ";
-            query += "IF(@includeDraft = 0) BEGIN DELETE FROM #VersionArtifactId WHERE VersionArtifactId IN (SELECT [VersionArtifactId] FROM #VersionArtifactId JOIN [dbo].[Items] as it ON VersionArtifactId = it.ItemId WHERE it.DraftVersion_VersionId IS NOT NULL) END ";
-            query += "SELECT[VersionArtifactId] FROM #VersionArtifactId GROUP BY [VersionArtifactId] ORDER BY [VersionArtifactId] OFFSET @Offset ROWS FETCH NEXT @Limit ROWS ONLY ";
+            query += "DELETE FROM #VersionArtifactId WHERE VersionArtifactId NOT IN (SELECT vaId.[VersionArtifactId] FROM #VersionArtifactId as vaId JOIN [dbo].[ItemVersions] as iv ON vaId.VersionArtifactId = iv.HolderId) ";
+            query += "IF(@includeDraft = 0) BEGIN DELETE FROM #VersionArtifactId WHERE VersionArtifactId IN (SELECT vaId.[VersionArtifactId] FROM #VersionArtifactId as vaId JOIN [dbo].[ItemVersions] as iv ON vaId.VersionArtifactId = iv.HolderId WHERE iv.StartRevision = 1 AND iv.EndRevision = 1) END ";
+            query += "SELECT [ArtifactId] FROM [dbo].[SearchItems] WHERE [ArtifactId] IN (SELECT [VersionArtifactId] FROM #VersionArtifactId) GROUP BY [ArtifactId] ORDER BY [ArtifactId] OFFSET @Offset ROWS FETCH NEXT @Limit ROWS ONLY ";
             query += "DROP TABLE #VersionArtifactId "; 
 
             return await _connectionWrapper.QueryAsync<int>(

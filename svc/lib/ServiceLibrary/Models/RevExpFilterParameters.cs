2bcad646809a857dd3e9304086087e14be3ef254
ggeles <guy.geles@blueprintsys.com>
2018-01-04 12:49:54 -0500

    stor-1162 - Implementaion

--- svc/lib/ServiceLibrary/Models/RevExpFilterParameters.cs
+++ svc/lib/ServiceLibrary/Models/RevExpFilterParameters.cs
@@ -9,7 +9,7 @@ namespace ServiceLibrary.Models
 
     public class RevExpFilterParameters
     {
-        public IEnumerable<int> ApprStsIds { get; set; }
+        public IEnumerable<int> ApprStsIds { get; set; } = new List<int>();
 
         public bool? IsApprovalRequired { get; set; }
     }

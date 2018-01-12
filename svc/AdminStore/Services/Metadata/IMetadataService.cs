316d40e64cc143a81bfb693aabcbf0a94d7c21fd
YlQm <ylqmriki@gmail.com>
2018-01-12 11:10:53 -0500

    add interface of MetadataService

--- svc/AdminStore/Services/Metadata/IMetadataService.cs
+++ svc/AdminStore/Services/Metadata/IMetadataService.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,8 @@ namespace AdminStore.Services.Metadata
 {
     public interface IMetadataService
     {
+        Task<ByteArrayContent> GetCustomItemTypeIcon(int itemTypeId, int revisionId = int.MaxValue);
+
+        void GetItemTypeIcon(int? typeId);
     }
 }

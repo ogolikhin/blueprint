20976200a0972ae90a450ae3122998e6ad06303c
BLUEPRINT\atkachev <alex.tkachev@blueprintsys.com>
2018-01-17 13:28:14 -0500

    STOR-1619: clean up usage

--- win_svc/BluePrintSys.Messaging.CrossCutting/Host/StatusCheckMessageHandler.cs
+++ win_svc/BluePrintSys.Messaging.CrossCutting/Host/StatusCheckMessageHandler.cs
@@ -1,9 +1,5 @@
 ﻿using BluePrintSys.Messaging.CrossCutting.Models;
 using NServiceBus;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace BluePrintSys.Messaging.CrossCutting.Host

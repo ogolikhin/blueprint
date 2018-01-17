005345d42f107854cdee84ebe46af9f57a7b0da8
BLUEPRINT\atkachev <alex.tkachev@blueprintsys.com>
2018-01-17 10:49:17 -0500

    STOR-1619: add new message handler at NServiceBus

---
+++ win_svc/BluePrintSys.Messaging.CrossCutting/Host/StatusCheckMessageHandler.cs
@@ -0,0 +1,18 @@
+﻿using BluePrintSys.Messaging.CrossCutting.Models;
+using NServiceBus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BluePrintSys.Messaging.CrossCutting.Host
+{
+    public class StatusMessageHandler : IHandleMessages<StatusCheckMessage>
+    {
+        public async Task Handle(StatusCheckMessage message, IMessageHandlerContext context)
+        {
+            return;
+        }
+    }
+}

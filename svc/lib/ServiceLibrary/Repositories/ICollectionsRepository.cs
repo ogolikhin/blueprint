082f846a5b0fb37f5131da0ecd8eeb5b0cacf1d0
Roman Dolgov <roman.dolgov@blueprintsys.com>
2018-01-16 20:18:27 -0500

    Implements retrieval of content artifact ids of a collection with tests.

--- svc/lib/ServiceLibrary/Repositories/ICollectionsRepository.cs
+++ svc/lib/ServiceLibrary/Repositories/ICollectionsRepository.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace ServiceLibrary.Repositories
 {
     public interface ICollectionsRepository
     {
-
+        Task<IReadOnlyList<int>> GetContentArtifactIdsAsync(int collectionId, int userId, bool addDrafts = true);
     }
 }
--- svc/lib/ServiceLibrary/Repositories/SqlCollectionsRepository.cs
+++ svc/lib/ServiceLibrary/Repositories/SqlCollectionsRepository.cs
@@ -1,10 +1,18 @@
-﻿using ServiceLibrary.Helpers;
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+using ServiceLibrary.Helpers;
 
 namespace ServiceLibrary.Repositories
 {
     public class SqlCollectionsRepository : ICollectionsRepository
     {
         private readonly ISqlConnectionWrapper _connectionWrapper;
+
+        internal const string GetArtifactIdsInCollectionQuery =
+            "SELECT * FROM [dbo].[GetArtifactIdsInCollection](@userId, @collectionId, @addDrafts)";
 
         public SqlCollectionsRepository() : this(new SqlConnectionWrapper(ServiceConstants.RaptorMain))
         {
@@ -13,6 +21,21 @@ namespace ServiceLibrary.Repositories
         public SqlCollectionsRepository(ISqlConnectionWrapper connectionWrapper)
         {
             _connectionWrapper = connectionWrapper;
+        }
+
+        public async Task<IReadOnlyList<int>> GetContentArtifactIdsAsync(int collectionId, int userId, bool addDrafts = true)
+        {
+            var parameters = new DynamicParameters();
+            parameters.Add("@userId", userId);
+            parameters.Add("@collectionId", collectionId);
+            parameters.Add("@addDrafts", addDrafts);
+
+            var result = await _connectionWrapper.QueryAsync<int>(
+                GetArtifactIdsInCollectionQuery,
+                parameters,
+                commandType: CommandType.Text);
+
+            return result.ToList();
         }
     }
 }

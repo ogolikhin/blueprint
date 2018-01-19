a9253bf4f342a1bbe8efe954b9f3e765bd66c58c
Roman Dolgov <roman.dolgov@blueprintsys.com>
2018-01-19 09:08:43 -0500

    Implements validation for collection-based operations.

--- svc/ArtifactStore/Services/Collections/CollectionsService.cs
+++ svc/ArtifactStore/Services/Collections/CollectionsService.cs
@@ -11,37 +11,62 @@ namespace ArtifactStore.Services.Collections
     public class CollectionsService : ICollectionsService
     {
         private readonly ICollectionsRepository _collectionsRepository;
+        private readonly IArtifactRepository _artifactRepository;
         private readonly IArtifactPermissionsRepository _artifactPermissionsRepository;
         private readonly ISearchEngineService _searchEngineService;
 
         public CollectionsService() : this(
             new SqlCollectionsRepository(),
+            new SqlArtifactRepository(),
             new SqlArtifactPermissionsRepository(),
             new SearchEngineService())
         {
         }
 
-        public CollectionsService(
+        private CollectionsService(
             ICollectionsRepository collectionsRepository,
+            IArtifactRepository artifactRepository,
             IArtifactPermissionsRepository artifactPermissionsRepository,
             ISearchEngineService searchEngineService)
         {
             _collectionsRepository = collectionsRepository;
+            _artifactRepository = artifactRepository;
             _artifactPermissionsRepository = artifactPermissionsRepository;
             _searchEngineService = searchEngineService;
         }
 
-        public async Task<CollectionArtifacts> GetArtifactsInCollectionAsync(
-            int collectionId, Pagination pagination, int userId)
+        private async Task<ArtifactBasicDetails> GetCollectionBasicDetailsAsync(int collectionId, int userId)
         {
+            var collection = await _artifactRepository.GetArtifactBasicDetails(collectionId, userId);
+
+            if (collection == null || collection.DraftDeleted || collection.LatestDeleted)
+            {
+                throw CollectionsExceptionHelper.NotFoundException(collectionId);
+            }
+
+            if (collection.PrimitiveItemTypePredefined != (int)ItemTypePredefined.ArtifactCollection)
+            {
+                throw CollectionsExceptionHelper.InvalidTypeException(collectionId);
+            }
+
             if (!await _artifactPermissionsRepository.HasReadPermissions(collectionId, userId))
             {
                 throw CollectionsExceptionHelper.NoAccessException(collectionId);
             }
 
-            var searchArtifactsResult = await _searchEngineService.Search(collectionId, pagination, ScopeType.Contents, true, userId);
+            return collection;
+        }
 
-            var artifacts = await _collectionsRepository.GetArtifactsWithPropertyValues(userId, searchArtifactsResult.ArtifactIds);
+        public async Task<CollectionArtifacts> GetArtifactsInCollectionAsync(
+            int collectionId, Pagination pagination, int userId)
+        {
+            var collection = await GetCollectionBasicDetailsAsync(collectionId, userId);
+
+            var searchArtifactsResult =
+                await _searchEngineService.Search(collection.ArtifactId, pagination, ScopeType.Contents, true, userId);
+
+            var artifacts =
+                await _collectionsRepository.GetArtifactsWithPropertyValues(userId, searchArtifactsResult.ArtifactIds);
             artifacts.ItemsCount = searchArtifactsResult.Total;
 
             return artifacts;

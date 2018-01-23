c7adcecf56c3aca260c749233b70f96b0a7e8a04
YlQm <ylqmriki@gmail.com>
2018-01-23 09:51:53 -0500

    build basecache attribute class

--- svc/lib/ServiceLibrary/Attributes/ResponseCacheAttribute.cs
+++ svc/lib/ServiceLibrary/Attributes/ResponseCacheAttribute.cs
@@ -7,13 +7,32 @@ namespace ServiceLibrary.Attributes
 
     public class ResponseCacheAttribute : BaseCacheAttribute
     {
+        private int? _duration;
+
+        // <summary>
+        // Specifies the maximum amount of time in seconds a resource will be considered fresh.
+        // </summary>
+        public int Duration
+        {
+            get { return _duration ?? 0; }
+            set { _duration = value; }
+        }
 
         public override void OnActionExecuted(HttpActionExecutedContext actionExecutedContext)
         {
             base.OnActionExecuted(actionExecutedContext);
             if (actionExecutedContext.Response != null)
             {
-                base.CustomizeHttpResponseHeaders(actionExecutedContext.Response.Headers);
+                CustomizeHttpResponseHeaders(actionExecutedContext.Response.Headers);
+            }
+        }
+
+        protected override void CustomizeHttpResponseHeaders(HttpResponseHeaders responseHeaders)
+        {
+            responseHeaders.CacheControl = new CacheControlHeaderValue();
+            if (_duration.HasValue)
+            {
+                responseHeaders.CacheControl.MaxAge = TimeSpan.FromSeconds(Duration);
             }
         }
 

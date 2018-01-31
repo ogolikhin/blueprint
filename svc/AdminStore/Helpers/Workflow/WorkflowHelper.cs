5763a64d5b09983f221a5776f56c1eb31102d276
Areag Osman <areag.osman@blueprintsys.com>
2018-01-31 12:34:36 -0500

    STOR-4397 - Update ToDo comments within WorkflowHelper and remove unused code

--- svc/AdminStore/Helpers/Workflow/WorkflowHelper.cs
+++ svc/AdminStore/Helpers/Workflow/WorkflowHelper.cs
@@ -486,15 +486,7 @@ namespace AdminStore.Helpers.Workflow
                         };
                     break;
                 case ActionTypes.Webhook:
-                    var dWebhookAction = dBaseAction as DWebhookAction;
-                    if (dWebhookAction != null)
-                    {
-                        return new IeWebhookAction
-                        {
-                            Name = dWebhookAction.Name
-                            // TODO
-                        };
-                    }
+                    // At the moment, we do not allow webhooks to be updated within the workflow diagram
                     break;
                 default:
                     return null;
@@ -567,15 +559,7 @@ namespace AdminStore.Helpers.Workflow
                     }
                     break;
                 case ActionTypes.Webhook:
-                    var ieWebhookAction = ieBaseAction as IeWebhookAction;
-                    if (ieWebhookAction != null)
-                    {
-                        return new DWebhookAction
-                        {
-                            Name = ieWebhookAction.Name
-                            // TODO
-                        };
-                    }
+                    // At the moment, we do not allow webhooks to be displayed within the workflow diagram
                     break;
                 default:
                     return null;

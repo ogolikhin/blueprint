19af47226b80729fd79cad4de8deb9efc2577d1d
ssnowdon <ssnowdon@VM1202PC>
2018-02-05 17:50:23 -0500

    STOR-8610 unit tests

--- win_svc/BlueprintSys.RC.Services.Tests/TransportTests.cs
+++ win_svc/BlueprintSys.RC.Services.Tests/TransportTests.cs
@@ -30,43 +30,60 @@ namespace BlueprintSys.RC.Services.Tests
         }
 
         [TestMethod]
-        public void TransportHost_StartsSuccessfully()
+        public async Task TransportHost_StartsSuccessfully()
         {
-            _transportHost.Start(false);
+            //arrange
+            _mockNServiceBusServer.Setup(m => m.Start(It.IsAny<string>(), It.IsAny<bool>())).ReturnsAsync(string.Empty);
+            //act
+            await _transportHost.Start(false);
+            //assert
+            _mockNServiceBusServer.Verify(m => m.Start(It.IsAny<string>(), It.IsAny<bool>()), Times.Once);
         }
 
         [TestMethod]
-        public void TransportHost_LogsError_WhenStartFails()
+        public async Task TransportHost_LogsError_WhenStartFails()
         {
+            //arrange
             LogManager.Manager.AddListener(Log4NetStandardLogListener.Instance);
             var appender = new MemoryAppender();
             log4net.Config.BasicConfigurator.Configure(appender);
-
             const string errorMessage = "error message";
             _mockNServiceBusServer.Setup(m => m.Start(It.IsAny<string>(), It.IsAny<bool>())).ReturnsAsync(errorMessage);
-            _transportHost.Start(false, () => true);
 
+            //act
+            await _transportHost.Start(false, () => true);
             //Give the logging time to finish
             Thread.Sleep(1000);
             var logEntries = appender.GetEvents();
             Log4NetStandardLogListener.Clear();
             LogManager.Manager.ClearListeners();
+
+            //assert
             Assert.AreEqual(1, logEntries.Length);
             Assert.AreEqual(errorMessage, logEntries.Single().RenderedMessage);
+            _mockNServiceBusServer.Verify(m => m.Start(It.IsAny<string>(), It.IsAny<bool>()), Times.Once);
         }
 
         [TestMethod]
         public async Task TransportHost_SendsSuccessfully()
         {
+            //arrange
             _mockNServiceBusServer.Setup(m => m.Send(It.IsAny<string>(), It.IsAny<IWorkflowMessage>())).Returns(Task.FromResult(true));
+            //act
             await _transportHost.SendAsync("tenantId", new NotificationMessage());
+            //assert
+            _mockNServiceBusServer.Verify(m => m.Send(It.IsAny<string>(), It.IsAny<IWorkflowMessage>()));
         }
 
         [TestMethod]
         public void TransportHost_StopsSuccessfully()
         {
+            //arrange
             _mockNServiceBusServer.Setup(m => m.Stop()).Returns(Task.FromResult(true));
+            //act
             _transportHost.Stop();
+            //assert
+            _mockNServiceBusServer.Verify(m => m.Stop());
         }
     }
 }

414ee7abdbbe55a895649fd8177b2d50ecd44878
M.Yakushkin <maxyak88@gmail.com>
2018-02-06 14:35:22 +0300

    added unit tests for ItemsRemovalParamsExtensions

---
+++ svc/lib/ServiceLibrary.Tests/Models/ItemsRemovalParamsExtensionsTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ServiceLibrary.Exceptions;
+
+namespace ServiceLibrary.Models
+{
+    [TestClass]
+    public class ItemsRemovalParamsExtensionsTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(BadRequestException))]
+        public void Validate_ItemsRemovalParamsIsNull_BadRequestException()
+        {
+            ItemsRemovalParams removalParams = null;
+            removalParams.Validate();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(BadRequestException))]
+        public void Validate_ItemsRemovalParamsIsEmpty_BadRequestException()
+        {
+            var removalParams = new ItemsRemovalParams()
+            {
+                ItemIds = new List<int>(),
+                SelectionType = SelectionType.Selected
+            };
+            removalParams.Validate();
+        }
+
+        [TestMethod]
+        public void Validate_AllParametersAreValid_Success()
+        {
+            var removalParams = new ItemsRemovalParams()
+            {
+                ItemIds = new List<int> { 1, 2, 3 }
+            };
+            removalParams.Validate();
+        }
+    }
+}

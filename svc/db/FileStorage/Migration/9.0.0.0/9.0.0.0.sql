ad893d7b46686a4c6bb25eed6fa7fbcf2aebbd84
Alexander Groman <alexander.groman@blueprintsys.com>
2018-04-03 17:01:10 -0400

    STOR-9363: renames Admin and File maintenance jobs to avoid name conflicts for single DB installation

--- svc/db/FileStorage/Migration/9.0.0.0/9.0.0.0.sql
+++ svc/db/FileStorage/Migration/9.0.0.0/9.0.0.0.sql
@@ -7,16 +7,16 @@ IF NOT ([FileStore].[IsSchemaVersionLessOrEqual](N'9.0.0') <> 0)
 Print 'Migrating 9.0.0.0 ...'
 -- -----------------------------------------------------------------------------------------------
 
-DECLARE @blueprintDB SYSNAME, @jobname SYSNAME
+DECLARE @blueprintDBDelete SYSNAME, @jobnameDelete SYSNAME
 
-SET @blueprintDB = DB_NAME()
-SET @jobname = @blueprintDB+N'_Maintenance'
+SET @blueprintDBDelete = DB_NAME()
+SET @jobnameDelete = @blueprintDBDelete+N'_Maintenance'
 
 -- drop the job if it exists
 -- We can't do the following line, because we don't have access to the table in Amazon RDS:
---      IF EXISTS (SELECT job_id FROM msdb.dbo.sysjobs j where j.name=@jobname)
+--      IF EXISTS (SELECT job_id FROM msdb.dbo.sysjobs j where j.name=@jobnameDelete)
 BEGIN TRY
-	EXEC msdb.dbo.sp_delete_job @job_name=@jobname, @delete_unused_schedule=1
+	EXEC msdb.dbo.sp_delete_job @job_name=@jobnameDelete, @delete_unused_schedule=1
 END TRY
 BEGIN CATCH
 END CATCH
